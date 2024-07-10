fd1cf9f8b2866bd4f81638a3de96aeb54a88939b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 15:04:17 +0000

    Add package atomic_dict/0.2.0/atomic_dict-0.2.0.tar.gz

---
+++ packages/atomic_dict/atomic_dict-0.2.0.tar.gz/atomic_dict-0.2.0/c_build.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, Extension, find_packages
+import sys
+
+cmd = ['build', '--build-lib', os.getcwd()]
+sys.argv.extend(cmd)
+
+atomic_dict_c_module = Extension(
+    "atomic_dict.atomic_dict_c",
+    sources=["c/init.c", "c/methods.c"],
+    extra_compile_args=["-O3"]
+)
+
+setup(
+    name="atomic_dict",
+    version="0.2.0",
+    packages=find_packages(),
+    ext_modules=[atomic_dict_c_module],
+)
