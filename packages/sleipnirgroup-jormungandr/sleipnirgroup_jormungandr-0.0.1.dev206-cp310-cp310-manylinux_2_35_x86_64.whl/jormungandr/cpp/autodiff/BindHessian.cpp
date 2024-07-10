cfa07ddcad7137806a0732b943801638d3c23d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:03:51 +0000

    Add package sleipnirgroup-jormungandr/0.0.1.dev206/sleipnirgroup_jormungandr-0.0.1.dev206-cp310-cp310-manylinux_2_35_x86_64.whl

---
+++ packages/sleipnirgroup-jormungandr/sleipnirgroup_jormungandr-0.0.1.dev206-cp310-cp310-manylinux_2_35_x86_64.whl/jormungandr/cpp/autodiff/BindHessian.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) Sleipnir contributors
+
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+#include <sleipnir/autodiff/Hessian.hpp>
+
+#include "Docstrings.hpp"
+
+namespace py = pybind11;
+
+namespace sleipnir {
+
+void BindHessian(py::class_<Hessian>& cls) {
+  using namespace py::literals;
+
+  cls.def(py::init<Variable, VariableMatrix>(), "variable"_a, "wrt"_a,
+          DOC(sleipnir, Hessian, Hessian));
+  cls.def("get", &Hessian::Get, DOC(sleipnir, Hessian, Get));
+  cls.def("value", &Hessian::Value, DOC(sleipnir, Hessian, Value));
+}
+
+}  // namespace sleipnir
