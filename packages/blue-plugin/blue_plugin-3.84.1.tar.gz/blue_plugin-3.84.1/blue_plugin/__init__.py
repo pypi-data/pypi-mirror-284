5563aa351e4fda68e94038586a50c17fbf1945cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:05 +0000

    Add package blue-plugin/3.84.1/blue_plugin-3.84.1.tar.gz

---
+++ packages/blue-plugin/blue_plugin-3.84.1.tar.gz/blue_plugin-3.84.1/blue_plugin/__init__.py
@@ -0,0 +1,13 @@
+NAME = "blue_plugin"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} a git template for an awesome-bash-cli plugin."
+
+VERSION = "3.84.1"
+
+REPO_NAME = "blue-plugin"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/blob/main/blue-plugin/marquee.png?raw=true"
+)
