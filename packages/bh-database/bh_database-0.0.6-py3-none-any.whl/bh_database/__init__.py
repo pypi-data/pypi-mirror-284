81d7d6fd1a3d3abaf4c32d7e09ddc25c472e7dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 15:03:25 +0000

    Add package bh-database/0.0.6/bh_database-0.0.6-py3-none-any.whl

---
+++ packages/bh-database/bh_database-0.0.6-py3-none-any.whl/bh_database/__init__.py
@@ -0,0 +1,12 @@
+"""bh database entry module
+"""
+
+"""This is version number.
+"""
+
+import logging
+
+__version__ = "0.0.6"
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
