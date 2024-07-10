08735f89b57eb7708bc857a99ccce9953f9199b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:02:47 +0000

    Add package pydefold/1.1.2/pydefold-1.1.2.tar.gz

---
+++ packages/pydefold/pydefold-1.1.2.tar.gz/pydefold-1.1.2/PyDefold/__init__.py
@@ -0,0 +1,3 @@
+from .particle_ddf_pb2 import Emitter,Modifier,ParticleFX,SplinePoint 
+from .input_ddf_pb2 import GamepadMap,GamepadMapEntry,GamepadMaps,GamepadModifier_t,GamepadTrigger,InputBinding,KeyTrigger,MouseTrigger,TextTrigger,TouchTrigger 
+from .rig_ddf_pb2 import AnimationInstanceDesc,AnimationSet,AnimationSetDesc,AnimationTrack,Bone,EventKey,EventTrack,IK,Mesh,MeshSet,Model,RigAnimation,RigScene,Skeleton 
