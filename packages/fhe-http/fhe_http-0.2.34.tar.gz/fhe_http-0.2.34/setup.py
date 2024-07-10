8c8c2a30d5758c6ee2ea04901638bef362f72a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:03 +0000

    Add package fhe-http/0.2.34/fhe_http-0.2.34.tar.gz

---
+++ packages/fhe-http/fhe_http-0.2.34.tar.gz/fhe_http-0.2.34/setup.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+from setuptools import setup, find_packages
+from setuptools_rust import RustExtension, Binding
+
+setup(
+    name="fhe-http",
+    version="0.2.34",
+    author="spockwall",
+    author_email="e1e1e1n9n9n9@gmail.com",
+    description="This is an test package for Python Fhe Http ",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/spockwall/fhe-http",  # Replace with your project's URL
+    packages=find_packages(),
+    install_requires=[
+        "maturin>=1.5.0",
+        "ply>=3.11",
+        "setuptools-rust>=1.9.0",
+        "setuptools>=68.2.2",
+        "wheel>=0.43.0",
+    ],
+    rust_extensions=[
+        RustExtension(
+            "fhe_http.fhe",
+            path="./fhe_http_python/fhe/Cargo.toml",
+            binding=Binding.PyO3,
+        ),
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",  # Adjust this based on your project's requirements
+)
