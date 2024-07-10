48991fc889074202a1c79193d584e9173b54cfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:03 +0000

    Add package scores/1.0.0/scores-1.0.0.tar.gz

---
+++ packages/scores/scores-1.0.0.tar.gz/scores-1.0.0/src/scores/continuous/__init__.py
@@ -0,0 +1,44 @@
+"""
+Import the functions from the implementations into the public API
+"""
+
+import scores.continuous.correlation  # noqa: F401
+from scores.continuous.consistent_impl import (
+    consistent_expectile_score,
+    consistent_huber_score,
+    consistent_quantile_score,
+)
+from scores.continuous.flip_flop_impl import (
+    flip_flop_index,
+    flip_flop_index_proportion_exceeding,
+)
+from scores.continuous.murphy_impl import murphy_score, murphy_thetas
+from scores.continuous.quantile_loss_impl import quantile_score
+from scores.continuous.standard_impl import (
+    additive_bias,
+    mae,
+    mean_error,
+    mse,
+    multiplicative_bias,
+    rmse,
+)
+from scores.processing.isoreg_impl import isotonic_fit
+
+__all__ = [
+    "flip_flop_index",
+    "flip_flop_index_proportion_exceeding",
+    "murphy_score",
+    "murphy_thetas",
+    "quantile_score",
+    "correlation",
+    "mae",
+    "mse",
+    "rmse",
+    "additive_bias",
+    "mean_error",
+    "multiplicative_bias",
+    "isotonic_fit",
+    "consistent_expectile_score",
+    "consistent_huber_score",
+    "consistent_quantile_score",
+]
