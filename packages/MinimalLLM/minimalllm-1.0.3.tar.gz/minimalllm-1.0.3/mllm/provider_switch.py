d7efd8d03203adde834c4c1270986d86121de1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:03:00 +0000

    Add package MinimalLLM/1.0.3/minimalllm-1.0.3.tar.gz

---
+++ packages/MinimalLLM/minimalllm-1.0.3.tar.gz/minimalllm-1.0.3/mllm/provider_switch.py
@@ -0,0 +1,29 @@
+from mllm.config import default_models
+from mllm.env_utils import check_replicate_env, check_anthropic_env, check_openai_env
+
+def set_default_to_gemini():
+    default_models["normal"] = "gemini/gemini-pro"
+    default_models["expensive"] = "gemini/gemini-1.5-pro-latest"
+    default_models["vision"] = "gemini/gemini-pro-vision"
+    default_models["embedding"] = "text-embedding-3-large"
+
+def set_default_to_openai(use_gpt_4=False):
+    check_openai_env()
+    default_models["normal"] = "gpt-3.5-turbo"
+    default_models["expensive"] = "gpt-4-turbo-preview" if not use_gpt_4 else "gpt-4"
+    default_models["vision"] = "gpt-4-vision-preview"
+    default_models["embedding"] = "text-embedding-3-large"
+
+def set_default_to_anthropic(expensive_vision_model=False):
+    check_anthropic_env()
+    default_models["normal"] = "claude-3-sonnet-20240229"
+    default_models["expensive"] = "claude-3-opus-20240229"
+    default_models["vision"] = "claude-3-sonnet-20240229" if not expensive_vision_model else "claude-3-opus-20240229"
+    default_models["embedding"] = "text-embedding-3-large"
+
+def set_default_to_llama():
+    check_replicate_env()
+    default_models["normal"] = "replicate/meta/llama-2-13b-chat"
+    default_models["expensive"] = "replicate/meta/llama-2-70b-chat"
+    default_models["vision"] = "gpt-4-vision-preview"
+    default_models["embedding"] = "text-embedding-3-large"
