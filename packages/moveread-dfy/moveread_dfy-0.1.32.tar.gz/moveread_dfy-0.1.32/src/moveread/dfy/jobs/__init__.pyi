81fb21d6494f1fc43fe49bdf0942d77c6447bafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:03:55 +0000

    Add package moveread-dfy/0.1.32/moveread_dfy-0.1.32.tar.gz

---
+++ packages/moveread-dfy/moveread_dfy-0.1.32.tar.gz/moveread_dfy-0.1.32/src/moveread/dfy/jobs/__init__.pyi
@@ -0,0 +1,3 @@
+from .pairings import scrape_all_pairings
+
+__all__ = ['scrape_all_pairings']
---
+++ packages/moveread-dfy/moveread_dfy-0.1.32.tar.gz/moveread_dfy-0.1.32/src/moveread/dfy/queries/__init__.pyi
@@ -0,0 +1,11 @@
+from .queries import group_games, round_games, exact_game, exact_round, \
+  select_game, select_group_games, select_tnmt, select_group, select_round, \
+  select_rounds, select_tnmt_groups, select_tnmt_rounds, select_model, \
+  uploaded_games, group_rounds, select_round_games
+
+__all__ = [
+  'group_games', 'round_games', 'exact_game', 'exact_round',
+  'select_game', 'select_group_games', 'select_tnmt', 'select_group', 'select_round',
+  'select_rounds', 'select_tnmt_groups', 'select_tnmt_rounds', 'select_model',
+  'uploaded_games', 'group_rounds', 'select_round_games',
+]
