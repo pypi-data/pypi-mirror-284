f15127f34213c95604747dd21ea1cba12699bcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:02:50 +0000

    Add package wwvb/4.0.0a0/wwvb-4.0.0a0.tar.gz

---
+++ packages/wwvb/wwvb-4.0.0a0.tar.gz/wwvb-4.0.0a0/src/wwvb/iersdata.py
@@ -0,0 +1,28 @@
+# -*- python3 -*-
+"""Retrieve iers data, possibly from user or site data or from the wwvbpy distribution"""
+
+# Copyright (C) 2021 Jeff Epler <jepler@gmail.com>
+# SPDX-FileCopyrightText: 2021 Jeff Epler
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+import datetime
+import pathlib
+
+import platformdirs
+
+__all__ = ["DUT1_DATA_START", "DUT1_OFFSETS", "start", "span", "end"]
+from .iersdata_dist import DUT1_DATA_START, DUT1_OFFSETS
+
+for location in [
+    platformdirs.user_data_dir("wwvbpy", "unpythonic.net"),
+    platformdirs.site_data_dir("wwvbpy", "unpythonic.net"),
+]:  # pragma no cover
+    path = pathlib.Path(location) / "wwvbpy_iersdata.py"
+    if path.exists():
+        exec(path.read_text(encoding="utf-8"), globals(), globals())
+        break
+
+start = datetime.datetime.combine(DUT1_DATA_START, datetime.time()).replace(tzinfo=datetime.timezone.utc)
+span = datetime.timedelta(days=len(DUT1_OFFSETS))
+end = start + span
