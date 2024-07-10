7d6b367edf8114b5ccd3416996d1d03bccf8c5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 15:03:58 +0000

    Add package emb-model/0.0.2/emb_model-0.0.2.tar.gz

---
+++ packages/emb-model/emb_model-0.0.2.tar.gz/emb_model-0.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='emb-model',
+    version='0.0.2',
+    author="ZhangLe",
+    author_email="zhangle@gmail.com",
+    description="simple useing for embedding models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cheerzhang/EmbeddingModel",
+    project_urls={
+        "Bug Tracker": "https://github.com/cheerzhang/EmbeddingModel/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages("."),
+    install_requires=[
+        'torch>=2.3.1',
+        'scikit-learn>=1.5.1'
+    ],
+    python_requires=">=3.11.2",
+)
