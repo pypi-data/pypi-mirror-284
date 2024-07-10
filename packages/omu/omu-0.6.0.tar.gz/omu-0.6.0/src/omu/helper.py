92da66a7c797284852210892cbd80ded6e5b6fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:02:52 +0000

    Add package omu/0.6.0/omu-0.6.0.tar.gz

---
+++ packages/omu/omu-0.6.0.tar.gz/omu-0.6.0/src/omu/helper.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import hashlib
+import re
+import typing
+
+type Coro[**P, T] = typing.Callable[P, typing.Coroutine[None, None, T]]
+type AsyncCallback[**P] = Coro[P, typing.Any]
+
+
+def instance[T](cls: type[T]) -> T:
+    return cls()
+
+
+def map_optional[V, T](
+    data: V | None, func: typing.Callable[[V], T], default: T | None = None
+) -> T | None:
+    if data is None:
+        return default
+    return func(data)
+
+
+sanitize_re = re.compile(r"[^\w]")
+
+
+def sanitize_filename(name: str) -> str:
+    return sanitize_re.sub("_", name)
+
+
+def generate_md5_hash(id: str) -> str:
+    return hashlib.md5(id.encode()).hexdigest()
+
+
+def batch_call(*funcs: typing.Callable[[], None]) -> typing.Callable[[], None]:
+    def wrapper():
+        for func in funcs:
+            func()
+
+    return wrapper
