0e398b2c61ac02af5cda2a2696f7e3f828009adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:02:53 +0000

    Add package omu-chatprovider/0.6.0/omu_chatprovider-0.6.0.tar.gz

---
+++ packages/omu-chatprovider/omu_chatprovider-0.6.0.tar.gz/omu_chatprovider-0.6.0/src/omu_chatprovider/service.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import abc
+from dataclasses import dataclass
+from importlib import metadata
+
+from omu import Omu
+from omu.helper import Coro
+from omu_chat import Channel, Chat, Provider, Room
+
+type ChatServiceFactory = Coro[[], ChatService]
+
+
+@dataclass(frozen=True, slots=True)
+class FetchedRoom:
+    room: Room
+    create: ChatServiceFactory
+
+
+class ProviderService(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, omu: Omu, chat: Chat): ...
+
+    @property
+    @abc.abstractmethod
+    def provider(self) -> Provider: ...
+
+    @abc.abstractmethod
+    async def fetch_rooms(self, channel: Channel) -> list[FetchedRoom]: ...
+
+    @abc.abstractmethod
+    async def is_online(self, room: Room) -> bool: ...
+
+
+class ChatService(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def room(self) -> Room: ...
+
+    @property
+    @abc.abstractmethod
+    def closed(self) -> bool: ...
+
+    @abc.abstractmethod
+    async def start(self): ...
+
+    @abc.abstractmethod
+    async def stop(self): ...
+
+
+def retrieve_services():
+    entry_points = metadata.entry_points(group="omu_chatprovider.services")
+
+    services: list[type[ProviderService]] = []
+    for entry_point in entry_points:
+        service = entry_point.load()
+        assert issubclass(
+            service, ProviderService
+        ), f"{service} is not a ProviderService"
+        services.append(service)
+
+    return services
+
+
+__all__ = [
+    "ChatService",
+    "FetchedRoom",
+    "ProviderService",
+    "retrieve_services",
+]
