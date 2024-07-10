971e598bef9352d491c7a12c1855f350aba705c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 16:03:12 +0000

    Add package gpctools/0.1.2/gpctools-0.1.2-py3-none-any.whl

---
+++ packages/gpctools/gpctools-0.1.2-py3-none-any.whl/gpctools/decorators/exectime.py
@@ -0,0 +1,20 @@
+from typing import Callable
+from functools import wraps
+from time import perf_counter
+
+
+def exectime(func: Callable) -> Callable:
+    """
+    Decorator that prints the running time of a function or method
+    Args: func (Callable): any function or method;
+    Returns: the same func;
+    Prints: Running time;
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> None:
+        start_time: float = perf_counter()
+        func(*args, **kwargs)
+        end_time: float = perf_counter()
+        print(f"get_runtime: {func.__name__} took {end_time - start_time:.3f}s to run.")
+
+    return wrapper
