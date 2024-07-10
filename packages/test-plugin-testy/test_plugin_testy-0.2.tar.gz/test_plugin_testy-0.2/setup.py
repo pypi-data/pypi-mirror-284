0f4a0a6c921419487fff45b43a11cd540bdd0579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 15:03:12 +0000

    Add package test-plugin-testy/0.2/test_plugin_testy-0.2.tar.gz

---
+++ packages/test-plugin-testy/test_plugin_testy-0.2.tar.gz/test_plugin_testy-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test_plugin_testy',
+    version='0.2',
+    description='Test plugin for uploading files',
+    packages=find_packages(),
+    install_requires=[
+        'django',
+        'testy',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'testy': ['test_plugin_testy=test_plugin'],
+    },
+)
