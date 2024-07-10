1f1a80f0adc78de0e4d308ad156980a7ee805429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:03:56 +0000

    Add package Kais-pgm/0.0.1/kais_pgm-0.0.1.tar.gz

---
+++ packages/Kais-pgm/kais_pgm-0.0.1.tar.gz/kais_pgm-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Kais-pgm',
+    version='0.0.1',
+    description='This is a vision feature created by kais',
+    author='misil han',
+    author_email='coko980715@gmail.com',
+    packages=find_packages(exclude=[]),
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
