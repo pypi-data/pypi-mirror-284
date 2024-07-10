b8f645bbf1171a99c40e56130386aa7f7c3ffd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:02:47 +0000

    Add package XGRCpy/1.1.2/XGRCpy-1.1.2-py3-none-any.whl

---
+++ packages/XGRCpy/XGRCpy-1.1.2-py3-none-any.whl/XGRCpy/__init__.py
@@ -0,0 +1,10 @@
+# Import key functions and classes
+from .XGRCpy import XGRCViewTable
+from .XGRCpy import XGRCSPView
+from .XGRCPowerBI import XGRCViewTablePowerBI
+
+# Optional: Define a version number for the package
+__version__ = "1.1.2"
+
+# Optional: Define the package's public interface
+__all__ = ["XGRCViewTable","XGRCSPView","XGRCViewTablePowerBI"]
