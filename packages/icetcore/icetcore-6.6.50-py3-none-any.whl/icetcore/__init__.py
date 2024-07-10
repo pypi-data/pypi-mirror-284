d871d103725ab5733fc6df2c6dc191131a5669fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 15:03:27 +0000

    Add package icetcore/6.6.50/icetcore-6.6.50-py3-none-any.whl

---
+++ packages/icetcore/icetcore-6.6.50-py3-none-any.whl/icetcore/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+__author__ = "zxlee"
+name = "icetcore"
+
+from icetcore.api import TCoreAPI, QuoteEvent, TradeEvent, TradeAPI, QuoteAPI
+from icetcore.constant import OrderStruct, BarType, GreeksType, SymbolType, OrderSide, TimeInForce, OrderType, PositionEffect
