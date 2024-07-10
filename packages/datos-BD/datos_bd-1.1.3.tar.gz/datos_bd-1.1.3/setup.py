f6c86091f0d0a1b4e1152274251724c2848afd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 15:04:08 +0000

    Add package datos-BD/1.1.3/datos_bd-1.1.3.tar.gz

---
+++ packages/datos-BD/datos_bd-1.1.3.tar.gz/datos_bd-1.1.3/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import find_packages, setup
+
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '1.1.3'
+PACKAGE_NAME = 'datos_BD'
+AUTHOR = 'Alan Adalberto Ortiz Pérez'
+AUTHOR_EMAIL = 'aortiz@isssteson.gob.mx'
+URL = 'https://github.com/ortizalan'
+
+LICENSE = '© ISSSTESON'
+DESCRIPTION = 'Librería para acceder a los Datos de conexión a servidores del Instituto de Seguridad y Servicios Sociales de los Trabajadores del Estado de Sonora (ISSSTESON).'''
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
+LONG_DESC_TYPE = 'text/markdown'
+
+# Paquetes necesarios para el funcionamiento de la Librería
+INSTALL_REQUIRES = [
+    'pycryptodome'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    #install_requires = INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
