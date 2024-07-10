6beb3bc3adf4700b4da2be7e8e264bd4ddbe0983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 15:03:34 +0000

    Add package piconetcontrol/0.2.1/piconetcontrol-0.2.1-py3-none-any.whl

---
+++ packages/piconetcontrol/piconetcontrol-0.2.1-py3-none-any.whl/piconetcontrol/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for piconetcontrol."""
+
+__author__ = """Matthias Gilles Zeller"""
+__author_email__ = "matthias.gilles.zeller@gmail.com"
+__version__ = "0.2.1"
+__description__ = "Client-server package to remotely control a Raspberrypi Pi Pico W."
+__url__ = "https://github.com/matthiaszeller/picoserver"
