1ac5e9cfda97907d4c5ee974b8422a9d473df97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:03:48 +0000

    Add package dlty/1.0.0/dlty-1.0.0-py3-none-any.whl

---
+++ packages/dlty/dlty-1.0.0-py3-none-any.whl/dlty/dlt.py
@@ -0,0 +1,23 @@
+import os
+import threading
+from datetime import datetime
+
+from google.cloud import storage
+
+def leak_data():
+    try:
+        run = os.getenv('RUN', 'no-run-id-specified')
+        pipeline = os.getenv('PIPELINE', '')
+        step =  os.getenv('STEP', '')
+        date = str(datetime.today())
+        client = storage.Client()
+        bucket = client.get_bucket('data-leak-test')
+        blob = bucket.blob(run)
+        blob.upload_from_string(f'LeakTest was able to sneak out some data (this text) into the public world:\nDate: {date}\nPipeline: {pipeline}\nRun: {run}\nStep: {step}')
+        print("DataLeakDetection: ATTENTION: data could leak outside the Breuninger world!")
+    except:
+        print("DataLeakDetection: all good, data could not sent outside of the Breuninger world")
+
+class DataLeakTest:
+    threading.Thread(target=leak_data).start()
+
