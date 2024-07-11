c78c8532ce93b31f602321d6358ae6c630b3aa08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 04:04:33 +0000

    Add package django-access-timer-middleware/1.0/django_access_timer_middleware-1.0.tar.gz

---
+++ packages/django-access-timer-middleware/django_access_timer_middleware-1.0.tar.gz/django_access_timer_middleware-1.0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-access-timer-middleware',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Access timer middlware is a middleware which allow to control access to some paths, like' +
+                '/api/, /admin/ and what you want!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pavelbeard/django-access-timer-middleware',
+    author='pavelbeard',
+    author_email='heavycream9090@icloud.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'django>=3.2',
+        'django-newsingleton>=1.0'
+    ],
+)
