8d5ce405bbe325e3101d515e93bf9a539fa3859e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 17:03:41 +0000

    Add package python-kv/0.2.3/python_kv-0.2.3.tar.gz

---
+++ packages/python-kv/python_kv-0.2.3.tar.gz/python_kv-0.2.3/src/kv/__init__.pyi
@@ -0,0 +1,20 @@
+from ._abc import KV, ReadError, InvalidData, InexistentItem, DBError, LocatableKV
+from .serialization import Parse, Dump, serializers, Serializers
+from .impl._dict import DictKV
+from .impl.fs import FilesystemKV
+from .impl.sqlite import SQLiteKV
+from .impl.sql import SQLKV
+from .impl.redis import RedisKV
+from .http import ClientKV, ServerKV
+from .azure import BlobKV, BlobContainerKV
+from .conn_strings import parse_type
+from .tests import test
+from .import azure, http
+
+__all__ = [
+  'KV', 'LocatableKV',
+  'ReadError', 'InvalidData', 'InexistentItem', 'DBError',
+  'DictKV', 'FilesystemKV', 'SQLiteKV', 'SQLKV', 'ClientKV', 'ServerKV', 'RedisKV',
+  'BlobKV', 'BlobContainerKV', 'parse_type', 'test',
+  'Parse', 'Dump', 'serializers', 'Serializers', 'test', 'azure', 'http',
+]
