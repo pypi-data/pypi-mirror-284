75d07e443f725588c6730e82ba0be5a3342419cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:04:43 +0000

    Add package arachnea/0.0.2/arachnea-0.0.2.tar.gz

---
+++ packages/arachnea/arachnea-0.0.2.tar.gz/arachnea-0.0.2/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='arachnea',
+    version='0.0.2',
+    packages=find_packages(),
+    description='A Python library for efficient array operations using a fluent API.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    keywords='array operations, data processing, lightweight, Python library',
+    url='https://github.com/yourusername/arachnea',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
