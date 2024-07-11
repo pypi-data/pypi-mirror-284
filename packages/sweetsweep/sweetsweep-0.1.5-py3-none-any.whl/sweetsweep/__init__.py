58d7a518276983ca9f17dc5f30b1f9777f2db0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:03:59 +0000

    Add package sweetsweep/0.1.5/sweetsweep-0.1.5-py3-none-any.whl

---
+++ packages/sweetsweep/sweetsweep-0.1.5-py3-none-any.whl/sweetsweep/__init__.py
@@ -0,0 +1,9 @@
+from .sweep import parameter_sweep, parameter_sweep_parallel, get_num_exp
+
+# Define version here
+__version__ = '0.1.5'
+
+
+# Command reminder for building the package
+# python3 -m build
+# python3 -m twine upload --skip-existing dist/*
