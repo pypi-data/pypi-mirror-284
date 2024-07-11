73e0da98be103023f2e2bd4c1289e0e6ac620b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 17:03:30 +0000

    Add package dataharvest/0.2.6/dataharvest-0.2.6.tar.gz

---
+++ packages/dataharvest/dataharvest-0.2.6.tar.gz/dataharvest-0.2.6/tests/test_searcher.py
@@ -0,0 +1,8 @@
+import logging
+
+
+def test_tavily_searcher():
+    from dataharvest.searcher.tavily_searcher import TavilySearcher
+
+    searcher = TavilySearcher()
+    logging.info(searcher.search("战国水晶杯"))
