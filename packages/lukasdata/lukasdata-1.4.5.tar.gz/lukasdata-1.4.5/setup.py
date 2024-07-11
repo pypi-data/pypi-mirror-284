e8c1726356dd976929fa42d2fdf48f9ef9900db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 07:03:45 +0000

    Add package lukasdata/1.4.5/lukasdata-1.4.5.tar.gz

---
+++ packages/lukasdata/lukasdata-1.4.5.tar.gz/lukasdata-1.4.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lukasdata',
+    packages=find_packages(),
+    version='1.4.5',
+    install_requires=["numpy","pandas","matplotlib"
+    ]
+)
