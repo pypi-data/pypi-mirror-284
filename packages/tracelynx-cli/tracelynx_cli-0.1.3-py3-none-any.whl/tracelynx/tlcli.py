f74af4c0fce285626ad89800839faee45905192b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:04:15 +0000

    Add package tracelynx-cli/0.1.3/tracelynx_cli-0.1.3-py3-none-any.whl

---
+++ packages/tracelynx-cli/tracelynx_cli-0.1.3-py3-none-any.whl/tracelynx/tlcli.py
@@ -0,0 +1,17 @@
+from tracelynx.xml_report import process_xml_files
+
+
+def authenticate(username: str, password: str) -> str:
+    print("authenticating: ", username)
+    return f"{username} authenticated"
+
+
+def process_xml(username: str, password: str, xml_input: str | None = None, xml_output: str | None = None,
+                data: str | None = None) -> None:
+    print(authenticate(username=username, password=password))
+    print(f"processing xml from {xml_input} to {xml_output}")
+
+    if data is None:
+        data = "tests/data/jest_test_suite/link_prediction_example.json"
+
+    process_xml_files(xml_input, data, xml_output)
