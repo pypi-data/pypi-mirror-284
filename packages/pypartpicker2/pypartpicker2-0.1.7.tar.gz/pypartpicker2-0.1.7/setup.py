6368725425a7d8892f35ff7430954c4c541fc231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 17:03:29 +0000

    Add package pypartpicker2/0.1.7/pypartpicker2-0.1.7.tar.gz

---
+++ packages/pypartpicker2/pypartpicker2-0.1.7.tar.gz/pypartpicker2-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name="pypartpicker2",
+    version="0.1.7",
+    description="A package that scrapes pcpartpicker.com and returns the results as objects.",
+    packages=["pypartpicker"],
+    url="https://github.com/giuseppe99barchetta/pypartpicker",
+    keywords=["pcpartpicker", "scraper", "list", "beautifulsoup", "pc", "parts"],
+    install_requires=["bs4", "requests"],
+    zip_safe=False,
+    download_url="https://github.com/giuseppe99barchetta/pypartpicker/archive/refs/tags/0.1.6.tar.gz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
