b3e1859be82ba6ca4611fb43e9aa57707d8a4183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:03:42 +0000

    Add package pdfconduit/3.0.0b2/pdfconduit-3.0.0b2.tar.gz

---
+++ packages/pdfconduit/pdfconduit-3.0.0b2.tar.gz/pdfconduit-3.0.0b2/pdfconduit/utils/info.py
@@ -0,0 +1,73 @@
+# Retrieve information about a PDF document
+from pdfconduit.utils._permissions import Permissions
+from pdfconduit.utils.read import pypdf_reader
+
+
+class Info:
+    def __init__(self, path, password=None):
+        self.pdf = pypdf_reader(path, password)
+
+    @staticmethod
+    def _resolved_objects(pdf, xobject):
+        """Retrieve rotation info."""
+        return [pdf.get_page(i).get(xobject) for i in range(pdf.get_num_pages())][0]
+
+    @property
+    def encrypted(self):
+        """Check weather a PDF is encrypted"""
+        return self.pdf.is_encrypted
+
+    @property
+    def decrypted(self):
+        """Check weather a PDF is encrypted"""
+        return not self.encrypted
+
+    @property
+    def pages(self):
+        """Retrieve PDF number of pages"""
+        return self.pdf.get_num_pages()
+
+    @property
+    def metadata(self):
+        """Retrieve PDF metadata"""
+        return self.pdf.metadata
+
+    def resources(self):
+        """Retrieve contents of each page of PDF"""
+        # todo: refactor to generator?
+        return [self.pdf.get_page(i) for i in range(self.pdf.get_num_pages())]
+
+    @property
+    def security(self):
+        """Print security object information for a pdf document"""
+        return {
+            k: v for i in self.pdf.resolved_objects.items() for k, v in i[1].items()
+        }
+
+    @property
+    def dimensions(self):
+        """Get width and height of a PDF"""
+        # todo: add page parameter?
+        # todo: add height & width methods?
+        size = self.pdf.get_page(0).mediabox
+        return {"w": float(size[2]), "h": float(size[3])}
+
+    @property
+    def size(self):
+        """Get width and height of a PDF"""
+        size = self.pdf.get_page(0).mediabox
+
+        return float(size[2]), float(size[3])
+
+    @property
+    def rotate(self):
+        """Retrieve rotation info."""
+        # todo: add page param
+        # todo: refactor to `rotation()`
+        # todo: add is_rotated
+        return self._resolved_objects(self.pdf, "/Rotate")
+
+    @property
+    def permissions(self):
+        """Retrieve user access permissions."""
+        return Permissions(self.pdf)
---
+++ packages/pdfconduit/pdfconduit-3.0.0b2.tar.gz/pdfconduit-3.0.0b2/tests/test_transform_slice.py
@@ -0,0 +1,64 @@
+import os
+import unittest
+from tempfile import TemporaryDirectory
+
+from looptools import Timer
+
+from pdfconduit import Info, slicer
+from tests import *
+
+
+class TestSlice(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.pdf_path = pdf_path
+
+    def setUp(self):
+        self.temp = TemporaryDirectory()
+
+    def tearDown(self):
+        self.temp.cleanup()
+
+    @Timer.decorator
+    def test_slice_pypdf_only_first_page(self):
+        """Slice a page range from a PDF to create a new 'trimmed' pdf file."""
+        fp = 1
+        lp = 1
+        sliced = slicer(
+            self.pdf_path,
+            first_page=fp,
+            last_page=lp,
+            tempdir=self.temp.name,
+        )
+
+        self.assertPdfExists(sliced)
+        self.assertCorrectPagesSliced(fp, lp, sliced)
+
+        expected_equals_output(function_name_to_file_name(), sliced)
+
+    @Timer.decorator
+    def test_slice_pypdf_4th_through_7th_pages(self):
+        """Slice a page range from a PDF to create a new 'trimmed' pdf file."""
+        fp = 4
+        lp = 7
+        sliced = slicer(
+            self.pdf_path,
+            first_page=fp,
+            last_page=lp,
+            tempdir=self.temp.name,
+        )
+
+        self.assertPdfExists(sliced)
+        self.assertCorrectPagesSliced(fp, lp, sliced)
+
+        expected_equals_output(function_name_to_file_name(), sliced)
+
+    def assertCorrectPagesSliced(self, fp, lp, sliced):
+        self.assertEqual(Info(sliced).pages, len(range(fp, lp + 1)))
+
+    def assertPdfExists(self, sliced):
+        self.assertTrue(os.path.isfile(sliced))
+
+
+if __name__ == "__main__":
+    unittest.main()
