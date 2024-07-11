17c24c045ecb813bd9fb14e195153d6d04ec71aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:03:55 +0000

    Add package gateapp/2.1.3/gateapp-2.1.3.tar.gz

---
+++ packages/gateapp/gateapp-2.1.3.tar.gz/gateapp-2.1.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gateapp',
+    version='2.1.3',
+    description='A Django application for configuring gateway settings with a web-based interface',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ankit Kumar',
+    author_email='bantiyadav16095@gmail.com',
+    url='https://github.com/ankit3388/BlockerGateway',
+    packages=find_packages(where='gateapp'),
+    package_dir={'': 'gateapp'},
+    include_package_data=True,
+    package_data={
+        '': ['templates/*', 'static/css/*'],
+    },
+    install_requires=[
+        'Django>=3.0,<5.0',
+        'gunicorn',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+    python_requires='>=3.8',
+    keywords='django gateway interface web configuration',
+    project_urls={
+        'Bug Reports': 'https://github.com/ankit3388/BlockerGateway/issues',
+        'Source': 'https://github.com/ankit3388/BlockerGateway',
+    },
+)
