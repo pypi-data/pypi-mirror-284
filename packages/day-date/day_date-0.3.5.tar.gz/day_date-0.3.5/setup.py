9798067da0724def76a5badb7a0a1628a9507ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 15:04:12 +0000

    Add package day-date/0.3.5/day_date-0.3.5.tar.gz

---
+++ packages/day-date/day_date-0.3.5.tar.gz/day_date-0.3.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='day_date',
+    version='0.3.5',
+    entry_points={
+        'console_scripts': [
+            # Exemple de point d'entrée de script
+            'day_date=day_date:main',
+        ],
+    },
+    author='Bouquet Cédric',
+    author_email='cedric-bouquet-7@outlook.fr',
+    description="Calcule le jour de la semaine d'une date donnée",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.0',
+)
