0c7e3d87c644be0e0cdf00c92bffb7b5487fe393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:12 +0000

    Add package mouse-jiggler-advanced/1.0.1/mouse_jiggler_advanced-1.0.1.tar.gz

---
+++ packages/mouse-jiggler-advanced/mouse_jiggler_advanced-1.0.1.tar.gz/mouse_jiggler_advanced-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mouse_jiggler_advanced",
+    version="1.0.1",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "jiggler=src.mouse_jiggler.jiggler:main",
+        ],
+    },
+    install_requires=[
+        "pytz",
+        "pyautogui",
+    ],
+    author="Ali Hamza",
+    author_email="alihamza19999@gmail.com",
+    description="A script to move the cursor randomly to prevent idle detection.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hurryingauto3/mouse_jiggler",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
