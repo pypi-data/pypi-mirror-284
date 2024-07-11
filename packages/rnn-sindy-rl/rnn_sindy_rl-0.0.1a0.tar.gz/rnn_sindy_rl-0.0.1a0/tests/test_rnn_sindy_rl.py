3b3166188734b21b8d53fdd204331f7f58440ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:04:15 +0000

    Add package rnn-sindy-rl/0.0.1a0/rnn_sindy_rl-0.0.1a0.tar.gz

---
+++ packages/rnn-sindy-rl/rnn_sindy_rl-0.0.1a0.tar.gz/rnn_sindy_rl-0.0.1a0/tests/test_rnn_sindy_rl.py
@@ -0,0 +1,8 @@
+from autora.theorist.rnn_sindy_rl import RNNSindy
+
+def test():
+    theorist = RNNSindy()
+    assert theorist is not None
+
+
+# Note: We encourage you to adjust this test and write more tests.
