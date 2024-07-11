540c4d01d5c9996e1c44920e8398c6f8a633dd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:03:55 +0000

    Add package tensordock/0.1.10/tensordock-0.1.10.tar.gz

---
+++ packages/tensordock/tensordock-0.1.10.tar.gz/tensordock-0.1.10/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tensordock",
+    version="0.1.10",  # Increment this if you've made changes
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    author="Ryan Huang",
+    author_email="ryan@stdint.com",
+    description="A Python SDK for TensorDock API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ryan-huang1/tensordock-python-sdk",
+    project_urls={
+        "Bug Tracker": "https://github.com/ryan-huang1/tensordock-python-sdk/issues",
+        "Documentation": "https://github.com/ryan-huang1/tensordock-python-sdk/blob/main/DOCS.md",
+        "Source Code": "https://github.com/ryan-huang1/tensordock-python-sdk",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
