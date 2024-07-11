a999c711ac2a454eda2699e7fffb6b9dbb7ed599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 04:03:20 +0000

    Add package xhydro-temp/0.3.7.dev4/xhydro_temp-0.3.7.dev4-py3-none-any.whl

---
+++ packages/xhydro-temp/xhydro_temp-0.3.7.dev4-py3-none-any.whl/xhydro_temp/__init__.py
@@ -0,0 +1,11 @@
+"""Hydrological analysis library built with xarray."""
+
+from . import cc, gis, indicators, utils
+
+# Import top-level functions
+# TODO: Decide which functions to import at the top level
+
+__author__ = """Thomas-Charles Fortier Filion"""
+__email__ = "tcff_hydro@outlook.com"
+# kamil: changing version to 3.6.7 for pypi upload
+__version__ = "0.3.7-dev.4"
