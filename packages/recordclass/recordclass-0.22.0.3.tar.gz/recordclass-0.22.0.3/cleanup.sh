a7c15d93327687f93b1e472af086e69738c15c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:05:43 +0000

    Add package recordclass/0.22.0.3/recordclass-0.22.0.3.tar.gz

---
+++ packages/recordclass/recordclass-0.22.0.3.tar.gz/recordclass-0.22.0.3/cleanup.sh
@@ -0,0 +1,4 @@
+#! sh
+rm lib/recordclass/_linkedlist.c
+rm lib/recordclass/_linkedlist.h
+
---
+++ packages/recordclass/recordclass-0.22.0.3.tar.gz/recordclass-0.22.0.3/lib/recordclass/__init__.py
@@ -0,0 +1,37 @@
+# The MIT License (MIT)
+#
+# Copyright (c) <2015-2023> <Shibzukhov Zaur, szport at gmail dot com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+from .datatype import datatype, MATCH
+from ._dataobject import dataobject, datastruct, astuple, asdict, clone, update, make, Factory
+from ._litelist import litelist, litelist_fromargs
+from ._litetuple import litetuple, mutabletuple
+from .recordclass import recordclass
+from .typing import RecordClass
+from .dataclass import make_dataclass, make_structclass, make_class, join_dataclasses
+from .dictclass import make_dictclass
+from .arrayclass import make_arrayclass
+from .adapter import as_dataclass, as_record
+
+structclass = make_structclass
+
+__version__ = '0.22.0.3'
