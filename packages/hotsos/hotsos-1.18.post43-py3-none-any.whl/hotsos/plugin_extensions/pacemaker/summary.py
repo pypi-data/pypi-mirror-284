5941d3bec87292ba7728d4c8378eb1e99d1ccc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 04:04:29 +0000

    Add package hotsos/1.18.post43/hotsos-1.18.post43-py3-none-any.whl

---
+++ packages/hotsos/hotsos-1.18.post43-py3-none-any.whl/hotsos/plugin_extensions/pacemaker/summary.py
@@ -0,0 +1,24 @@
+from hotsos.core.plugins.pacemaker import PacemakerChecks
+
+
+class PacemakerSummary(PacemakerChecks):
+    """ Implementation of Pacemaker summary. """
+    summary_part_index = 0
+
+    def __0_summary_services(self):
+        if self.systemd.services:
+            return self.systemd.summary
+
+        return None
+
+    def __1_summary_dpkg(self):
+        return self.apt.all_formatted or None
+
+    def __2_summary_nodes(self):
+        nodes = {}
+        if self.online_nodes:
+            nodes["online"] = self.online_nodes
+        if self.offline_nodes:
+            nodes["offline"] = self.offline_nodes
+
+        return nodes or None
