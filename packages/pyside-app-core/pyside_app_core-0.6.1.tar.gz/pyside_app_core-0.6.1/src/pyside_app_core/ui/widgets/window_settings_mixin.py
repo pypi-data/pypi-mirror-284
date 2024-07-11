1310d82b3e4b8202678bd091f2203812c9d27b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 04:03:14 +0000

    Add package pyside-app-core/0.6.1/pyside_app_core-0.6.1.tar.gz

---
+++ packages/pyside-app-core/pyside_app_core-0.6.1.tar.gz/pyside_app_core-0.6.1/src/pyside_app_core/ui/widgets/window_settings_mixin.py
@@ -0,0 +1,26 @@
+from typing import cast
+
+from PySide6.QtCore import QByteArray, QObject
+from PySide6.QtWidgets import QMainWindow, QWidget
+
+from pyside_app_core.ui.widgets.object_name_mixin import ObjectNameMixin
+from pyside_app_core.ui.widgets.settings_mixin import SettingsMixin
+
+
+class WindowSettingsMixin(ObjectNameMixin, SettingsMixin):
+    def __init__(self, parent: QObject | None = None, *args: object, **kwargs: object) -> None:
+        super().__init__(*args, parent=parent, **kwargs)
+
+    def _store_state(self) -> None:
+        self._settings.setValue(f"{self.obj_name}_geometry", cast(QWidget, self).saveGeometry())
+
+        if isinstance(self, QMainWindow):
+            cast(SettingsMixin, self)._settings.setValue(  # noqa: SLF001
+                f"{cast(ObjectNameMixin, self).obj_name}_window_state", self.saveState()
+            )
+
+    def _restore_state(self) -> None:
+        cast(QWidget, self).restoreGeometry(cast(QByteArray, self._settings.value(f"{self.obj_name}_geometry")))
+
+        if isinstance(self, QMainWindow):
+            self.restoreState(cast(QByteArray, self._settings.value(f"{self.obj_name}_window_state")))
