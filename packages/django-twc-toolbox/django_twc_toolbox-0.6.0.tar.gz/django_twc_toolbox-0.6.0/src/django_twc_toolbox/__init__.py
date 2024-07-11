29827e413326d585187a772152e5957e8dee3d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:04:38 +0000

    Add package django-twc-toolbox/0.6.0/django_twc_toolbox-0.6.0.tar.gz

---
+++ packages/django-twc-toolbox/django_twc_toolbox-0.6.0.tar.gz/django_twc_toolbox-0.6.0/src/django_twc_toolbox/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+__version__ = "0.6.0"
+__template_version__ = "2024.17"
---
+++ packages/django-twc-toolbox/django_twc_toolbox-0.6.0.tar.gz/django_twc_toolbox-0.6.0/tests/conftest.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import logging
+
+from django.conf import settings
+
+from .settings import DEFAULT_SETTINGS
+
+pytest_plugins = []  # type: ignore
+
+
+def pytest_configure(config):
+    logging.disable(logging.CRITICAL)
+
+    settings.configure(**DEFAULT_SETTINGS, **TEST_SETTINGS)
+
+
+TEST_SETTINGS = {
+    "INSTALLED_APPS": [
+        "django_twc_toolbox",
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "simple_history",
+        "tests.dummy",
+    ]
+}
