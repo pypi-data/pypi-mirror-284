1f4faad2518f5c4bf3b3ae3dda8b41219f4bba7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:03:58 +0000

    Add package quick-django-docker-setup/0.3.0/quick_django_docker_setup-0.3.0-py3-none-any.whl

---
+++ packages/quick-django-docker-setup/quick_django_docker_setup-0.3.0-py3-none-any.whl/quick_django_docker_setup/package_manager.py
@@ -0,0 +1,119 @@
+import subprocess
+import sys
+import shutil
+from quick_django_docker_setup.utils import create_file_and_insert
+
+
+class PackageManager:
+    def __init__(
+        self,
+        use_postgres: bool,
+        use_poetry: bool,
+        use_jwt: bool,
+        use_registration: bool,
+        use_celery: bool,
+        use_swagger: bool,
+        project_name: str,
+        outer_foldername: str,
+    ):
+        self.project_name = project_name
+        self.outer_foldername = outer_foldername
+
+        self.use_poetry = use_poetry
+        self.use_postgres = use_postgres
+        self.use_jwt = use_jwt
+        self.use_registration = use_registration
+        self.use_celery = use_celery
+        self.use_swagger = use_swagger
+
+    def check_poetry_installed(self):
+        try:
+            subprocess.run(
+                [sys.executable, "-m", "pip", "show", "poetry"],
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
+    def install_poetry(self):
+        subprocess.run(
+            [sys.executable, "-m", "pip", "install", "poetry"],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+    def uninstall_poetry(self):
+        subprocess.run(
+            [sys.executable, "-m", "pip", "uninstall", "-y", "poetry"],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+    def get_dependencies(self):
+        dependencies = ["django", "django-environ", "djangorestframework"]
+
+        if self.use_postgres:
+            dependencies.extend(["psycopg2-binary"])
+        if self.use_jwt:
+            dependencies.extend(["djangorestframework-simplejwt", "dj-rest-auth"])
+        if self.use_registration:
+            dependencies.extend(["dj-rest-auth[with_social]"])
+        if self.use_celery:
+            dependencies.extend(["celery[redis]", "django-celery-beat"])
+        if self.use_swagger:
+            dependencies.extend(["drf-yasg"])
+
+        return dependencies
+
+    def poetry_install_dependencies(self):
+        answers = (
+            "\n".join(
+                [self.project_name, "0.1.0", self.project_name, "n", "", "", "n", "n"]
+            )
+            + "\n"
+        )
+
+        process = subprocess.Popen(
+            ["poetry", "init"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        process.communicate(input=answers)
+
+        subprocess.run(
+            ["poetry", "add"] + self.get_dependencies(),
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        # copy poetry file into subfolder
+        shutil.move("pyproject.toml", f"./{self.outer_foldername}/pyproject.toml")
+        shutil.move("poetry.lock", f"./{self.outer_foldername}/poetry.lock")
+
+    def run_requirements(self):
+        text = "\n".join(dependency for dependency in self.get_dependencies())
+
+        create_file_and_insert("requirements.txt", text)
+
+    def run_poetry(self):
+        if self.check_poetry_installed():
+            self.poetry_install_dependencies()
+        else:
+            self.install_poetry()
+            self.poetry_install_dependencies()
+            self.uninstall_poetry()
+
+    def run(self):
+        if self.use_poetry:
+            self.run_poetry()
+        else:
+            self.run_requirements()
---
+++ packages/quick-django-docker-setup/quick_django_docker_setup-0.3.0-py3-none-any.whl/quick_django_docker_setup/text_to_insert/docker_compose_to_insert.py
@@ -0,0 +1,107 @@
+dockerignore = """**/.git
+**/.gitignore
+**/.vscode
+**/coverage
+**/.env
+**/.aws
+**/.ssh
+Dockerfile
+README.md
+docker-compose.yml
+**/.DS_Store
+**/venv
+**/env"""
+
+
+def get_backend_service_text(use_postgres: bool, outer_foldername: str) -> str:
+    text = f"""services:
+  backend:
+    container_name: backend
+    build:
+      context: ./
+      dockerfile: ./deployment/docker/Dockerfile
+    command: python manage.py runserver 0.0.0.0:8000
+    volumes:
+      - ./{outer_foldername}/:/{outer_foldername}
+    ports:
+      - 8000:8000
+    env_file:
+      - ./{outer_foldername}/config/.env
+"""
+
+    if use_postgres:
+        text += """    depends_on:
+      - database
+      """
+    else:
+        text += "\n"
+
+    return text
+
+
+def get_database_service_text(outer_foldername: str) -> str:
+    return (
+        "\n"
+        + f"""  database:
+    container_name: database
+    image: postgis/postgis:15-3.3-alpine
+    env_file:
+      - ./{outer_foldername}/config/.env
+    volumes:
+      - postgres-data:/var/lib/postgresql/data/
+      """
+    )
+
+
+def get_celery_service_text(
+    use_postgres: bool, outer_foldername: str, project_name: str
+) -> str:
+    if use_postgres:
+        database_text = "\n" + """      - database"""
+    else:
+        database_text = ""
+
+    return (
+        "\n"
+        + f"""  redis:
+    container_name: redis
+    image: redis:alpine
+    env_file:
+      - ./{outer_foldername}/config/.env
+    expose:
+      - 6379
+
+  celery-worker:
+    container_name: celery-worker
+    build:
+      context: ./
+      dockerfile: ./deployment/docker/Dockerfile
+    command: celery -A {project_name} worker -l info
+    volumes:
+      - ./{outer_foldername}/:/{outer_foldername}
+    env_file:
+      - ./{outer_foldername}/config/.env
+    depends_on:
+      - redis{database_text}
+
+  celery-beat:
+    container_name: celery-beat
+    build:
+      context: ./
+      dockerfile: ./deployment/docker/Dockerfile
+    command: celery -A {project_name} beat -l info
+    volumes:
+      - ./{outer_foldername}/:/{outer_foldername}
+    env_file:
+      - ./{outer_foldername}/config/.env
+    depends_on:
+      - redis
+      - celery-worker{database_text}"""
+    )
+
+
+def get_volume_text() -> str:
+    return """
+    
+volumes:
+  postgres-data:"""
