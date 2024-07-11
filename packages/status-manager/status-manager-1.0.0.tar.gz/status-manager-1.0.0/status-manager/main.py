c164c556bcfb505167f18b34e4073c6cbc2bc019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:03:55 +0000

    Add package status-manager/1.0.0/status-manager-1.0.0.tar.gz

---
+++ packages/status-manager/status-manager-1.0.0.tar.gz/status-manager-1.0.0/status-manager/main.py
@@ -0,0 +1,57 @@
+from enum import Enum
+
+
+class StatusType(Enum):
+    unchecked = 'unchecked'
+    started = 'started'
+    finished = 'finished'
+
+
+class CheckStatus:
+    def __ini__(self):
+        pass
+
+    def read_status(self, dir_path) -> StatusType:
+        with open(dir_path+'/status.txt', 'r') as f:
+            data = StatusType.unchecked
+            for raw_line in f.readlines():
+                line = raw_line.strip()
+                if line == '':
+                    continue
+
+                for status in StatusType:
+                    if line.startswith(status.name):
+                        data = status
+
+        return data
+
+    def init_status(self, dir_path):
+        with open(dir_path+'/status.txt', 'w') as f:
+            f.write(StatusType.unchecked.value)
+
+    def write_status(self, dir_path, status: StatusType):
+        with open(dir_path+'/status.txt', 'a') as f:
+            f.write('\n')
+            f.write(status.value)
+
+    def check(self, dir_path, first_time=True):
+        try:
+            return self.read_status(dir_path)
+        except FileNotFoundError:
+            if not first_time:
+                raise Exception("Error: There is a problem in "+str(dir_path))
+            self.init_status(dir_path)
+            return self.check(dir_path, first_time=False)
+
+
+# if __name__ == "__main__":
+#     s = CheckStatus()
+#     # for i in range(1, 11):
+#     #     data = s.check(str(i))
+#     #     if data == Status.unchecked.value:
+#     #         s.write_status(str(i), Status.started)
+#     #     elif data == Status.finished:
+#     #         pass
+#     #     print(f"folder {i} has {data}")
+#     print(s.check('1/'))
+#     print(s.check('1/') == StatusType.finished)
