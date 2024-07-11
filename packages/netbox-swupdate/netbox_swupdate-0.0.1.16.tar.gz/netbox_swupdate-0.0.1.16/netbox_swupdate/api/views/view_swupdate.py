22307e5851117370020c038bc6266884019171ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:04:20 +0000

    Add package netbox-swupdate/0.0.1.16/netbox_swupdate-0.0.1.16.tar.gz

---
+++ packages/netbox-swupdate/netbox_swupdate-0.0.1.16.tar.gz/netbox_swupdate-0.0.1.16/netbox_swupdate/api/views/view_swupdate.py
@@ -0,0 +1,57 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+
+class RouteDownloadView(APIView):
+    """Download firmware."""
+
+    permission_classes = [IsAuthenticated]
+
+    # def route_download(request, filepath):
+    #     """
+    #     Maneja la descarga de archivos de firmware desde la instancia de SWUpdate.
+    #
+    #     Args:
+    #         request (HttpRequest): La solicitud HTTP entrante.
+    #         filepath (str): La ruta del archivo de firmware solicitado.
+    #
+    #     Returns:
+    #         HttpResponse: La respuesta HTTP que contiene el archivo de firmware o un mensaje de error.
+    #     """
+    #     try:
+    #         # Realiza una solicitud GET a la API de SWUpdate para obtener el archivo de firmware
+    #         response = requests.get(f"{SWUPDATE_URL}/firmware/{filepath}", stream=True)
+    #     except requests.exceptions.RequestException as e:
+    #         # Maneja cualquier error de conexión con la API de SWUpdate
+    #         return HttpResponse(
+    #             f"500 Error connecting to SWUpdate: {e}",
+    #             status=500,
+    #             content_type="text/plain; charset=utf-8",
+    #         )
+    #
+    #     if response.status_code == 200:
+    #         # Si la solicitud tiene éxito, devuelve el contenido del archivo de firmware
+    #         return HttpResponse(
+    #             response.content, content_type="application/octet-stream"
+    #         )
+    #     else:
+    #         # Si hay algún error (por ejemplo, archivo no encontrado), devuelve el mensaje de error
+    #         return HttpResponse(
+    #             response.content,
+    #             status=response.status_code,
+    #             content_type=response.headers.get(
+    #                 "Content-Type", "text/plain; charset=utf-8"
+    #             ),
+    #         )
+
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            # - debemos obtener el dispositivo, el id.
+            # - buscar en deploy si ese dispositivo tiene actualizaciones.
+            #     - Se busca por ID y por fecha.
+            #     - Se verifica si el Device ya ha sido actualizado con el deploy configurado.
+            #     - Se verifica el estado con el que termino el deploy.
+            pass
+        else:
+            pass
