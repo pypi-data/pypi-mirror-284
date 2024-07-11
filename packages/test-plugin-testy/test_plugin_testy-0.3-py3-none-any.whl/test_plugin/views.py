ca219d1ee6b630f5597a3278a87e611a5917a0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:04:03 +0000

    Add package test-plugin-testy/0.3/test_plugin_testy-0.3-py3-none-any.whl

---
+++ packages/test-plugin-testy/test_plugin_testy-0.3-py3-none-any.whl/test_plugin/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from .forms import UploadFileForm
+
+class UploadFileFormView(View):
+    form_class = UploadFileForm
+    template_name = 'test_plugin/upload_form.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            # Save the file or handle it as needed
+            return redirect(reverse('plugins:test_plugin:upload-success'))
+        return render(request, self.template_name, {'form': form})
+
+class UploadSuccessView(View):
+    template_name = 'test_plugin/upload_success.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
