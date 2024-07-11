dd1bc6c39ff1a1a4ae462ae7598e1c891cd04c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:03:37 +0000

    Add package Str2D/0.2.6/str2d-0.2.6.tar.gz

---
+++ packages/Str2D/str2d-0.2.6.tar.gz/str2d-0.2.6/README.md
@@ -0,0 +1,19 @@
+# Str2D
+
+[![PyPI version](https://badge.fury.io/py/str2d.svg)](https://badge.fury.io/py/str2d)
+[![Documentation Status](https://readthedocs.org/projects/str2d/badge/?version=latest)](https://str2d.readthedocs.io/en/latest/?badge=latest)
+
+[![Build Status](https://github.com/pirsquared/str2d/actions/workflows/publish-pypi-pages.yml/badge.svg)](https://github.com/pirsquared/str2d/actions)
+
+## Overview
+
+Manipulate strings in 2D.
+
+## Documentation
+
+Full documentation is available at [Str2D](https://pirsquared.github.io/Str2D/).
+
+## Installation
+
+`pip install str2d`
+
---
+++ packages/Str2D/str2d-0.2.6.tar.gz/str2d-0.2.6/setup.py
@@ -0,0 +1,37 @@
+"""setup.py.
+
+This setup.py file is included to support editable installations using pip's `-e`
+option. The primary project configuration is specified in the pyproject.toml file. This
+setup.py is only used for development installations and ensures compatibility with tools
+and workflows that rely on setup.py.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Str2D",
+    version="0.2.6",  # Update this version number before releasing a new version
+    description="Manipulate 2D strings in Python.",
+    author="Sean Smith",
+    author_email="pirsquared.pirr@gmail.com",
+    url="https://github.com/pirsquared/Str2D",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas", "mpmath"],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "build",
+            "twine",
+            "black",
+            "pre-commit",
+            "pylint",
+            "sphinx",
+            "sphinx-rtd-theme",
+            "sphinx-copybutton",
+            "sphinx-autobuild",
+            "sphinx-autodoc-typehints",
+            "ipython",
+        ],
+    },
+)
