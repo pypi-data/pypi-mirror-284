c42ed393dc59ba7eca5a148163207319eefdcbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:03:41 +0000

    Add package pyorganoid/0.1.1/pyorganoid-0.1.1.tar.gz

---
+++ packages/pyorganoid/pyorganoid-0.1.1.tar.gz/pyorganoid-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyorganoid',
+    version='0.1.1',
+    packages=find_packages(),
+    author='Daniel Szelogowski',
+    description='A Python package for the simulation of organoids for the purpose of studying '
+                'Organoid Intelligence (OI) and Organoid Learning (OL).',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'graphviz',
+    ],
+    extras_require={
+        "tensorflow": ["tensorflow<=2.15"],
+        "torch": ["torch"],
+        "sklearn": ["scikit-learn", "joblib"],
+        "onnx": ["onnxruntime", "onnxmltools"],
+        "all": ["tensorflow<=2.15", "torch", "scikit-learn", "joblib", "onnxruntime", "onnxmltools"]
+    },
+    entry_points={
+        'console_scripts': [
+            'pyorganoid=pyorganoid:main',
+        ],
+    },
+)
