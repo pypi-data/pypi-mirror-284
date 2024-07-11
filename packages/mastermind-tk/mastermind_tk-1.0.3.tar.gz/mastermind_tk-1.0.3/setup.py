ad9dd4c00fe4d75fd0cecd8416592de4dc66ed9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:13 +0000

    Add package mastermind-tk/1.0.3/mastermind_tk-1.0.3.tar.gz

---
+++ packages/mastermind-tk/mastermind_tk-1.0.3.tar.gz/mastermind_tk-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mastermind_tk',
+    version='1.0.3',
+    requires=["tkinter", "PIL"],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mastermind=mastermind_tk.mastermind_tk:main'
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['resources/*']
+    },
+    author='Bouquet Cédric',
+    author_email='cedric-bouquet-7@outlook.fr',
+    description="Jeu du mastermind en version graphique",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    python_requires='>=3.0',
+)
