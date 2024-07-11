3480387c108f823b6911ca739cfd07b03cc9f9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 17:03:43 +0000

    Add package deepbays/0.0.7/deepbays-0.0.7.tar.gz

---
+++ packages/deepbays/deepbays-0.0.7.tar.gz/deepbays-0.0.7/deepbays/kernels/__init__.py
@@ -0,0 +1,8 @@
+from .kernels import kernel_erf
+from .kernels import kernel_erf_torch
+from .kernels import kernel_relu
+from .kernels import kernel_relu_bias
+from .kernels import mean_relu
+from .kernels import computeKmatrix
+
+
