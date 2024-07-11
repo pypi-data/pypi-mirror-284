1113cfe2f970d6120b0d1cd8981894c36777e464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:04:01 +0000

    Add package ansible-dev-environment/24.7.0/ansible_dev_environment-24.7.0-py3-none-any.whl

---
+++ packages/ansible-dev-environment/ansible_dev_environment-24.7.0-py3-none-any.whl/ansible_dev_environment/__main__.py
@@ -0,0 +1,11 @@
+"""A runpy entry point for ansible-dev-environment.
+
+This makes it possible to invoke CLI
+via :command:`python -m ansible_dev_environment`.
+"""
+
+from ansible_dev_environment.cli import main
+
+
+if __name__ == "__main__":
+    main()
