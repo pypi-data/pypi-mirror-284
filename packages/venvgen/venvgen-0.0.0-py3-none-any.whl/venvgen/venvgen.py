58c7215240b62e090f226c399755123e0517f8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:05:51 +0000

    Add package venvgen/0.0.0/venvgen-0.0.0-py3-none-any.whl

---
+++ packages/venvgen/venvgen-0.0.0-py3-none-any.whl/venvgen/venvgen.py
@@ -0,0 +1,20 @@
+from .utils.ui_controler import *
+from .utils.all_system_control import check_system
+import warnings
+from .utils.package_directory_manager import *
+
+def main():
+    warnings.filterwarnings("ignore")
+    create_database_dir()
+
+    THIS_MACHINE = check_system()
+    end_program = False
+
+    while not end_program:
+        introduction_screen()
+        end_program = menu_screen()
+
+
+
+
+
