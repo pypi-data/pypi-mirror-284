be79e1b21d889b8d72c1cca90f9971bb930bb003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 15:04:05 +0000

    Add package patchwork-cli/0.0.30/patchwork_cli-0.0.30.tar.gz

---
+++ packages/patchwork-cli/patchwork_cli-0.0.30.tar.gz/patchwork_cli-0.0.30/patchwork/steps/ExtractDiff/typed.py
@@ -0,0 +1,25 @@
+from typing_extensions import Annotated, Iterable, TypedDict
+
+from patchwork.common.utils.typing import IS_CONFIG
+
+
+class __ExtractDiffRequiredInputs(TypedDict):
+    update_info: "UpdateInfo"
+    libraries_api_key: Annotated[str, IS_CONFIG]
+    github_api_key: Annotated[str, IS_CONFIG]
+
+
+class ExtractDiffInputs(__ExtractDiffRequiredInputs, total=False):
+    severity: Annotated[str, IS_CONFIG]
+
+
+class ExtractDiffOutputs(TypedDict):
+    prompt_values: Iterable[dict]
+    library_name: str
+    platform_type: str
+
+
+class UpdateInfo(TypedDict):
+    vuln_version: str
+    fixed_version: str
+    purl: str
