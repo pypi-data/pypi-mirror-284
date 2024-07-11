ecad3f6d9bdf498ead0cd88b1fa0ba60db7bef92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:04:26 +0000

    Add package jupyterhub-share/0.1.2/jupyterhub_share-0.1.2.tar.gz

---
+++ packages/jupyterhub-share/jupyterhub_share-0.1.2.tar.gz/jupyterhub_share-0.1.2/jupyterhub_config.py
@@ -0,0 +1,33 @@
+from jupyterhub.auth import DummyAuthenticator
+from jupyterhub.spawner import SimpleLocalProcessSpawner
+
+import jupyterhub_share
+
+c.JupyterHub.authenticator_class = DummyAuthenticator
+c.JupyterHub.spawner_class = SimpleLocalProcessSpawner
+
+c.SimpleLocalProcessSpawner.options_form = """
+          Choose a system:
+          <select name="system">
+              <option value="Local">Local</option>
+              <option value="Remote-A">Remote A</option>
+              <option value="Remote-B">Remote B</option>
+          </select>
+          <select name="image">
+              <option value="a">Image A</option>
+              <option value="b">Image B</option>
+          </select>
+          <input type="checkbox" name="Check02" value="true" checked="true" />
+          <input type="checkbox" name="Check01" value="true" />
+          <input type="text" name="textf" class="form-control" id="repository" data-lpignore="true" placeholder="GitHub repository name or URL">
+          <input type="number" name="runtime-input" value="30" min="10" max="1440" required="required">
+          <input type="range" name="range1" min="0" max="100" value="90" step="10" />
+      """
+c.SimpleLocalProcessSpawner.default_url = "/hub/home"
+
+import os
+
+s = os.getcwd()
+c.JupyterHub.template_paths = os.path.join(s, "share", "templates")
+
+c.JupyterHub.allow_named_servers = True
