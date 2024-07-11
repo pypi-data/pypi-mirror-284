d1635075d1f56ead81b3da0ea761930ea9dde476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:04:22 +0000

    Add package fletura/1.0.8/fletura-1.0.8.tar.gz

---
+++ packages/fletura/fletura-1.0.8.tar.gz/fletura-1.0.8/fletura/paper.py
@@ -0,0 +1,73 @@
+import flet as ft
+from flet import *
+
+
+# A component that displays its content on an elevated surface
+class Paper(Container):
+    def __init__(
+        self,
+        outlined: bool = False,
+        elevation: int = 1,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.elevation: int = elevation
+        # Define the shadow parameters based on the distance
+        self.shadow_params: dict = {
+            0: (0, 0, 0, 0),
+            1: (1, 2, 0.1, 1),
+            2: (2, 4, 0.15, 2),
+            3: (3, 6, 0.2, 3),
+            4: (4, 8, 0.25, 4),
+            8: (6, 10, 0.3, 8),
+            12: (8, 12, 0.35, 12),
+            16: (10, 14, 0.4, 16),
+            24: (12, 14, 0.4, 16),
+        }
+        spread_radius, blur_radius, opacity, offset_y = self.shadow_params.get(
+            self.elevation, (0, 0, 0, 0)
+        )
+        # Create the shadow based on the parameters
+        shadows = [
+            ft.BoxShadow(
+                spread_radius=spread_radius,
+                blur_radius=blur_radius,
+                color=ft.colors.with_opacity(opacity, "black"),
+                offset=ft.Offset(0, offset_y),
+            )
+        ]
+
+        # Create the container with the specified shadow
+        if outlined:
+            self.border = border.all(width=2, color="#80868B")
+        self.shadow = shadows
+
+
+def main(page: Page):
+    page.scroll = "always"
+    # page.bgcolor = "white"
+    page.theme_mode = "light"
+    page.spacing = 50
+    page.padding = 0
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+    # Test with different distances
+    distances = [0, 1, 2, 3, 4, 8, 12, 16, 24]
+    for dist in distances:
+        container = Paper(
+            elevation=dist,
+            width=200,
+            height=50,
+            bgcolor="white",
+            outlined=True,
+            content=ft.Text(f"Elevation {dist}", color="black"),
+        )
+        container.border_radius = 5
+        page.add(container)
+        if dist == 24:
+            container.margin = margin.only(bottom=50)
+        container.update()
+
+
+if __name__ == "__main__":
+    app(target=main)
---
+++ packages/fletura/fletura-1.0.8.tar.gz/fletura-1.0.8/fletura/timeline.py
@@ -0,0 +1,206 @@
+from flet import *
+
+
+class TimelineDot(Container):
+    def __init__(
+        self,
+        icon: str = None,
+        icon_size: int = 0,
+        icon_color: str = colors.WHITE,
+        dot_size: int = 5,
+        dot_border_radius: int = 50,
+        border: Border = border.all(3, color="white"),
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.content = (
+            Icon(
+                icon,
+                size=icon_size,
+                color=icon_color,
+            )
+            if icon != None
+            else None
+        )
+        self.alignment = alignment.center
+        self.padding = padding.all(dot_size)
+        self.border = border
+        self.border_radius = dot_border_radius
+
+
+class TimelineSeparator(Container):
+    def __init__(
+        self, color: str = colors.WHITE, width: int = 2, height: int = 50, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.content = Container(
+            width=width,
+            height=height,
+            bgcolor=color,
+            alignment=alignment.center,
+        )
+
+
+class TimelineEvent(Container):
+    def __init__(
+        self,
+        title: str,
+        dot_props: dict = {},
+        separator_props: dict = {},
+        title_style: TextStyle = None,
+        description: str = None,
+        description_style: TextStyle = None,
+        timestamp: str = None,
+        timestamp_style: TextStyle = None,
+        content_position: int = 8,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.title = title
+        self.description = description
+        self.timestamp = timestamp
+
+        self.content_position = content_position
+        self.title_style = title_style
+        self.description_style = description_style
+        self.timestamp_style = timestamp_style
+
+        self.dot_props = dot_props
+        self.separator_props = separator_props
+
+        self.content = Row(
+            [
+                Column(
+                    [
+                        TimelineDot(**self.dot_props),
+                        TimelineSeparator(**self.separator_props),
+                    ],
+                    alignment="center",
+                    horizontal_alignment="center",
+                    spacing=0,
+                ),
+                Container(
+                    Column(
+                        [
+                            Text(
+                                self.title,
+                                weight=FontWeight.BOLD,
+                                size=16,
+                                style=self.title_style,
+                            ),
+                            Text(
+                                self.description, size=14, style=self.description_style
+                            ),
+                            Text(
+                                self.timestamp,
+                                size=12,
+                                color=colors.GREY,
+                                style=self.timestamp_style,
+                            ),
+                            Container(
+                                height=self.content_position
+                            ),  # A container to adjust the content position
+                        ],
+                        alignment="start",
+                        spacing=5,
+                    ),
+                    padding=padding.only(0, top=0),
+                    margin=margin.all(0),
+                ),
+            ],
+            alignment="start",
+            spacing=10,
+        )
+
+        self.margin = margin.only(top=10)
+
+
+class Timeline(Container):
+    def __init__(self, events: list, **kwargs):
+        super().__init__(**kwargs)
+        self.events = events
+
+        self.content = Column(
+            [TimelineEvent(**event) for index, event in enumerate(self.events)],
+            alignment="start",
+            spacing=0,
+        )
+
+
+def main(page: Page):
+    page.vertical_alignment = "start"
+    page.horizontal_alignment = "center"
+    page.padding = 20
+
+    events = [
+        {
+            "title": "Event 1",
+            "description": "This is the description for event 1.",
+            "timestamp": "2023-01-01 10:00 AM",
+            "dot_props": {
+                # "icon": icons.EVENT,
+                # "icon_size": 30,
+                # "border_color": "red",
+                # "border_radius": 2,
+                # "dot_border_radius": 0,
+                "border": border.all(2, color="blue"),
+            },
+            "content_position": 0,
+            "separator_props": {
+                "color": colors.GREEN_500,
+                # "width": 3,
+                "height": 70,
+                "margin": margin.only(top=5),
+            },
+            "content_position": 25,
+        },
+        {
+            "title": "Event 2",
+            "description": "This is the description for event 2.",
+            "timestamp": "2023-02-01 12:00 PM",
+            # "icon": icons.CALENDAR_TODAY,
+            # "icon_size": 25,
+            "dot_props": {
+                # "border_color": "red",
+                # "border_radius": 2,
+                # "dot_border_radius": 0,
+                "border": border.all(2, color="red"),
+            },
+            "content_position": 0,
+            "separator_props": {
+                # "color": colors.BLUE,
+                # "width": 3,
+                "height": 70,
+                "margin": margin.only(top=5),
+            },
+            "content_position": 25,
+        },
+        {
+            "title": "Event 3",
+            "description": "This is the description for event 3.",
+            "timestamp": "2023-03-01 02:00 PM",
+            # "icon": icons.DATE_RANGE,
+            # "icon_size": 20,
+            "dot_props": {
+                # "border_radius": 2,
+                # "dot_border_radius": 0,
+                "border": border.all(2, color="green"),
+            },
+            "content_position": 0,
+            "separator_props": {
+                "color": colors.BLUE_200,
+                # "width": 3,
+                "height": 70,
+                "margin": margin.only(top=5),
+            },
+            "content_position": 20,
+        },
+    ]
+
+    timeline = Timeline(events)
+
+    page.add(timeline)
+
+
+if __name__ == "__main__":
+    app(target=main)
