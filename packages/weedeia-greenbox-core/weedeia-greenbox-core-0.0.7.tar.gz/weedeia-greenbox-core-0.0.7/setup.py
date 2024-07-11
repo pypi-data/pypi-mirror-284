ef05a702522c16940ae5ad699702ec1fb5aad908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:04:38 +0000

    Add package weedeia-greenbox-core/0.0.7/weedeia-greenbox-core-0.0.7.tar.gz

---
+++ packages/weedeia-greenbox-core/weedeia-greenbox-core-0.0.7.tar.gz/weedeia-greenbox-core-0.0.7/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weedeia-greenbox-core",
+    version="0.0.7",
+    author="Paulo Porto",
+    description="API for GPIO admnistration",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+      "console_scripts": [
+          "weedeia-greenbox-core=main:main",
+      ],
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'wheel',
+        'fastapi',
+        'uvicorn',
+        'rpi.gpio'
+    ]
+)
