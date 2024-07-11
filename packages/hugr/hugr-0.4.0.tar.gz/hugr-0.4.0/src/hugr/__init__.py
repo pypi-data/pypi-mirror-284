45ea01f7ff756d2c54b2e2f69859c16cbff6ec6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:03:36 +0000

    Add package hugr/0.4.0/hugr-0.4.0.tar.gz

---
+++ packages/hugr/hugr-0.4.0.tar.gz/hugr-0.4.0/src/hugr/__init__.py
@@ -0,0 +1,7 @@
+"""`hugr` is a Python package for the Quantinuum HUGR common
+representation.
+"""
+
+# This is updated by our release-please workflow, triggered by this
+# annotation: x-release-please-version
+__version__ = "0.4.0"
