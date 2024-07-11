c5486eca7b789a3686ddbe2aa4f7ad77d0aafdd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:03:47 +0000

    Add package safari-to-sqlite/0.2.0/safari_to_sqlite-0.2.0-py3-none-any.whl

---
+++ packages/safari-to-sqlite/safari_to_sqlite-0.2.0-py3-none-any.whl/safari_to_sqlite/__main__.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import logging
+import time
+from collections import Counter
+from functools import reduce
+from socket import gethostname
+from sys import argv, stderr
+
+from loguru import logger
+
+from safari_to_sqlite.blacklist import filter_blacklist
+from safari_to_sqlite.constants import EXTRACT_FAILED
+from safari_to_sqlite.download import extract_body
+from safari_to_sqlite.errors import FailedDownloadError
+from safari_to_sqlite.safari import get_safari_tabs
+from safari_to_sqlite.turso import get_auth_creds_from_json, save_auth, turso_setup
+
+from .datastore import Datastore
+
+
+def auth(auth_path: str) -> None:
+    """Save authentication credentials to a JSON file."""
+    turso_url = input(
+        "Enter your Turso database URL e.g. libsql://<yours>.turso.io\n"
+        "(Leave this blank to start new DB setup)\n> ",
+    )
+    if turso_url == "":
+        (turso_url, turso_auth_token) = turso_setup()
+        save_auth(auth_path, turso_url, turso_auth_token)
+    elif not turso_url.startswith("libsql://"):
+        logger.error("Invalid libsql URL, please try again.")
+        return
+    else:
+        turso_auth_token = input(
+            "Enter your Turso database token\n"
+            "(Create this by running `turso db tokens create <your DB>`)\n> ",
+        )
+        save_auth(auth_path, turso_url, turso_auth_token)
+
+
+def save(
+    db_path: str,
+    auth_json: str,
+) -> None:
+    """Save Safari tabs to SQLite database."""
+    host = gethostname()
+    first_seen = int(time.time())
+    logger.info(f"Loading tabs from Safari for {host}...")
+
+    tabs, urls = get_safari_tabs(host, first_seen)
+    logger.info(f"Finished loading tabs, connecting to database: {db_path}")
+
+    initial_count = len(tabs)
+    tabs = [tab for tab in tabs if filter_blacklist(tab.url)]
+    duplicate_count = reduce(lambda acc, val: acc + val - 1, Counter(urls).values())
+    blacklist_count = initial_count - len(tabs)
+    logger.info(
+        f"Found {len(tabs)} tabs ({duplicate_count} duplicates, "
+        f"{blacklist_count} blacklisted)",
+    )
+
+    db = Datastore(db_path, **get_auth_creds_from_json(auth_json))
+    db.insert_tabs(tabs)
+
+
+def _configure_logging() -> None:
+    # Ours
+    logger.remove()
+    logger.add(
+        stderr,
+        colorize=True,
+        format="{time:HH:mm:ss.SS} | <level>{message}</level>",
+    )
+    # Turso
+    replication_logger = logging.getLogger("libsql_replication")
+    remote_client_logger = logging.getLogger("libsql.replication.remote_client")
+    replication_logger.setLevel(logging.WARNING)
+    remote_client_logger.setLevel(logging.WARNING)
+
+
+def request_missing_bodies(db_path: str, auth_json: str) -> None:
+    """Request body when missing and save extracted contents."""
+    db = Datastore(db_path, **get_auth_creds_from_json(auth_json))
+    for url, title in db.find_empty_body():
+        logger.info(f"Downloading and extracting body for {title} @ {url}")
+        try:
+            body = extract_body(url)
+            if not body:
+                logger.error(f"Failed to extract: {url}")
+            db.update_body(url, body or EXTRACT_FAILED)
+        except FailedDownloadError as e:
+            logger.error(f"Failed to download ({e.code}): {url}")
+            db.update_body(url, e.code)
+
+
+def main() -> None:
+    """Start main entry point."""
+    _configure_logging()
+    db_default = "safari_tabs.db"
+    auth_default = "auth.json"
+    if len(argv) == 1 or argv[1].endswith(".db"):
+        db = argv[1] if len(argv) > 1 else db_default
+        auth_path = argv[2] if len(argv) > 2 else auth_default  # noqa: PLR2004
+        save(db, auth_path)
+    elif argv[1] == "auth":
+        auth_path = argv[1] if len(argv) > 1 else auth_default
+        auth(auth_path)
+    elif argv[1] == "download":
+        db = argv[2] if len(argv) > 2 else db_default  # noqa: PLR2004
+        auth_path = argv[3] if len(argv) > 3 else auth_default  # noqa: PLR2004
+        request_missing_bodies(db, auth_path)
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/safari-to-sqlite/safari_to_sqlite-0.2.0-py3-none-any.whl/safari_to_sqlite/datastore.py
@@ -0,0 +1,123 @@
+from collections.abc import Iterable
+from typing import NamedTuple
+
+import libsql_experimental as libsql
+from loguru import logger
+
+from safari_to_sqlite.constants import (
+    BODY,
+    FIRST_SEEN,
+    HOST,
+    SCRAPE_STATUS,
+    TAB_INDEX,
+    TABS,
+    TITLE,
+    URL,
+    WINDOW_ID,
+)
+
+
+class TabRow(NamedTuple):
+    """NamedTuple for tab data."""
+
+    url: str
+    title: str
+    body: str
+    window_id: int
+    tab_index: int
+    host: str
+    first_seen: int
+    scrape_status: int
+
+
+class Datastore:
+    """Object responsible for all database interactions."""
+
+    def __init__(
+        self,
+        db_path: str,
+        turso_url: str | None,
+        turso_auth_token: str | None,
+    ) -> None:
+        """Instantiate and ensure tables exist with expected columns."""
+        self.is_remote = turso_url is not None and turso_auth_token is not None
+
+        if self.is_remote:
+            logger.info(f"Connecting to remote db: {turso_url}")
+        else:
+            logger.info(f"Connecting to local db: {db_path}")
+        self.con = (
+            libsql.connect(db_path)
+            if not self.is_remote
+            else libsql.connect(
+                db_path,
+                sync_url=turso_url,
+                auth_token=turso_auth_token,
+            )
+        )
+        self._prepare_db()
+
+    def _prepare_db(self) -> None:
+        create_tabs = f"""
+        CREATE TABLE IF NOT EXISTS {TABS} (
+            {URL} TEXT PRIMARY KEY,
+            {TITLE} TEXT NOT NULL,
+            {BODY} TEXT,
+            {WINDOW_ID} INTEGER,
+            {TAB_INDEX} INTEGER,
+            {HOST} TEXT,
+            {FIRST_SEEN} INTEGER,
+            {SCRAPE_STATUS} INTEGER
+        );"""
+        self.con.execute(create_tabs)
+        self.con.commit()
+        if self.is_remote:
+            self.con.sync()
+
+    def get_tabs(self) -> Iterable[TabRow]:
+        """Return all tabs."""
+        cur: libsql.Cursor = self.con.cursor()
+        yield from cur.execute(
+            (
+                "SELECT "
+                f"{URL}, {TITLE}, {BODY}, {WINDOW_ID}, "
+                f"{TAB_INDEX}, {HOST}, {FIRST_SEEN}, {SCRAPE_STATUS} "
+                f"FROM {TABS};",
+            ),
+        )
+        cur.close()
+
+    def insert_tabs(self, tabs: list[TabRow]) -> None:
+        """Insert tabs into the database."""
+        insert = f"INSERT OR IGNORE INTO {TABS} VALUES(?, ?, ?, ?, ?, ?, ?, ?);"
+        self.con.executemany(insert, tabs)
+        self.con.commit()
+        logger.info("Local database updated successfully")
+        if self.is_remote:
+            logger.info("Syncing to remote database")
+            self.con.sync()
+            logger.info("Finished syncing")
+
+    def find_empty_body(self) -> Iterable[tuple[str, str]]:
+        """Find tabs with empty body and request the body."""
+        cur: libsql.Cursor = self.con.execute(
+            f"SELECT {URL}, {TITLE} "
+            f"FROM {TABS} "
+            f"WHERE ({BODY} IS NULL OR {BODY} = '') AND {SCRAPE_STATUS} < 400;",
+        )
+        # Can't iterate cursor directly with libsql-experimental
+        while (row := cur.fetchone()) is not None:
+            yield row
+        cur.close()
+
+    def update_body(self, url: str, body: str | int) -> None:
+        """Update body for the URL. If body is an int, update scrape status instead."""
+        if isinstance(body, int):
+            stmt = f"UPDATE {TABS} SET {SCRAPE_STATUS} = ? WHERE {URL} = ?;"
+        else:
+            stmt = (
+                f"UPDATE {TABS} SET {BODY} = ?, {SCRAPE_STATUS} = 200 "
+                f"WHERE {URL} = ?;"
+            )
+        self.con.execute(stmt, (body, url))
+        self.con.commit()
