4bb4ffcd4ba881c85f661aa8d5635a131f1b9033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 04:03:47 +0000

    Add package finch-api/0.21.9/finch_api-0.21.9-py3-none-any.whl

---
+++ packages/finch-api/finch_api-0.21.9-py3-none-any.whl/finch/types/hris/company_benefit.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from ..._models import BaseModel
+from .benefit_type import BenefitType
+from .benefit_frequency import BenefitFrequency
+from .benefit_contribution import BenefitContribution
+
+__all__ = ["CompanyBenefit"]
+
+
+class CompanyBenefit(BaseModel):
+    benefit_id: str
+
+    company_contribution: Optional[BenefitContribution] = None
+
+    description: Optional[str] = None
+
+    employee_deduction: Optional[BenefitContribution] = None
+
+    frequency: Optional[BenefitFrequency] = None
+
+    type: Optional[BenefitType] = None
+    """Type of benefit."""
