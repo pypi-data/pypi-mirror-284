9890a44df2492c54269adfc91de09b45d05f099d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 04:03:36 +0000

    Add package types-wxpython/0.9.7/types_wxpython-0.9.7-py3-none-any.whl

---
+++ packages/types-wxpython/types_wxpython-0.9.7-py3-none-any.whl/wx-stubs/TopLevelWindow/__init__.pyi
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from typing import Any, ContextManager, Optional, TypeAlias, Union
+
+class GeometrySerializer:
+    """ Class used with SaveGeometry() and RestoreToGeometry().
+
+        Source: https://docs.wxpython.org/wx.TopLevelWindow.GeometrySerializer.html
+    """
+
+
