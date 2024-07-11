d6767233c2f8f5137ef48e102c6067899e6550cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:03:40 +0000

    Add package ada-python/0.3.1/ada_python-0.3.1-py3-none-any.whl

---
+++ packages/ada-python/ada_python-0.3.1-py3-none-any.whl/abcs/models.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class UsageStats(BaseModel):
+    input_tokens: int
+    output_tokens: int
+    extra: object
+
+class PromptResponse(BaseModel):
+    content: str
+    raw_response: Any
+    error: object
+    usage: UsageStats
+
+class OllamaMessage(BaseModel):
+    role: str
+    content: str
+
+class OllamaResponse(BaseModel):
+    model: str
+    created_at: str
+    message: OllamaMessage
+    done: bool
+    total_duration: int
+    load_duration: int
+    prompt_eval_count: int
+    prompt_eval_duration: int
+    eval_count: int
+    eval_duration: int
