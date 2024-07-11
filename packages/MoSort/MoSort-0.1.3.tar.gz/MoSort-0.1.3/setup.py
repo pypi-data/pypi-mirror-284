195a8a20ac7d79f928a493f4c317d00b19adac7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:03:56 +0000

    Add package MoSort/0.1.3/MoSort-0.1.3.tar.gz

---
+++ packages/MoSort/MoSort-0.1.3.tar.gz/MoSort-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MoSort',
+    version='0.1.3',
+    author='Mohammad.E Eslami',
+    author_email='mohamad.slami@gmail.com',
+    description = 'A Python library that provides various sorting algorithms including Bubble Sort, Quick Sort, Merge Sort, Insertion Sort, Selection Sort, and Heap Sort. Written by Mohammad E. Eslami.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Mohammades/',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
