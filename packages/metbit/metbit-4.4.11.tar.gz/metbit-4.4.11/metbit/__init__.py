d4a35f4cf039286fab5464590519e03fdc4baea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:39 +0000

    Add package metbit/4.4.11/metbit-4.4.11.tar.gz

---
+++ packages/metbit/metbit-4.4.11.tar.gz/metbit-4.4.11/metbit/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .metbit import opls_da, pca
+from .utility import *
+from .lazy_opls_da import lazy_opls_da
+
+__author__ = 'aeiwz'
+__email__ = 'theerayut_aeiw_123@hotmail.com'
+
+
+
