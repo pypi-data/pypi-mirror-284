cea0901e19b8d498f43a1f25fb4bc0509e2be4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:04:37 +0000

    Add package asaas-sdk-wlc/0.1.6/asaas_sdk_wlc-0.1.6.tar.gz

---
+++ packages/asaas-sdk-wlc/asaas_sdk_wlc-0.1.6.tar.gz/asaas_sdk_wlc-0.1.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import (
+    find_packages,
+    setup
+)
+
+setup(
+    name='asaas-sdk-wlc',
+    packages=find_packages(include=['asaas']),
+    version='0.1.6',
+    license='Apache-2.0',
+    description='SDK for Asaas API',
+    author='WLC Soluções',
+    author_email='fernando.leite@wlcsolucoes.com.br',
+    url='https://github.com/wlc-solucoes/asaas-sdk/',
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.12',
+    ]
+)
