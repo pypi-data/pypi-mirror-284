b7afb56acbb4bc82d94abc5fe2aadcac080af83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:31 +0000

    Add package valiant-valiant-inequality-prover/1.0.2/valiant_valiant_inequality_prover-1.0.2.tar.gz

---
+++ packages/valiant-valiant-inequality-prover/valiant_valiant_inequality_prover-1.0.2.tar.gz/valiant_valiant_inequality_prover-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="valiant_valiant_inequality_prover",
+    author="Richard Riis",
+    author_email="rprobot2@gmail.com",
+    description=("Python implementation of the automatic inequality prover proposed by Valiant & Valiant (https://doi.org/10.1137/151002526)"),
+    url="http://packages.python.org/valiant_valiant_inequality_prover",
+    packages=find_packages(),
+)
