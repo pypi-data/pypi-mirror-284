183412e12791678ce413f9ca46b45d1225daec47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:12 +0000

    Add package stop-iter/0.2.2/stop_iter-0.2.2.tar.gz

---
+++ packages/stop-iter/stop_iter-0.2.2.tar.gz/stop_iter-0.2.2/stop_iter/__init__.py
@@ -0,0 +1,42 @@
+
+
+import signal
+from typing import Iterable, TypeVar
+
+A = TypeVar('A')
+
+def stop_iter(iterable: Iterable[A], /):
+
+  interrupt = False
+  def _interrupt_handler(sig, frame):
+    nonlocal interrupt
+    interrupt = True
+  current_handler = signal.signal(signal.SIGINT, _interrupt_handler)
+
+  try:
+    for elem in iterable:
+      if interrupt:
+          break
+      yield elem
+      if interrupt:
+          break
+
+  finally:
+    signal.signal(signal.SIGINT, current_handler)
+    
+  
+__version__ = "0.2.2"
+
+if __name__ == "__main__":
+  import time
+
+  def slow_integers():
+    count = 0
+    while True:
+      yield (count := count + 1)
+      time.sleep(1)
+
+  for n in stop_iter(slow_integers()):
+    print(n)
+  print("\ninfinity")
+
