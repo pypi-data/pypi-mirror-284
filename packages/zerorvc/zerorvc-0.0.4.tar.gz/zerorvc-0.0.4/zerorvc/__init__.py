fd4204c4e7d41e2968b02271aaeacb748e764c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 07:03:39 +0000

    Add package zerorvc/0.0.4/zerorvc-0.0.4.tar.gz

---
+++ packages/zerorvc/zerorvc-0.0.4.tar.gz/zerorvc-0.0.4/zerorvc/__init__.py
@@ -0,0 +1,7 @@
+from .rvc import RVC
+from .trainer import RVCTrainer
+from .dataset import prepare
+from .synthesizer import SynthesizerTrnMs768NSFsid
+from .pretrained import pretrained_checkpoints
+from .f0 import load_rmvpe, RMVPE, F0Extractor
+from .hubert import load_hubert, HubertModel, HubertFeatureExtractor
