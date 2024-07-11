69be49a57b77da9fee1beb5ba06b3072d3957d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:24 +0000

    Add package crossword-puzzle/2.2.97/crossword_puzzle-2.2.97.tar.gz

---
+++ packages/crossword-puzzle/crossword_puzzle-2.2.97.tar.gz/crossword_puzzle-2.2.97/setup.py
@@ -0,0 +1,51 @@
+from setuptools import find_namespace_packages, setup
+
+
+setup(
+    name="crossword_puzzle",
+    version="2.2.97",  # If I import crossword_puzzle.version.__version__, 
+                       # setuptools fails to build the project for some reason
+    author="Tomas Vana",
+    url="https://github.com/tomasvana10/crossword_puzzle",
+    description="Select, generate and play always-unique crosswords.",
+    long_description=open("RENAMED.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_namespace_packages(exclude=["venv", "venv.*"]),
+    license="MIT",
+    platforms="any",
+    include_package_data=True,
+    install_requires=[
+        "Babel",
+        "customtkinter",
+        "Flask",
+        "flask_babel",
+        "Pillow",
+        "regex",
+        "platformdirs",
+        "pathvalidate",
+        "CTkToolTip",
+        "pywebview",
+    ],
+    entry_points={
+        "gui_scripts": [
+            "crossword-ctk = crossword_puzzle.main:main"
+        ]
+    },
+    classifiers=[
+        "Topic :: Multimedia",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: JavaScript",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Framework :: Flask",
+        "Topic :: Software Development :: Localization",
+        "Topic :: Education",
+        "Topic :: Games/Entertainment"
+    ]
+)
