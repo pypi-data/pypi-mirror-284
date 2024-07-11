8b6dbed05c19cbc74dca7079028b274dd695d100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:24 +0000

    Add package cybsi-sdk/2.13.0a1/cybsi_sdk-2.13.0a1-py3-none-any.whl

---
+++ packages/cybsi-sdk/cybsi_sdk-2.13.0a1-py3-none-any.whl/cybsi/api/search/enums.py
@@ -0,0 +1,10 @@
+from enum_tools.documentation import document_enum
+
+from cybsi.api.enum import CybsiAPIEnum
+
+
+@document_enum
+class QueryCompatibility(CybsiAPIEnum):
+    """Stored query compatibility."""
+
+    Replist = "Replist"  # doc: Replists.
