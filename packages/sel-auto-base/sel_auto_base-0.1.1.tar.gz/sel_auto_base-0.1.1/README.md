203037775617e1c25da69d43ff0166d40f14c6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 15:04:29 +0000

    Add package sel-auto-base/0.1.1/sel_auto_base-0.1.1.tar.gz

---
+++ packages/sel-auto-base/sel_auto_base-0.1.1.tar.gz/sel_auto_base-0.1.1/README.md
@@ -0,0 +1,8 @@
+# selenium_base
+
+A basic Selenium wrapper for Python.
+
+## Installation
+
+```bash
+pip install selenium_base
---
+++ packages/sel-auto-base/sel_auto_base-0.1.1.tar.gz/sel_auto_base-0.1.1/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sel_auto_base',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+    ],
+    author='QAR-LEAF PRIVATE LIMITED',
+    author_email='support@qarleaf.com',
+    description='A basic Selenium wrapper for Python',
+    # url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
