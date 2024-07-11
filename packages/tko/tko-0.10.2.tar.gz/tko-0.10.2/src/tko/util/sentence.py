49aba1652eeed7705d95797013b3d1d3bcde277a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:04:01 +0000

    Add package tko/0.10.2/tko-0.10.2.tar.gz

---
+++ packages/tko/tko-0.10.2.tar.gz/tko-0.10.2/src/tko/util/sentence.py
@@ -0,0 +1,140 @@
+from __future__ import annotations
+from typing import List, Tuple
+
+
+class Token:
+    def __init__(self, fmt: str, text: str):
+        self.fmt = fmt
+        self.text = text
+    
+    def __len__(self):
+        return len(self.text)
+
+class Sentence:
+    def __init__(self):
+        self.data: List[Token] = []
+    
+    def addf(self, fmt: str, text: str):
+        self.data.append(Token(fmt, text))
+        return self
+    
+    def addt(self, text: str):
+        self.data.append(Token("", text))
+        return self
+    
+    def __getitem__(self, index: int):
+        return self.data[index]
+    
+    def __setitem__(self, index: int, value: Token):
+        self.data[index] = value
+        return None
+
+    def adds(self, fmt, sentence):
+        for _, t in sentence.data:
+            self.data.append(Token(fmt, t))
+        return self
+    
+    def concat(self, sentence: Sentence):
+        self.data += sentence.data
+        return self
+    
+    def ljust(self, width: int, fillchar: str = " ", fmt = ""):
+        total = self.len()
+        if total < width:
+            self.addf(fmt, fillchar * (width - total))
+        return self
+    
+    def rjust(self, width: int, fillchar: str = " ", fmt = ""):
+        total = self.len()
+        if total < width:
+            self.data = [Token(fmt, fillchar * (width - total))] + self.data
+        return self
+    
+    def center(self, width: int, fillchar: str = " ", fmt = ""):
+        total = self.len()
+        if total < width:
+            left = (width - total) // 2
+            right = width - total - left
+            self.data = [Token(fmt, fillchar * left)] + self.data + [Token(fmt, fillchar * right)]
+        return self
+    
+    def len(self):
+        total = 0
+        for t in self.data:
+            total += len(t.text)
+        return total
+    
+    def __len__(self):
+        return self.len()
+    
+    def get(self):
+        return self.data
+
+    def trim_alfa(self, limit: int):
+        if limit < 0:
+            return self
+        index = len(self.data) - 1
+        size = self.len()
+        while True:
+            if index < 0 or size <= limit:
+                break
+            token = self.data[index]
+            if len(token.text) == 0:
+                del self.data[index]
+                index -= 1
+            elif token.text[-1].isalpha():
+                token.text = token.text[:-1]
+                size -= 1
+            else:
+                index -= 1
+        return self
+
+    def trim_spaces(self, limit: int):
+        if limit < 0:
+            return self
+        index = len(self.data) - 1
+        size = self.len()
+        while True:
+            if index < 0 or size <= limit:
+                break
+            token = self.data[index]
+            if len(token.text) == 0:
+                del self.data[index]
+                index -= 1
+            elif token.text[-1] == " ":
+                token.text = token.text[:-1]
+                size -= 1
+            else:
+                index -= 1
+            
+        return self
+
+    def trim_end(self, width: int):
+        if width < 0:
+            return self
+        size = self.len()
+        index = len(self.data) - 1
+        while True:
+            if size <= width or index < 0:
+                break
+            token = self.data[index]
+            if len(token.text) == 0:
+                del self.data[index]
+                index -= 1
+            else:
+                token.text = token.text[:-1]
+                size -= 1
+        return self
+
+    @staticmethod
+    def build_bar(text: str, percent: float, length: int, fmt_true: str = "/kC",
+                  fmt_false: str = "/kY") -> Sentence:
+        prefix = (length - len(text)) // 2
+        suffix = length - len(text) - prefix
+        text = " " * prefix + text + " " * suffix
+        xp_bar = Sentence()
+        total = length
+        full_line = text
+        done_len = int(percent * total)
+        xp_bar.addf(fmt_true, full_line[:done_len]).addf(fmt_false, full_line[done_len:])
+        return xp_bar
