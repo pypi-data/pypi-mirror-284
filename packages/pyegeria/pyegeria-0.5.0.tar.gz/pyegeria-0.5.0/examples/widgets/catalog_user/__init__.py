01dcca858dd804662dbf09da940454f1315f40f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:05:13 +0000

    Add package pyegeria/0.5.0/pyegeria-0.5.0.tar.gz

---
+++ packages/pyegeria/pyegeria-0.5.0.tar.gz/pyegeria-0.5.0/examples/widgets/catalog_user/__init__.py
@@ -0,0 +1,10 @@
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright Contributors to the ODPi Egeria project.
+
+catalog user widgets
+"""
+import list_assets
+import view_asset_graph
+import view_glossary
+import view_collection
