58ccbdfc6f15ea922188dc2a42cdd06e86ae319c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:05:31 +0000

    Add package bohrium-open-sdk/0.1.0a18/bohrium_open_sdk-0.1.0a18-py3-none-any.whl

---
+++ packages/bohrium-open-sdk/bohrium_open_sdk-0.1.0a18-py3-none-any.whl/bohrium_open_sdk/view/gradio/__init__.py
@@ -0,0 +1,15 @@
+from gradio_materialviewer import MaterialViewer as material_viewer
+from gradio_mapping import Mapping as mapping
+from gradio_perturbation_graph import perturbation_graph
+from gradio_dp_machine import dp_machine
+from gradio_dp_project import dp_project
+from gradio_uni_view import uni_view
+
+__all__ = [
+    "material_viewer",
+    "mapping",
+    "perturbation_graph",
+    "dp_machine",
+    "dp_project",
+    "uni_view",
+]
