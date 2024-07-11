582b87edd75eb91c1a83e37467523f201a597829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:03:50 +0000

    Add package colorate-console/0.1.1/colorate_console-0.1.1.tar.gz

---
+++ packages/colorate-console/colorate_console-0.1.1.tar.gz/colorate_console-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colorate_console',
+    version='0.1.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    author='Bouquet Cédric',
+    author_email='cedric-bouquet-7@outlook.fr',
+    description="Colorateur de textes dans un terminal",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.0',
+)
