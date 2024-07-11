37b059f8a793d3ae69ddd145e7577e8058eb65d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:04:04 +0000

    Add package singer-sdk/0.39.0a1/singer_sdk-0.39.0a1.tar.gz

---
+++ packages/singer-sdk/singer_sdk-0.39.0a1.tar.gz/singer_sdk-0.39.0a1/singer_sdk/helpers/_util.py
@@ -0,0 +1,29 @@
+"""General helper functions, helper classes, and decorators."""
+
+from __future__ import annotations
+
+import datetime
+import json
+import typing as t
+from pathlib import Path, PurePath
+
+
+def read_json_file(path: PurePath | str) -> dict[str, t.Any]:
+    """Read json file, throwing an error if missing."""
+    if not path:
+        msg = "Could not open file. Filepath not provided."
+        raise RuntimeError(msg)
+
+    if not Path(path).exists():
+        msg = f"File at '{path}' was not found."
+        for template in [f"{path}.template"]:
+            if Path(template).exists():
+                msg += f"\nFor more info, please see the sample template at: {template}"
+        raise FileExistsError(msg)
+
+    return t.cast(dict, json.loads(Path(path).read_text(encoding="utf-8")))
+
+
+def utc_now() -> datetime.datetime:
+    """Return current time in UTC."""
+    return datetime.datetime.now(datetime.timezone.utc)
