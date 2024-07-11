10c0e341f544e5e677fbdfe35f56e18c8b56c2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:03:36 +0000

    Add package pytrilogy/0.0.1.107/pytrilogy-0.0.1.107.tar.gz

---
+++ packages/pytrilogy/pytrilogy-0.0.1.107.tar.gz/pytrilogy-0.0.1.107/trilogy/constants.py
@@ -0,0 +1,31 @@
+from logging import getLogger
+from dataclasses import dataclass
+from enum import Enum
+
+logger = getLogger("preql")
+
+DEFAULT_NAMESPACE = "local"
+
+VIRTUAL_CONCEPT_PREFIX = "_virtual"
+
+ENV_CACHE_NAME = ".preql_cache.json"
+
+
+class MagicConstants(Enum):
+    NULL = "null"
+
+
+NULL_VALUE = MagicConstants.NULL
+
+
+# TODO: support loading from environments
+@dataclass
+class Config:
+    strict_mode: bool = True
+    human_identifiers: bool = True
+    inline_datasources: bool = True
+
+
+CONFIG = Config()
+
+CONFIG.strict_mode = True
