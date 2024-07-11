bcf77d11ed0c70099bb712af5ad8fb4a7e6fed55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 15:04:14 +0000

    Add package sales-intelligence-flow/0.0.3/sales_intelligence_flow-0.0.3.tar.gz

---
+++ packages/sales-intelligence-flow/sales_intelligence_flow-0.0.3.tar.gz/sales_intelligence_flow-0.0.3/tests/test_sales_api_tool.py
@@ -0,0 +1,34 @@
+import pytest
+import unittest
+
+from promptflow.connections import CustomConnection
+from sales.tools.sales_api_tool import get_sales_intelligence_flow_api
+
+
+@pytest.fixture
+def my_custom_connection() -> CustomConnection:
+    my_custom_connection = CustomConnection(
+        {
+            "api-key": "my-api-key",
+            "api-secret": "my-api-secret",
+            "api-url": "my-api-url",
+        }
+    )
+    return my_custom_connection
+
+
+class TestTool:
+    def test_get_sales_intelligence_flow_api(self):
+        result = get_sales_intelligence_flow_api(
+            allowed=True,
+            chat_input={"message": "test", "image": ""},
+            end_point="test",
+            api_key="test",
+            deployment_name="test",
+        )
+        assert result
+
+
+# Run the unit tests
+if __name__ == "__main__":
+    unittest.main()
