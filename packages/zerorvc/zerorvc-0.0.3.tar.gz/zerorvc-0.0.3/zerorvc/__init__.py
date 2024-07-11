f86083ce8412b526a3084afd22af493c8c2f0bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:03:38 +0000

    Add package zerorvc/0.0.3/zerorvc-0.0.3.tar.gz

---
+++ packages/zerorvc/zerorvc-0.0.3.tar.gz/zerorvc-0.0.3/zerorvc/__init__.py
@@ -0,0 +1,5 @@
+from .rvc import RVC
+from .trainer import RVCTrainer
+from .dataset import prepare
+from .synthesizer import SynthesizerTrnMs768NSFsid
+from .pretrained import pretrained_checkpoints
