940cad255ff5bc041d8b546b755f3c3caef257d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 04:04:57 +0000

    Add package bhb/0.0.2024071001/bhb-0.0.2024071001.tar.gz

---
+++ packages/bhb/bhb-0.0.2024071001.tar.gz/bhb-0.0.2024071001/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2024071001'
+DESCRIPTION = 'bhb'
+
+# Setting up
+setup(
+    name="bhb",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
