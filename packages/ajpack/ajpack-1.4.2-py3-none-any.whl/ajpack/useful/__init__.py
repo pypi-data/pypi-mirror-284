3517286b47864cf440996c27a46ca6604fca8618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:01 +0000

    Add package ajpack/1.4.2/ajpack-1.4.2-py3-none-any.whl

---
+++ packages/ajpack/ajpack-1.4.2-py3-none-any.whl/ajpack/useful/__init__.py
@@ -0,0 +1,8 @@
+from .convert import remove_duplicates
+from .wait import waiter
+from .shortcut import create_shortcut
+from .notifications import desktop_msg
+from .format import table
+from .stripping import rma_str
+
+__all__ = ["remove_duplicates", "waiter", "create_shortcut", "desktop_msg", "table", "rma_str"]
