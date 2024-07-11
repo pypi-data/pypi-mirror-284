e202eee486dcf1190e773dee15cadc413bd14852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 14:04:01 +0000

    Add package twJTools/0.1.0.1/twJTools-0.1.0.1.tar.gz

---
+++ packages/twJTools/twJTools-0.1.0.1.tar.gz/twJTools-0.1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="twJTools",
+    version="0.1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "webdriver-manager>=4.0.1",
+        "selenium>=4.22.0",
+        "psycopg2-binary>=2.9.9",
+    ],
+    author="Jasper",
+    author_email="tatsuno46@gmail.com",
+    description="A core utility library by Jasper",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/tatsuno/twJTools",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
