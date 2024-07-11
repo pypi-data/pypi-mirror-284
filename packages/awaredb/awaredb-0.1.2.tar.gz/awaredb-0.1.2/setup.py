a1eeaa62bdc4b1987c9977f46f914890bf5f696a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:04:01 +0000

    Add package awaredb/0.1.2/awaredb-0.1.2.tar.gz

---
+++ packages/awaredb/awaredb-0.1.2.tar.gz/awaredb-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="awaredb",
+    packages=["awaredb"],
+    version="0.1.2",
+    license="MIT",
+    description="AwareDB: data modularity, simplified. Python API.",
+    author="Nelson Monteiro",
+    author_email="nelson@awaredb.io",
+    url="https://github.com/nelsonmonteiro/awaredb-python-api",
+    download_url="https://github.com/nelsonmonteiro/awaredb-python-api/archive/v_0_1_1.tar.gz",
+    keywords=["AwareDB", "modularity"],
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Database :: Front-Ends",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
