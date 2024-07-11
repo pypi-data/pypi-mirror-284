295c0be2558b60e7fec3aa5dacfd29de82212589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 03:04:14 +0000

    Add package wiselib/0.1.5/wiselib-0.1.5.tar.gz

---
+++ packages/wiselib/wiselib-0.1.5.tar.gz/wiselib-0.1.5/wise/utils/rate_limit.py
@@ -0,0 +1,19 @@
+from datetime import timedelta
+
+from wise.utils.cache import cache_get_key
+from wise.utils.redis import get_redis_client
+
+
+def rate_limit_decorator(period: int | timedelta):  # period is in seconds
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            r = get_redis_client()
+            cache_key = "_wise:rate_limit:" + func.__name__ + ":" + cache_get_key(*args, **kwargs)
+            if r.get(cache_key):
+                return
+            result = func(*args, **kwargs)
+            r.set(cache_key, 1, ex=period)
+            return result
+        return wrapper
+    return decorator
+
