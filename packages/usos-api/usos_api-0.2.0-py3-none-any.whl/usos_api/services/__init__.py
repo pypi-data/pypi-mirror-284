c7a21a630337edc7e920c1deacd24194e175e6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:04:09 +0000

    Add package usos-api/0.2.0/usos_api-0.2.0-py3-none-any.whl

---
+++ packages/usos-api/usos_api-0.2.0-py3-none-any.whl/usos_api/services/__init__.py
@@ -0,0 +1,17 @@
+from .api_documentation import APIDocumentationService
+from .api_server import APIServerService
+from .courses import CourseService
+from .grades import GradeService
+from .groups import GroupService
+from .terms import TermService
+from .users import UserService
+
+__all__ = [
+    "APIDocumentationService",
+    "APIServerService",
+    "CourseService",
+    "GradeService",
+    "GroupService",
+    "TermService",
+    "UserService",
+]
