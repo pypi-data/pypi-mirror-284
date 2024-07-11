4f6ae13ed24c51b10f2b756138150bcc94c41a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 06:04:32 +0000

    Add package langchain-openai-api-bridge/0.8.0/langchain_openai_api_bridge-0.8.0-py3-none-any.whl

---
+++ packages/langchain-openai-api-bridge/langchain_openai_api_bridge-0.8.0-py3-none-any.whl/langchain_openai_api_bridge/assistant/adapter/openai_message_factory.py
@@ -0,0 +1,54 @@
+import time
+from typing import Iterable, List, Literal, Optional, Union
+from openai.types.beta.threads import (
+    Message,
+    MessageDelta,
+    MessageContentPartParam,
+    AnnotationDelta,
+    TextDeltaBlock,
+    TextDelta,
+)
+
+from langchain_openai_api_bridge.assistant.adapter.openai_message_content_adapter import (
+    to_openai_message_content_list,
+)
+
+
+def create_message(
+    id: str,
+    thread_id: str,
+    role: Literal["user", "assistant"],
+    content: Union[str, Iterable[MessageContentPartParam], None] = None,
+    status: Literal["in_progress", "incomplete", "completed"] = "completed",
+    run_id: Optional[str] = None,
+    metadata: Optional[object] = {},
+) -> Message:
+
+    return Message(
+        id=id,
+        thread_id=thread_id,
+        role=role,
+        status=status,
+        object="thread.message",
+        created_at=int(time.time()),
+        content=to_openai_message_content_list(content=content),
+        run_id=run_id,
+        metadata=metadata,
+    )
+
+
+def create_text_message_delta(
+    content: str,
+    role: str,
+    annotations: Optional[List[AnnotationDelta]] = [],
+) -> MessageDelta:
+    return MessageDelta(
+        content=[
+            TextDeltaBlock(
+                index=0,
+                type="text",
+                text=TextDelta(value=content, annotations=annotations),
+            )
+        ],
+        role=role,
+    )
