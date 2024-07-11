0ee087743625d6a57439e789a30dcd182592fbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:03:36 +0000

    Add package wivi-graph-client-py/2.1.7/wivi_graph_client_py-2.1.7-py3-none-any.whl

---
+++ packages/wivi-graph-client-py/wivi_graph_client_py-2.1.7-py3-none-any.whl/wivi_graph_client_py/signal.py
@@ -0,0 +1,54 @@
+class Signals_Query:
+    get_signals_query = """
+        query signals($input: SignalFilterInput) {
+            signal(input: $input) {
+                name
+                unit
+                paramType
+                paramId
+                messageName
+                configurationId
+                messageId
+                networkName
+            }
+        }
+    """
+
+    get_signals_data_query = """
+        query signalData($input: SignalFilterInput) {
+            signalData(input: $input) {
+                vehicleId
+                data {
+                    value
+                    signalType
+                    time
+                    signalId
+                    stateId
+                    svalue
+                }
+            }
+        }
+    """
+
+class Signals_Mutation:
+    upsert_signal_data_mutation = """
+        mutation UpsertSignalData($input: UpsertSignalDataInput) {
+            upsertSignalData(input: $input) {
+                configurationId
+                messageId
+                messageName
+                name
+                paramType
+                unit
+            }
+        }
+    """
+
+    delete_signal_data_mutation = """
+        mutation DeleteSignalData($input: DeleteSignalDataInput) {
+            deleteSignalData(input: $input) {
+               configurations
+            }
+        }
+    """
+
