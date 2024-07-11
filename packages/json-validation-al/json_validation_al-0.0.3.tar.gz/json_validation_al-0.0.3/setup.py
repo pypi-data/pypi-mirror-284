7c6f7bc75a3a1753b0d641b6b63e50c6a099d83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:05:36 +0000

    Add package json-validation-al/0.0.3/json_validation_al-0.0.3.tar.gz

---
+++ packages/json-validation-al/json_validation_al-0.0.3.tar.gz/json_validation_al-0.0.3/setup.py
@@ -0,0 +1,55 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="sampleproject",  # Required
+    version="0.0.3",  # Required
+    description=" A wrapper package that simplifies the use of the jsonschema pacakge",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        # Pick your license as you wish
+        "License :: MIT License",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+
+    keywords="json, validate, validation, schema, jsonschema",  # Optional
+    packages=find_packages(where="src"),  # Required
+    include_package_data=True,  # Ensure this is set to include all data files
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match. See
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires=">=3.8, <4",
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/discussions/install-requires-vs-requirements/
+    install_requires=["jsonschema==4.23.0"],
+)
