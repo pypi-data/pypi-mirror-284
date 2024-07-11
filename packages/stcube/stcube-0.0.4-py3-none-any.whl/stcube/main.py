ce68fa1f89ed15953ddad510318f1de2168210c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:05:14 +0000

    Add package stcube/0.0.4/stcube-0.0.4-py3-none-any.whl

---
+++ packages/stcube/stcube-0.0.4-py3-none-any.whl/stcube/main.py
@@ -0,0 +1,29 @@
+from stcube.core import CommandExecutor
+from stcube.modules import Module
+from stcube.library import Library
+from stcube.body import FNew, FOpen, FUpdate
+
+def cmd_entry():
+    ce = CommandExecutor()
+
+    ce.add(Library)
+    ce.add(Module)
+    ce.add(FNew)
+    ce.add(FOpen)
+    ce.add(FUpdate)
+
+    ce()  # start the command executor
+
+
+if __name__ == '__main__':
+    ce = CommandExecutor()
+
+    ce.add(Library)
+    ce.add(Module)
+    ce.add(FNew)
+    ce.add(FOpen)
+    ce.add(FUpdate)
+
+    ce.current = r"C:\Users\22290\Desktop\test_H7"
+
+    ce()  # start the command executor
