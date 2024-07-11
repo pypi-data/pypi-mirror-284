07fdeba4595dc5d8831ee7d21af54fa550498407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 16:06:04 +0000

    Add package py-ce-forms-api/0.1.2/py_ce_forms_api-0.1.2.tar.gz

---
+++ packages/py-ce-forms-api/py_ce_forms_api-0.1.2.tar.gz/py_ce_forms_api-0.1.2/py_ce_forms_api/__init__.py
@@ -0,0 +1,12 @@
+from .api import APIClient
+from .form import Form, FormBlock, FormBlockAssoc
+from .query import FormsQuery, FormMutate, FormsRes, FormsResIterable
+from .accounts import Accounts
+from .client import CeFormsClient
+from .assets import Assets
+from .processing import Processing, Task, TaskPool
+from .processing_client import ProcessingClient
+from .projects import Projects, Project
+from .forms import Forms
+
+__title__ = 'py_ce_forms_api'
