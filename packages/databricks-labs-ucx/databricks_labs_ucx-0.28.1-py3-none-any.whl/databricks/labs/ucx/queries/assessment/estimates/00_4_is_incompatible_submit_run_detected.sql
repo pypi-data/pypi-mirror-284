bca5169ad802cfa400b8b4df44b3ef0172a56ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-11 05:03:51 +0000

    Add package databricks-labs-ucx/0.28.1/databricks_labs_ucx-0.28.1-py3-none-any.whl

---
+++ packages/databricks-labs-ucx/databricks_labs_ucx-0.28.1-py3-none-any.whl/databricks/labs/ucx/queries/assessment/estimates/00_4_is_incompatible_submit_run_detected.sql
@@ -0,0 +1,8 @@
+-- viz type=table, name=Incompatible submit runs detected, columns=object_type,object_id,finding
+-- widget title=Incompatible submit runs, row=0, col=2, size_x=4, size_y=8
+SELECT * FROM
+(SELECT object_type, object_id, EXPLODE(from_json(failures, 'array<string>')) AS finding
+FROM $inventory.objects)
+WHERE finding = "no data security mode specified"
+  AND object_type = "submit_runs"
+
