345d16c6059137f9aef49fe510671fd35e1aad9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:04:07 +0000

    Add package runloop-api-client/0.1.0a5/runloop_api_client-0.1.0a5-py3-none-any.whl

---
+++ packages/runloop-api-client/runloop_api_client-0.1.0a5-py3-none-any.whl/runloop_api_client/types/devbox_view.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from .._models import BaseModel
+
+__all__ = ["DevboxView"]
+
+
+class DevboxView(BaseModel):
+    id: Optional[str] = None
+    """The id of the Devbox."""
+
+    create_time_ms: Optional[int] = None
+    """Creation time of the Devbox (Unix timestamp milliseconds)."""
+
+    name: Optional[str] = None
+    """The name of the Devbox."""
+
+    status: Optional[str] = None
+    """
+    The current status of the Devbox (provisioning, initializing, running, failure,
+    shutdown).
+    """
