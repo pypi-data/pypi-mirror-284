9a680e465efa735573b8cbccdf192dbc02748767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:03:51 +0000

    Add package ob-metaflow-stubs/4.4/ob_metaflow_stubs-4.4-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-4.4-py2.py3-none-any.whl/metaflow-stubs/plugins/resources_decorator.pyi
@@ -0,0 +1,15 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.7.1+ob(v1)                                                    #
+# Generated on 2024-07-11T17:01:16.269825                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class ResourcesDecorator(metaflow.decorators.StepDecorator, metaclass=type):
+    ...
+
