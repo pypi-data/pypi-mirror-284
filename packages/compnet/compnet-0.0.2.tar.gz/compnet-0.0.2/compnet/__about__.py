0bc7dbafac4cf3e7e31a09789f499b3cb856f396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 16:04:20 +0000

    Add package compnet/0.0.2/compnet-0.0.2.tar.gz

---
+++ packages/compnet/compnet-0.0.2.tar.gz/compnet-0.0.2/compnet/__about__.py
@@ -0,0 +1,12 @@
+"""  Created on 26/05/2023::
+------------- __about__.py -------------
+
+**Authors**: L. Mingarelli
+"""
+
+__about__ = "A library for sampling correlated binary variates."
+__version__= '0.0.2'
+__author__ = 'Luca Mingarelli'
+__email__ = "lucamingarelli@me.com"
+__url__ = "https://github.com/LucaMingarelli/compnet"
+
