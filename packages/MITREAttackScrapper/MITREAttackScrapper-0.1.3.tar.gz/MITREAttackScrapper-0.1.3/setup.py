b7b90237938c5411c1604042b725a9ea7f4fde9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:04:48 +0000

    Add package MITREAttackScrapper/0.1.3/MITREAttackScrapper-0.1.3.tar.gz

---
+++ packages/MITREAttackScrapper/MITREAttackScrapper-0.1.3.tar.gz/MITREAttackScrapper-0.1.3/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MITREAttackScrapper",
+    version="0.1.3",
+    author="KnightChaser",
+    author_email="agerio100@naver.com",
+    description="A package for conveniently retrieving MITRE ATT&CK data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'beautifulsoup4',
+        'httpx'
+    ],
+    url="https://github.com/KnightChaser/MITREAttackScrapper",
+    packages=setuptools.find_packages(include=['MITREAttackScrapper', 
+                                               'MITREAttackScrapper.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
