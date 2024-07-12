a6b0dd985f6834d3367d17e53d77c60a6bcfc85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:03:51 +0000

    Add package volstreet/10.1.1/volstreet-10.1.1.tar.gz

---
+++ packages/volstreet/volstreet-10.1.1.tar.gz/volstreet-10.1.1/volstreet/angel_interface/tools.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+def format_quote_response(response: list) -> list:
+    return [
+        {
+            "token": quote["symbolToken"],
+            "ltp": quote["ltp"],
+            "best_bid": quote["depth"]["buy"][0]["price"],
+            "best_bid_qty": quote["depth"]["buy"][0]["quantity"],
+            "best_ask": quote["depth"]["sell"][0]["price"],
+            "best_ask_qty": quote["depth"]["sell"][0]["quantity"],
+            "timestamp": datetime.strptime(quote["exchFeedTime"], "%d-%b-%Y %H:%M:%S"),
+            "last_traded_datetime": datetime.strptime(
+                quote["exchTradeTime"], "%d-%b-%Y %H:%M:%S"
+            ),
+        }
+        for quote in response
+    ]
