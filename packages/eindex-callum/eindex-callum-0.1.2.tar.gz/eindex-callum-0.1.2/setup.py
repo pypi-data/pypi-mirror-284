0add5a609e6f91b86232558f1e5da3b1c8349e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:04:45 +0000

    Add package eindex-callum/0.1.2/eindex-callum-0.1.2.tar.gz

---
+++ packages/eindex-callum/eindex-callum-0.1.2.tar.gz/eindex-callum-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'eindex-callum',
+    version  =  '0.1.2',
+    packages = find_packages(),
+    install_requires = [
+        'torch',
+        'einops'
+    ],
+    author = 'Callum McDougall',
+    author_email = 'cal.s.mcdougall@gmail.com',
+    description = 'My interpretation of einops-like indexing',
+    url = 'https://github.com/callummcdougall/eindex',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
