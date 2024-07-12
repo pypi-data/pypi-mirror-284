961ba2e1845b01e65d66cbe41f094e833ac42eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:08 +0000

    Add package hotsos/1.18.post46/hotsos-1.18.post46-py3-none-any.whl

---
+++ packages/hotsos/hotsos-1.18.post46-py3-none-any.whl/hotsos/plugin_extensions/sosreport/summary.py
@@ -0,0 +1,24 @@
+from hotsos.core.plugins.sosreport import SOSReportChecks
+
+
+class SOSReportSummary(SOSReportChecks):
+    """ Implementation of SOSReport summary. """
+    summary_part_index = 0
+
+    def __0_summary_version(self):
+        if self.version is not None:
+            return self.version
+
+        return None
+
+    def __1_summary_dpkg(self):
+        if self.apt.core:
+            return self.apt.all_formatted
+
+        return None
+
+    def __2_summary_plugin_timeouts(self):
+        if self.timed_out_plugins:
+            return self.timed_out_plugins
+
+        return None
