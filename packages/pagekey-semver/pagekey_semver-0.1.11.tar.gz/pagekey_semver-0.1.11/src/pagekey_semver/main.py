69cabdcae78fa8c1ecbcd3e289c89f1f4d8f31c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:30 +0000

    Add package pagekey-semver/0.1.11/pagekey_semver-0.1.11.tar.gz

---
+++ packages/pagekey-semver/pagekey_semver-0.1.11.tar.gz/pagekey_semver-0.1.11/src/pagekey_semver/main.py
@@ -0,0 +1,28 @@
+import sys
+
+from pagekey_semver.lib import (
+    apply_tag,
+    compute_next_version,
+    compute_release_type,
+    get_biggest_tag,
+    get_commit_messages_since,
+    get_git_tags,
+)
+
+
+def cli_entrypoint(args=sys.argv[1:]):
+    dry_run = "--dry-run" in args
+    tags = get_git_tags()
+    max_tag = get_biggest_tag(tags)
+    commits = get_commit_messages_since(max_tag)
+    release_type = compute_release_type(commits)
+    next_version = compute_next_version(release_type, tags)
+    print("Next version:", next_version)
+    if not dry_run:
+        apply_tag(tags, next_version)
+    else:
+        print("Dry run mode - not applying version.")
+
+
+if __name__ == "__main__":
+    cli_entrypoint()
