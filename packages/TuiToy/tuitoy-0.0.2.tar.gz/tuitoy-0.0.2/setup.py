1141e93cc07f075910efbfafbe7a5a2cd29653d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:06:22 +0000

    Add package TuiToy/0.0.2/tuitoy-0.0.2.tar.gz

---
+++ packages/TuiToy/tuitoy-0.0.2.tar.gz/tuitoy-0.0.2/setup.py
@@ -0,0 +1,32 @@
+"""
+Copyright (C) 2023 Austin Choi
+See end of file for extended copyright information
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TuiToy',
+    version='0.0.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'TuiToy = tuitoy.tuitoy:main',
+            ],
+    },
+    install_requires=[
+        'curses',
+        'windows-curses',
+    ],
+)
+
+"""
+Copyright (C) 2023 Austin Choi
+
+Tuitoy
+
+A library to make pretty Terminal projects by drawing screens, menus, and other components. Uses Curses under the hood
+
+This code is licensed under the MIT License.
+Please see the LICENSE file in the root directory of this project for the full license details.
+"""
