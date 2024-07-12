52bdcae07e89c2ae14c6204d824d8daf3aca3c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:06 +0000

    Add package influxdb3-python/0.7.0/influxdb3_python-0.7.0.tar.gz

---
+++ packages/influxdb3-python/influxdb3_python-0.7.0.tar.gz/influxdb3_python-0.7.0/tests/test_influxdb_client_3.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import patch
+
+from influxdb_client_3 import InfluxDBClient3
+
+
+class TestInfluxDBClient3(unittest.TestCase):
+
+    @patch('influxdb_client_3._InfluxDBClient')
+    @patch('influxdb_client_3._WriteApi')
+    @patch('influxdb_client_3._QueryApi')
+    def setUp(self, mock_query_api, mock_write_api, mock_influx_db_client):
+        self.mock_influx_db_client = mock_influx_db_client
+        self.mock_write_api = mock_write_api
+        self.mock_query_api = mock_query_api
+        self.client = InfluxDBClient3(
+            host="localhost",
+            org="my_org",
+            database="my_db",
+            token="my_token"
+        )
+
+    def test_init(self):
+        self.assertEqual(self.client._org, "my_org")
+        self.assertEqual(self.client._database, "my_db")
+        self.assertEqual(self.client._client, self.mock_influx_db_client.return_value)
+        self.assertEqual(self.client._write_api, self.mock_write_api.return_value)
+        self.assertEqual(self.client._query_api, self.mock_query_api.return_value)
+
+
+if __name__ == '__main__':
+    unittest.main()
