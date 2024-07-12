40c46c8732c57be2a7804339820e9da775380381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 04:06:03 +0000

    Add package v4/0.2.4/v4-0.2.4.tar.gz

---
+++ packages/v4/v4-0.2.4.tar.gz/v4-0.2.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='v4',
+    version='0.2.4',
+    description='python tools for v4 images',
+    license='GPL',
+    packages=['v4'],
+    author='Anthony P. Reeves',
+    author_email='reeves@d4vision.com',
+    classifiers=[
+         'Development Status :: 4 - Beta',
+                ],
+    keywords=['images'],
+    url='https://www.via.cornell.edu/visionx'
+)
