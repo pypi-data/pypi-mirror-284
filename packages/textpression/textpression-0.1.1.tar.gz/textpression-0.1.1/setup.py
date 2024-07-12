49a80f2e68a45c800f6d3ad1438c9f3f3a896b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:30 +0000

    Add package textpression/0.1.1/textpression-0.1.1.tar.gz

---
+++ packages/textpression/textpression-0.1.1.tar.gz/textpression-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="textpression",
+    description="tools for making expressive unicode text messages",
+    url="https://github.com/rwnx/textpression",
+    python_requires=">3.9.0",
+    install_requires=[
+        "typer[all]>=0.9.0",
+    ],
+    entry_points={
+      'console_scripts': [
+          'textpression = textpression.cli:main',
+      ],
+    },
+    author="Rowan Twell",
+    author_email="<rowantwell@gmail.com>",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+)
