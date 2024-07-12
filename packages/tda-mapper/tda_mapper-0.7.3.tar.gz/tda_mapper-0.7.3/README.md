460050dd73d7c40a75f71585721bac074325419d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 07:04:16 +0000

    Add package tda-mapper/0.7.3/tda_mapper-0.7.3.tar.gz

---
+++ packages/tda-mapper/tda_mapper-0.7.3.tar.gz/tda_mapper-0.7.3/README.md
@@ -0,0 +1,122 @@
+![Logo](https://github.com/lucasimi/tda-mapper-python/raw/main/docs/source/logos/tda-mapper-logo-horizontal.png)
+
+[![PyPI version](https://badge.fury.io/py/tda-mapper.svg)](https://badge.fury.io/py/tda-mapper)
+[![downloads](https://img.shields.io/pypi/dm/tda-mapper)](https://pypi.python.org/pypi/tda-mapper/)
+[![test](https://github.com/lucasimi/tda-mapper-python/actions/workflows/test.yml/badge.svg)](https://github.com/lucasimi/tda-mapper-python/actions/workflows/test.yml)
+[![deploy](https://github.com/lucasimi/tda-mapper-python/actions/workflows/deploy.yml/badge.svg)](https://github.com/lucasimi/tda-mapper-python/actions/workflows/deploy.yml)
+[![docs](https://readthedocs.org/projects/tda-mapper/badge/?version=main)](https://tda-mapper.readthedocs.io/en/main/?badge=main)
+[![codecov](https://codecov.io/github/lucasimi/tda-mapper-python/graph/badge.svg?token=FWSD8JUG6R)](https://codecov.io/github/lucasimi/tda-mapper-python)
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.10642381.svg)](https://doi.org/10.5281/zenodo.10642381)
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://tda-mapper-app.streamlit.app/)
+
+# tda-mapper
+
+A simple and efficient Python implementation of Mapper algorithm for
+Topological Data Analysis
+
+* **Installation**: `pip install tda-mapper`
+
+* **Documentation**: https://tda-mapper.readthedocs.io/en/main/
+
+* **Demo App**: https://tda-mapper-app.streamlit.app/
+
+The Mapper algorithm is a well-known technique in the field of topological
+data analysis that allows data to be represented as a graph.
+Mapper is used in various fields such as machine learning, data mining, and
+social sciences, due to its ability to preserve topological features of the
+underlying space, providing a visual representation that facilitates
+exploration and interpretation. For an in-depth coverage of Mapper you can
+read
+[the original paper](https://research.math.osu.edu/tgda/mapperPBG.pdf).
+
+This library contains an implementation of Mapper, where the construction 
+of open covers is based on *vp-trees* for improved performance and scalability.
+The details about this methodology are contained in
+[our preprint](https://doi.org/10.5281/zenodo.10659652).
+
+| Step 1 | Step 2 | Step 3 | Step 4 |
+| ------ | ------ | ------ | ------ |
+| ![Step 1](https://github.com/lucasimi/tda-mapper-python/raw/main/resources/mapper_1.png) | ![Step 2](https://github.com/lucasimi/tda-mapper-python/raw/main/resources/mapper_2.png) | ![Step 3](https://github.com/lucasimi/tda-mapper-python/raw/main/resources/mapper_3.png) | ![Step 2](https://github.com/lucasimi/tda-mapper-python/raw/main/resources/mapper_4.png) |
+| Chose lens | Cover image | Run clustering | Build graph |
+
+## Example
+
+[Here](https://github.com/lucasimi/tda-mapper-python/raw/main/tests/example.py)
+you can find an example to use to kickstart your analysis. In this toy-example
+we use a two-dimensional dataset of two concentric circles. The Mapper graph
+is a topological summary of the whole point cloud.
+
+```python
+import numpy as np
+
+from sklearn.datasets import make_circles
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
+
+from tdamapper.core import MapperAlgorithm
+from tdamapper.cover import CubicalCover
+from tdamapper.plot import MapperLayoutInteractive
+
+X, y = make_circles(                # load a labelled dataset
+    n_samples=5000,
+    noise=0.05,
+    factor=0.3,
+    random_state=42)
+lens = PCA(2).fit_transform(X)
+
+mapper_algo = MapperAlgorithm(
+    cover=CubicalCover(
+        n_intervals=10,
+        overlap_frac=0.3),
+    clustering=DBSCAN())
+mapper_graph = mapper_algo.fit_transform(X, lens)
+
+mapper_plot = MapperLayoutInteractive(
+    mapper_graph,
+    colors=y,                       # color according to categorical values
+    cmap='jet',                     # Jet colormap, for classes
+    agg=np.nanmean,                 # aggregate on nodes according to mean
+    dim=2,
+    iterations=60,
+    seed=42,
+    width=600,
+    height=600)
+
+fig_mean = mapper_plot.plot()
+fig_mean.show(config={'scrollZoom': True})
+
+mapper_plot.update(                 # reuse the plot with the same positions
+    colors=y,
+    cmap='viridis',                 # viridis colormap, for ranges
+    agg=np.nanstd,                  # aggregate on nodes according to std
+)
+
+fig_std = mapper_plot.plot()
+fig_std.show(config={'scrollZoom': True})
+```
+
+| Dataset | Mapper graph (average) | Mapper graph (deviation) |
+| ------- | ---------------------- | ------------------------ |
+| ![Dataset](https://github.com/lucasimi/tda-mapper-python/raw/main/resources/circles_dataset.png) | ![Mapper graph (average)](https://github.com/lucasimi/tda-mapper-python/raw/main/resources/circles_mean.png) | ![Mapper graph (standard deviation)](https://github.com/lucasimi/tda-mapper-python/raw/main/resources/circles_std.png) |
+
+More examples can be found in the
+[documentation](https://tda-mapper.readthedocs.io/en/main/).
+
+### Demo App
+
+You can also run a demo app locally by running
+
+```
+pip install -r app/requirements.txt
+streamlit run app/streamlit_app.py
+```
+
+## Citations
+
+If you want to use **tda-mapper** in your work or research, you can cite the
+[archive uploaded on Zenodo](https://doi.org/10.5281/zenodo.10642381),
+pointing to the specific version of the software used in your work.
+
+If you want to cite the methodology on which **tda-mapper** is based, you can
+use the
+[preprint](https://doi.org/10.5281/zenodo.10659651).
---
+++ packages/tda-mapper/tda_mapper-0.7.3.tar.gz/tda_mapper-0.7.3/src/tdamapper/cover.py
@@ -0,0 +1,239 @@
+"""
+Open cover construction for the Mapper algorithm.
+
+An open cover is a collection of open subsets of a dataset whose union spans the
+whole dataset. Unlike clustering, open subsets do not need to be disjoint.
+Indeed, the overlaps of the open subsets define the edges of the Mapper graph.
+"""
+
+from tdamapper.proximity import (
+    proximity_net,
+    BallProximity,
+    KNNProximity,
+    CubicalProximity,
+    TrivialProximity)
+
+
+class Cover:
+    """
+    Abstract interface for cover algorithms.
+
+    This is a naive implementation. Subclasses should override the methods of
+    this class to implement more meaningful cover algorithms.
+    """
+
+    def apply(self, X):
+        """
+        Covers the dataset with a single open set.
+
+        This is a naive implementation that should be overridden by subclasses
+        to implement more meaningful cover algorithms.
+
+        :param X: A dataset of n points to be covered with open subsets.
+        :type X: array-like of shape (n, m) or list-like of length n
+        :return: A generator that produces a single list of ints whose elements
+            are the indices of the data points, ranging from 0 to n - 1.
+        :rtype: generator of lists of ints
+        """
+        yield list(range(0, len(X)))
+
+
+class ProximityCover(Cover):
+    """
+    Cover algorithm based on proximity-net.
+
+    This class creates an open cover by calling the function
+    :func:`tdamapper.proximity.proximity_net`.
+
+    :param proximity: The proximity function to use.
+    :type proximity: Anything compatible with class
+        :class:`tdamapper.proximity.Proximity`
+    """
+
+    def __init__(self, proximity):
+        self.__proximity = proximity
+
+    def apply(self, X):
+        """
+        Covers the dataset using proximity-net on the specified proximity
+        function.
+
+        The proximity function is used to create an open set whenever a point is
+        picked from :func:`tdamapper.proximity.proximity_net`.
+
+        :param X: A dataset of n points to be covered with open subsets.
+        :type X: array-like of shape (n, m) or list-like of length n
+        :return: A generator yielding lists of ints whose elements are the
+            indices of the data points.
+        :rtype: generator of lists of ints
+        """
+        return proximity_net(X, self.__proximity)
+
+
+class BallCover(ProximityCover):
+    """
+    Cover algorithm based on `ball proximity function` implemented as
+    :class:`tdamapper.proximity.BallProximity`.
+
+    :param radius: The radius of the open balls. Must be a positive value.
+    :type radius: float
+    :param metric: The metric used to define the distance between points.
+        Accepts any value compatible with `tdamapper.utils.metrics.get_metric`.
+        Defaults to 'euclidean'.
+    :type metric: str or callable
+    :param metric_params: Additional parameters for the metric function, to be
+        passed to `tdamapper.utils.metrics.get_metric`. Defaults to None.
+    :type metric_params: dict, optional
+    :param kind: Specifies whether to use a flat or a hierarchical vantage
+        point tree. Acceptable values are 'flat' or 'hierarchical'. Defaults
+        to 'flat'.
+    :type kind: str
+    :param leaf_capacity: The maximum number of points in a leaf node of the
+        vantage point tree. Must be a positive value. Defaults to 1.
+    :type leaf_capacity: int
+    :param leaf_radius: The radius of the leaf nodes. If not specified, it
+        defaults to the value of `radius`. Must be a positive value. Defaults
+        to None.
+    :type leaf_radius: float, optional
+    :param pivoting: The method used for pivoting in the vantage point tree.
+        Acceptable values are None, 'random', or 'furthest'. Defaults to None.
+    :type pivoting: str or callable, optional
+    """
+
+    def __init__(
+        self,
+        radius,
+        metric='euclidean',
+        metric_params=None,
+        kind='flat',
+        leaf_capacity=1,
+        leaf_radius=None,
+        pivoting=None,
+    ):
+        prox = BallProximity(
+            radius=radius,
+            metric=metric,
+            metric_params=metric_params,
+            kind=kind,
+            leaf_capacity=leaf_capacity,
+            leaf_radius=leaf_radius,
+            pivoting=pivoting,
+        )
+        super().__init__(proximity=prox)
+
+
+class KNNCover(ProximityCover):
+    """
+    Cover algorithm based on `knn proximity function` implemented as
+    :class:`tdamapper.proximity.KNNProximity`.
+
+    :param neighbors: The number of neighbors to use for the KNN Proximity
+        function, must be positive and less than the length of the dataset.
+    :type neighbors: int
+    :param metric: The metric used to define the distance between points.
+        Accepts any value compatible with `tdamapper.utils.metrics.get_metric`.
+        Defaults to 'euclidean'.
+    :type metric: str or callable
+    :param metric_params: Additional parameters for the metric function, to be
+        passed to `tdamapper.utils.metrics.get_metric`. Defaults to None.
+    :type metric_params: dict, optional
+    :param kind: Specifies whether to use a flat or a hierarchical vantage
+        point tree. Acceptable values are 'flat' or 'hierarchical'. Defaults
+        to 'flat'.
+    :type kind: str
+    :param leaf_capacity: The maximum number of points in a leaf node of the
+        vantage point tree. If not specified, it defaults to the value of
+        `neighbors`. Must be a positive value. Defaults to None.
+    :type leaf_capacity: int, optional
+    :param leaf_radius: The radius of the leaf nodes. Must be a positive value.
+        Defaults to 0.0.
+    :type leaf_radius: float
+    :param pivoting: The method used for pivoting in the vantage point tree.
+        Acceptable values are None, 'random', or 'furthest'. Defaults to None.
+    :type pivoting: str or callable, optional
+    """
+
+    def __init__(
+        self,
+        neighbors,
+        metric='euclidean',
+        metric_params=None,
+        kind='flat',
+        leaf_capacity=None,
+        leaf_radius=0.0,
+        pivoting=None,
+    ):
+        prox = KNNProximity(
+            neighbors=neighbors,
+            metric=metric,
+            metric_params=metric_params,
+            kind=kind,
+            leaf_capacity=leaf_capacity,
+            leaf_radius=leaf_radius,
+            pivoting=pivoting,
+        )
+        super().__init__(proximity=prox)
+
+
+class CubicalCover(ProximityCover):
+    """
+    Cover algorithm based on the `cubical proximity function` implemented as
+    :class:`tdamapper.proximity.CubicalProximity`.
+
+    :param n_intervals: The number of intervals to use for each dimension.
+        Must be positive and less than or equal to the length of the dataset.
+    :type n_intervals: int
+    :param overlap_frac: The fraction of overlap between adjacent intervals on
+        each dimension, must be in the range (0.0, 1.0).
+    :type overlap_frac: float
+    :param metric: The metric used to define the distance between points.
+        Accepts any value compatible with `tdamapper.utils.metrics.get_metric`.
+        Defaults to 'euclidean'.
+    :type metric: str or callable
+    :param metric_params: Additional parameters for the metric function, to be
+        passed to `tdamapper.utils.metrics.get_metric`. Defaults to None.
+    :type metric_params: dict, optional
+    :param kind: Specifies whether to use a flat or a hierarchical vantage
+        point tree. Acceptable values are 'flat' or 'hierarchical'. Defaults
+        to 'flat'.
+    :type kind: str
+    :param leaf_capacity: The maximum number of points in a leaf node of the
+        vantage point tree. Must be a positive value. Defaults to 1.
+    :type leaf_capacity: int
+    :param leaf_radius: The radius of the leaf nodes. If not specified, it
+        defaults to the value of `radius`. Must be a positive value. Defaults
+        to None.
+    :type leaf_radius: float, optional
+    :param pivoting: The method used for pivoting in the vantage point tree.
+        Acceptable values are None, 'random', or 'furthest'. Defaults to None.
+    :type pivoting: str or callable, optional
+    """
+
+    def __init__(
+        self,
+        n_intervals,
+        overlap_frac,
+        kind='flat',
+        leaf_capacity=1,
+        leaf_radius=None,
+        pivoting=None,
+    ):
+        prox = CubicalProximity(
+            n_intervals=n_intervals, 
+            overlap_frac=overlap_frac,
+            kind=kind,
+            leaf_capacity=leaf_capacity,
+            leaf_radius=leaf_radius,
+            pivoting=pivoting,
+        )
+        super().__init__(proximity=prox)
+
+
+class TrivialCover(ProximityCover):
+    """
+    Cover algorithm based on the `trivial proximity function` implemented as
+    :class:`tdamapper.proximity.TrivialProximity`.
+    """
+
+    def __init__(self):
+        super().__init__(proximity=TrivialProximity())
