74c284273dd06b983d81efdd6b90ea93d46548c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:06:19 +0000

    Add package wame-engine/0.0.1.dev2/wame_engine-0.0.1.dev2-py3-none-any.whl

---
+++ packages/wame-engine/wame_engine-0.0.1.dev2-py3-none-any.whl/wame/__init__.py
@@ -0,0 +1,16 @@
+'''
+### Pygame Wrapper Designed to Make Games Easier to Create and Manage
+Created with the people in mind by WilDev Studios
+
+#### Website: https://wame.wildevstudios.net
+#### Documentation: https://github.com/WilDev-Studios/Wame
+'''
+
+from pygame.font import Font, SysFont
+
+from wame.engine import Engine
+from wame.vector import IntVector2, IntVector3
+from wame.button import Button
+from wame.scene import Scene
+from wame.area import Area
+from wame.text import Text
