9703cead0cadbe7f40ad8f1e0450c441277be75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:05:12 +0000

    Add package TuiToy/0.0.3/tuitoy-0.0.3.tar.gz

---
+++ packages/TuiToy/tuitoy-0.0.3.tar.gz/tuitoy-0.0.3/tuitoy/__init__.py
@@ -0,0 +1,22 @@
+"""
+Copyright (C) 2023 Austin "Choisauce" Choi
+See end of file for extended copyright information
+"""
+
+__all__ = ["app", "scenes", "windows", "tuitoy"]
+
+from .app import *
+from .scenes import *
+from .windows import *
+from .tuitoy import *
+
+"""
+Copyright (C) 2023 Austin "Choisauce" Choi
+
+Tuitoy
+
+A library to make pretty Terminal projects by drawing screens, menus, and other components. Uses Curses under the hood
+
+This code is licensed under the MIT License.
+Please see the LICENSE file in the root directory of this project for the full license details.
+"""
