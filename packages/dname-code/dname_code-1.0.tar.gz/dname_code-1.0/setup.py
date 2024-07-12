ef6d413cf2595dbfb2bbb2f3c7bf3bed003be9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:03 +0000

    Add package dname-code/1.0/dname_code-1.0.tar.gz

---
+++ packages/dname-code/dname_code-1.0.tar.gz/dname_code-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dname_code',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+    # author='Daris',
+    # author_email='hei.raymond.2@gmail.com',
+    # description='A simple Python package',
+    # url='https://github.com/dnamecode/simple-python-package',
+)
