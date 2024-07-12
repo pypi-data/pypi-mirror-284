d72eac0700d69331c89cba5107457e5abc849a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:10 +0000

    Add package itshire/0.1.10/itshire-0.1.10-py3-none-any.whl

---
+++ packages/itshire/itshire-0.1.10-py3-none-any.whl/itshire/cli.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Add sections to markdown files.")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Enable verbose output"
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    addstores_parser = subparsers.add_parser(
+        "addstores", help="Add stores as sections to markdown files from stdin"
+    )
+    addstores_parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Enable verbose output"
+    )
+
+    args = parser.parse_args()
+    return args
