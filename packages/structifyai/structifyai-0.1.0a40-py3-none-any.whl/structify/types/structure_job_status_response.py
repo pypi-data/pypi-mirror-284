5b1c882b546a4e52c7d6d8e4e8c2c49278aaf65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:04:59 +0000

    Add package structifyai/0.1.0a40/structifyai-0.1.0a40-py3-none-any.whl

---
+++ packages/structifyai/structifyai-0.1.0a40-py3-none-any.whl/structify/types/structure_job_status_response.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+__all__ = ["StructureJobStatusResponse", "StructureJobStatusResponseItem"]
+
+
+class StructureJobStatusResponseItem:
+    pass
+
+
+StructureJobStatusResponse = List[StructureJobStatusResponseItem]
