bac05c7615e1306d659ad94ff1ff8f5f45c2f306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 04:04:07 +0000

    Add package kleenscan/0.5.0/kleenscan-0.5.0-py3-none-any.whl

---
+++ packages/kleenscan/kleenscan-0.5.0-py3-none-any.whl/kleenscan/lib/errors.py
@@ -0,0 +1,57 @@
+# Custom library imports:
+from .config import *
+
+
+
+class KsInvalidTokenError(Exception):
+    def __init__(self):
+        super().__init__('Invalid API token. After creating an account, generate a new one at https://kleenscan.com/profile.')
+
+
+
+class KsApiError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
+
+
+
+class KsFileTooLargeError(Exception):
+    def __init__(self):
+        super().__init__(f'The provided file is too large for the kleenscan API (Max: {MAX_FILE_MB} MB).')
+
+
+
+class KsFileEmptyError(Exception):
+    def __init__(self):
+        super().__init__(f'The provided file is empty, provide a file with data.')
+
+
+
+class KsRemoteFileTooLargeError(Exception):
+    def __init__(self):
+        super().__init__(f'The remote file is too large for the kleenscan API (Max: {MAX_FILE_MB} MB).')
+
+
+
+class KsGetFileInfoFailedError(Exception):
+    def __init__(self, message: str):
+        super().__init__(f'Failed to get file info, HTTP status code: {message}')
+
+
+
+class KsNoFileHostedError(Exception):
+    def __init__(self):
+        super().__init__(f'No file hosted on provided URL/server. Please provide a URL/server which hosts a file, e.g. https://malicious.com/file.exe')
+
+
+
+class KsFileDownloadFailedError(Exception):
+    def __init__(self, message: str):
+        super().__init__(f'Failed to download file, HTTP status code: {message}')
+
+
+
+class KsDeadLinkError(Exception):
+    def __init__(self, message: str):
+        super().__init__(f'The URL/server hosting the file cannot be conneceted to: {message}')
---
+++ packages/kleenscan/kleenscan-0.5.0-py3-none-any.whl/kleenscan/lib/http.py
@@ -0,0 +1,98 @@
+import requests
+import json
+from typing import *
+
+
+# Custom Library imports:
+from .files import read_file
+from .errors import *
+
+class Ks_http:
+	def __init__(self, x_auth_token: str):
+		self.headers = {'X-Auth-Token': x_auth_token}
+
+
+
+	@staticmethod
+	def __handle_api_errors(response_text: str) -> None:
+		if '"httpResponseCode":200' not in response_text:
+			raise KsApiError(response_text)
+
+
+
+	def post_scan(self, url: str, data: dict, files: Optional[str]=None) -> dict:
+		with requests.post(url,
+			headers=self.headers,
+			files=files,
+			data=data
+		) as response:
+			self.__handle_api_errors(response.text)
+			return json.loads(response.text)
+
+
+
+	def get_req(self, url: str) -> str:
+		with requests.get(url,
+			headers=self.headers,
+		) as response:
+			self.__handle_api_errors(response.text)
+			return response.text
+
+
+
+	def get_req_json(self, url: str) -> dict:
+		with requests.get(url,
+			headers=self.headers,
+		) as response:
+			self.__handle_api_errors(response.text)
+			return json.loads(response.text)
+
+
+
+	def get_req_json_noerr(self, url: str) -> dict:
+		with requests.get(url,
+			headers=self.headers,
+		) as response:
+			return json.loads(response.text)
+
+
+
+	@staticmethod
+	def download_file_memory(url: str) -> bytes:
+		try:
+			# Send a HEAD request to get the file size.
+			with requests.head(url) as response:
+				# Check if the request was successful.
+				if response.status_code != 200:
+					raise KsGetFileInfoFailedError(response.status_code)
+
+				# Get the content type from the headers
+				content_type = response.headers.get('Content-Type', '')
+
+	    		# Check if the content type is HTML/text.
+				if 'text/html' in content_type:
+					raise KsNoFileHostedError
+	    
+				# Get the file size in MB from the Content-Length header.
+				file_size_mb = int(response.headers.get('Content-Length', 0)) / 1024 / 1024
+
+				# Check if the file size in MB exceeds the KS max size in MB.
+				if file_size_mb >= MAX_FILE_MB:
+					raise KsRemoteFileTooLargeError
+
+				# Send a GET request to download the file.
+				with requests.get(url, stream=True) as response:
+
+					# Check if the request was successful.
+					if response.status_code != 200:
+						raise KsFileDownloadFailedError(response.status_code)
+
+					# Return file content.
+					return response.content
+
+			return b''
+		except (requests.ConnectionError, requests.Timeout, requests.RequestException) as e:
+			raise KsDeadLinkError(f'{e} caused by {type(e)}')
+
+
+
