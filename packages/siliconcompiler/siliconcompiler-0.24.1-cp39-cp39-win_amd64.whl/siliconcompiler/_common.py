80a55863e32e0a627cd115eb7aeb80eaaad0a50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 07:04:07 +0000

    Add package siliconcompiler/0.24.1/siliconcompiler-0.24.1-cp39-cp39-win_amd64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.24.1-cp39-cp39-win_amd64.whl/siliconcompiler/_common.py
@@ -0,0 +1,21 @@
+class NodeStatus():
+    # Could use Python 'enum' class here, but that doesn't work nicely with
+    # schema.
+    PENDING = 'pending'
+    SUCCESS = 'success'
+    ERROR = 'error'
+
+
+###############################################################################
+# Package Customization classes
+###############################################################################
+class SiliconCompilerError(Exception):
+    ''' Minimal Exception wrapper used to raise sc runtime errors.
+    '''
+    def __init__(self, message, chip=None):
+        super(Exception, self).__init__(message)
+
+        if chip:
+            logger = getattr(chip, 'logger', None)
+            if logger:
+                logger.error(message)
