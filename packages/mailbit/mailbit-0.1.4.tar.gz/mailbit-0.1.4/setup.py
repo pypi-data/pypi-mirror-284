03603d6430f0dd83c8bcec75286761ab4483a831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:45 +0000

    Add package mailbit/0.1.4/mailbit-0.1.4.tar.gz

---
+++ packages/mailbit/mailbit-0.1.4.tar.gz/mailbit-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mailbit",
+    version="0.1.4",
+    description="A simple library to send emails using the Mailbit API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Marcos",
+    author_email="marcos.santos.filho.fl@gmail.com",
+    url="https://github.com/marcossantosfl/mailbit-library-python",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+)
