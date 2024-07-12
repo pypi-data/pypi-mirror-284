799673d513d51683b093e139f3fd22fec9d92574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:34 +0000

    Add package cmsplugin-blocks/1.3.2/cmsplugin_blocks-1.3.2.tar.gz

---
+++ packages/cmsplugin-blocks/cmsplugin_blocks-1.3.2.tar.gz/cmsplugin_blocks-1.3.2/cmsplugin_blocks/forms/__init__.py
@@ -0,0 +1,19 @@
+from .album import AlbumForm, AlbumItemForm
+from .card import CardForm
+from .container import ContainerForm
+from .feature import FeatureForm, FeatureImportForm
+from .hero import HeroForm
+from .slider import SliderForm, SlideItemForm
+
+
+__all__ = [
+    "AlbumForm",
+    "AlbumItemForm",
+    "CardForm",
+    "ContainerForm",
+    "FeatureForm",
+    "FeatureImportForm",
+    "HeroForm",
+    "SliderForm",
+    "SlideItemForm",
+]
---
+++ packages/cmsplugin-blocks/cmsplugin_blocks-1.3.2.tar.gz/cmsplugin_blocks-1.3.2/cmsplugin_blocks/models/__init__.py
@@ -0,0 +1,18 @@
+from .album import Album, AlbumItem
+from .card import Card
+from .container import Container
+from .feature import Feature
+from .hero import Hero
+from .slider import Slider, SlideItem
+
+
+__all__ = [
+    "Album",
+    "AlbumItem",
+    "Card",
+    "Container",
+    "Feature",
+    "Hero",
+    "Slider",
+    "SlideItem",
+]
