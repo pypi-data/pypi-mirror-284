36b74a4cd9a40c5951bbc849bdd3f5475c747040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:05:46 +0000

    Add package wiederverwendbar/0.2.21/wiederverwendbar-0.2.21-py3-none-any.whl

---
+++ packages/wiederverwendbar/wiederverwendbar-0.2.21-py3-none-any.whl/wiederverwendbar/starlette_admin/mongoengine/__init__.py
@@ -0,0 +1,11 @@
+from wiederverwendbar.starlette_admin.mongoengine.admin import Admin
+
+from wiederverwendbar.starlette_admin.mongoengine.view import ModelView as FixedModelView
+
+from wiederverwendbar.starlette_admin.mongoengine.fixed_collection_field import Converter as CollectionConverter
+from wiederverwendbar.starlette_admin.mongoengine.fixed_collection_field import CollectionField
+
+from wiederverwendbar.starlette_admin.mongoengine.generic_embedded_document_field import Converter as GenericEmbeddedConverter
+from wiederverwendbar.starlette_admin.mongoengine.generic_embedded_document_field import GenericEmbeddedDocumentField
+
+from wiederverwendbar.starlette_admin.mongoengine.ipv4_field.converter import Converter as IPv4Converter
