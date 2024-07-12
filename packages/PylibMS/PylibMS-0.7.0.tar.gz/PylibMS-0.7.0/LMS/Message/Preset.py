d1e4f2513b5fe3004734a40a97dea53b878dab53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 16:04:18 +0000

    Add package PylibMS/0.7.0/PylibMS-0.7.0.tar.gz

---
+++ packages/PylibMS/PylibMS-0.7.0.tar.gz/PylibMS-0.7.0/LMS/Message/Preset.py
@@ -0,0 +1,163 @@
+from LMS.Project.MSBP import MSBP
+from LMS.Common.LMS_Enum import LMS_BinaryTypes
+from LMS.Stream.Reader import Reader
+from LMS.Stream.Writer import Writer
+
+from LMS.Message.Preset_Constants import RESTRICTED_GLOBALS, ALLOWED_ATTRIBUTES, SYSTEM_STRUCTURE
+
+from lupa.lua54 import LuaRuntime
+
+import importlib.resources
+import json  
+
+def filter_attribute_access(obj, attr_name, is_setting):  
+     if isinstance(attr_name, str):
+        if attr_name not in ALLOWED_ATTRIBUTES:
+            raise AttributeError('You cannot access this attribute.')
+        return attr_name
+
+class Preset:
+    """A static class for loading and storing a preset."""
+    def __init__(self, global_list: list[str] = None):
+        self.lua_runtime = LuaRuntime(attribute_filter=filter_attribute_access)
+        self.stream_functions: dict[str:function] = {}
+        self.structure: list[dict] = [SYSTEM_STRUCTURE]
+
+        with importlib.resources.open_text("LMS.Message", "System.lua") as file:
+            self.base_preset = dict(self.lua_runtime.execute(file.read()))
+            self.stream_functions = self.base_preset
+        
+        globals = self.lua_runtime.globals()
+
+        if global_list is not None:
+            for name in global_list:
+                globals[name] = None 
+        else:
+            for name in RESTRICTED_GLOBALS:
+                globals[name] = None
+       
+    @staticmethod
+    def msbp_structure_to_dict(msbp: MSBP):
+        # Skip System
+        structure = msbp.get_tag_structure()[1:]
+
+        result = []
+        for group in structure:
+            group_dict =  {"name": group.name, "tags": []}
+            
+            for i, tag in enumerate(group.tags):
+                group_dict["tags"].append({"name": tag.name, "parameters": []})
+
+                for parameter in tag.parameters:
+                    group_dict["tags"][i]["parameters"].append({
+                        "name": parameter.name,
+                        "type": parameter.type.value,
+                        "list_items": parameter.list_items
+                    })
+
+            result.append(group_dict)
+
+        return result
+             
+    def load_preset_file(self, preset_path: str, structure_path: str) -> None:
+        self.structure = [SYSTEM_STRUCTURE]
+        self.stream_functions = self.base_preset
+
+        with open(preset_path, "r") as file:
+            self.stream_functions.update(dict(self.lua_runtime.execute(file.read())))
+
+        with open(structure_path, "r") as file:
+            self.structure += json.load(file)
+
+    @staticmethod
+    def create_preset_file(preset_path: str, json_path: str, msbp: MSBP) -> None:
+        structure = msbp.get_tag_structure()
+
+        function_template = """
+local function {name}()
+{list_items}
+    local function read(data, reader)
+{read_body}    end
+    local function write(data, writer)
+{write_body}    end 
+    return {{read, write}}
+end
+"""         
+        # Lua lacks a .index function for tables, so define a table.index method for use with list items
+        # Define it without function_template as its a one liner 
+        index_function = "local function index(table, value) for i, v in ipairs(table) do if v == value then return i - 1 end end end\n"
+        
+        # Tags can use special characters like paranthesis which lua dislikes
+        # Instead, set the functions in a dictionary so that function names can be edited
+        # and still indexed properly with the raw name 
+        stream_functions = {}
+
+        comment_message = """
+--[[ 
+   This is a preset used for parsing tags. 
+   DO NOT edit any variable which are list items (tables that appear at the top of a tag function). It will lead to errors.
+   Make sure you peserve parameter names when editing a function.
+--]]
+"""
+        with open(f"{preset_path}", "a+") as file:
+            file.write(comment_message)
+            file.truncate()
+
+            file.write("local stream_functions = {}\n\n")
+            file.write(index_function)
+            for group in structure[1:]:
+                for tag in group.tags:
+                    read_body = ""
+                    write_body = ""
+                    list_body = ""
+
+                    name = f"{group.name.lower()}_{tag.name.lower()}"
+                    stream_functions[name] = name 
+                    for parameter in tag.parameters:
+                        list_items = parameter.list_items
+                        if len(list_items):
+                            list_body += f"\tlocal {parameter.name}_items = {set(list_items)}\n"
+
+                        match parameter.type:
+                            case LMS_BinaryTypes.FLOAT:
+                                read_body += f"\t\t\tdata['{parameter.name}'] = reader.read_float()\n"
+                                write_body += f"\t\t\twriter.write_float(tonumber(data['{parameter.name}']))\n"
+                            case LMS_BinaryTypes.LIST_INDEX:
+                                read_body += f"\t\t\tdata['{parameter.name}'] = {parameter.name}_items[reader.read_uint8({{lua_index=true}})]\n"
+                                write_body += f"\t\twriter.write_uint8(index({parameter.name}_items, data['{parameter.name}']))\n"
+                            case LMS_BinaryTypes.STRING:
+                                read_body += f"\t\t\tdata['{parameter.name}'] = reader.read_len_prefixed_utf16_string()\n"
+                                write_body += f"\t\t\twriter.write_len_prefixed_utf16_string(data['{parameter.name}'])\n"
+                            # UInt8, UInt16, UInt32 handling here
+                            case _:
+                                bit_value = LMS_BinaryTypes._get_bits(parameter.type)
+                                read_body += f"\t\t\tdata['{parameter.name}'] = reader.read_uint{bit_value}()\n"
+                                write_body += f"\t\t\twriter.write_uint{bit_value}(tonumber(data['{parameter.name}']))\n"
+
+                    file.write(function_template.format(name=name, list_items=list_body, read_body=read_body, write_body=write_body))
+            
+            file.write("\nstream_functions = {\n")
+            
+            for name in stream_functions:
+                file.write(f"\t['{name}'] = {name},\n")
+            file.write("}\n\n")
+            
+            file.write("return stream_functions")
+            
+            with open(json_path, "w+") as file:
+                json.dump(Preset.msbp_structure_to_dict(msbp), file, indent=2)
+
+      
+
+
+
+
+            
+                
+
+
+
+           
+        
+
+    
---
+++ packages/PylibMS/PylibMS-0.7.0.tar.gz/PylibMS-0.7.0/LMS/Message/TXT2.py
@@ -0,0 +1,94 @@
+from LMS.Common.LMS_Block import LMS_Block
+from LMS.Stream.Reader import Reader
+from LMS.Stream.Writer import Writer
+from LMS.Project.MSBP import MSBP
+
+from LMS.Message.Tags import Tag_Utility
+
+
+class TXT2:
+    """A class that represents a TXT2 block in a MSBT file.
+
+    https://github.com/kinnay/Nintendo-File-Formats/wiki/MSBT-File-Format#txt2-block"""
+
+    def __init__(self, msbt=None):
+        from LMS.Message.MSBT import MSBT
+
+        self.msbt: MSBT = None
+
+        self.block: LMS_Block = LMS_Block()
+        self.messages: list[str] = []
+
+    def read(self, reader: Reader, preset) -> None:
+        """Reads the TXT2 block from a stream.
+
+        :param `reader`: A Reader object.
+        :param `msbp`: A MSBP object used for decoding attributes and tags
+        """
+        self.block.read_header(reader)
+        message_count = reader.read_uint32()
+
+        encoding = reader.get_utf16_encoding()
+        tag_indicator = b"\x0E\x00" if reader.byte_order == "little" else b"\x00\x0E"
+
+        offsets = self.block.get_item_offsets(reader, message_count)
+
+        for i, offset in enumerate(offsets):
+            if i < len(offsets) - 1:
+                next_offset = offsets[i + 1]
+            else:
+                next_offset = self.block.data_start + self.block.size
+
+            reader.seek(offset)
+
+            message = b""
+            while reader.tell() < next_offset:
+                bytes = reader.read_bytes(2)
+                message += Tag_Utility.read_tag(reader, preset).encode(encoding) if bytes == tag_indicator else bytes 
+            
+            self.messages.append(message.decode(encoding))
+
+        self.block.seek_to_end(reader)
+
+    def write(self, writer: Writer, preset, msbp: MSBP = None) -> None:
+        """Writes the TXT2 block to a stream.
+
+        :param `writer`: A Writer object.
+        :param `msbp`: A MSBP object used for writing decoded attributes and tags.
+        """
+        self.block.magic = "TXT2"
+        self.block.size = 0
+        self.block.write_header(writer)
+        self.block.data_start = writer.tell()
+        message_count = len(self.messages)
+        writer.write_uint32(message_count)
+        message_offset = message_count * 4 + 4
+
+        tag_indicator = b"\x0E\x00" if writer.byte_order == "little" else b"\x00\x0E"
+
+        encoded_messages = []
+        for message in self.messages:
+            split_message = Tag_Utility.split_message_by_tag(message)
+            # Use a writer object to simplfy the encoding process
+            message_writer = Writer(b"", writer.byte_order)
+
+            for part in split_message:
+                if Tag_Utility.is_tag(part):
+                    message_writer.write_bytes(tag_indicator)
+                    Tag_Utility.write_tag(message_writer, part, preset)
+                else:
+                    message_writer.write_utf16_string(part)
+
+            encoded_messages.append(message_writer.get_data())
+
+        size = message_count * 4 + 4
+        for message in encoded_messages:
+            writer.write_uint32(message_offset)
+            message_offset += len(message)
+            size += len(message)
+
+        for message in encoded_messages:
+            writer.write_bytes(message)
+
+        self.block.size = size
+        self.block.write_end_data(writer)
