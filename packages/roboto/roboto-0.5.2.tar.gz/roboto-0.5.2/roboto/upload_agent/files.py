31f3c1ec45d0b45a492c3a03d694311b35ab68fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 04:06:01 +0000

    Add package roboto/0.5.2/roboto-0.5.2.tar.gz

---
+++ packages/roboto/roboto-0.5.2.tar.gz/roboto-0.5.2/roboto/upload_agent/files.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2024 Roboto Technologies, Inc.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import pathlib
+import typing
+
+import pydantic
+
+from ..domain.datasets import CreateDatasetRequest
+
+
+class UploadConfigFileDatasetSection(CreateDatasetRequest):
+    org_id: typing.Optional[str] = None
+
+
+class UploadConfigFileUploadSection(pydantic.BaseModel):
+    exclude_patterns: typing.Optional[typing.List[str]] = None
+
+
+class UploadConfigFile(pydantic.BaseModel):
+    version: typing.Literal["v1"] = "v1"
+    dataset: UploadConfigFileDatasetSection = pydantic.Field(
+        default_factory=UploadConfigFileDatasetSection
+    )
+    upload: UploadConfigFileUploadSection = pydantic.Field(
+        default_factory=UploadConfigFileUploadSection
+    )
+
+
+class UploadAgentConfig(pydantic.BaseModel):
+    version: typing.Literal["v1"] = "v1"
+    search_paths: list[pathlib.Path]
+    upload_config_filename: str = ".roboto_upload.json"
