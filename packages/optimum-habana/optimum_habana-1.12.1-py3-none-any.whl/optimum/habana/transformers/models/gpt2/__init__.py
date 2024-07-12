7084864313fdb950c23527898ba255e1abe8c125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 04:04:01 +0000

    Add package optimum-habana/1.12.1/optimum_habana-1.12.1-py3-none-any.whl

---
+++ packages/optimum-habana/optimum_habana-1.12.1-py3-none-any.whl/optimum/habana/transformers/models/gpt2/__init__.py
@@ -0,0 +1,1 @@
+from .modeling_gpt2 import GaudiGPT2Attention, GaudiGPT2Block, GaudiGPT2LMHeadModel, gaudi_gpt2_forward
---
+++ packages/optimum-habana/optimum_habana-1.12.1-py3-none-any.whl/optimum/habana/transformers/models/gptj/__init__.py
@@ -0,0 +1,6 @@
+from .modeling_gptj import (
+    GaudiGPTJAttention,
+    GaudiGPTJBlock,
+    GaudiGPTJForCausalLM,
+    gaudi_gptj_model_forward,
+)
