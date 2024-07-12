d56bfda05f83bd0e6f026275fcdfc0ac4c37f397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 04:04:04 +0000

    Add package ndonnx/0.6.0/ndonnx-0.6.0-py3-none-any.whl

---
+++ packages/ndonnx/ndonnx-0.6.0-py3-none-any.whl/ndonnx/_core/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) QuantCo 2023-2024
+# SPDX-License-Identifier: BSD-3-Clause
+
+from ._impl import CoreOperationsImpl
+from ._interface import OperationsBlock
+
+__all__ = ["CoreOperationsImpl", "OperationsBlock"]
