69358b6426526492cab0ede10d8cec6ad2f96d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:05:21 +0000

    Add package osm-fieldwork/0.13.0/osm-fieldwork-0.13.0.tar.gz

---
+++ packages/osm-fieldwork/osm-fieldwork-0.13.0.tar.gz/osm-fieldwork-0.13.0/osm_fieldwork/xlsforms/__init__.py
@@ -0,0 +1,10 @@
+"""Index of XLSForm file paths."""
+
+import os
+
+xlsforms_path = os.path.dirname(os.path.abspath(__file__))
+
+entities_registration = f"{xlsforms_path}/entities_registration.xls"
+entities_registration_xml = f"{xlsforms_path}/entities_registration.xml"
+
+buildings = f"{xlsforms_path}/buildings.xls"
