a66ca3fa0f80ffe9f747379d02d9f9f2ea9700fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:22 +0000

    Add package couchbase-sqlalchemy/0.1.5/couchbase_sqlalchemy-0.1.5-py3-none-any.whl

---
+++ packages/couchbase-sqlalchemy/couchbase_sqlalchemy-0.1.5-py3-none-any.whl/src/dialect/couchbase_dialect.py
@@ -0,0 +1,227 @@
+from sqlalchemy.engine.default import DefaultDialect
+from sqlalchemy.sql.compiler import IdentifierPreparer
+from sqlalchemy.engine.default import DefaultDialect
+from sqlalchemy.sql.compiler import SQLCompiler
+from sqlalchemy.types import (
+    BIGINT,
+    BOOLEAN,
+    DATE,
+    DATETIME,
+    FLOAT,
+    TIME,
+    VARCHAR,
+)
+from sqlalchemy.engine.default import DefaultDialect
+from sqlalchemy.engine.url import make_url
+import re
+from urllib.parse import urlparse, parse_qs, urlencode
+from sqlalchemy.sql.compiler import SQLCompiler
+from sqlalchemy import select
+from urllib.parse import unquote
+
+class CouchbaseIdentifierPreparer(IdentifierPreparer):
+    def __init__(self, dialect, **kw):
+        quote = '`'
+        super().__init__(dialect, initial_quote=quote, escape_quote=quote)
+
+
+    def _quote_free_identifiers(self, *ids):
+        """
+        Unilaterally identifier-quote any number of strings.
+        """
+        print("schema in quote_free_schema is ")
+        return tuple(self.quote(i) for i in ids if i is not None)
+
+    def quote_schema(self, schema, force=None):
+        """
+        Split schema by a dot and merge with required quotes
+        """
+        idents = self._split_schema_by_dot(schema)
+        return ".".join(self._quote_free_identifiers(*idents))
+
+    def _split_schema_by_dot(self, schema):
+        ret = []
+        idx = 0
+        pre_idx = 0
+        in_quote = False
+        while idx < len(schema):
+            if not in_quote:
+                if schema[idx] == "." and pre_idx < idx:
+                    ret.append(schema[pre_idx:idx])
+                    pre_idx = idx + 1
+                elif schema[idx] == '"':
+                    in_quote = True
+                    pre_idx = idx + 1
+            else:
+                if schema[idx] == '"' and pre_idx < idx:
+                    ret.append(schema[pre_idx:idx])
+                    in_quote = False
+                    pre_idx = idx + 1
+            idx += 1
+            if pre_idx < len(schema) and schema[pre_idx] == ".":
+                pre_idx += 1
+        if pre_idx < idx:
+            ret.append(schema[pre_idx:idx])
+        print("returns : ",ret)
+        return ret
+
+class CouchbaseDialect(DefaultDialect):
+    name = "couchbasedb"
+    driver = "couchbasedb"
+    preparer = CouchbaseIdentifierPreparer
+    supports_alter = False
+    max_identifier_length = 255
+    default_paramstyle = 'pyformat'
+    paramstyle = 'pyformat'
+    supports_native_enum = False
+    supports_native_boolean = True
+    supports_statement_cache = True
+
+    @classmethod
+    def dbapi(cls):
+        from src.dbapi import couchbase_dbapi
+        return couchbase_dbapi
+
+    def create_connect_args(self, url):
+        # Parse the URL
+        parsed_url = urlparse(str(url))
+        username = unquote(parsed_url.username)
+        password = unquote(parsed_url.password)
+        host = unquote(parsed_url.hostname)
+        port = parsed_url.port
+        query_params = parse_qs(parsed_url.query, keep_blank_values=True)
+        # Determine the protocol based on the SSL parameter
+        ssl_enabled = query_params.get('ssl', ['true'])[0].lower()
+        protocol = 'couchbases' if ssl_enabled=='true' else 'couchbase'
+        # Remove the SSL parameter from the query parameters 
+        query_params.pop('ssl', None)
+        query_string = urlencode(query_params, doseq=True)
+        # Construct the new connection string
+        connection_string = f"{protocol}://{host}"
+        if port:
+            connection_string += f":{port}"
+        if query_string:
+            connection_string += f"?{query_string}"
+        print("connection strin is ",connection_string)
+        return ([connection_string, username, password], {})
+
+    
+
+    def has_table(self, connection, table_name, schema=None):
+        """
+        Checks if the table exists
+        """
+        return self._has_object(connection,"TABLE",table_name,schema)
+    
+    def _get_table_columns(self, connection, table_name, schema=None):
+        return self.get_columns(self, connection, table_name, schema)
+        
+    def _has_object(self, connection, object_type, object_name, schema=None):
+        try:
+            results = connection.execute(
+                """ 
+                SELECT d.DatasetName
+                FROM Metadata.`Dataset` d
+                Where d.DatasetName= "{object_name}}" 
+                """
+            )
+            row = results.fetchone()
+            have = row is not None
+            return have
+        except Exception as e:
+            raise
+
+    def get_schema_names(self, connection, **kw):
+        query = """
+            SELECT d.DatabaseName, d.DataverseName
+            FROM Metadata.`Dataverse` d
+            """
+        result = connection.execute(query)
+        schemas = []
+        for r in result:
+            schemas.append((r['DatabaseName'][1:]+'.'+ r['DataverseName'][1:]))
+        return schemas
+
+    def get_table_names(self, connection, schema=None, **kw):
+        # we define views inplace of tables.
+        return []
+        
+    def get_view_names(self, connection, schema=None, **kw):        
+        database_name, dataverse_name = schema.split('.')
+        print("Database Name ",database_name," ",dataverse_name)
+        query = """
+                SELECT ds.DatasetName
+                FROM Metadata.`Dataset` ds
+                JOIN Metadata.`Datatype` dt
+                ON ds.DatatypeDataverseName = dt.DataverseName
+                AND ds.DatasetType='VIEW'
+                AND ds.DatatypeName = dt.DatatypeName
+                AND array_length(dt.Derived.Record.Fields) > 0
+                AND ds.DataverseName = '{dataverse_name}'
+                AND ds.DatabaseName = '{database_name}';
+                """.format(database_name=database_name, dataverse_name=dataverse_name)
+        result = connection.execute(query)
+        views = [row['DatasetName'][1:] for row in result]
+        return views
+    
+    
+    def get_indexes(self, connection, table_name, schema=None, **kw):
+        return []
+
+    def get_pk_constraint(self, connection, table_name, schema=None, **kw):
+        query = f"""
+                SELECT RAW d.ViewDetails.PrimaryKey
+                FROM Metadata.`Dataset` d 
+                WHERE d.DatasetType = 'VIEW'
+                """
+        result = connection.execute(query)
+        keys = result.fetchall()
+        Primary_Keys= [item for sublist in keys for inner_list in sublist for item in inner_list]
+        pk_info = {
+            'constrained_columns': [],
+            'name': None
+        }
+        pk_info['constrained_columns'] = Primary_Keys
+        return pk_info
+    
+    def get_columns(self, connection, table_name, schema=None, **kw):
+        query = f"""
+                SELECT d.Derived.Record.Fields 
+                FROM Metadata.`Datatype` d 
+                WHERE d.DatatypeName = '$d$t$i${table_name}';
+                """
+        
+        result = connection.execute(query)
+        columns_data = result.fetchall()
+    # Assuming 'columns_data' returns a list of dictionaries under the key 'Fields'
+        fields = columns_data[0]['Fields']
+    
+    # Map to translate JSON types to SQLAlchemy types
+        type_map = {
+        "string": VARCHAR,
+        "int64": BIGINT,
+        "datetime": DATETIME,
+        "boolean": BOOLEAN,
+        "double": FLOAT,
+        "date":DATE,
+        "time":TIME
+        }
+
+    # Constructing the list of dictionaries as per SQLAlchemy requirements
+        columns = []
+        for field in fields:
+            column_info = {
+            'name': f"{field['FieldName'][1:]}",
+            'type': type_map[field['FieldType'][1:]](),
+            'nullable': field['IsNullable'],
+            'default': None,
+            'autoincrement': field.get('IsAutoincrement', False)
+            }
+            columns.append(column_info)
+        
+        return columns
+
+
+    def get_foreign_keys(self, connection, table_name, schema=None, **kw):
+        return []
+    
