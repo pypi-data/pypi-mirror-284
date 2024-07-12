892c645f496dd2d6e7b5c9a5004ab292951659c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 04:05:14 +0000

    Add package lensepy/0.1.7/lensepy-0.1.7.tar.gz

---
+++ packages/lensepy/lensepy-0.1.7.tar.gz/lensepy-0.1.7/src/lensepy/css.py
@@ -0,0 +1,22 @@
+
+#### Style Sheet for LEnsE API
+
+# Colors
+# ------
+BLUE_IOGS = '#0A3250'
+ORANGE_IOGS = '#FF960A'
+WHITE = '#000000'
+GRAY = '#727272'
+BLACK = '#FFFFFF'
+
+# Styles
+# ------
+styleH1 = f"font-size:20px; padding:7px; color:{BLUE_IOGS};font-weight: bold;"
+styleH2 = f"font-size:15px; padding:7px; color:{BLUE_IOGS}; font-weight: bold;"
+styleH3 = f"font-size:15px; padding:7px; color:{BLUE_IOGS};"
+styleCheckbox = f"font-size: 12px; padding: 7px; color: {BLUE_IOGS}; font-weight: normal;"
+no_style = f"background-color:{GRAY}; color:{BLACK}; font-size:15px;"
+
+disabled_button = f"background-color:{GRAY}; color:{BLACK}; font-size:15px; border-radius: 10px;"
+unactived_button = f"background-color:{BLUE_IOGS}; color:white; font-size:15px; font-weight:bold; border-radius: 10px;"
+actived_button = f"background-color:{ORANGE_IOGS}; color:white; font-size:15px; font-weight:bold; border-radius: 10px;"
