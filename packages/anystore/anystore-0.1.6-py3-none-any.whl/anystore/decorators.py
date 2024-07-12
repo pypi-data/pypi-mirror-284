edc4aaabfd5c208d7a923f4b9e5ca33b8adb8011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:14 +0000

    Add package anystore/0.1.6/anystore-0.1.6-py3-none-any.whl

---
+++ packages/anystore/anystore-0.1.6-py3-none-any.whl/anystore/decorators.py
@@ -0,0 +1,31 @@
+from typing import Callable
+from anystore.exceptions import DoesNotExist
+from anystore.store import get_store
+from anystore.util import make_signature_key
+
+
+def anycache(**store_kwargs):
+    key_func: Callable = store_kwargs.pop("key_func", make_signature_key)
+    serialize_func: Callable = store_kwargs.pop("serialize_func", None)
+    store = store_kwargs.pop("store", get_store(**store_kwargs))
+    store = store.model_copy()
+    store.raise_on_nonexist = True
+
+    def _decorator(func):
+        def _inner(*args, **kwargs):
+            key = key_func(*args, **kwargs)
+            try:
+                if key is not None:
+                    return store.get(key)
+                raise DoesNotExist
+            except DoesNotExist:
+                res = func(*args, **kwargs)
+                if serialize_func is not None:
+                    res = serialize_func(res)
+                if key is not None:
+                    store.put(key, res)
+                return res
+
+        return _inner
+
+    return _decorator
