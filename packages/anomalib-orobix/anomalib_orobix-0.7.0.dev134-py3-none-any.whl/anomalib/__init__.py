445542e62d4b350f784174ab46d6a1c5ee8de415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:40 +0000

    Add package anomalib-orobix/0.7.0.dev134/anomalib_orobix-0.7.0.dev134-py3-none-any.whl

---
+++ packages/anomalib-orobix/anomalib_orobix-0.7.0.dev134-py3-none-any.whl/anomalib/__init__.py
@@ -0,0 +1,9 @@
+"""Anomalib library for research and benchmarking."""
+
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+anomalib_version = "0.7.0"
+custom_orobix_version = "1.3.4"
+
+__version__ = f"{anomalib_version}+obx.{custom_orobix_version}"
