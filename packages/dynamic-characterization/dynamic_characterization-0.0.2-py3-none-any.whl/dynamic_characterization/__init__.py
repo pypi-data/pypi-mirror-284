a680a056fd8276808870697a1a894059f7a2d990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:29 +0000

    Add package dynamic-characterization/0.0.2/dynamic_characterization-0.0.2-py3-none-any.whl

---
+++ packages/dynamic-characterization/dynamic_characterization-0.0.2-py3-none-any.whl/dynamic_characterization/__init__.py
@@ -0,0 +1,15 @@
+"""
+Collection of dynamic characterization functions for life cycle inventories with temporal information.
+"""
+
+__all__ = (
+    "__version__",
+    "temporalis",
+    "timex",
+    # Add functions and variables you want exposed in `dynamic_characterization.` namespace here
+)
+
+__version__ = "0.0.2"
+
+from . import temporalis
+from . import timex
