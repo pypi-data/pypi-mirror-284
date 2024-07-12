951823aabb08c77113fc6de936a34c4fb54c9035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:03:49 +0000

    Add package pysmelt/0.5.0/pysmelt-0.5.0.tar.gz

---
+++ packages/pysmelt/pysmelt-0.5.0.tar.gz/pysmelt-0.5.0/crates/smelt-graph/src/executor/common.rs
@@ -0,0 +1,146 @@
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
+use crate::Command;
+
+use dice::DiceData;
+
+use smelt_data::{
+    executed_tests::{
+        artifact_pointer::Pointer, ArtifactPointer, ExecutedTestResult, TestOutputs, TestResult,
+    },
+    Event,
+};
+
+use smelt_events::runtime_support::GetSmeltRoot;
+use tokio::{fs::File, io::AsyncWriteExt, sync::mpsc::Sender};
+
+pub(crate) struct Workspace {
+    pub(crate) script_file: PathBuf,
+    pub(crate) stdout: File,
+}
+
+/// Creates all of the directory scaffolding expected by a command
+///
+/// This function is currently used across all executors, and is always executed in the host
+/// filesystem
+pub(crate) async fn prepare_workspace(
+    command: &Command,
+    smelt_root: PathBuf,
+    command_working_dir: &Path,
+) -> anyhow::Result<Workspace> {
+    // TODO -- maybe parameterize?
+    let smeltoutdir = "smelt-out";
+
+    let working_dir = command.default_target_root(smelt_root.as_path())?;
+    let script_file = working_dir.join(Command::script_file());
+    let stdout_file = working_dir.join(Command::stdout_file());
+    tokio::fs::create_dir_all(&working_dir).await?;
+    let mut file = File::create(&script_file).await?;
+
+    let stdout = File::create(&stdout_file).await?;
+
+    let mut buf: Vec<u8> = Vec::new();
+
+    writeln!(buf, "export SMELT_ROOT={}", smelt_root.to_string_lossy())?;
+
+    writeln!(
+        buf,
+        "export TARGET_ROOT={}/{}/{}",
+        smelt_root.to_string_lossy(),
+        smeltoutdir,
+        command.name
+    )?;
+
+    writeln!(buf, "cd {}", command_working_dir.to_string_lossy())?;
+
+    for script_line in &command.script {
+        writeln!(buf, "{}", script_line)?;
+    }
+
+    file.write_all(&buf).await?;
+    file.flush().await?;
+    Ok(Workspace {
+        script_file,
+        stdout,
+    })
+}
+
+pub(crate) async fn handle_line(
+    command: &Command,
+    line: String,
+    trace_id: String,
+    tx_chan: &Sender<Event>,
+    stdout: &mut File,
+    avoid_message: bool,
+) {
+    if !avoid_message {
+        let _handleme = tx_chan
+            .send(Event::command_stdout(
+                command.name.clone(),
+                trace_id.clone(),
+                line.clone(),
+            ))
+            .await;
+    }
+    let bytes = line.as_str();
+    let _unhandled = stdout.write(bytes.as_bytes()).await;
+    let _unhandled = stdout.write(&[b'\n']).await;
+}
+
+pub(crate) fn create_test_result(
+    command: &Command,
+    exit_code: i32,
+    global_data: &DiceData,
+) -> ExecutedTestResult {
+    let command_default_dir = command.working_dir.clone();
+    let smelt_root = global_data.get_smelt_root();
+    let mut missing_artifacts = vec![];
+    let mut artifacts = vec![ArtifactPointer {
+        artifact_name: "smelt_log".into(),
+        //TODO: smelt-out shouldn't be hardcoded here, sorry for sinning mom
+        pointer: Some(Pointer::Path(format!(
+            "{}/smelt-out/{}/command.out",
+            smelt_root.to_string_lossy(),
+            command.name,
+        ))),
+    }];
+
+    for output in command.outputs.iter() {
+        let path = output.to_path(command_default_dir.as_path(), smelt_root.as_path());
+        let path_exists = path.exists();
+        let default_name = path
+            .file_name()
+            .expect("Filename missing from an artifact")
+            .to_string_lossy()
+            .to_string();
+        let artifact = ArtifactPointer::file_artifact(default_name, path);
+        if !path_exists {
+            tracing::debug!(
+                "Missing artifact {:?} for command {}",
+                artifact,
+                command.name
+            );
+            missing_artifacts.push(artifact)
+        } else {
+            artifacts.push(artifact);
+        }
+    }
+
+    let test_result = TestResult {
+        test_name: command.name.clone(),
+        outputs: Some(TestOutputs {
+            artifacts,
+            exit_code,
+        }),
+    };
+
+    if missing_artifacts.is_empty() {
+        ExecutedTestResult::Success(test_result)
+    } else {
+        ExecutedTestResult::MissingFiles {
+            test_result,
+            missing_artifacts,
+        }
+    }
+}
---
+++ packages/pysmelt/pysmelt-0.5.0.tar.gz/pysmelt-0.5.0/crates/smelt-graph/src/executor/profiler.rs
@@ -0,0 +1,111 @@
+use std::time::Duration;
+
+use libproc::{self, pid_rusage::PIDRUsage, processes};
+use smelt_data::{command_event::CommandVariant, CommandProfile, Event};
+use tokio::{sync::mpsc::Sender, time::Instant};
+const MILIS_TO_NANOS: u64 = 1_000;
+#[derive(Debug)]
+struct SampleStruct {
+    /// Memory used by a command in bytes
+    ///
+    /// currently calculated by summing the memory used by the command process and all of its
+    /// children
+    memory_used: u64,
+
+    /// cpu time used, in nanoseconds
+    cpu_time_delta: u64,
+}
+
+#[cfg(target_os = "linux")]
+fn get_rusage_and_add(pid: i32, timeused: &mut u64, memused: &mut u64) {
+    use libproc::pid_rusage::RUsageInfoV0;
+    if let Some(val) = libproc::pid_rusage::pidrusage::<RUsageInfoV0>(pid as i32).ok() {
+        *memused += val.memory_used();
+        *timeused += (val.ri_system_time + val.ri_user_time);
+    }
+}
+
+#[cfg(target_os = "macos")]
+fn get_rusage_and_add(pid: i32, timeused: &mut u64, memused: &mut u64) {
+    use libproc::pid_rusage::RUsageInfoV3;
+    use mach2::mach_time::mach_timebase_info;
+
+    let mut timebase = mach_timebase_info::default();
+
+    unsafe { mach_timebase_info(&mut timebase) };
+    if let Ok(val) = libproc::pid_rusage::pidrusage::<RUsageInfoV3>(pid) {
+        *memused += val.memory_used();
+        *timeused += ((val.ri_system_time + val.ri_user_time) * timebase.numer as u64)
+            / timebase.denom as u64;
+    }
+}
+
+fn sample_memory_and_load(ppid: u32) -> Option<SampleStruct> {
+    let filter = libproc::processes::ProcFilter::ByParentProcess { ppid };
+    let mut timeused = 0;
+    let mut memused = 0;
+
+    if let Ok(pids) = processes::pids_by_type(filter) {
+        for pid in pids {
+            get_rusage_and_add(pid as i32, &mut timeused, &mut memused);
+        }
+    }
+
+    get_rusage_and_add(ppid as i32, &mut timeused, &mut memused);
+
+    Some(SampleStruct {
+        memory_used: memused,
+        cpu_time_delta: timeused,
+    })
+}
+
+pub async fn profile_cmd(
+    pid: u32,
+    tx: Sender<Event>,
+    sample_freq_ms: u64,
+    command_ref: String,
+    trace_id: String,
+) {
+    let mut prev_sample = None;
+    let mut prev_sample_time = Instant::now();
+
+    loop {
+        let new_sample = sample_memory_and_load(pid);
+        let new_sample_time = Instant::now();
+        if let Some(ref sample) = new_sample {
+            if let Some(ref _prev) = prev_sample {
+                let time_since = (new_sample_time - prev_sample_time).as_micros() as u64;
+                let _ = tx
+                    .send(profile_event(
+                        &trace_id,
+                        &command_ref,
+                        &sample,
+                        &_prev,
+                        time_since,
+                    ))
+                    .await;
+            }
+
+            prev_sample = new_sample;
+        }
+        prev_sample_time = new_sample_time;
+
+        tokio::time::sleep(Duration::from_millis(sample_freq_ms)).await;
+    }
+}
+
+fn profile_event(
+    trace_id: &String,
+    command_ref: &String,
+    sample: &SampleStruct,
+    prev: &SampleStruct,
+    sample_freq_ms: u64,
+) -> Event {
+    let variant = CommandVariant::Profile(CommandProfile {
+        memory_used: sample.memory_used,
+        cpu_load: ((sample.cpu_time_delta.saturating_sub(prev.cpu_time_delta)) as f32
+            / MILIS_TO_NANOS as f32) as f32
+            / sample_freq_ms as f32,
+    });
+    Event::from_command_variant(command_ref.clone(), trace_id.clone(), variant)
+}
