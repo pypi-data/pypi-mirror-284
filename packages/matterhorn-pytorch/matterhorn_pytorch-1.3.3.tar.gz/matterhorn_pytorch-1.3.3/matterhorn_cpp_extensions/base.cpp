1c58c00784fe697d8c627fae944190d21aa41116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:49 +0000

    Add package matterhorn-pytorch/1.3.3/matterhorn_pytorch-1.3.3.tar.gz

---
+++ packages/matterhorn-pytorch/matterhorn_pytorch-1.3.3.tar.gz/matterhorn_pytorch-1.3.3/matterhorn_cpp_extensions/base.cpp
@@ -0,0 +1,14 @@
+#ifndef _MATTERHORN_BASE
+#define _MATTERHORN_BASE
+
+
+#include <ATen/ATen.h>
+#include <ATen/Functions.h>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+#endif
