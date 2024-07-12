9ee34452f871888f7f9fc9335cc174a84fc65bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:36 +0000

    Add package formant/1.176.10/formant-1.176.10.tar.gz

---
+++ packages/formant/formant-1.176.10.tar.gz/formant-1.176.10/formant/sdk/cloud/v2/src/resources/views/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import PartialView, Location, LocationViewConfigurationBasemap, ViewLayoutType, TransformTreeViewConfiguration, LocationViewport, ImageViewConfiguration, PartialViewLayoutType, Camera, BitsetViewConfiguration, ViewListResponse, View, Vector3, ImageViewConfigurationMode, LocalizationViewConfiguration, ViewConfiguration, ViewConfigurationType, Filter, LocationViewConfiguration, ViewTags, PointCloudViewConfiguration, NumericViewConfiguration, PartialViewTags
+from formant.sdk.cloud.v2.formant_admin_api_client.api.view import view_controller_get_one, view_controller_get_all, view_controller_patch
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
