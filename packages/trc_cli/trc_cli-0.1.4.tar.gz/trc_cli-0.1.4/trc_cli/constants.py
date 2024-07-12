58b7d824a7b66ab9fc5783285859c7ca4a4ad13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 07:04:11 +0000

    Add package trc_cli/0.1.4/trc_cli-0.1.4.tar.gz

---
+++ packages/trc_cli/trc_cli-0.1.4.tar.gz/trc_cli-0.1.4/trc_cli/constants.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+current_file_path = Path(__file__).resolve()
+base_dir = current_file_path.parent
+
+FEATURE_FLAGS = (
+    "integration.technologyDiscovery",
+    "technologydiscovery.factsheetsbomlink",
+    "technologydiscovery.libraryinventory",
+)
+WORKSPACE_READINESS_FIELD = 'lxIsSBOMAttached'
+
+MICROSERVICES_PATH = base_dir / 'static' / 'microservices.json'
+SBOMS_PATH = base_dir / 'static' / 'sboms'
+TEMP_STORAGE = base_dir / 'static' / 'factsheetids.json'
