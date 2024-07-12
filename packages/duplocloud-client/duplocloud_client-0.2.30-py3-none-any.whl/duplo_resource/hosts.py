adb6e3fe5fea846d01e0ffd34a5ad7902150997e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:28 +0000

    Add package duplocloud-client/0.2.30/duplocloud_client-0.2.30-py3-none-any.whl

---
+++ packages/duplocloud-client/duplocloud_client-0.2.30-py3-none-any.whl/duplo_resource/hosts.py
@@ -0,0 +1,136 @@
+from duplocloud.client import DuploClient
+from duplocloud.resource import DuploTenantResourceV2
+from duplocloud.errors import DuploError, DuploFailedResource
+from duplocloud.commander import Command, Resource
+import duplocloud.args as args
+
+@Resource("hosts")
+class DuploHosts(DuploTenantResourceV2):
+  
+  def __init__(self, duplo: DuploClient):
+    super().__init__(duplo)
+    self.paths = {
+      "list": "GetNativeHosts"
+    }
+  
+  @Command()
+  def create(self,
+             body: args.BODY,
+             wait: args.WAIT=False):
+    """Create a host."""
+    def wait_check():
+      name = self.name_from_body(body)
+      h = self.find(name)
+      if h["Status"] != "running":
+        if h["Status"] != "pending":
+          raise DuploFailedResource(f"Host '{name}' failed to create.")
+        raise DuploError(f"Host '{name}' not ready", 404)
+    # let's get started
+    if body.get("ImageId", None) is None:
+      body["ImageId"] = self.discover_image(body.get("AgentPlatform", 0))
+    res = self.duplo.post(self.endpoint("CreateNativeHost"), body)
+    if wait:
+      self.wait(wait_check)
+    return {
+      "message": f"Successfully created host '{body['FriendlyName']}'",
+      "id": res.json()
+    }
+  
+  @Command()
+  def delete(self,
+             name: args.NAME,
+             wait: args.WAIT=False):
+    """Delete a host."""
+    host = self.find(name)
+    inst_id = host["InstanceId"]
+    res = self.duplo.post(self.endpoint(f"TerminateNativeHost/{inst_id}"), host)
+    def wait_check():
+      h = None 
+      try:
+        h = self.find(name)
+      except DuploError as e:
+        if e.code == 404:
+          return None # if 404 then it's gone so finish waiting
+        else:
+          raise DuploFailedResource(f"Host '{name}' failed to delete.")
+      if h["Status"] == "shutting-down" or h["Status"] == "running":
+        raise DuploError(f"Host '{name}' not terminated", 404)
+    if wait:
+      self.wait(wait_check, 500)
+    return {
+      "message": f"Successfully deleted host '{name}'",
+      "data": res.json()
+    }
+  
+  @Command()
+  def stop(self,
+             name: args.NAME,
+             wait: args.WAIT=False):
+    """Stop a host."""
+    host = self.find(name)
+    inst_id = host["InstanceId"]
+    res = self.duplo.post(self.endpoint(f"stopNativeHost/{inst_id}"), host)
+    def wait_check():
+      h = self.find(name)
+      if h["Status"] == "running":
+        raise DuploError(f"Host '{name}' not ready", 404)
+      if h["Status"] != "stopped":
+        if h["Status"] != "stopping":
+          raise DuploFailedResource(f"Host '{name}' failed to stop.")
+        raise DuploError(f"Host '{name}' not ready", 404)
+    if wait:
+      self.wait(wait_check, 500)
+    return {
+      "message": f"Successfully stopped host '{name}'",
+      "data": res.json()
+    }
+  
+  @Command()
+  def start(self,
+             name: args.NAME,
+             wait: args.WAIT=False):
+    """Start a host."""
+    host = self.find(name)
+    inst_id = host["InstanceId"]
+    res = self.duplo.post(self.endpoint(f"startNativeHost/{inst_id}"), host)
+    def wait_check():
+      h = self.find(name)
+      if h["Status"] == "stopped":
+        raise DuploError(f"Host '{name}' not ready", 404)
+      if h["Status"] != "running":
+        if h["Status"] != "pending":
+          raise DuploFailedResource(f"Host '{name}' failed to stop.")
+        raise DuploError(f"Host '{name}' not ready", 404)
+    if wait:
+      self.wait(wait_check, 500)
+    return {
+      "message": f"Successfully started host '{name}'",
+      "data": res.json()
+    }
+  
+  @Command()
+  def reboot(self,
+             name: args.NAME):
+    """Reboot a host."""
+    host = self.find(name)
+    inst_id = host["InstanceId"]
+    res = self.duplo.post(self.endpoint(f"RebootNativeHost/{inst_id}"), host)
+    return {
+      "message": f"Successfully rebooted host '{name}'",
+      "data": res.json()
+    }
+    
+  def name_from_body(self, body):
+    prefix = f"duploservices-{self.tenant['AccountName']}"
+    name =  body["FriendlyName"]
+    if not name.startswith(prefix):
+      name = f"{prefix}-{name}"
+    return name
+  
+  def discover_image(self, agent, arch="amd64"):
+    imgs = self.tenant_svc.host_images(self.tenant['AccountName'])
+    try:
+      img = [i for i in imgs if i["Agent"] == agent and i["Arch"] == arch][0]
+      return img.get("ImageId")
+    except IndexError:
+      raise DuploError(f"Image for agent '{agent}' not found", 404)
---
+++ packages/duplocloud-client/duplocloud_client-0.2.30-py3-none-any.whl/duplo_resource/lambda.py
@@ -0,0 +1,111 @@
+from duplocloud.client import DuploClient
+from duplocloud.resource import DuploTenantResourceV2
+from duplocloud.errors import DuploError
+from duplocloud.commander import Command, Resource
+import duplocloud.args as args
+
+@Resource("lambda")
+class DuploLambda(DuploTenantResourceV2):
+  
+  def __init__(self, duplo: DuploClient):
+    super().__init__(duplo)
+  
+  @Command()
+  def list(self):
+    """Retrieve a list of all lambdas in a tenant."""
+    tenant_id = self.tenant["TenantId"]
+    tenant_name = self.tenant["AccountName"]
+    response = self.duplo.get(f"subscriptions/{tenant_id}/GetLambdaFunctions")
+    if (data := response.json()):
+      return data
+    else:
+      raise DuploError(f"No lambda functions found in tenant '{tenant_name}'", 404)
+  
+  @Command()
+  def find(self, 
+           name: args.NAME):
+    """Find a Lambda function by name.
+    
+    Args:
+      name (str): The name of the lambda to find.
+    Returns: 
+      The lambda object.
+    Raises:
+      DuploError: If the lambda could not be found.
+    """
+    try:
+      return [s for s in self.list() if s["FunctionName"] == name][0]
+    except IndexError:
+      raise DuploError(f"Lambda '{name}' not found", 404)
+    
+  @Command()
+  def create(self, 
+             body: args.BODY,
+             wait: args.WAIT = False):
+    """Create a new tenant."""
+    def wait_check():
+      name = self.name_from_body(body)
+      self.find(name)
+    tenant_id = self.tenant["TenantId"]
+    self.duplo.post(f"subscriptions/{tenant_id}/CreateLambdaFunction", body)
+    if wait:
+      self.wait(wait_check, 400)
+    return {
+      "message": f"Lambda {body['FunctionName']} created"
+    }
+  
+  @Command()
+  def delete(self, 
+             name: args.NAME):
+    """Delete a lambda."""
+    tenant_id = self.tenant["TenantId"]
+    self.duplo.post(f"subscriptions/{tenant_id}/DeleteLambdaFunction/{name}")
+    return {
+      "message": f"Lambda {name} deleted"
+    }
+
+  @Command()
+  def update_image(self, 
+                   name: args.NAME, 
+                   image: args.IMAGE):
+    """Update the image of a lambda.
+    
+    Args:
+      name (str): The name of the lambda to update.
+      image (str): The new image to use for the lambda.
+    """
+    tenant_id = self.tenant["TenantId"]
+    data = {
+      "FunctionName": name,
+      "ImageUri": image
+    }
+    response = self.duplo.post(f"subscriptions/{tenant_id}/UpdateLambdaFunction", data)
+    return response.json()
+
+  @Command()
+  def update_s3(self, 
+                   name: args.NAME, 
+                   bucket: args.S3BUCKET,
+                   key: args.S3KEY):
+    """Update the s3 bucket and key of a lambda.
+    
+    Args:
+      name (str): The name of the lambda to update.
+      s3bucket (str): The s3 bucket to use for the lambda.
+      s3key (str): The s3 key (file path) to use for the lambda.
+    """
+    tenant_id = self.tenant["TenantId"]
+    data = {
+      "FunctionName": name,
+      "S3Bucket": bucket,
+      "S3Key": key
+    }
+    response = self.duplo.post(f"subscriptions/{tenant_id}/UpdateLambdaFunction", data)
+    return response.json()
+
+  def name_from_body(self, body):
+    prefix = f"duploservices-{self.tenant['AccountName']}"
+    name =  body["FunctionName"]
+    if not name.startswith(prefix):
+      name = f"{prefix}-{name}"
+    return name
