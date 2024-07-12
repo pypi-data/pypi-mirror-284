afd8c295630c5d1cacb6f1956595ac7c7534dcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:17 +0000

    Add package ecodev-front/0.0.51/ecodev_front-0.0.51.tar.gz

---
+++ packages/ecodev-front/ecodev_front-0.0.51.tar.gz/ecodev_front-0.0.51/ecodev_front/app_header.py
@@ -0,0 +1,43 @@
+"""
+Module implementing the navbar header components
+"""
+import os
+
+import dash_mantine_components as dmc
+from dash import html
+
+
+def app_logo(logo_path: str = '/assets/logo.png',
+             ratio: float = 570 / 128,
+             width: str = '120px',
+             style: dict[str, str] | None = None
+             ) -> dmc.AspectRatio:
+    """
+    Application logo component.
+    """
+    style = style or {'margin-left': '10px', 'width': width}
+    return dmc.AspectRatio(dmc.Image(src=logo_path),
+                           ratio=ratio, style=style)
+
+
+def app_title(app_name: str | None = None, color='white') -> html.Div:
+    """
+    Application title component.
+    """
+    app_name = app_name or os.getenv('app_name')
+    return html.Div(dmc.Title(app_name, c=color, fz=32))
+
+
+def app_header(logo: html.Div, title: html.Div) -> dmc.GridCol:
+    """
+    Application header, composed of an app logo and title components.
+    """
+    return dmc.GridCol(span='auto',
+                       visibleFrom='md',
+                       children=[
+                           dmc.Anchor(href='/', children=[
+                               dmc.Group([logo, title],
+                                         justify='space-around',
+                                         style={'margin-top': '5px'}),
+                           ])
+                       ])
