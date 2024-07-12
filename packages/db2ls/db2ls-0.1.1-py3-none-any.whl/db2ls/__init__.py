550d4fd154c03d157a59251cbacd6efba0b2dbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:17 +0000

    Add package db2ls/0.1.1/db2ls-0.1.1-py3-none-any.whl

---
+++ packages/db2ls/db2ls-0.1.1-py3-none-any.whl/db2ls/__init__.py
@@ -0,0 +1,13 @@
+from .db2ls import *
+
+# import os
+# import sys
+
+# # Get the directory of the current script
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# print(current_dir)
+# # Construct the path to the directory containing db2ls.py
+# db2ls_dir = os.path.join(current_dir, 'db2ls', 'db2ls')
+
+# # Add this directory to sys.path
+# sys.path.append(db2ls_dir)
