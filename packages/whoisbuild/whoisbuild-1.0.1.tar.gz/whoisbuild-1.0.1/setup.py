2b2defbb0c0362f0435b41244d6e2cb4df7f8375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:04:32 +0000

    Add package whoisbuild/1.0.1/whoisbuild-1.0.1.tar.gz

---
+++ packages/whoisbuild/whoisbuild-1.0.1.tar.gz/whoisbuild-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whoisbuild',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=['discord.py'],
+    author='Taylor Kalos',
+    author_email='talosfk@example.com',
+    description='A module to facilitate the creation of Embeds Discord',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/votrecompte/discord_embed_helper',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
