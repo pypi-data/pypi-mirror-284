12a856cab3ddcfdc31271d14aa58dc38feb0be78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:04:03 +0000

    Add package py-plugin-feeds/1.0.0/py_plugin_feeds-1.0.0.tar.gz

---
+++ packages/py-plugin-feeds/py_plugin_feeds-1.0.0.tar.gz/py_plugin_feeds-1.0.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+VERSION = "1.0.0" 
+
+NAME = "py-plugin-feeds"
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    return [line.strip() for line in lines if line.strip() and not line.startswith('#')]
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    description="Streamlit Connection for Google Sheets.",
+    url="https://github.com/GoPlugin/py-plugin-feeds",
+    project_urls={
+        "Source Code": "https://github.com/GoPlugin/py-plugin-feeds",
+    },
+    author="Logeswaran",
+    author_email="meta2web3@gmail.com",
+    license="Apache License 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+    install_requires=parse_requirements("requirements.txt"),
+    packages=["py_plugin_feeds"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
---
+++ packages/py-plugin-feeds/py_plugin_feeds-1.0.0.tar.gz/py_plugin_feeds-1.0.0/tests/test.py
@@ -0,0 +1,13 @@
+from web3 import HTTPProvider, Web3
+from py_plugin_feeds import get_token_price
+
+def main():
+    json_rpc_url = "https://rpc-amoy.polygon.technology"
+    pair = "CIFI/USDT"
+    web3 = Web3(HTTPProvider(json_rpc_url))
+    web3.middleware_onion.clear()
+    result= get_token_price(web3,pair,"latestAnswer")
+    print("result:::",result)
+
+if __name__ == "__main__":
+    main()
