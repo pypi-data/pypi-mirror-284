76c5db4fe9385c3ccc7cbc47907f61316de81274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:02 +0000

    Add package autoplot/0.9.2/autoplot-0.9.2.tar.gz

---
+++ packages/autoplot/autoplot-0.9.2.tar.gz/autoplot-0.9.2/autoplot/__init__.py
@@ -0,0 +1,13 @@
+# Allow "from autoplot import javaaddpath"
+from autoplot.autoplot import javaaddpath
+from autoplot.autoplot import init
+from autoplot.autoplot import start
+from autoplot.autoplot import APDataSet
+from autoplot.autoplot import applot
+from autoplot.autoplot import plot
+from autoplot.autoplot import to_qdataset
+from autoplot.autoplot import to_ndarray
+from autoplot.autoplot import show_completions
+from autoplot.autoplot import getDataSet
+from autoplot.autoplot import formatDataSet
+__version__='0.9.2'
