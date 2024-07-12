61b2d6d3a376817a8d73db5c7f00744dd000ca6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:06:32 +0000

    Add package flask-fs2/0.7.27/flask_fs2-0.7.27-py2.py3-none-any.whl

---
+++ packages/flask-fs2/flask_fs2-0.7.27-py2.py3-none-any.whl/flask_fs/backends/mock.py
@@ -0,0 +1,13 @@
+import logging
+
+from . import BaseBackend
+
+log = logging.getLogger(__name__)
+
+
+class MockBackend(BaseBackend):
+    """
+    A backend with only purpose of being mocked
+    """
+
+    pass
