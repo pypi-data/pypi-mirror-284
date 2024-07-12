b1afa22819b1e6538fcbf8cf7a0ca7619bb895df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:04:39 +0000

    Add package time-measure/0.1.0/time_measure-0.1.0-py3-none-any.whl

---
+++ packages/time-measure/time_measure-0.1.0-py3-none-any.whl/time_measure/__init__.py
@@ -0,0 +1,15 @@
+from .main import (
+    time_measure_decorator,
+    time_measure_context,
+    TimeMeasureContextManager,
+    TimeMeasureWrapper,
+    measure_time
+)
+
+__all__ = [
+    'time_measure_decorator',
+    'time_measure_context',
+    'TimeMeasureContextManager',
+    'TimeMeasureWrapper',
+    'measure_time'
+]
