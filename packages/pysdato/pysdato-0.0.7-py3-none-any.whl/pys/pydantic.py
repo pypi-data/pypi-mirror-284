d0723cc8b4c1812a9c9a8f505162de414b2005fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:03:51 +0000

    Add package pysdato/0.0.7/pysdato-0.0.7-py3-none-any.whl

---
+++ packages/pysdato/pysdato-0.0.7-py3-none-any.whl/pys/pydantic.py
@@ -0,0 +1,13 @@
+import uuid
+
+from pydantic import BaseModel
+
+from . import saveable
+
+
+@saveable(field_as_id='id')
+class ModelWithID(BaseModel):
+    """
+    Base class for models with `id` field prefilled with random UUID if not initialized.
+    """
+    id: str = None
