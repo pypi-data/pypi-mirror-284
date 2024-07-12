e1ded3ecf4f9e49cf68528d31f06b0269bad3549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:04:08 +0000

    Add package synapseclient/4.3.1/synapseclient-4.3.1.tar.gz

---
+++ packages/synapseclient/synapseclient-4.3.1.tar.gz/synapseclient-4.3.1/synapseclient/__init__.py
@@ -0,0 +1,100 @@
+import importlib.resources
+
+import json
+import requests  # ensure user-agent is set to track Synapse Python client usage
+
+from .activity import Activity
+from .annotations import Annotations
+from .client import PUBLIC, AUTHENTICATED_USERS
+
+# public APIs
+from .client import Synapse, login
+from .core.version_check import check_for_updates, release_notes
+from .entity import Entity, Project, Folder, File, Link, DockerRepository
+from .evaluation import Evaluation, Submission, SubmissionStatus
+from .table import (
+    Schema,
+    EntityViewSchema,
+    Column,
+    RowSet,
+    Row,
+    as_table_columns,
+    Table,
+    PartialRowset,
+    EntityViewType,
+    build_table,
+    SubmissionViewSchema,
+    MaterializedViewSchema,
+    Dataset,
+    delete_rows,
+)
+from .team import Team, UserProfile, UserGroupHeader, TeamMember
+from .wiki import Wiki
+from .core.models.permission import Permissions
+
+# ref = importlib.resources.files(__name__).joinpath("synapsePythonClient")
+# with ref.open("r") as fp:
+#     __version__ = json.load(fp)["latestVersion"]
+# TODO: switch to the above after python 3.8 is deprecated
+with importlib.resources.path(__name__, "synapsePythonClient") as ref:
+    __version__ = json.load(open(ref))["latestVersion"]
+
+__all__ = [
+    # objects
+    "Synapse",
+    "Activity",
+    "Entity",
+    "Project",
+    "Folder",
+    "File",
+    "Link",
+    "DockerRepository",
+    "Evaluation",
+    "Submission",
+    "SubmissionStatus",
+    "Schema",
+    "EntityViewSchema",
+    "Column",
+    "Row",
+    "RowSet",
+    "Table",
+    "PartialRowset",
+    "Team",
+    "UserProfile",
+    "UserGroupHeader",
+    "TeamMember",
+    "Wiki",
+    "Annotations",
+    "SubmissionViewSchema",
+    "MaterializedViewSchema",
+    "Dataset",
+    "Permissions",
+    # functions
+    "login",
+    "build_table",
+    "delete_rows",
+    "as_table_columns",
+    "check_for_updates",
+    "release_notes",
+    # enum
+    "EntityViewType",
+    # constants
+    "PUBLIC",
+    "AUTHENTICATED_USERS",
+]
+
+USER_AGENT = {
+    "User-Agent": "synapseclient/%s %s"
+    % (__version__, requests.utils.default_user_agent())
+}
+
+USER_AGENT_COMMAND_LINE = {
+    "User-Agent": "synapsecommandlineclient/%s %s"
+    % (__version__, requests.utils.default_user_agent())
+}
+
+# patch json
+from .core.models import custom_json  # noqa
+
+# patch logging
+from .core import logging_setup  # noqa
---
+++ packages/synapseclient/synapseclient-4.3.1.tar.gz/synapseclient-4.3.1/synapseclient/api/__init__.py
@@ -0,0 +1,56 @@
+# These are all of the models that are used by the Synapse client.
+from .annotations import set_annotations
+from .entity_bundle_services_v2 import (
+    get_entity_id_bundle2,
+    get_entity_id_version_bundle2,
+    post_entity_bundle2_create,
+    put_entity_id_bundle2,
+)
+from .entity_services import (
+    get_entity,
+    get_upload_destination,
+    get_upload_destination_location,
+    post_entity,
+    put_entity,
+    create_access_requirements_if_none,
+    delete_entity_generated_by,
+)
+from .file_services import (
+    get_file_handle,
+    post_external_filehandle,
+    post_external_object_store_filehandle,
+    post_external_s3_file_handle,
+    post_file_multipart,
+    put_file_multipart_complete,
+    post_file_multipart_presigned_urls,
+    put_file_multipart_add,
+    AddPartResponse,
+)
+
+__all__ = [
+    # annotations
+    "set_annotations",
+    "get_entity_id_bundle2",
+    "get_entity_id_version_bundle2",
+    "post_entity_bundle2_create",
+    "put_entity_id_bundle2",
+    # file_services
+    "post_file_multipart",
+    "put_file_multipart_add",
+    "put_file_multipart_complete",
+    "post_external_object_store_filehandle",
+    "post_external_s3_file_handle",
+    "get_file_handle",
+    "post_external_filehandle",
+    "post_file_multipart_presigned_urls",
+    "put_file_multipart_add",
+    "AddPartResponse",
+    # entity_services
+    "get_entity",
+    "put_entity",
+    "post_entity",
+    "get_upload_destination",
+    "get_upload_destination_location",
+    "create_access_requirements_if_none",
+    "delete_entity_generated_by",
+]
