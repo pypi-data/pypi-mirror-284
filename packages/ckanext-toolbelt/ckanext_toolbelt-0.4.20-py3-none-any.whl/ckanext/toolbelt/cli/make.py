299066598195255d5cc934e9df77c9c1af865b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:19 +0000

    Add package ckanext-toolbelt/0.4.20/ckanext_toolbelt-0.4.20-py3-none-any.whl

---
+++ packages/ckanext-toolbelt/ckanext_toolbelt-0.4.20-py3-none-any.whl/ckanext/toolbelt/cli/make.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import click
+
+from . import make_config, make_gh_action, make_readme, make_template
+
+
+
+@click.group()
+def make():
+    """Generate, make, produce, print different things."""
+
+
+make.add_command(make_readme.readme)
+make.add_command(make_template.template)
+make.add_command(make_gh_action.gh_action)
+make.add_command(make_config.config)
+
+try:
+    from . import make_ckanext
+except ImportError:
+    pass
+else:
+    make.add_command(make_ckanext.ckanext)
