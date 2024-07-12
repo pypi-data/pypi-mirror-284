4f5a0aa4acb23db6532e7f3af5e9863933f7e9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 04:04:03 +0000

    Add package asciinema-automation/0.2.2/asciinema_automation-0.2.2.tar.gz

---
+++ packages/asciinema-automation/asciinema_automation-0.2.2.tar.gz/asciinema_automation-0.2.2/asciinema_automation/script.py
@@ -0,0 +1,60 @@
+import logging
+import pathlib
+import time
+from dataclasses import dataclass
+
+import pexpect
+
+logger = logging.getLogger(__name__)
+
+
+class Instruction:
+    def run(self, script: "Script") -> None:
+        logger.info(self.__class__.__name__)
+
+
+@dataclass
+class Script:
+    outputfile: pathlib.Path
+    asciinema_arguments: str
+    timeout: float
+    wait: float
+    delay: float
+    standard_deviation: float
+    instructions: list[Instruction]
+    delaybeforesend: float | None = None
+
+    def execute(self) -> None:
+        spawn_command = (
+            "asciinema rec " + str(self.outputfile) + " " + self.asciinema_arguments
+        )
+        logger.info(spawn_command)
+        self.process = pexpect.spawn(spawn_command, timeout=self.timeout, logfile=None)
+        self.process.delaybeforesend = self.delaybeforesend
+
+        self.process.expect("\n")
+        logger.debug(self.process.before)
+        if not (
+            f"recording asciicast to {self.outputfile}" in str(self.process.before)
+            or f"appending to asciicast at {self.outputfile}"
+            in str(self.process.before)
+        ):
+            self.process.expect(pexpect.EOF)
+            self.process.close()
+            logger.debug("Exit status:" + str(self.process.exitstatus))
+            logger.debug("Signal status:" + str(self.process.signalstatus))
+        else:
+            self.process.expect("\n")
+            logger.debug(self.process.before)
+            logger.debug(self.process.after)
+            logger.info("Start reading instructions")
+            for instruction in self.instructions:
+                time.sleep(self.wait)
+                instruction.run(self)
+            time.sleep(self.wait)
+            logger.info("Finished reading instructions")
+            self.process.sendcontrol("d")
+            self.process.expect(pexpect.EOF)
+            self.process.close()
+            logger.debug("Exit status:" + str(self.process.exitstatus))
+            logger.debug("Signal status:" + str(self.process.signalstatus))
---
+++ packages/asciinema-automation/asciinema_automation-0.2.2.tar.gz/asciinema_automation-0.2.2/tests/test_regression.py
@@ -0,0 +1,69 @@
+import pathlib
+import re
+import shutil
+
+import pytest
+from pytest import FixtureRequest, MonkeyPatch
+
+from asciinema_automation.cli import cli
+
+path_to_example_folder = pathlib.Path("examples")
+if not path_to_example_folder.is_dir():
+    raise ValueError(
+        "input-folder argument from command line must be a valid path to a folder."
+    )
+
+
+@pytest.fixture(autouse=True)
+def change_test_directory(tmp_path: pathlib.Path, monkeypatch: MonkeyPatch) -> None:
+    pathlib.Path.mkdir(pathlib.Path(tmp_path / "tmp"), exist_ok=True)
+    monkeypatch.chdir(tmp_path / "tmp")
+
+
+# path_to_example_folder.iterdir()
+@pytest.mark.parametrize(
+    "inputfile",
+    path_to_example_folder.iterdir(),
+    ids=[example.name for example in path_to_example_folder.iterdir()],
+)
+def test_regression(
+    tmp_path: pathlib.Path, request: FixtureRequest, inputfile: pathlib.Path
+) -> None:
+    example_folder = request.config.invocation_params.dir / "examples"
+    reference_folder = (
+        request.config.invocation_params.dir / "tests" / "reference_output"
+    )
+    custom_rc_file = (
+        request.config.invocation_params.dir / "tests" / "custom_rc_file_dumb_term"
+    )
+    output_folder = tmp_path
+    output_file_name = inputfile.stem + ".cast"
+    print(pathlib.Path().cwd())
+
+    if inputfile.name in ["backward_search.sh", "history.sh", "man_page.sh"]:
+        custom_rc_file = (
+            request.config.invocation_params.dir / "tests" / "custom_rc_file_xterm_term"
+        )
+
+    if inputfile.name in ["asciinema_automation_hello_world.sh"]:
+        shutil.copy(example_folder / "hello_world.sh", tmp_path / "tmp")
+
+    cli(
+        [
+            "--debug",
+            "--asciinema-arguments",
+            """--raw --overwrite -c 'env -i bash --noprofile --rcfile """
+            + str(custom_rc_file)
+            + """'""",
+            str(example_folder / inputfile.name),
+            str(output_folder / output_file_name),
+        ]
+    )
+
+    with open(output_folder / output_file_name) as output_file, pathlib.Path(
+        reference_folder / output_file_name
+    ).open() as reference_file:
+        ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])", re.VERBOSE)
+        output_text = output_file.read()
+        cleaned_output_text = ansi_escape.sub("", output_text)
+        assert cleaned_output_text == reference_file.read()
