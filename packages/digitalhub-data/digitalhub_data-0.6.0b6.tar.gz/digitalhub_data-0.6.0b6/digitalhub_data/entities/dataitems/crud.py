abe0447ae05d6979d5690201930d3af91ba03761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:42 +0000

    Add package digitalhub-data/0.6.0b6/digitalhub_data-0.6.0b6.tar.gz

---
+++ packages/digitalhub-data/digitalhub_data-0.6.0b6.tar.gz/digitalhub_data-0.6.0b6/digitalhub_data/entities/dataitems/crud.py
@@ -0,0 +1,269 @@
+from __future__ import annotations
+
+import typing
+
+from digitalhub_core.context.builder import check_context, get_context
+from digitalhub_core.utils.api import api_ctx_delete, api_ctx_list, api_ctx_read, api_ctx_update
+from digitalhub_core.utils.generic_utils import parse_entity_key
+from digitalhub_core.utils.io_utils import read_yaml
+from digitalhub_data.entities.dataitems.builder import dataitem_from_dict, dataitem_from_parameters
+from digitalhub_data.entities.entity_types import EntityTypes
+
+if typing.TYPE_CHECKING:
+    from digitalhub_data.entities.dataitems.entity._base import Dataitem
+
+
+ENTITY_TYPE = EntityTypes.DATAITEMS.value
+
+
+def create_dataitem(**kwargs) -> Dataitem:
+    """
+    Create a new data item with the provided parameters.
+
+    Parameters
+    ----------
+    **kwargs : dict
+        Keyword arguments.
+
+    Returns
+    -------
+    Dataitem
+        Object instance.
+    """
+    return dataitem_from_parameters(**kwargs)
+
+
+def create_dataitem_from_dict(obj: dict) -> Dataitem:
+    """
+    Create a new Dataitem instance from a dictionary.
+
+    Parameters
+    ----------
+    obj : dict
+        Dictionary to create object from.
+
+    Returns
+    -------
+    Dataitem
+        Dataitem object.
+    """
+    check_context(obj.get("project"))
+    return dataitem_from_dict(obj)
+
+
+def new_dataitem(
+    project: str,
+    name: str,
+    kind: str,
+    uuid: str | None = None,
+    description: str | None = None,
+    git_source: str | None = None,
+    labels: list[str] | None = None,
+    embedded: bool = True,
+    path: str | None = None,
+    **kwargs,
+) -> Dataitem:
+    """
+    Create a new object instance.
+
+    Parameters
+    ----------
+    project : str
+        Project name.
+    name : str
+        Name that identifies the object.
+    kind : str
+        Kind of the object.
+    uuid : str
+        ID of the object in form of UUID.
+    description : str
+        Description of the object.
+    git_source : str
+        Remote git source for object.
+    labels : list[str]
+        List of labels.
+    embedded : bool
+        Flag to determine if object must be embedded in project.
+    path : str
+        Object path on local file system or remote storage.
+        If not provided, it's generated.
+    **kwargs : dict
+        Spec keyword arguments.
+
+    Returns
+    -------
+    Dataitem
+        Object instance.
+    """
+    obj = create_dataitem(
+        project=project,
+        name=name,
+        kind=kind,
+        path=path,
+        uuid=uuid,
+        description=description,
+        git_source=git_source,
+        labels=labels,
+        embedded=embedded,
+        **kwargs,
+    )
+    obj.save()
+    return obj
+
+
+def get_dataitem(project: str, entity_name: str | None = None, entity_id: str | None = None, **kwargs) -> Dataitem:
+    """
+    Get object from backend.
+
+    Parameters
+    ----------
+    project : str
+        Project name.
+    entity_name : str
+        Entity name.
+    entity_id : str
+        Entity ID.
+    **kwargs : dict
+        Parameters to pass to the API call.
+
+    Returns
+    -------
+    Dataitem
+        Object instance.
+    """
+    if (entity_id is None) and (entity_name is None):
+        raise ValueError("Either entity_name or entity_id must be provided.")
+
+    context = get_context(project)
+
+    if entity_name is not None:
+        params = kwargs.get("params", {})
+        if params is None or not params:
+            kwargs["params"] = {}
+
+        api = api_ctx_list(project, ENTITY_TYPE)
+        kwargs["params"]["name"] = entity_name
+        obj = context.list_objects(api, **kwargs)[0]
+    else:
+        api = api_ctx_read(project, ENTITY_TYPE, entity_id)
+        obj = context.read_object(api, **kwargs)
+    return create_dataitem_from_dict(obj)
+
+
+def get_dataitem_from_key(key: str) -> Dataitem:
+    """
+    Get dataitem from key.
+
+    Parameters
+    ----------
+    key : str
+        Key of the dataitem.
+        It's format is store://<project>/dataitems/<kind>/<name>:<uuid>.
+    """
+    project, _, _, _, uuid = parse_entity_key(key)
+    return get_dataitem(project, entity_id=uuid)
+
+
+def import_dataitem(file: str) -> Dataitem:
+    """
+    Get object from file.
+
+    Parameters
+    ----------
+    file : str
+        Path to the file.
+
+    Returns
+    -------
+    Dataitem
+        Object instance.
+    """
+    obj: dict = read_yaml(file)
+    return create_dataitem_from_dict(obj)
+
+
+def delete_dataitem(
+    project: str,
+    entity_name: str | None = None,
+    entity_id: str | None = None,
+    delete_all_versions: bool = False,
+    **kwargs,
+) -> dict:
+    """
+    Delete object from backend.
+
+    Parameters
+    ----------
+    project : str
+        Project name.
+    entity_name : str
+        Entity name.
+    entity_id : str
+        Entity ID.
+    delete_all_versions : bool
+        Delete all versions of the named entity. Entity name is required.
+    **kwargs : dict
+        Parameters to pass to the API call.
+
+    Returns
+    -------
+    dict
+        Response from backend.
+    """
+    if (entity_id is None) and (entity_name is None):
+        raise ValueError("Either entity_name or entity_id must be provided.")
+
+    context = get_context(project)
+
+    params = kwargs.get("params", {})
+    if params is None or not params:
+        kwargs["params"] = {}
+
+    if entity_id is not None:
+        api = api_ctx_delete(project, ENTITY_TYPE, entity_id)
+    else:
+        kwargs["params"]["name"] = entity_name
+        api = api_ctx_list(project, ENTITY_TYPE)
+        if delete_all_versions:
+            return context.delete_object(api, **kwargs)
+        obj = context.list_objects(api, **kwargs)[0]
+        entity_id = obj["id"]
+
+    api = api_ctx_delete(project, ENTITY_TYPE, entity_id)
+    return context.delete_object(api, **kwargs)
+
+
+def update_dataitem(entity: Dataitem, **kwargs) -> dict:
+    """
+    Update object in backend.
+
+    Parameters
+    ----------
+    entity : Dataitem
+        The object to update.
+
+    Returns
+    -------
+    dict
+        Response from backend.
+    """
+    api = api_ctx_update(entity.project, ENTITY_TYPE, entity_id=entity.id)
+    return get_context(entity.project).update_object(api, entity.to_dict(), **kwargs)
+
+
+def list_dataitems(project: str, **kwargs) -> list[dict]:
+    """
+    List all objects from backend.
+
+    Parameters
+    ----------
+    project : str
+        Project name.
+
+    Returns
+    -------
+    list[dict]
+        List of dataitems dict representations.
+    """
+    api = api_ctx_list(project, ENTITY_TYPE)
+    return get_context(project).list_objects(api, **kwargs)
---
+++ packages/digitalhub-data/digitalhub_data-0.6.0b6.tar.gz/digitalhub_data-0.6.0b6/digitalhub_data/entities/projects/entity.py
@@ -0,0 +1,279 @@
+from __future__ import annotations
+
+import typing
+from typing import Any
+
+from digitalhub_core.entities._builders.metadata import build_metadata
+from digitalhub_core.entities._builders.spec import build_spec
+from digitalhub_core.entities._builders.status import build_status
+from digitalhub_core.entities.projects.entity import CTX_ENTITIES, FUNC_MAP, Project
+from digitalhub_data.entities.dataitems.crud import (
+    create_dataitem_from_dict,
+    delete_dataitem,
+    get_dataitem,
+    list_dataitems,
+    new_dataitem,
+)
+from digitalhub_data.entities.entity_types import EntityTypes
+
+if typing.TYPE_CHECKING:
+    from digitalhub_data.entities.dataitems.entity._base import Dataitem
+
+
+DATAITEMS = EntityTypes.DATAITEMS.value
+
+CTX_ENTITIES.append(DATAITEMS)
+FUNC_MAP[DATAITEMS] = create_dataitem_from_dict
+
+
+class ProjectData(Project):
+
+    """
+    ProjectData class.
+    """
+
+    #############################
+    #  Dataitems
+    #############################
+
+    def new_dataitem(self, **kwargs) -> Dataitem:
+        """
+        Create a Dataitem.
+
+        Parameters
+        ----------
+        **kwargs : dict
+            Keyword arguments.
+
+        Returns
+        -------
+        Dataitem
+            Object instance.
+        """
+        kwargs["project"] = self.name
+        obj = new_dataitem(**kwargs)
+        self._add_object(obj, DATAITEMS)
+        return obj
+
+    def get_dataitem(self, entity_name: str | None = None, entity_id: str | None = None, **kwargs) -> Dataitem:
+        """
+        Get object from backend.
+
+        Parameters
+        ----------
+        entity_name : str
+            Entity name.
+        entity_id : str
+            Entity ID.
+        **kwargs : dict
+            Parameters to pass to the API call.
+
+        Returns
+        -------
+        Dataitem
+            Instance of Dataitem class.
+        """
+        obj = get_dataitem(self.name, entity_name=entity_name, entity_id=entity_id, **kwargs)
+        self._add_object(obj, DATAITEMS)
+        return obj
+
+    def delete_dataitem(self, entity_name: str | None = None, entity_id: str | None = None, **kwargs) -> None:
+        """
+        Delete a Dataitem from project.
+
+        Parameters
+        ----------
+        entity_name : str
+            Entity name.
+        entity_id : str
+            Entity ID.
+        **kwargs : dict
+            Parameters to pass to the API call.
+
+        Returns
+        -------
+        None
+        """
+        delete_dataitem(self.name, entity_name=entity_name, entity_id=entity_id, **kwargs)
+        self._delete_object(DATAITEMS, entity_name, entity_id)
+
+    def set_dataitem(self, dataitem: Dataitem) -> None:
+        """
+        Set a Dataitem.
+
+        Parameters
+        ----------
+        dataitem : Dataitem
+            Dataitem to set.
+
+        Returns
+        -------
+        None
+        """
+        self._add_object(dataitem, DATAITEMS)
+
+    def list_dataitems(self, **kwargs) -> list[dict]:
+        """
+        List dataitems associated with the project.
+
+        Parameters
+        ----------
+        **kwargs : dict
+            Filters to apply to the list. Shold be params={"filter": "value"}.
+
+        Returns
+        -------
+        list[dict]
+            List of objects related to project.
+        """
+        return list_dataitems(self.name, **kwargs)
+
+    def log_dataitem(
+        self,
+        name: str,
+        kind: str,
+        path: str | None = None,
+        data: Any | None = None,
+        extension: str | None = None,
+        **kwargs,
+    ) -> Dataitem:
+        """
+        Log a dataitem to the project.
+
+        Parameters
+        ----------
+        name : str
+            Name that identifies the object.
+        kind : str
+            Kind of the dataitem.
+        path : str
+            Destination path of the dataitem.
+        data : Any
+            Dataframe to log.
+        extension : str
+            Extension of the dataitem.
+        **kwargs : dict
+            New dataitem parameters.
+
+        Returns
+        -------
+        Dataitem
+            Object instance.
+        """
+        dataitem = new_dataitem(project=self.name, name=name, kind=kind, path=path, **kwargs)
+        if kind == "table":
+            dataitem.write_df(df=data, extension=extension)
+        return dataitem
+
+    @staticmethod
+    def _parse_dict(obj: dict, validate: bool = True) -> dict:
+        """
+        Get dictionary and parse it to a valid entity dictionary.
+
+        Parameters
+        ----------
+        entity : str
+            Entity type.
+        obj : dict
+            Dictionary to parse.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the attributes of the entity instance.
+        """
+        # Override methods to search in digitalhub_data
+        name = obj.get("name")
+        kind = obj.get("kind")
+        metadata = build_metadata(kind, **obj.get("metadata", {}))
+        spec = build_spec(kind, validate=validate, **obj.get("spec", {}))
+        status = build_status(kind, **obj.get("status", {}))
+        user = obj.get("user")
+        local = obj.get("local", False)
+        return {
+            "name": name,
+            "kind": kind,
+            "metadata": metadata,
+            "spec": spec,
+            "status": status,
+            "user": user,
+            "local": local,
+        }
+
+
+def project_from_parameters(
+    name: str,
+    kind: str,
+    description: str | None = None,
+    git_source: str | None = None,
+    labels: list[str] | None = None,
+    local: bool = False,
+    context: str | None = None,
+    **kwargs,
+) -> ProjectData:
+    """
+    Create project.
+
+    Parameters
+    ----------
+    name : str
+        Name that identifies the object.
+    kind : str
+        Kind of the object.
+    description : str
+        Description of the object.
+    git_source : str
+        Remote git source for object.
+    labels : list[str]
+        List of labels.
+    local : bool
+        Flag to determine if object will be exported to backend.
+    context : str
+        The context of the project.
+    **kwargs : dict
+        Spec keyword arguments.
+
+    Returns
+    -------
+    ProjectData
+        ProjectData object.
+    """
+    spec = build_spec(
+        kind,
+        context=context,
+        **kwargs,
+    )
+    metadata = build_metadata(
+        kind,
+        project=name,
+        name=name,
+        description=description,
+        labels=labels,
+        source=git_source,
+    )
+    status = build_status(kind)
+    return ProjectData(
+        name=name,
+        kind=kind,
+        metadata=metadata,
+        spec=spec,
+        status=status,
+        local=local,
+    )
+
+
+def project_from_dict(obj: dict) -> ProjectData:
+    """
+    Create project from dictionary.
+
+    Parameters
+    ----------
+    obj : dict
+        Dictionary to create object from.
+
+    Returns
+    -------
+    ProjectData
+        ProjectData object.
+    """
+    return ProjectData.from_dict(obj)
