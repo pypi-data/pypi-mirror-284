27c732fa86dcf6424b95bedf5e42e6bc842d2280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:49 +0000

    Add package omoospace/0.1.10/omoospace-0.1.10-py3-none-any.whl

---
+++ packages/omoospace/omoospace-0.1.10-py3-none-any.whl/omoospace/types.py
@@ -0,0 +1,14 @@
+
+import os
+from pathlib import Path
+from typing import TypedDict, Union
+
+Structure = dict
+
+Entity = Path
+
+Item = Path
+
+Route = list[str]
+
+PathLike = Union[str, os.PathLike]
