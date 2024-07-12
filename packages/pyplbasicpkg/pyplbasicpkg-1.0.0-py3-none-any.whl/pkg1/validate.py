158469ce4166687c2017e663564755f405d93dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:03:45 +0000

    Add package pyplbasicpkg/1.0.0/pyplbasicpkg-1.0.0-py3-none-any.whl

---
+++ packages/pyplbasicpkg/pyplbasicpkg-1.0.0-py3-none-any.whl/pkg1/validate.py
@@ -0,0 +1,5 @@
+def is_greater_than(value):
+   return value>100
+
+def is_eligible(age):
+   return age>=18
---
+++ packages/pyplbasicpkg/pyplbasicpkg-1.0.0-py3-none-any.whl/pkg2/calculator.py
@@ -0,0 +1,11 @@
+def add(a1, a2):
+   return a1+a2
+
+def sub(a1, a2):
+   return a1-a2
+
+def mul(a1, a2):
+   return a1*a2
+
+def div(a1, a2):
+   return a1/a2
