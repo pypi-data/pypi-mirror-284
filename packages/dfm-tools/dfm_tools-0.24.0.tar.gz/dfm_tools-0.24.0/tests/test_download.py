4ef345081e95ea0fa647389a282508191f408802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:05:03 +0000

    Add package dfm-tools/0.24.0/dfm_tools-0.24.0.tar.gz

---
+++ packages/dfm-tools/dfm_tools-0.24.0.tar.gz/dfm_tools-0.24.0/tests/test_download.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 09:41:49 2023
+
+@author: veenstra
+"""
+
+import os
+import pytest
+import pandas as pd
+from dfm_tools.download import cds_credentials, copernicusmarine_credentials
+import dfm_tools as dfmt
+import xarray as xr
+import glob
+
+
+@pytest.mark.requiressecrets
+@pytest.mark.unittest
+def test_cds_credentials():
+    # check if the credentials are present on this system
+    val = cds_credentials()
+    assert not val
+    
+    # some platforms depend on environ for url/apikey, check if the default cdsapi_url is set after running cds_credentials()
+    assert "CDSAPI_URL" in os.environ.keys()
+
+
+@pytest.mark.requiressecrets
+@pytest.mark.unittest
+def test_copernicusmarine_credentials():
+    copernicusmarine_credentials()
+
+
+@pytest.mark.requiressecrets
+@pytest.mark.unittest
+@pytest.mark.timeout(60) # useful since CDS downloads are terribly slow sometimes, so skip in that case
+def test_download_era5(file_nc_era5_pattern):
+    # file_nc_era5_pattern comes from conftest.py
+    file_list = glob.glob(file_nc_era5_pattern)
+    assert len(file_list) == 2
+    
+    ds = xr.open_mfdataset(file_nc_era5_pattern)
+    assert ds.sizes["time"] == 1416
+    assert ds.time.to_pandas().iloc[0] == pd.Timestamp('2010-01-01')
+    assert ds.time.to_pandas().iloc[-1] == pd.Timestamp('2010-02-28 23:00')
+
+
+@pytest.mark.requiressecrets
+@pytest.mark.unittest
+@pytest.mark.timeout(60) # useful since CDS downloads are terribly slow sometimes, so skip in that case
+def test_download_era5_unsupported_varkey():
+    date_min = '2010-01-31'
+    date_max = '2010-02-01'
+    longitude_min, longitude_max, latitude_min, latitude_max =    2,   3,  51, 52 #test domain
+    varkey = 'unexisting'
+    with pytest.raises(KeyError) as e:
+        dfmt.download_ERA5(varkey, 
+                            longitude_min=longitude_min, longitude_max=longitude_max, latitude_min=latitude_min, latitude_max=latitude_max,
+                            date_min=date_min, date_max=date_max,
+                            dir_output='.', overwrite=True)
+    assert '"unexisting" not available' in str(e.value)
+
+
+@pytest.mark.requiressecrets
+@pytest.mark.unittest
+def test_download_cmems_my(tmp_path):
+    date_min = '2010-01-01'
+    date_max = '2010-01-02'
+    longitude_min, longitude_max, latitude_min, latitude_max =    2,   3,  51, 52 #test domain
+    varlist_cmems = ['bottomT','no3'] # avaliable variables differ per product, examples are ['bottomT','mlotst','siconc','sithick','so','thetao','uo','vo','usi','vsi','zos','no3']. More info on https://data.marine.copernicus.eu/products
+    for varkey in varlist_cmems:
+        file_prefix = 'cmems_'
+        dfmt.download_CMEMS(varkey=varkey,
+                            longitude_min=longitude_min, longitude_max=longitude_max, latitude_min=latitude_min, latitude_max=latitude_max,
+                            date_min=date_min, date_max=date_max,
+                            dir_output=tmp_path, file_prefix=file_prefix, overwrite=True)
+    
+    # assert downloaded files
+    file_nc_pat = os.path.join(tmp_path, "*.nc")
+    ds = xr.open_mfdataset(file_nc_pat)
+    assert ds.sizes["time"] == 2
+    assert ds.time.to_pandas().iloc[0] == pd.Timestamp('2010-01-01')
+    assert ds.time.to_pandas().iloc[-1] == pd.Timestamp('2010-01-02')
+
+
+@pytest.mark.requiressecrets
+@pytest.mark.unittest
+def test_download_cmems_forecast(tmp_path):
+    date_min = pd.Timestamp.today()
+    date_max = pd.Timestamp.today() + pd.Timedelta(days=1)
+    longitude_min, longitude_max, latitude_min, latitude_max =    2,   3,  51, 52 #test domain
+    varlist_cmems = ['tob','no3'] # avaliable variables differ per product, examples are ['bottomT','mlotst','siconc','sithick','so','thetao','uo','vo','usi','vsi','zos','no3']. More info on https://data.marine.copernicus.eu/products
+    for varkey in varlist_cmems:
+        file_prefix = 'cmems_'
+        dfmt.download_CMEMS(varkey=varkey,
+                            longitude_min=longitude_min, longitude_max=longitude_max, latitude_min=latitude_min, latitude_max=latitude_max,
+                            date_min=date_min, date_max=date_max,
+                            dir_output=tmp_path, file_prefix=file_prefix, overwrite=True)
+
+    # assert downloaded files
+    file_nc_pat = os.path.join(tmp_path, "*.nc")
+    ds = xr.open_mfdataset(file_nc_pat)
+    assert ds.sizes["time"] == 3
+    assert ds.time.to_pandas().iloc[0] == date_min.floor("D")
+    assert ds.time.to_pandas().iloc[-1] == date_max.ceil("D")
+
+
+@pytest.mark.unittest
+def test_download_hycom(tmp_path):
+    # domain
+    longitude_min, longitude_max, latitude_min, latitude_max =    2,   3,  51, 52 #test domain
+    date_min = '2010-01-01'
+    date_max = '2010-01-02'
+    varlist_hycom = ['surf_el']#'water_temp'] #['tau','water_u','water_v','water_temp','salinity','surf_el']
+    
+    for varkey in varlist_hycom:
+        # Path(dir_output).mkdir(parents=True, exist_ok=True)
+        period_range_years = pd.period_range(date_min,date_max,freq='Y')
+        dataset_url = [f'https://tds.hycom.org/thredds/dodsC/GLBu0.08/expt_19.1/{year}' for year in period_range_years] #list is possible with hycom, since it uses xr.open_mfdataset()
+        file_prefix = 'hycom_'
+        dfmt.download_OPeNDAP(dataset_url=dataset_url,
+                              varkey=varkey,
+                              longitude_min=longitude_min, longitude_max=longitude_max, latitude_min=latitude_min, latitude_max=latitude_max,
+                              date_min=date_min, date_max=date_max,
+                              dir_output=tmp_path, file_prefix=file_prefix, overwrite=True)
+
+    # assert downloaded files
+    file_nc_pat = os.path.join(tmp_path, "*.nc")
+    ds = xr.open_mfdataset(file_nc_pat)
+    assert ds.sizes["time"] == 2
+    assert ds.time.to_pandas().iloc[0] == pd.Timestamp('2010-01-01')
+    assert ds.time.to_pandas().iloc[-1] == pd.Timestamp('2010-01-02')
---
+++ packages/dfm-tools/dfm_tools-0.24.0.tar.gz/dfm_tools-0.24.0/tests/test_hydrolib_helpers.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 16:09:20 2023
+
+@author: veenstra
+"""
+
+import pytest
+import dfm_tools as dfmt
+import hydrolib.core.dflowfm as hcdfm
+
+
+@pytest.mark.unittest
+def test_geodataframe_with_Polygon_to_PolyFile():
+    """
+    converting a geodataframe with Polygon geometries to hcdfm.PolyFile
+    """
+    lon_min, lon_max, lat_min, lat_max = -6, 2, 48.5, 51.2
+    bbox = (lon_min, lat_min, lon_max, lat_max)
+    
+    # gdf containing shapely.Polygon geometries
+    coastlines_gdf = dfmt.get_coastlines_gdb(bbox=bbox, res='h')
+    
+    polyfile = dfmt.geodataframe_to_PolyFile(coastlines_gdf)
+    assert isinstance(polyfile, hcdfm.PolyFile)
+
+
+@pytest.mark.unittest
+def test_geodataframe_with_LineString_to_PolyFile(tmp_path):
+    """
+    converting a geodataframe with Linestring geometries to hcdfm.PolyFile
+    """
+    # write polygon file
+    file_pol = tmp_path / 'temp_coastlines.pol'
+    lon_min, lon_max, lat_min, lat_max = -68.45, -68.1, 12, 12.35
+    bbox = (lon_min, lat_min, lon_max, lat_max)
+    coastlines_gdf = dfmt.get_coastlines_gdb(bbox=bbox, res='h')
+    polyfile = dfmt.geodataframe_to_PolyFile(coastlines_gdf)
+    polyfile.save(file_pol)
+    
+    # read polygon file
+    polyfile_object = hcdfm.PolyFile(file_pol)
+
+    # gdf containing shapely.LineString geometries
+    gdf_polyfile = dfmt.PolyFile_to_geodataframe_linestrings(polyfile_object,crs=None)
+    
+    polyfile = dfmt.geodataframe_to_PolyFile(gdf_polyfile)
+    assert isinstance(polyfile, hcdfm.PolyFile)
+
+
+@pytest.fixture(scope='session')
+def bnd_gdf():
+    dxy = 0.02
+    crs = 4326
+    lon_min, lon_max, lat_min, lat_max = -68.31, -68.27, 12.10, 12.21
+    mk_object = dfmt.make_basegrid(lon_min, lon_max, lat_min, lat_max, dx=dxy, dy=dxy, crs=crs)
+    bnd_gdf = dfmt.generate_bndpli_cutland(mk=mk_object, res='h', buffer=0.01)
+    return bnd_gdf
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_name_default(bnd_gdf):
+    polyfile_obj = dfmt.geodataframe_to_PolyFile(bnd_gdf)
+    names = [x.metadata.name for x in polyfile_obj.objects]
+    assert names == ['L1', 'L2']
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_name_some(bnd_gdf):
+    polyfile_obj = dfmt.geodataframe_to_PolyFile(bnd_gdf, name="test_model")
+    names = [x.metadata.name for x in polyfile_obj.objects]
+    assert names == ['test_model1', 'test_model2']
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_name_invalidtype(bnd_gdf):
+    with pytest.raises(TypeError) as e:
+        dfmt.geodataframe_to_PolyFile(bnd_gdf, name=None)
+    assert 'name should be a string' in str(e.value)
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_name_incorrect(bnd_gdf):
+    with pytest.raises(ValueError) as e:
+        dfmt.geodataframe_to_PolyFile(bnd_gdf, name='1')
+    assert 'names in polyfile do not all start with a letter' in str(e.value)
+    
+    with pytest.raises(ValueError) as e:
+        dfmt.geodataframe_to_PolyFile(bnd_gdf, name='-')
+    assert 'names in polyfile do not all start with a letter' in str(e.value)
+    
+    with pytest.raises(ValueError) as e:
+        dfmt.geodataframe_to_PolyFile(bnd_gdf, name='')
+    assert 'names in polyfile do not all start with a letter' in str(e.value)
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_namecolumn_some(bnd_gdf):
+    bnd_gdf['name'] = ['test_model1','test_model2']
+    polyfile_obj = dfmt.geodataframe_to_PolyFile(bnd_gdf)
+    names = [x.metadata.name for x in polyfile_obj.objects]
+    assert names == ['test_model1', 'test_model2']
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_namecolumn_name_both(bnd_gdf):
+    # name argument is ignored if name column is provided
+    # not per se desired, but also not completely wrong
+    bnd_gdf['name'] = ['test_model1','test_model2']
+    polyfile_obj = dfmt.geodataframe_to_PolyFile(bnd_gdf, name='dummy')
+    names = [x.metadata.name for x in polyfile_obj.objects]
+    assert names == ['test_model1', 'test_model2']
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_namecolumn_duplicated_names(bnd_gdf):
+    # deliberately giving all polylines the same name
+    bnd_gdf['name'] = 'duplicate_bnd'
+    with pytest.raises(ValueError) as e:
+        dfmt.geodataframe_to_PolyFile(bnd_gdf)
+    assert 'duplicate polyline names found in polyfile' in str(e.value)
+
+    # deliberately giving all polylines the same empty name
+    bnd_gdf['name'] = ''
+    with pytest.raises(ValueError) as e:
+        dfmt.geodataframe_to_PolyFile(bnd_gdf)
+    assert "duplicate polyline names found in polyfile" in str(e.value)
+
+
+@pytest.mark.unittest
+def test_geodataframe_to_PolyFile_namecolumn_numeric_start(bnd_gdf):
+    bnd_gdf['name'] = ['1','2']
+    with pytest.raises(ValueError) as e:
+        dfmt.geodataframe_to_PolyFile(bnd_gdf)
+    assert 'names in polyfile do not all start with a letter' in str(e.value)
+
+
