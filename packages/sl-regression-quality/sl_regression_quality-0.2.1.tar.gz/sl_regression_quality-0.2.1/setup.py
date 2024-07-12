a88a364eeafe0156cf2c35217325953f6e2a2d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:03:44 +0000

    Add package sl-regression-quality/0.2.1/sl_regression_quality-0.2.1.tar.gz

---
+++ packages/sl-regression-quality/sl_regression_quality-0.2.1.tar.gz/sl_regression_quality-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup (
+    name='sl_regression_quality',
+    version='0.2.1',
+    license='MIT',
+    description='simple linear regression quality',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author= 'EdelH, Aplata',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'sl_regression_quality': ['data/*.csv'],
+    },
+    install_requires = ['numpy','pandas','statsmodels','scikit-learn','art'],
+
+    url='https://github.com/aplatag/project_SL_regression_quality.git'
+)
