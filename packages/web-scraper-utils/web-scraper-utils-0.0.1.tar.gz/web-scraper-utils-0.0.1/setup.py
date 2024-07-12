ac581ae466d06f740b9462cc4a0cdcdbc131aa3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:06:45 +0000

    Add package web-scraper-utils/0.0.1/web-scraper-utils-0.0.1.tar.gz

---
+++ packages/web-scraper-utils/web-scraper-utils-0.0.1.tar.gz/web-scraper-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='web-scraper-utils',
+    version='0.0.1',
+    description='Common utility functions for scraping',
+    author='Dennis Ho',
+    author_email='hkhdennis@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+)
