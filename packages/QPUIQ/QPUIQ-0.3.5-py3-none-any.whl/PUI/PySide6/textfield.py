c39bf643b8480761eef1d0c90642361a141b40e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:04:19 +0000

    Add package QPUIQ/0.3.5/QPUIQ-0.3.5-py3-none-any.whl

---
+++ packages/QPUIQ/QPUIQ-0.3.5-py3-none-any.whl/PUI/PySide6/textfield.py
@@ -0,0 +1,44 @@
+from .. import *
+from .base import *
+
+class TextField(QtBaseWidget):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.editing = False
+        self.changed_cb = None
+
+    def update(self, prev):
+        model_value = str(self.model.value)
+        if prev and prev.ui:
+            self.editing = prev.editing
+            self.ui = prev.ui
+            self.ui.focusOutEvent = self.focusOutEvent
+            self.curr_value = prev.curr_value
+            if self.curr_value.set(model_value):
+                self.ui.textChanged.disconnect()
+                self.ui.setText(model_value)
+                self.ui.textChanged.connect(self.on_textchanged)
+        else:
+            self.ui = QtWidgets.QLineEdit()
+            self.ui.origFocusOutEvent = self.ui.focusOutEvent
+            self.ui.focusOutEvent = self.focusOutEvent
+            self.ui.setText(model_value)
+            self.curr_value = Prop(model_value)
+            self.ui.textChanged.connect(self.on_textchanged)
+        super().update(prev)
+
+    def change(self, cb, *args, **kwargs):
+        self.changed_cb = (cb, args, kwargs)
+
+    def on_textchanged(self):
+        node = self.get_node()
+        node.editing = True
+        node.model.value = self.ui.text()
+        if node.changed_cb:
+            node.changed_cb[0](*node.changed_cb[1], **node.changed_cb[2])
+
+    def focusOutEvent(self, event):
+        node = self.get_node()
+        node.editing = False
+        node.ui.origFocusOutEvent(event)
