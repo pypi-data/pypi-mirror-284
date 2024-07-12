143301f81d9a58532b912fd482126e58829ec560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:06:35 +0000

    Add package wyc-pack1/1.0.0/wyc_pack1-1.0.0.tar.gz

---
+++ packages/wyc-pack1/wyc_pack1-1.0.0.tar.gz/wyc_pack1-1.0.0/wyc_pack1/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File     : __init__.py.py
+@Date     : 2024/7/12 上午11:37
+@Author   : wangyicheng
+@Version  : 1.0
+@Desc     : None
+'''
---
+++ packages/wyc-pack1/wyc_pack1-1.0.0.tar.gz/wyc_pack1-1.0.0/wyc_pack1/test1.py
@@ -0,0 +1,9 @@
+
+def print_hello1():
+    print("hello world1")
+
+def print_goodbye1():
+    print("goodbye world1")
+
+if __name__ == '__main__':
+    print_hello1()
