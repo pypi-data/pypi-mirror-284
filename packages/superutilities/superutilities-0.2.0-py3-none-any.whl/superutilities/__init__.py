9b7911ab77f533ff8c115308c882e08792bcf376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:05:43 +0000

    Add package superutilities/0.2.0/superutilities-0.2.0-py3-none-any.whl

---
+++ packages/superutilities/superutilities-0.2.0-py3-none-any.whl/superutilities/__init__.py
@@ -0,0 +1,6 @@
+from .main import ConflictingInputsDetected
+from .main import SanitizeString
+from .main import SInput
+from .main import PermutationCalculator
+from .main import getMedian
+import re
