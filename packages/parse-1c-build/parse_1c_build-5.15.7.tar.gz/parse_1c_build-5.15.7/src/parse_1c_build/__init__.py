74eadd30df5c711b28d7c3fa69e93aa19bbc8350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:03 +0000

    Add package parse-1c-build/5.15.7/parse_1c_build-5.15.7.tar.gz

---
+++ packages/parse-1c-build/parse_1c_build-5.15.7.tar.gz/parse_1c_build-5.15.7/src/parse_1c_build/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from loguru import logger
+
+from parse_1c_build.__about__ import __version__
+from parse_1c_build.build import Builder
+from parse_1c_build.parse import Parser
---
+++ packages/parse-1c-build/parse_1c_build-5.15.7.tar.gz/parse_1c_build-5.15.7/tests/test_base.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+import pytest
+
+from parse_1c_build.base import Processor
+
+
+def test_processor_1():
+    with pytest.raises(Exception) as e:
+        Processor(settings_file_path=Path("tests/data/settings.yaml"))
+        assert e == "There is no GComp in settings"
+
+
+def test_processor_2():
+    with pytest.raises(Exception) as e:
+        Processor(gcomp_file_path=Path(""))  # todo
+        assert e == "GComp does not exist"
