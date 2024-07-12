e5297c632019a6fd6c0c9d148014d93a982208a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:06:26 +0000

    Add package ob-metaflow-stubs/4.5/ob_metaflow_stubs-4.5-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-4.5-py2.py3-none-any.whl/metaflow-stubs/plugins/pypi/__init__.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.7.1+ob(v1)                                                    #
+# Generated on 2024-07-12T00:19:39.769355                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+class MetaflowException(Exception, metaclass=type):
+    def __init__(self, msg = "", lineno = None):
+        ...
+    def __str__(self):
+        ...
+    ...
+
+MAGIC_FILE: str
+
