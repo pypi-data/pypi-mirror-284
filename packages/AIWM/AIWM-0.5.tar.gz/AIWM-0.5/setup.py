185f1915ba75360c65e7ac5c1163ef6646fa5fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:30 +0000

    Add package AIWM/0.5/AIWM-0.5.tar.gz

---
+++ packages/AIWM/AIWM-0.5.tar.gz/AIWM-0.5/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension("Predict", ["AIWM/Predict.py"]),
+]
+
+setup(
+    name='AIWM',
+    version='0.5',
+    packages=['AIWM'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'pmdarima',
+        'statsmodels',
+        'requests',
+    ],
+)
