18e8f8c6869811c5d298a475e7bede8a491b0622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 14:06:26 +0000

    Add package ob-metaflow-stubs/4.5/ob-metaflow-stubs-4.5.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-4.5.tar.gz/ob-metaflow-stubs-4.5/metaflow-stubs/plugins/pypi/pypi_decorator.pyi
@@ -0,0 +1,22 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.7.1+ob(v1)                                                    #
+# Generated on 2024-07-12T00:19:39.798888                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class PyPIStepDecorator(metaflow.decorators.StepDecorator, metaclass=type):
+    def step_init(self, flow, graph, step, decos, environment, flow_datastore, logger):
+        ...
+    ...
+
+class PyPIFlowDecorator(metaflow.decorators.FlowDecorator, metaclass=type):
+    def flow_init(self, flow, graph, environment, flow_datastore, metadata, logger, echo, options):
+        ...
+    ...
+
