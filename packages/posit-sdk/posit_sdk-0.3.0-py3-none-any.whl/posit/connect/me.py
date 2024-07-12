d140f2918df4d97a53888c6b8fac6b8f3690f259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:03:52 +0000

    Add package posit-sdk/0.3.0/posit_sdk-0.3.0-py3-none-any.whl

---
+++ packages/posit-sdk/posit_sdk-0.3.0-py3-none-any.whl/posit/connect/me.py
@@ -0,0 +1,23 @@
+import requests
+
+from . import urls
+
+from .config import Config
+from .users import User
+
+
+def get(config: Config, session: requests.Session) -> User:
+    """
+    Gets the current user.
+
+    Args:
+        config (Config): The configuration object containing the URL.
+        session (requests.Session): The session object used for making HTTP requests.
+
+    Returns
+    -------
+        User: The current user.
+    """
+    url = urls.append(config.url, "v1/user")
+    response = session.get(url)
+    return User(config, session, **response.json())
