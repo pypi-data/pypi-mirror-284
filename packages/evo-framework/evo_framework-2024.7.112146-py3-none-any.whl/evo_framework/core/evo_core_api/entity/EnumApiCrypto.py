29fac19a317bc93af65245440c9481f30c93f842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:11 +0000

    Add package evo-framework/2024.7.112146/evo_framework-2024.7.112146-py3-none-any.whl

---
+++ packages/evo-framework/evo_framework-2024.7.112146-py3-none-any.whl/evo_framework/core/evo_core_api/entity/EnumApiCrypto.py
@@ -0,0 +1,8 @@
+#========================================================================================================================================
+# CyborgAI CC BY-NC-ND 4.0 Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 Internation	https://github.com/cyborg-ai-git # 
+#========================================================================================================================================
+
+from enum import Enum
+class EnumApiCrypto(Enum):
+	ECC = 0
+	PQ = 1
