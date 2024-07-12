72fe83ac91d06f823b0040bf6a86845e0c9a8661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 16:04:21 +0000

    Add package jsoniq-jupyterlab-extension/0.1.0/jsoniq_jupyterlab_extension-0.1.0-py3-none-any.whl

---
+++ packages/jsoniq-jupyterlab-extension/jsoniq_jupyterlab_extension-0.1.0-py3-none-any.whl/jsoniq_jupyterlab_extension/jsoniq_lsp.py
@@ -0,0 +1,45 @@
+import shutil
+import pathlib
+import os
+import subprocess
+import sys
+from datetime import datetime
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    filename="/tmp/jsoniq-language-server-entrypoint.log",
+    level=logging.INFO,
+)
+cwd = os.getcwd()
+
+NODE_LOCATION = (
+    shutil.which("node") or shutil.which("node.exe") or shutil.which("node.cmd")
+)
+NODE = str(pathlib.Path(NODE_LOCATION).resolve())
+PATH_TO_BIN_JS = str(
+    (
+        pathlib.Path(__file__).parent.parent
+        / "node_modules"
+        / "jsoniq-language-server"
+        / "dest"
+        / "server.js"
+    ).resolve()
+)
+
+
+def main():
+    # Run the language server
+    logging.info(f"Starting language server...")
+    server_proc = subprocess.Popen(
+        [NODE, PATH_TO_BIN_JS, "--stdio", *sys.argv[1:]],
+        stdin=sys.stdin,
+        stdout=sys.stdout,
+    )
+    logging.info(f"Running language server...")
+    logging.info(f"Process pid: {server_proc.pid}")
+    sys.exit(server_proc.wait())
+
+
+if __name__ == "__main__":
+    main()
