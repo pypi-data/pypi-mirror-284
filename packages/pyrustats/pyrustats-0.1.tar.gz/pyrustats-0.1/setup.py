bfba80b103c09995233893d373dfbeff9bc2bbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 07:04:17 +0000

    Add package pyrustats/0.1/pyrustats-0.1.tar.gz

---
+++ packages/pyrustats/pyrustats-0.1.tar.gz/pyrustats-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pyrustats',
+      version='0.1',
+      description='Statistics: Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Ruukha',
+      author_email = 'ruukha3@gmail.com',
+    zip_safe=False)
