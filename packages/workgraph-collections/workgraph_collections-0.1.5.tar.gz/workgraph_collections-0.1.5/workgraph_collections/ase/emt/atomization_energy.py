743df6d7a48408365c090fc82e07be3554dbc770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 07:04:00 +0000

    Add package workgraph-collections/0.1.5/workgraph_collections-0.1.5.tar.gz

---
+++ packages/workgraph-collections/workgraph_collections-0.1.5.tar.gz/workgraph_collections-0.1.5/workgraph_collections/ase/emt/atomization_energy.py
@@ -0,0 +1,34 @@
+from aiida_workgraph import task, WorkGraph
+from ase import Atoms
+
+
+@task()
+def calc_atomization_energy(molecule: Atoms, molecule_output: dict, atom_output: dict):
+    energy = atom_output["energy"] * len(molecule) - molecule_output["energy"]
+    return energy
+
+
+@task.graph_builder(
+    outputs=[{"name": "result", "from": "calc_atomization_energy.result"}]
+)
+def atomization_energy(atom: Atoms = None, molecule: Atoms = None):
+    """Workgraph for atomization energy calculation using EMT calculator."""
+    from .base import emt_calculator
+
+    wg = WorkGraph("Atomization energy")
+    pw_atom = wg.tasks.new(
+        "PythonJob", function=emt_calculator, name="scf_atom", atoms=atom
+    )
+    pw_mol = wg.tasks.new(
+        "PythonJob", function=emt_calculator, name="scf_mol", atoms=molecule
+    )
+    # create the task to calculate the atomization energy
+    wg.tasks.new(
+        "PythonJob",
+        function=calc_atomization_energy,
+        name="calc_atomization_energy",
+        molecule=molecule,
+        atom_output=pw_atom.outputs["results"],
+        molecule_output=pw_mol.outputs["results"],
+    )
+    return wg
