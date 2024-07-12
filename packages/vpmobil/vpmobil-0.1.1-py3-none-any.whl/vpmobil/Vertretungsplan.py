84c54efb74019f6e85a6f29701ecbbe88c044cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 03:05:13 +0000

    Add package vpmobil/0.1.1/vpmobil-0.1.1-py3-none-any.whl

---
+++ packages/vpmobil/vpmobil-0.1.1-py3-none-any.whl/vpmobil/Vertretungsplan.py
@@ -0,0 +1,46 @@
+from datetime import datetime, date
+#Modules shall be imported as a 3-letter code
+import requests as WEB
+
+from .Exceptions import FetchingError
+from .VpDay import VpDay
+
+# ╭──────────────────────────────────────────────────────────────────────────────────────────╮
+# │                                    Vertretungsplan                                       │ 
+# ╰──────────────────────────────────────────────────────────────────────────────────────────╯
+
+class Vertretungsplan():
+    """
+    Enthält die notwendigen Daten um auf einen stundenplan24.de-Vertretungsplan zuzugreifen
+
+    #### Argumente
+    - schulnummer (int)
+    - benutzername (str)
+    - passwort (str)
+
+    #### Attribute & Methoden
+    - .fetch()
+    """
+
+    def __init__(self, schulnummer: int, benutzername: str, passwort: str):
+        self.schulnummer = schulnummer
+        self.benutzername = benutzername
+        self.passwort = passwort
+        self.webpath = f"http://{benutzername}:{passwort}@stundenplan24.de/{schulnummer}/mobil/mobdaten/"
+
+    def fetch(self, date: int | date = datetime.today().date()):
+        """
+        Ruft alle Daten für einen bestimmten Tag ab und schreibt sie in ein VpDay-Objekt
+        Ein Error wird erhoben, wenn für den angegebenen Tag keine Daten verfügbar sind.
+
+        - date: Bestimmter Tag; Integer im yyyymmdd-Format oder date-Objekt. Leer lassen, um das heutige Datum abzurufen
+        """
+
+        date = date if isinstance(date, int) else date.strftime('%Y%m%d')
+        uri = f"{self.webpath}PlanKl{date}.xml"  
+        response = WEB.get(uri)
+
+        if response.status_code != 200:
+            raise FetchingError(f"Die Daten für das Datum {date} konnten nicht abgerufen werden. Statuscode: {response.status_code}")
+
+        return VpDay(xmldata=response.content)
