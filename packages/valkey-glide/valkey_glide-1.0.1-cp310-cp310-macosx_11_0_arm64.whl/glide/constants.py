ce6543d0bd34df89578089c6d019f6a10e842c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:05 +0000

    Add package valkey-glide/1.0.1/valkey_glide-1.0.1-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/valkey-glide/valkey_glide-1.0.1-cp310-cp310-macosx_11_0_arm64.whl/glide/constants.py
@@ -0,0 +1,66 @@
+# Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+from typing import Dict, List, Literal, Mapping, Optional, Set, TypeVar, Union
+
+from glide.protobuf.command_request_pb2 import CommandRequest
+from glide.protobuf.connection_request_pb2 import ConnectionRequest
+from glide.routes import ByAddressRoute, RandomNode, SlotIdRoute, SlotKeyRoute
+
+OK: str = "OK"
+DEFAULT_READ_BYTES_SIZE: int = pow(2, 16)
+# Typing
+T = TypeVar("T")
+TOK = Literal["OK"]
+TResult = Union[
+    TOK,
+    str,
+    List[List[str]],
+    int,
+    None,
+    Dict[str, T],
+    Mapping[str, "TResult"],
+    float,
+    Set[T],
+    List[T],
+    bytes,
+    Dict[bytes, "TResult"],
+    Mapping[bytes, "TResult"],
+]
+TRequest = Union[CommandRequest, ConnectionRequest]
+# When routing to a single node, response will be T
+# Otherwise, response will be : {Address : response , ... } with type of Dict[str, T].
+TClusterResponse = Union[T, Dict[bytes, T]]
+TSingleNodeRoute = Union[RandomNode, SlotKeyRoute, SlotIdRoute, ByAddressRoute]
+# When specifying legacy path (path doesn't start with `$`), response will be T
+# Otherwise, (when specifying JSONPath), response will be List[Optional[T]].
+# For more information, see: https://redis.io/docs/data-types/json/path/ .
+TJsonResponse = Union[T, List[Optional[T]]]
+TEncodable = Union[str, bytes]
+TFunctionListResponse = List[
+    Mapping[
+        bytes,
+        Union[bytes, List[Mapping[bytes, Union[bytes, Set[bytes]]]]],
+    ]
+]
+TFunctionStatsResponse = Mapping[
+    bytes,
+    Union[
+        None,
+        Mapping[
+            bytes, Union[Mapping[bytes, Mapping[bytes, int]], bytes, int, List[bytes]]
+        ],
+    ],
+]
+
+TXInfoStreamResponse = Mapping[
+    bytes, Union[bytes, int, Mapping[bytes, Optional[List[List[bytes]]]]]
+]
+TXInfoStreamFullResponse = Mapping[
+    bytes,
+    Union[
+        bytes,
+        int,
+        Mapping[bytes, List[List[bytes]]],
+        List[Mapping[bytes, Union[bytes, int, List[List[Union[bytes, int]]]]]],
+    ],
+]
