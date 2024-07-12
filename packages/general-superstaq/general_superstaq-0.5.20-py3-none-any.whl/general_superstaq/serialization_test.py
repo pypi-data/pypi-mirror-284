267a7da269dcb549fc406204ffc29a049ae27bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 05:04:52 +0000

    Add package general-superstaq/0.5.20/general_superstaq-0.5.20-py3-none-any.whl

---
+++ packages/general-superstaq/general_superstaq-0.5.20-py3-none-any.whl/general_superstaq/serialization_test.py
@@ -0,0 +1,9 @@
+# pylint: disable=missing-function-docstring,missing-class-docstring
+import general_superstaq as gss
+
+
+def test_serialization() -> None:
+    obj = {"object": ["to", "serialize"]}
+    serialized_obj = gss.serialization.serialize(obj)
+    assert isinstance(serialized_obj, str)
+    assert gss.serialization.deserialize(serialized_obj) == obj
