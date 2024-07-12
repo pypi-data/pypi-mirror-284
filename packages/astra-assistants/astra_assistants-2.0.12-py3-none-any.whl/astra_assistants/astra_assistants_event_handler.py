87f404694a2ab52b364211e0c63ba8e7cd244e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 06:04:24 +0000

    Add package astra-assistants/2.0.12/astra_assistants-2.0.12-py3-none-any.whl

---
+++ packages/astra-assistants/astra_assistants-2.0.12-py3-none-any.whl/astra_assistants/astra_assistants_event_handler.py
@@ -0,0 +1,54 @@
+import json
+import logging
+from openai.lib.streaming import AssistantEventHandler
+from typing_extensions import override
+from pydantic import BaseModel
+
+
+class AstraEventHandler(AssistantEventHandler):
+    def __init__(self, client):
+        super().__init__()  # Initialize the base class
+        self.client = client
+        self.logger = logging.getLogger(__name__)
+        self.tools = {}
+        self.tool_outputs = []
+        self.stream = None
+
+    def register_tool(self, tool):
+        self.tools[tool.to_function()['function']['name']] = tool
+
+    @override
+    def on_tool_call_done(self, tool_call):
+        self.logger.info(tool_call)
+        self.logger.info(f'arguments: {tool_call.function.arguments}')
+        results = self.run_tool(tool_call)
+        self.tool_outputs.append({
+            'tool_call_id': tool_call.id,
+            'output': results
+        })
+
+        self.stream = self.client.beta.threads.runs.submit_tool_outputs_stream(
+            thread_id=self._AssistantEventHandler__current_run.thread_id,
+            run_id=self._AssistantEventHandler__current_run.id,
+            tool_outputs=self.tool_outputs,
+            event_handler=AssistantEventHandler()
+        )
+        print("got the stream")
+
+    def run_tool(self, tool_call):
+        tool_name = tool_call.function.name
+        if tool_name in self.tools:
+            try:
+                tool = self.tools[tool_name]
+                arguments = json.loads(tool_call.function.arguments)
+                model: BaseModel = tool.get_model()
+                if issubclass(model, BaseModel):
+                    arguments = model(**arguments)
+                results = tool.call(arguments)
+                return results
+            except Exception as e:
+                self.logger.error(f"Error running tool {tool_name}: {e}")
+                raise e
+        else:
+            self.logger.error(f"Tool {tool_name} not found.")
+            return None
