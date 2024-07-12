85d5c2d2bc358f0608f9801fc83ed8d8937b44c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:04:34 +0000

    Add package ParTree/0.0.3/ParTree-0.0.3.tar.gz

---
+++ packages/ParTree/ParTree-0.0.3.tar.gz/ParTree-0.0.3/ParTree/algorithms/server_utils.py
@@ -0,0 +1,34 @@
+import json
+
+
+def eval_node(node):
+    if type(node) is list:
+        return [eval_node(el) for el in node]
+    else:
+        value_type = node["type"]
+        value = node["value"]
+
+        if value_type == 'bool':
+            value = value == 'True'
+        elif value_type in ('int', 'longlong', 'int64'):
+            value = int(value)
+        elif value_type in ('float', 'float64'):
+            value = float(value)
+        else:
+            value = value
+
+        return value
+
+
+def eval_response(res_txt):
+    res = json.loads(res_txt)
+
+    return_value = {
+        "labels": [int(x) for x in res["labels"]],
+        "rules": []
+    }
+
+    for rule_node in res["rules"]:
+        return_value["rules"].append(eval_node(rule_node))
+
+    return return_value
