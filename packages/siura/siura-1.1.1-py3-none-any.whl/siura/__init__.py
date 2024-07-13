bc16aec7abf03c86fa756d457189f1630e11f8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 06:04:58 +0000

    Add package siura/1.1.1/siura-1.1.1-py3-none-any.whl

---
+++ packages/siura/siura-1.1.1-py3-none-any.whl/siura/__init__.py
@@ -0,0 +1,12 @@
+from .base import Base
+from .auth import Auth
+from .meli import Meli
+from .template import Template
+
+__all__ = [
+    'BaseClass1',
+    'BaseClass2',
+    'AuthClass',
+    'MeliClass',
+    'TemplateClass',
+]
