91c1ecfc8356f4bd6ffe791b5e20d16498c73167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 14:04:09 +0000

    Add package s3func/0.5.2/s3func-0.5.2-py3-none-any.whl

---
+++ packages/s3func/s3func-0.5.2-py3-none-any.whl/s3func/__init__.py
@@ -0,0 +1,7 @@
+"""Simple functions for working with S3"""
+from s3func.s3 import S3Session
+from s3func.b2 import B2Session
+from s3func.http_url import HttpSession
+from s3func import s3, http_url, utils, b2
+
+__version__ = '0.5.2'
