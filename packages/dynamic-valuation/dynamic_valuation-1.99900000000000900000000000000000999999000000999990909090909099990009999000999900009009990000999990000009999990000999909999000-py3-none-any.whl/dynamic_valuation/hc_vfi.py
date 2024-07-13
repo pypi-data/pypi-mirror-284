00e6b8bbacd36cea2fa3fc3074b50cfac6b66a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:05:05 +0000

    Add package dynamic-valuation/1.99900000000000900000000000000000999999000000999990909090909099990009999000999900009009990000999990000009999990000999909999000/dynamic_valuation-1.99900000000000900000000000000000999999000000999990909090909099990009999000999900009009990000999990000009999990000999909999000-py3-none-any.whl

---
+++ packages/dynamic-valuation/dynamic_valuation-1.99900000000000900000000000000000999999000000999990909090909099990009999000999900009009990000999990000009999990000999909999000-py3-none-any.whl/dynamic_valuation/hc_vfi.py
@@ -0,0 +1,4 @@
+from dynamic_valuation import*
+#what I'm going to accomplish here is run a vfi where the policy iterations are done in 1 step through my method
+def hcvfi(cl):
+	return vfi.vfi_plot(cl)
