47a821dbf5ba249a612e0165c643b3b3a4d0dca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:04:37 +0000

    Add package per/0.0.0/per-0.0.0.tar.gz

---
+++ packages/per/per-0.0.0.tar.gz/per-0.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="per",
+    version="0.0.0",
+    author="false",
+    author_email="false@false.false",
+    description="false",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+    python_requires=">=3.6",
+)
