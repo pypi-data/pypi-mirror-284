8b29a945d24fc8e8cf9d3798c635aaad11c092a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:04:48 +0000

    Add package msteams-tickler/1.0.0/msteams_tickler-1.0.0.tar.gz

---
+++ packages/msteams-tickler/msteams_tickler-1.0.0.tar.gz/msteams_tickler-1.0.0/src/msteams_tickler/classic/config.py
@@ -0,0 +1,1 @@
+DEFAULT_COOKIES_PATH = "~/Library/Application Support/Microsoft/Teams/Cookies"
---
+++ packages/msteams-tickler/msteams_tickler-1.0.0.tar.gz/msteams_tickler-1.0.0/src/msteams_tickler/config.py
@@ -0,0 +1,1 @@
+DEFAULT_COOKIES_PATH = "~/Library/Containers/com.microsoft.teams2/Data/Library/Cookies/Cookies.binarycookies"
