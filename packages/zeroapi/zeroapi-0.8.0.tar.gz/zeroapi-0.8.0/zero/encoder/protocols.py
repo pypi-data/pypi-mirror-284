9ceb604da259f112d9a3ae5736f1ab152ca7783c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 06:04:40 +0000

    Add package zeroapi/0.8.0/zeroapi-0.8.0.tar.gz

---
+++ packages/zeroapi/zeroapi-0.8.0.tar.gz/zeroapi-0.8.0/zero/encoder/protocols.py
@@ -0,0 +1,13 @@
+from typing import Any, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class Encoder(Protocol):  # pragma: no cover
+    def encode(self, data: Any) -> bytes:
+        ...
+
+    def decode(self, data: bytes) -> Any:
+        ...
+
+    def decode_type(self, data: bytes, typ: Any) -> Any:
+        ...
