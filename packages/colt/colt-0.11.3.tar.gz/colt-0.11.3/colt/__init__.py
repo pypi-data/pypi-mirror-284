2b52a6d0b32fb57fc3c9acbcdab25be2406d9063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 05:05:25 +0000

    Add package colt/0.11.3/colt-0.11.3.tar.gz

---
+++ packages/colt/colt-0.11.3.tar.gz/colt-0.11.3/colt/__init__.py
@@ -0,0 +1,58 @@
+from importlib.metadata import version
+from typing import Any, Callable, Optional, Type, TypeVar, Union, overload
+
+from colt.builder import ColtBuilder
+from colt.default_registry import DefaultRegistry
+from colt.lazy import Lazy  # noqa: F401
+from colt.registrable import Registrable  # noqa: F401
+from colt.utils import import_modules  # noqa: F401
+
+__version__ = version("colt")
+
+T = TypeVar("T")
+
+
+def register(
+    name: str,
+    constructor: Optional[str] = None,
+    exist_ok: bool = False,
+) -> Callable[[Type[T]], Type[T]]:
+    def decorator(cls: Type[T]) -> Type[T]:
+        DefaultRegistry.register(name, constructor, exist_ok)(cls)
+        return cls
+
+    return decorator
+
+
+@overload
+def build(
+    config: Any,
+    cls: Type[T],
+    *,
+    typekey: Optional[str] = ...,
+    argskey: Optional[str] = ...,
+    strict: bool = ...,
+) -> T: ...
+
+
+@overload
+def build(
+    config: Any,
+    cls: None = ...,
+    *,
+    typekey: Optional[str] = ...,
+    argskey: Optional[str] = ...,
+    strict: bool = ...,
+) -> Any: ...
+
+
+def build(
+    config: Any,
+    cls: Optional[Type[T]] = None,
+    *,
+    typekey: Optional[str] = None,
+    argskey: Optional[str] = None,
+    strict: bool = False,
+) -> Union[T, Any]:
+    builder = ColtBuilder(typekey, argskey, strict)
+    return builder(config, cls)
