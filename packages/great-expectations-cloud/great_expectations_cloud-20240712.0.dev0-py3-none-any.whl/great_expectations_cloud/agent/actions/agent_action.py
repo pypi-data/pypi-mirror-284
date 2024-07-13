729de700224fb0d3d954ac488826e128e2c9da98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 05:06:27 +0000

    Add package great-expectations-cloud/20240712.0.dev0/great_expectations_cloud-20240712.0.dev0-py3-none-any.whl

---
+++ packages/great-expectations-cloud/great_expectations_cloud-20240712.0.dev0-py3-none-any.whl/great_expectations_cloud/agent/actions/agent_action.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import TYPE_CHECKING, Generic, Sequence, TypeVar
+
+from pydantic.v1 import BaseModel
+
+from great_expectations_cloud.agent.models import CreatedResource, Event
+
+if TYPE_CHECKING:
+    from great_expectations.data_context import CloudDataContext
+
+
+class ActionResult(BaseModel):
+    id: str
+    type: str
+    created_resources: Sequence[CreatedResource]
+
+
+_EventT = TypeVar("_EventT", bound=Event)
+
+
+class AgentAction(Generic[_EventT]):
+    def __init__(self, context: CloudDataContext):
+        self._context = context
+
+    @abstractmethod
+    def run(self, event: _EventT, id: str) -> ActionResult: ...
