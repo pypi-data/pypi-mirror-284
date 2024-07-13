76eb22667dd6d37a6f8d4483289cc008acb7b7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:04:39 +0000

    Add package satcompression/0.0.1/satcompression-0.0.1.tar.gz

---
+++ packages/satcompression/satcompression-0.0.1.tar.gz/satcompression-0.0.1/satcompression/__init__.py
@@ -0,0 +1,5 @@
+from satcompression.main import (
+    compress_and_encode_image_data,
+    reconstruct_image_data
+)
+from satcompression.image_compressor import quadtree_mtf
---
+++ packages/satcompression/satcompression-0.0.1.tar.gz/satcompression-0.0.1/satcompression/reconstruct_node.py
@@ -0,0 +1,23 @@
+from satcompression.quadtree import QuadTreeNode
+
+class ReconstructNode(QuadTreeNode):
+    """ QuadTree node for reconstructing a compressed image. """
+
+    def __init__(self, position, size, subdivided_flags: list, colors: list):
+        super().__init__(position, size)
+
+        # Hint:
+        # subdivided_flags and colors must be reversed!
+        # (Improves performance, as popping a value off the back is faster than removing from the front)
+
+        self._subdivided_flags = subdivided_flags
+        self._colors = colors
+
+        is_subdivided = subdivided_flags.pop()
+        if is_subdivided:
+            self.subdivide()
+        else:
+            self.color = colors.pop()
+
+    def _create_child_node(self, position, size):
+        return ReconstructNode(position, size, self._subdivided_flags, self._colors)
