6966600e31acfb042295cb2dfb53c1296441fdc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 14:04:24 +0000

    Add package botocore-a-la-carte-lookoutmetrics/1.34.144/botocore-a-la-carte-lookoutmetrics-1.34.144.tar.gz

---
+++ packages/botocore-a-la-carte-lookoutmetrics/botocore-a-la-carte-lookoutmetrics-1.34.144.tar.gz/botocore-a-la-carte-lookoutmetrics-1.34.144/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='botocore-a-la-carte-lookoutmetrics',
+    version="1.34.144",
+    description='lookoutmetrics data for botocore. See the `botocore-a-la-carte` package for more info.',
+    author='Amazon Web Services',
+    url='https://github.com/thejcannon/botocore-a-la-carte',
+    scripts=[],
+    packages=["botocore"],
+    package_data={
+        'botocore': ['data/lookoutmetrics/*/*.json'],
+    },
+    include_package_data=True,
+    license="Apache License 2.0",
+    python_requires=">= 3.7",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ]
+)
