52546fb9b5b32620cc2e02c433d8536296e72c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:04:59 +0000

    Add package mksite/0.0.1/mksite-0.0.1-py3-none-any.whl

---
+++ packages/mksite/mksite-0.0.1-py3-none-any.whl/mksite/cli/__init__.py
@@ -0,0 +1,12 @@
+import click
+
+from mksite.__about__ import __version__
+
+
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
+@click.version_option(version=__version__, prog_name="mksite")
+def mksite() -> None:
+    click.echo("Hello world!")
