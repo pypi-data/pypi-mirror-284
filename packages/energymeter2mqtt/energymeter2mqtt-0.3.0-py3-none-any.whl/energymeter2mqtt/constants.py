d7a8d1ce3de9d668fd01b1084a90600b33a04470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:04:41 +0000

    Add package energymeter2mqtt/0.3.0/energymeter2mqtt-0.3.0-py3-none-any.whl

---
+++ packages/energymeter2mqtt/energymeter2mqtt-0.3.0-py3-none-any.whl/energymeter2mqtt/constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+CLI_EPILOG = 'Project Homepage: https://github.com/jedie/energymeter2mqtt'
+
+
+SETTINGS_DIR_NAME = 'energymeter2mqtt'
+SETTINGS_FILE_NAME = 'energymeter2mqtt'
+
+BASE_PATH = Path(__file__).parent
