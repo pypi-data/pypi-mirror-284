1ebac221dfcc9263addc513b87882fdf7aa26742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:04:35 +0000

    Add package phaistos/0.2.0/phaistos-0.2.0.tar.gz

---
+++ packages/phaistos/phaistos-0.2.0.tar.gz/phaistos-0.2.0/docs/noauto.md
@@ -0,0 +1,81 @@
+## Manual manifest loading
+
+There is an option to disable automatic schema discovery to then
+load the schema manifests manually, either with data read from a file
+or by constructing the schema directly in the code.
+
+To do this, set the `PHAISTOS__DISABLE_SCHEMA_DISCOVERY` environment variable
+to any value.
+
+```bash
+export PHAISTOS__DISABLE_SCHEMA_DISCOVERY=1
+```
+
+This will prevent the automatic schema discovery during the first initialization
+of the `Manager` class. Then new schemas can be inserted manually, using
+`load_schema` method.
+
+Here is an example of how you can load a schema manifest from a file:
+
+```python
+from phaistos import Manager
+
+# Initialize the manager
+manager = Manager.start()
+
+# Create schema manually
+
+schema = {
+    "version": "v1",
+    "description": "A schema for a person",
+    "name": "Person",
+    "properties": {
+        "name": {
+            "type": "str",
+            "description": "The name of the person"
+        },
+        "age": {
+            "type": "int",
+            "description": "The age of the person",
+            "manager": 'if value < 18: raise ValueError("The age must be at least 18")'
+        },
+        "email": {
+            "type": "str",
+            "description": "The email of the person"
+        }
+    }
+}
+
+# Load the schema - this will return the name of the schema
+# that can be used in the validate method
+schema_name = manager.load_schema(schema)
+
+# Validate data against the schema
+data = {
+    "name": "John Doe",
+    "age": 30,
+    "email": "xxx@gmail.com"
+}
+
+# Validate the data against the schema
+result = manager.validate(
+    data=data,
+    schema=schema_name
+)
+```
+
+In this example, we first initialize the `Manager` class and then load a schema
+manually. The schema is a dictionary that describes the schema manifest and
+adheres to the `SchemaInputFile` structure:
+
+**phaistos.typings.SchemaInputFile**
+
+::: phaistos.typings.SchemaInputFile
+
+**phaistos.typings.RawSchemaProperty**
+
+::: phaistos.typings.RawSchemaProperty
+
+The `load_schema` method is used to load the schema into the `Manager` object,
+which can be then used as a target for `validate` method. The `schema`
+argument **must match** the `name` field in the schema manifest (here: `Person`).
---
+++ packages/phaistos/phaistos-0.2.0.tar.gz/phaistos-0.2.0/phaistos/manager.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+import logging
+import os
+import typing
+import yaml
+
+
+from phaistos.transpiler import Transpiler
+from phaistos.typings import (
+    SchemaInputFile,
+    ValidationResults
+)
+from phaistos.schema import TranspiledSchema, SchemaInstancesFactory
+from phaistos.consts import DISCOVERY_EXCEPTIONS, MANAGER_LOGGER
+from phaistos.exceptions import SchemaLoadingException
+
+
+class Manager:
+    _schemas: dict[str, SchemaInstancesFactory] = {}
+    logger: typing.ClassVar[logging.Logger] = MANAGER_LOGGER
+
+    __instance: typing.ClassVar[Manager | None] = None
+    __started: typing.ClassVar[bool] = False
+    __last_used_schemas_dir: typing.ClassVar[str] = ''
+
+    def validate(self, data: dict, schema: str) -> ValidationResults:
+        self.logger.info(f'Validating data against schema: {schema}')
+        return self.get_factory(schema).validate(data)
+
+    @classmethod
+    def start(cls) -> Manager:
+        if cls.__started and cls.__last_used_schemas_dir != os.environ.get('PHAISTOS__SCHEMA_PATH', ''):
+            cls.logger.info('Schema path has changed. Reloading schemas.')
+            cls.__instance = None
+            cls.__started = False
+        if not cls.__instance:
+            cls.logger.info('Starting Phaistos manager!')
+            cls.__started = True
+            cls.__last_used_schemas_dir = os.environ.get('PHAISTOS__SCHEMA_PATH', '')
+            cls.__instance = cls()
+        return cls.__instance
+
+    def __init__(self) -> None:
+        if not self.__started:
+            raise RuntimeError(
+                'Validator must be started using Manager.start()'
+            )
+        if not os.environ.get('PHAISTOS__DISABLE_SCHEMA_DISCOVERY'):
+            self._schemas = self.get_available_schemas()
+
+    def get_factory(self, name: str) -> SchemaInstancesFactory:
+        """
+        Get a schema factory by name
+
+        Args:
+            name (str): The name of the schema
+
+        Returns:
+            SchemaInstancesFactory: The schema factory, that can be used to validate data and create instances of the model
+        """
+        if name not in self._schemas:
+            raise SchemaLoadingException(
+                f'Schema {name} not found'
+            )
+        return self._schemas[name]
+
+    def get_available_schemas(self, path: str = '') -> dict[str, SchemaInstancesFactory]:
+        discovered_schemas = getattr(self, '_schemas', {})
+        try:
+            schemas_dir = path or os.environ['PHAISTOS__SCHEMA_PATH']
+            for schema in self.__discover_schemas(schemas_dir):
+                discovered_schemas[schema.transpilation_name] = SchemaInstancesFactory(
+                    name=schema.transpilation_name,
+                    _model=schema
+                )
+        except tuple(DISCOVERY_EXCEPTIONS.keys()) as schema_discovery_error:
+            self.logger.error(
+                DISCOVERY_EXCEPTIONS.get(type(schema_discovery_error), f'Error while discovering schemas: {schema_discovery_error}')
+            )
+            raise schema_discovery_error
+
+        self.logger.info(
+            f'Available schemas: {", ".join(discovered_schemas.keys())}'
+        )
+        return discovered_schemas
+
+    def __discover_schemas(self, target_path: str) -> list[type[TranspiledSchema]]:
+        self.logger.info(f'Discovering schemas in: {target_path}')
+        schemas: list[type[TranspiledSchema]] = []
+        for schema in os.listdir(target_path):
+            if schema.startswith('_'):
+                continue
+
+            schema_path = f'{target_path}/{schema}'
+            if not os.path.isdir(schema_path):
+                self.logger.info(f'Importing schema: {schema_path}')
+
+                with open(schema_path, 'r', encoding='utf-8') as schema_file:
+                    schema_data = yaml.safe_load(schema_file)
+                    self.load_schema(schema_data)
+                continue
+
+            nested_schemas = self.__discover_schemas(schema_path)
+            schemas.extend(nested_schemas)
+        return schemas
+
+    def load_schema(self, schema: SchemaInputFile) -> str:
+        self.logger.info(f'Loading schema: {schema["name"]}')
+        schema_class = Transpiler.make_schema(schema)
+        self._schemas[schema['name']] = SchemaInstancesFactory(
+            name=schema_class.transpilation_name,
+            _model=schema_class
+        )
+        return schema_class.transpilation_name
