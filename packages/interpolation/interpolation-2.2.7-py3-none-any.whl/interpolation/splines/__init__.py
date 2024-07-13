c0252313eda9027180f266b47e95b18e6a2a0a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 05:06:28 +0000

    Add package interpolation/2.2.7/interpolation-2.2.7-py3-none-any.whl

---
+++ packages/interpolation/interpolation-2.2.7-py3-none-any.whl/interpolation/splines/__init__.py
@@ -0,0 +1,42 @@
+from .splines import CubicSpline, CubicSplines
+from .multilinear import LinearSpline, LinearSplines
+from .eval_splines import options, eval_linear, eval_cubic, eval_spline
+from .prefilter_cubic import filter_cubic, prefilter
+from .option_types import options as extrap_options
+
+import numba
+import numpy as np
+
+
+# dummy functions
+def UCGrid(*args):
+    tt = numba.typeof((10.0, 1.0, 1))
+    for a in args:
+        assert numba.typeof(a) == tt
+        min, max, n = a
+        assert min < max
+        assert n > 1
+
+    return tuple(args)
+
+
+def CGrid(*args):
+    tt = numba.typeof((10.0, 1.0, 1))
+    for a in args:
+        if isinstance(a, np.ndarray):
+            assert a.ndim == 1
+            assert a.shape[0] >= 2
+        elif numba.typeof(a) == tt:
+            min, max, n = a
+            assert min < max
+            assert n > 1
+        else:
+            raise Exception(f"Unknown dimension specification: {a}")
+
+    return tuple(args)
+
+
+def nodes(grid):
+    from interpolation.cartesian import mlinspace
+
+    return mlinspace([g[0] for g in grid], [g[1] for g in grid], [g[2] for g in grid])
