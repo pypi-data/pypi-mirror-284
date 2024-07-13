7d12d776a97dfd87c03b9b58778efc5aa52c6010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 05:05:42 +0000

    Add package x-chrismo/1.1/x_chrismo-1.1.tar.gz

---
+++ packages/x-chrismo/x_chrismo-1.1.tar.gz/x_chrismo-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='x_chrismo',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['x_chrismo'],
+      author = 'Ayo Mosaku',
+      zip_safe=False)
