4fcb57fb96e8eb8e7180984792eddadc047bbbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 14:04:17 +0000

    Add package dynamic-valuation/1.9990000000000090000000000000000099999900000099999090909090909999000999900099990000900999000099999000000999999000099990999900099999900099900099/dynamic_valuation-1.9990000000000090000000000000000099999900000099999090909090909999000999900099990000900999000099999000000999999000099990999900099999900099900099.tar.gz

---
+++ packages/dynamic-valuation/dynamic_valuation-1.9990000000000090000000000000000099999900000099999090909090909999000999900099990000900999000099999000000999999000099990999900099999900099900099.tar.gz/dynamic_valuation-1.9990000000000090000000000000000099999900000099999090909090909999000999900099990000900999000099999000000999999000099990999900099999900099900099/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dynamic_valuation",
+    version="1.9990000000000090000000000000000099999900000099999090909090909999000999900099990000900999000099999000000999999000099990999900099999900099900099",
+    author="Eric Larson",
+    author_email="ericl3@illinois.edu",
+    description="Find the present value of a capital asset stocks and/or\
+    flows subject to an equation of motion.",
+    packages=['dynamic_valuation'],
+    install_requires=["numpy","matplotlib","scipy","pandas"]
+)
