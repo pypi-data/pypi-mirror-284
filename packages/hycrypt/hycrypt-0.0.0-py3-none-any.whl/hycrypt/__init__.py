9f4f524e60e4cf747d83030def7128a7ea73509b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 05:06:23 +0000

    Add package hycrypt/0.0.0/hycrypt-0.0.0-py3-none-any.whl

---
+++ packages/hycrypt/hycrypt-0.0.0-py3-none-any.whl/hycrypt/__init__.py
@@ -0,0 +1,11 @@
+# hycrypt/__init__.py
+# Created on Monday, 8th July 2024 1:32:08 pm
+# Author: Sira Pornsiriprasert <code@psira.me>
+# 
+# Last modified on Saturday, 13th July 2024 2:14:55 am
+# By Sira Pornsiriprasert <code@psira.me>
+# 
+# The 3-Clause BSD License
+# hycrypt Copyright 2024 Sira Pornsiriprasert
+
+from . import hycrypt
