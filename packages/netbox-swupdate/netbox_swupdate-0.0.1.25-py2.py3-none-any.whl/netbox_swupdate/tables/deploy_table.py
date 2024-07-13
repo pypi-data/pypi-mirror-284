ead75bd91ada61d887737dc2dff6cade833902b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:05:34 +0000

    Add package netbox-swupdate/0.0.1.25/netbox_swupdate-0.0.1.25-py2.py3-none-any.whl

---
+++ packages/netbox-swupdate/netbox_swupdate-0.0.1.25-py2.py3-none-any.whl/netbox_swupdate/tables/deploy_table.py
@@ -0,0 +1,25 @@
+import django_tables2 as tables
+from netbox.tables import NetBoxTable
+
+from netbox_swupdate.models import Deploy
+
+__all__ = ("DeployTable",)
+
+
+class DeployTable(NetBoxTable):
+    name = tables.Column(linkify=True)
+
+    class Meta:
+        model = Deploy
+        fields = [
+            "pk",
+            "id",
+            "name",
+            "state",
+            "type",
+            "deploy_time",
+            "devices",
+            "software",
+        ]
+        attrs = {"class": "table table-hover table-headings"}
+        default_columns = fields[2:]
