bf485c0a4f8f159866398547a9f4893cff8d9ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:05:09 +0000

    Add package jsonyx/0.0.1/jsonyx-0.0.1.tar.gz

---
+++ packages/jsonyx/jsonyx-0.0.1.tar.gz/jsonyx-0.0.1/jsonyx/_accelerator.pyi
@@ -0,0 +1,22 @@
+# Copyright (C) 2024 Nice Zombies
+"""JSON accelerator."""
+__all__: list[str] = ["DuplicateKey", "make_scanner", "scanstring"]
+
+from collections.abc import Callable
+
+from jsonyx.decoder import JSONDecoder
+from typing_extensions import Any
+
+
+def make_scanner(context: JSONDecoder) -> (
+    Callable[[str, str, int], tuple[Any, int]]
+):
+    """Make JSON scanner."""
+
+
+def scanstring(filename: str, s: str, end: int, /) -> tuple[str, int]:
+    """Scan JSON string."""
+
+
+class DuplicateKey(str):  # noqa: SLOT000
+    """Duplicate key."""
