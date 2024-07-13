3e2c6ca319725f808599e4b1aec286738d23b7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 14:04:32 +0000

    Add package sec-parsers/0.526/sec_parsers-0.526-py3-none-any.whl

---
+++ packages/sec-parsers/sec_parsers-0.526-py3-none-any.whl/sec_parsers/cleaning.py
@@ -0,0 +1,26 @@
+import re
+# Module to clean the text
+
+
+def clean_title(text): #  adjust to format items and parts into standardized format, e.g. no periods
+    text = text.strip()
+    text = re.sub("[^\S \t\n\r\f\v]+",' ',text)
+
+    # remove line breaks
+    text = re.sub('\n', ' ', text)
+
+    # replace multiple spaces with single space
+    text = re.sub('\s+', ' ', text)
+
+    return text
+
+def clean_string_for_style_detection(text):
+    """WIP"""
+    chars_to_remove = ['-','_']
+    # remove chars_to_remove
+    for char in chars_to_remove:
+        text = re.sub(char,'',text)
+
+    text = text.strip()
+    return text
+
