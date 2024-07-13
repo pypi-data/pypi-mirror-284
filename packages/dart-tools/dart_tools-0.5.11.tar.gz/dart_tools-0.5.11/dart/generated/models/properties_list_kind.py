6136014959942a2d956ad16f017ec2fa356a6d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 06:04:39 +0000

    Add package dart-tools/0.5.11/dart_tools-0.5.11.tar.gz

---
+++ packages/dart-tools/dart_tools-0.5.11.tar.gz/dart_tools-0.5.11/dart/generated/models/properties_list_kind.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class PropertiesListKind(str, Enum):
+    CHECKBOX = "Checkbox"
+    DATES = "Dates"
+    DEFAULT_ASSIGNEES = "Default: Assignees"
+    DEFAULT_ATTACHMENTS = "Default: Attachments"
+    DEFAULT_CREATED = "Default: Created"
+    DEFAULT_CREATED_BY = "Default: Created by"
+    DEFAULT_DARTBOARD = "Default: Dartboard"
+    DEFAULT_DATES = "Default: Dates"
+    DEFAULT_DESCRIPTION = "Default: Description"
+    DEFAULT_LAST_UPDATED = "Default: Last updated"
+    DEFAULT_LAST_UPDATED_BY = "Default: Last updated by"
+    DEFAULT_PRIORITY = "Default: Priority"
+    DEFAULT_SIZE = "Default: Size"
+    DEFAULT_STATUS = "Default: Status"
+    DEFAULT_TAGS = "Default: Tags"
+    DEFAULT_TITLE = "Default: Title"
+    MULTISELECT = "Multiselect"
+    NUMBER = "Number"
+    SELECT = "Select"
+    STATUS = "Status"
+    TEXT = "Text"
+    USER = "User"
+
+    def __str__(self) -> str:
+        return str(self.value)
