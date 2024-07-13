e06eaf63707debb8c449907a5b6c5d1d96ebb7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 14:04:26 +0000

    Add package plotstandarddeviation/0.0.1/plotstandarddeviation-0.0.1.tar.gz

---
+++ packages/plotstandarddeviation/plotstandarddeviation-0.0.1.tar.gz/plotstandarddeviation-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Science/Research',
+  'Operating System :: Microsoft :: Windows :: Windows 11',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+  'Natural Language :: English'
+]
+ 
+setup(
+  name='plotstandarddeviation',
+  version='0.0.1',
+  description="Calculates all standard deviations from the mean within a dataset's range.",
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Alexander Levon Schulze',
+  author_email='alexanderlevonschulze@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['standard_deviation','calculator'],
+  packages=find_packages(),
+  install_requires=['numpy', 'pandas'] 
+)
