92c3d8555b1f3c49a07d72b831def2185beddd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 05:06:25 +0000

    Add package dname-code/1.3.0/dname_code-1.3.0.tar.gz

---
+++ packages/dname-code/dname_code-1.3.0.tar.gz/dname_code-1.3.0/dname_code/main.py
@@ -0,0 +1,2 @@
+def hello():
+    print("D. Name Code is successfully publish on PyPi with GitHub Action")
---
+++ packages/dname-code/dname_code-1.3.0.tar.gz/dname_code-1.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION='1.3.0'
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='dname_code',
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+    entry_points={
+        "console_scripts": [
+            "dname-code = dname_code:hello"
+        ],
+    },
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='Daris',
+    author_email='<hei.raymond.2@gmail.com>',
+    description='A simple Python package',
+    # url='https://github.com/dnamecode/simple-python-package',
+)
