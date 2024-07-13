6a5101abf3e3ed32677597a8180b2c4a68a89084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:04:44 +0000

    Add package dsm-services/0.0.6/dsm_services-0.0.6.tar.gz

---
+++ packages/dsm-services/dsm_services-0.0.6.tar.gz/dsm_services-0.0.6/dsm_services/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'DigitalStoreMesh Services'
+__version__ = '0.0.6'
+__author__ = 'DigitalStoreMesh Co.,Ltd'
+__license__ = 'MIT'
+
+VERSION = __version__
+
+from . import nlp
+from . import pdf
+from . import utils
+from . import analyzer
