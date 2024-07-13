922c9dfe4c9633d7d7e6a37d93101604e2e90804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:05:06 +0000

    Add package invoice-pdfgen/1.2.2/invoice_pdfgen-1.2.2-py3-none-any.whl

---
+++ packages/invoice-pdfgen/invoice_pdfgen-1.2.2-py3-none-any.whl/invoice_pdfgen/__init__.py
@@ -0,0 +1,5 @@
+# Import the PDFInvoiceGenerator class from the pdf_invoice_generator module
+from .pdf_invoice_generator import PDFInvoiceGenerator
+
+# List of symbols exported by the package when using `from pdf_invoice import *`
+__all__ = ['PDFInvoiceGenerator']
