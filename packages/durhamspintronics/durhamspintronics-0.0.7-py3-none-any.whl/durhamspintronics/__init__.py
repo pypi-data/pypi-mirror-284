79b4c08153fdc9514b59efbdd3f2a62274ffe19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:04:47 +0000

    Add package durhamspintronics/0.0.7/durhamspintronics-0.0.7-py3-none-any.whl

---
+++ packages/durhamspintronics/durhamspintronics-0.0.7-py3-none-any.whl/durhamspintronics/__init__.py
@@ -0,0 +1,7 @@
+# This program is in the public domain
+# Author: ben Nicholson
+"""
+A collection of instrument control and analysis tools for the Spintronics group at Durham University.
+"""
+
+__version__ = "0.0.7"
