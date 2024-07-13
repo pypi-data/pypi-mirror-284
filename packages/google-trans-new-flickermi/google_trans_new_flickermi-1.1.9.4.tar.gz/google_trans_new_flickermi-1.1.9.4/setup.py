770e9d854ab2045c6ad232c40768b520852d959e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:04:59 +0000

    Add package google-trans-new-flickermi/1.1.9.4/google_trans_new_flickermi-1.1.9.4.tar.gz

---
+++ packages/google-trans-new-flickermi/google_trans_new_flickermi-1.1.9.4.tar.gz/google_trans_new_flickermi-1.1.9.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Flicker
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "google_trans_new_flickermi",
+    version = "1.1.9.4",
+    description = "A free and unlimited python tools for google translate api.",
+    long_description = "fix v1.1.9: json.decoder.JSONDecodeError: Extra data: line 1 column 300 (char 299)",
+    license = "MIT Licence",
+    url = "https://github.com/FlickerMi/google_trans_new",
+    author = "Flicker",
+    author_email = "libmi@foxmail.com",
+    py_modules=["constant", "google_trans_new"]
+)
