c752193778e2cb618957845c1d1b867b158fa7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:05:23 +0000

    Add package exspy/0.2.1/exspy-0.2.1-py3-none-any.whl

---
+++ packages/exspy/exspy-0.2.1-py3-none-any.whl/exspy/misc/eels/__init__.py
@@ -0,0 +1,5 @@
+from exspy.misc.eels.hydrogenic_gos import HydrogenicGOS
+from exspy.misc.eels.gosh_gos import GoshGOS
+from exspy.misc.eels.hartree_slater_gos import HartreeSlaterGOS
+
+__all__ = ["HydrogenicGOS", "GoshGOS", "HartreeSlaterGOS"]
