a8d4b32bc85860a6574624684d97e83555240e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:05:01 +0000

    Add package stricto/0.0.4/stricto-0.0.4-py3-none-any.whl

---
+++ packages/stricto/stricto-0.0.4-py3-none-any.whl/stricto/__init__.py
@@ -0,0 +1,14 @@
+"""
+Module stricto.
+export all classes
+"""
+from .string import String
+from .tuple import Tuple
+from .int import Int
+from .dict import Dict
+from .list import List
+from .in_type import In
+from .bool import Bool
+from .float import Float
+from .generic import GenericType
+from .error import Error, ErrorType
