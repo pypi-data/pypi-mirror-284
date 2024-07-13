9b7ceaf901966ba0967cc30a89197b500c223859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:04:56 +0000

    Add package internal-utils/0.0.1/internal_utils-0.0.1.tar.gz

---
+++ packages/internal-utils/internal_utils-0.0.1.tar.gz/internal_utils-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class InstallCommand(install):
+    def run(self):
+        raise RuntimeError("You are trying to install a stub package. Maybe you are using the wrong pypi?")
+
+
+setup(
+    name='internal-utils',
+    version='0.0.1',
+    author='Egnyte',
+    url='https://egnyte.com',
+    readme="README.md",
+    long_description="""This is a security placeholder package.""",
+    long_description_content_type='text/markdown',
+    description='A package to prevent Dependency Confusion attacks',
+    cmdclass={
+        'install': InstallCommand,
+    },
+)
