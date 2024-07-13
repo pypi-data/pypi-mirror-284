de2f472b54baded7048119afed31c856ea06d155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:05:04 +0000

    Add package fastapi-build/0.2.10/fastapi_build-0.2.10.tar.gz

---
+++ packages/fastapi-build/fastapi_build-0.2.10.tar.gz/fastapi_build-0.2.10/fastapi_build/api/demo_user_api/view.py
@@ -0,0 +1,61 @@
+# -- coding: utf-8 --
+# @Time : 2024/5/24 14:42
+# @Author : PinBar
+# @File : demo.py
+from fastapi import Depends
+
+from auth.hashers import verify_password, create_access_token
+from .request_schema import UserQueryParams, UserCreateModel, UserLoginModel, UserLoginResponseModel
+from .response_schema import UserListResponse, UserItemResponse
+from core.decorator import api_description
+from core.base_view import BaseView
+from db.models.user import User
+from core.response import Res
+from auth.authentication import TokenAuthentication
+from exceptions.custom_exception import PasswordError
+
+
+class DemoView(BaseView):
+    authentication_classes = [TokenAuthentication]
+
+    @api_description(summary="用户详情", response_model=Res(UserItemResponse))
+    def detail(self, _id: int):
+        user = User.objects.get(User.id == _id, raise_not_found=True, to_dict=True)
+        return self.message(data=user)
+
+    @api_description(summary="用户查询", response_model=Res(UserListResponse), authentication_classes=[])
+    async def get(self, query: UserQueryParams = Depends(UserQueryParams)):
+        total, users = await User.objects.search(query)
+        return self.message(data={'total': total, 'results': users})
+
+    @api_description(summary="用户创建", response_model=Res(UserItemResponse))
+    async def post(self, body: UserCreateModel):
+        user = await User.objects.a_create(**body.model_dump())
+        return self.message(data=user)
+
+    @api_description(summary="用户更新")
+    def put(self,):
+        ...
+
+    @api_description(summary="用户删除")
+    def delete(self,):
+        ...
+
+    @api_description(summary="用户批量更新")
+    def multi_put(self,):
+        ...
+
+    @api_description(summary="用户批量删除")
+    def multi_delete(self,):
+        ...
+
+
+class LoginView(BaseView):
+    @api_description(summary="登录")
+    async def post(self, body: UserLoginModel) -> Res(UserLoginResponseModel):
+        user: User = await User.objects.aget(User.username == body.username, raise_not_found=True)
+        if verify_password(body.password, user.password):
+            token = create_access_token({"user_id": user.id, "username": user.username})
+            return self.message(data={**user.to_dict(), "token": token})
+        else:
+            raise PasswordError()
