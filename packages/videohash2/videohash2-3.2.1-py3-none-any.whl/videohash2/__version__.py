177f6042d5dae803e85366e77cfdca3a60b9f04a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 14:04:27 +0000

    Add package videohash2/3.2.1/videohash2-3.2.1-py3-none-any.whl

---
+++ packages/videohash2/videohash2-3.2.1-py3-none-any.whl/videohash2/__version__.py
@@ -0,0 +1,14 @@
+__title__ = "videohash2"
+__description__ = (
+    "Python package for Near Duplicate Video Detection "
+    + "(Perceptual Video Hashing) - Get a 64-bit comparable "
+    + "hash-value for any video."
+)
+
+__url__ = "https://demmenie.github.io/videohash2/"
+__version__ = "3.2.1"
+__status__ = "production"
+__author__ = "Akash Mahanty and Chico Demmenie"
+__author_email__ = "cdemmenie@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024 Akash Mahanty"
