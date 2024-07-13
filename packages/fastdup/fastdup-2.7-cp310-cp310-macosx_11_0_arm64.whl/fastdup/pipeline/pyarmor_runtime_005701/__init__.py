9535b0e144a048d2e6763952f4cd274d4c2a521d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:04:34 +0000

    Add package fastdup/2.7/fastdup-2.7-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/fastdup/fastdup-2.7-cp310-cp310-macosx_11_0_arm64.whl/fastdup/pipeline/pyarmor_runtime_005701/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.8 (pro), 005701, 2024-07-13T05:32:09.470528
+from .pyarmor_runtime import __pyarmor__
---
+++ packages/fastdup/fastdup-2.7-cp310-cp310-macosx_11_0_arm64.whl/fastdup/vl/common/const.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+UNLABELED = "unlabeled"
+APP_PATH = Path(__file__).parent.parent.parent
