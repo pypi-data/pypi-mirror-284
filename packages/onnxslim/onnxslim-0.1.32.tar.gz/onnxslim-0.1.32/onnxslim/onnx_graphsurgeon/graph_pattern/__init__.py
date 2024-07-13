e9d5afee1e574e267363c92faef699b33387e4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:05:09 +0000

    Add package onnxslim/0.1.32/onnxslim-0.1.32.tar.gz

---
+++ packages/onnxslim/onnxslim-0.1.32.tar.gz/onnxslim-0.1.32/onnxslim/onnx_graphsurgeon/graph_pattern/__init__.py
@@ -0,0 +1,4 @@
+from onnxslim.onnx_graphsurgeon.graph_pattern.graph_pattern import (
+    GraphPattern,
+    PatternMapping,
+)
---
+++ packages/onnxslim/onnxslim-0.1.32.tar.gz/onnxslim-0.1.32/onnxslim/onnx_graphsurgeon/logger/__init__.py
@@ -0,0 +1,1 @@
+from onnxslim.onnx_graphsurgeon.logger.logger import G_LOGGER, LogMode
