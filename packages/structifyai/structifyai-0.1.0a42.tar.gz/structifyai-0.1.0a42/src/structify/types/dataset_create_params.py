8ef9e4a5237903a5e195a12114aab7b56909d18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 05:05:36 +0000

    Add package structifyai/0.1.0a42/structifyai-0.1.0a42.tar.gz

---
+++ packages/structifyai/structifyai-0.1.0a42.tar.gz/structifyai-0.1.0a42/src/structify/types/dataset_create_params.py
@@ -0,0 +1,44 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Required, TypedDict
+
+__all__ = ["DatasetCreateParams", "Relationship", "Table", "TableProperty"]
+
+
+class DatasetCreateParams(TypedDict, total=False):
+    description: Required[str]
+
+    name: Required[str]
+
+    relationships: Required[Iterable[Relationship]]
+
+    tables: Required[Iterable[Table]]
+
+
+class Relationship(TypedDict, total=False):
+    description: Required[str]
+
+    name: Required[str]
+
+    source_table: Required[str]
+
+    target_table: Required[str]
+
+
+class TableProperty(TypedDict, total=False):
+    description: Required[str]
+
+    name: Required[str]
+
+
+class Table(TypedDict, total=False):
+    description: Required[str]
+
+    name: Required[str]
+    """Organized in a name, description format."""
+
+    properties: Required[Iterable[TableProperty]]
+    """Organized in a name, description format."""
