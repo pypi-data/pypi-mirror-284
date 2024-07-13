6f85bdebd2a04b93ab98cf39ccc7174ef31ce4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:04:13 +0000

    Add package libcasm-composition/2.0a3/libcasm_composition-2.0a3-cp312-cp312-manylinux2014_aarch64.whl

---
+++ packages/libcasm-composition/libcasm_composition-2.0a3-cp312-cp312-manylinux2014_aarch64.whl/libcasm/composition/__init__.py
@@ -0,0 +1,13 @@
+"""CASM composition axes, conversions, and calculations """
+from ._composition import (
+    CompositionCalculator,
+    CompositionConverter,
+    get_occupant,
+    make_chemical_subsystems,
+    make_composition_space,
+    make_end_members,
+    make_exchange_chemical_potential,
+    make_null_composition_space,
+    make_standard_origin_and_end_members,
+    set_occupant,
+)
