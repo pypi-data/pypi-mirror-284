2de255ddd229c3b6afbc8858c113f5fc011a4c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:04:17 +0000

    Add package torchmanager-diffusion/1.0.3/torchmanager_diffusion-1.0.3.tar.gz

---
+++ packages/torchmanager-diffusion/torchmanager_diffusion-1.0.3.tar.gz/torchmanager_diffusion-1.0.3/diffusion/nn/__init__.py
@@ -0,0 +1,5 @@
+from .attention import Attention, LinearAttention
+from .conv import ConvBlock, ConvNextBlock, Residual, ResnetBlock, WeightStandardizedConv2d
+from .diffusion import TimedModule
+from .embeddings import SinusoidalPositionEmbeddings
+from .norm import PreNorm
---
+++ packages/torchmanager-diffusion/torchmanager_diffusion-1.0.3.tar.gz/torchmanager_diffusion-1.0.3/diffusion/version.py
@@ -0,0 +1,5 @@
+from torchmanager_core import Version
+
+API = Version("v1.0")
+CURRENT = Version("v1.0.3")
+DESCRIPTION = f"Torchmanager Implementation for Diffusion Model ({CURRENT})"
