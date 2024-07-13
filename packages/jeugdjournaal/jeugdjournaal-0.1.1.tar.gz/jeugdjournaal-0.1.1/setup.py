78f41637b75a00ee36483654372121ca6e02ec9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:04:51 +0000

    Add package jeugdjournaal/0.1.1/jeugdjournaal-0.1.1.tar.gz

---
+++ packages/jeugdjournaal/jeugdjournaal-0.1.1.tar.gz/jeugdjournaal-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='jeugdjournaal',
+    version='0.1.1',
+    author='hcr5',
+    author_email='hcr5.hcr@gmail.com',
+    description='Python library to interact with the jeugdjournaal API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hcr5/jeugdjournaalpy',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license='MIT'
+)
