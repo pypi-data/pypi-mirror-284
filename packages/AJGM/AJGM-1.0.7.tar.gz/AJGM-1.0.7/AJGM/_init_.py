805383d6450cc6ac455a28cdd9d1dae69ad92afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:05:07 +0000

    Add package AJGM/1.0.7/AJGM-1.0.7.tar.gz

---
+++ packages/AJGM/AJGM-1.0.7.tar.gz/AJGM-1.0.7/AJGM/_init_.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from AJGM import *
+from .AJGM import *
+name = "AJGM"
---
+++ packages/AJGM/AJGM-1.0.7.tar.gz/AJGM-1.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='AJGM',
+    version='1.0.7',
+    author='stevenyang',
+    author_email='yangsq@hnu.edu.cn',
+    description='.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(include=['AJGM', 'AJGM.*']),
+    include_package_data=True,
+    install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn'],
+    license='MIT',
+    python_requires='>=3.6',
+)
