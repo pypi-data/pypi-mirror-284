c35c2e740291a90a501a86665f29df1caecfb1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 14:04:31 +0000

    Add package PCreate/0.2/PCreate-0.2.tar.gz

---
+++ packages/PCreate/PCreate-0.2.tar.gz/PCreate-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PCreate',
+    version='0.2',
+    description='A package to quickly initialize Python projects',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Sarthak Walia',
+    author_email='waliasarthak0009@gmail.com',
+    license='MIT',
+    url='https://github.com/sarthak09walia/pcreate',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pcreate=pcreate.create_project:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
