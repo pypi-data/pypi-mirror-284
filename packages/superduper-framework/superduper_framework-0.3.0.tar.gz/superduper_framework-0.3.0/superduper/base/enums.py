8a14a0047422e4c10c499764c3c33919d9751e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:05:17 +0000

    Add package superduper-framework/0.3.0/superduper_framework-0.3.0.tar.gz

---
+++ packages/superduper-framework/superduper_framework-0.3.0.tar.gz/superduper_framework-0.3.0/superduper/base/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class DBType(str, Enum):
+    """
+    DBType is an enumeration of the supported database types.
+
+    # noqa
+    """
+
+    SQL = "SQL"
+    MONGODB = "MONGODB"
