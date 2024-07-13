56f9101413b4b3ece4ba71e8c8379188d5575bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 15:04:37 +0000

    Add package sunken-world-lib/0.1/sunken_world_lib-0.1.tar.gz

---
+++ packages/sunken-world-lib/sunken_world_lib-0.1.tar.gz/sunken_world_lib-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+from setuptools import find_packages, setup
+setup(
+    name = 'sunken_world_lib',
+    version = '0.1',
+    packages=find_packages(),
+    py_modules=['sunken_world_lib'],
+)
