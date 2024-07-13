d3b900aedfa6a2fb27a34ffcbf8d12cb3ad4812a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 04:04:08 +0000

    Add package inference-cpu/0.14.0/inference_cpu-0.14.0-py3-none-any.whl

---
+++ packages/inference-cpu/inference_cpu-0.14.0-py3-none-any.whl/inference/core/workflows/core_steps/loader.py
@@ -0,0 +1,112 @@
+from typing import List, Type
+
+from inference.core.workflows.core_steps.flow_control.continue_if import ContinueIfBlock
+from inference.core.workflows.core_steps.formatters.expression import ExpressionBlock
+from inference.core.workflows.core_steps.formatters.first_non_empty_or_default import (
+    FirstNonEmptyOrDefaultBlock,
+)
+from inference.core.workflows.core_steps.formatters.property_definition import (
+    PropertyDefinitionBlock,
+)
+from inference.core.workflows.core_steps.fusion.detections_classes_replacement import (
+    DetectionsClassesReplacementBlock,
+)
+from inference.core.workflows.core_steps.fusion.detections_consensus import (
+    DetectionsConsensusBlock,
+)
+from inference.core.workflows.core_steps.fusion.dimension_collapse import (
+    DimensionCollapseBlock,
+)
+from inference.core.workflows.core_steps.models.foundation.clip_comparison import (
+    ClipComparisonBlock,
+)
+from inference.core.workflows.core_steps.models.foundation.lmm import LMMBlock
+from inference.core.workflows.core_steps.models.foundation.lmm_classifier import (
+    LMMForClassificationBlock,
+)
+from inference.core.workflows.core_steps.models.foundation.ocr import OCRModelBlock
+from inference.core.workflows.core_steps.models.foundation.yolo_world import (
+    YoloWorldModelBlock,
+)
+from inference.core.workflows.core_steps.models.roboflow.instance_segmentation import (
+    RoboflowInstanceSegmentationModelBlock,
+)
+from inference.core.workflows.core_steps.models.roboflow.keypoint_detection import (
+    RoboflowKeypointDetectionModelBlock,
+)
+from inference.core.workflows.core_steps.models.roboflow.multi_class_classification import (
+    RoboflowClassificationModelBlock,
+)
+from inference.core.workflows.core_steps.models.roboflow.multi_label_classification import (
+    RoboflowMultiLabelClassificationModelBlock,
+)
+from inference.core.workflows.core_steps.models.roboflow.object_detection import (
+    RoboflowObjectDetectionModelBlock,
+)
+from inference.core.workflows.core_steps.models.third_party.barcode_detection import (
+    BarcodeDetectorBlock,
+)
+from inference.core.workflows.core_steps.models.third_party.qr_code_detection import (
+    QRCodeDetectorBlock,
+)
+from inference.core.workflows.core_steps.sinks.roboflow.roboflow_dataset_upload import (
+    RoboflowDatasetUploadBlock,
+)
+from inference.core.workflows.core_steps.transformations.absolute_static_crop import (
+    AbsoluteStaticCropBlock,
+)
+from inference.core.workflows.core_steps.transformations.detection_offset import (
+    DetectionOffsetBlock,
+)
+from inference.core.workflows.core_steps.transformations.detections_filter import (
+    DetectionsFilterBlock,
+)
+from inference.core.workflows.core_steps.transformations.detections_transformation import (
+    DetectionsTransformationBlock,
+)
+from inference.core.workflows.core_steps.transformations.dynamic_crop import (
+    DynamicCropBlock,
+)
+from inference.core.workflows.core_steps.transformations.dynamic_zones import (
+    DynamicZonesBlock,
+)
+from inference.core.workflows.core_steps.transformations.perspective_correction import (
+    PerspectiveCorrectionBlock,
+)
+from inference.core.workflows.core_steps.transformations.relative_static_crop import (
+    RelativeStaticCropBlock,
+)
+from inference.core.workflows.prototypes.block import WorkflowBlock
+
+
+def load_blocks() -> List[Type[WorkflowBlock]]:
+    return [
+        DetectionsConsensusBlock,
+        ClipComparisonBlock,
+        LMMBlock,
+        LMMForClassificationBlock,
+        OCRModelBlock,
+        YoloWorldModelBlock,
+        RoboflowInstanceSegmentationModelBlock,
+        RoboflowKeypointDetectionModelBlock,
+        RoboflowClassificationModelBlock,
+        RoboflowMultiLabelClassificationModelBlock,
+        RoboflowObjectDetectionModelBlock,
+        BarcodeDetectorBlock,
+        QRCodeDetectorBlock,
+        AbsoluteStaticCropBlock,
+        DynamicCropBlock,
+        DetectionsFilterBlock,
+        DetectionOffsetBlock,
+        RelativeStaticCropBlock,
+        DetectionsTransformationBlock,
+        RoboflowDatasetUploadBlock,
+        ContinueIfBlock,
+        PerspectiveCorrectionBlock,
+        DynamicZonesBlock,
+        DetectionsClassesReplacementBlock,
+        ExpressionBlock,
+        PropertyDefinitionBlock,
+        DimensionCollapseBlock,
+        FirstNonEmptyOrDefaultBlock,
+    ]
