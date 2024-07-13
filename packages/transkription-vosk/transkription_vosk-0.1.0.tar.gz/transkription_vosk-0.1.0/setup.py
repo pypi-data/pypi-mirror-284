0ca18094f0d091fdf7d3730a50c327ead43084ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-13 03:04:24 +0000

    Add package transkription-vosk/0.1.0/transkription_vosk-0.1.0.tar.gz

---
+++ packages/transkription-vosk/transkription_vosk-0.1.0.tar.gz/transkription_vosk-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="transkription_vosk",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "transkription_vosk=transkription_vosk.transkription_vosk:main",
+        ],
+    },
+    install_requires=[
+        "vosk",
+        "pydub",
+    ],
+    author="Martial Job",
+    author_email="transkription_vosk@mjob.me",
+    description="Ein Paket zur Transkription von Audiodateien mit Vosk-Modellen, lokal auf dem Rechner.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/martialjob/transkription_vosk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
