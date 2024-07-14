bfa99af494d63cdf41691f008547fed9e655ddc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:54 +0000

    Add package fireapi/0.4/fireapi-0.4.tar.gz

---
+++ packages/fireapi/fireapi-0.4.tar.gz/fireapi-0.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+# Read the contents of your README file
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="fireapi",
+    packages=["fireapi"],
+    version="0.4",
+    license="AGPL-3.0",
+    description="A simple API wrapper for the 24Fire REST API",
+    long_description=long_description,  # Set the long description
+    long_description_content_type="text/markdown",  # Specify the content type. Important for rendering markdown!
+    author="EvickaStudio",
+    author_email="hello@evicka.de",
+    url="https://github.com/EvickaStudio/24-Fire-REST-API",
+    keywords=["API", "24Fire", "KVM", "Server Management"],
+    install_requires=["aiohttp", "requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
