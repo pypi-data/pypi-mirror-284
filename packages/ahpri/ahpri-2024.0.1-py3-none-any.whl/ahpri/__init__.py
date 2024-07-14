eff3dceb63983d8b71fa633249b4876970366ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:07 +0000

    Add package ahpri/2024.0.1/ahpri-2024.0.1-py3-none-any.whl

---
+++ packages/ahpri/ahpri-2024.0.1-py3-none-any.whl/ahpri/__init__.py
@@ -0,0 +1,7 @@
+"""
+AHPri is the python implementation of Saaty's random index
+generator for the analytical hierarchy process
+
+"""
+
+from .calcri import *
---
+++ packages/ahpri/ahpri-2024.0.1-py3-none-any.whl/ahpri/calcri.py
@@ -0,0 +1,10 @@
+"""
+Hauptpackage
+
+# c|_| markup len oOooOooOooOo ~ oOooOooOooOooOo ~ oOooOooOooOo <`)))))<
+# c|_| code len oOoOooOooOooOo ~ oOooOooOooOooOo ~ oOooOooOooOooOo ~ ~ <`)))))<
+
+"""
+
+from os import path
+
