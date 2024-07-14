0dfe49160cef6f1b03363593bf2de532e8842d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:21 +0000

    Add package jupyterlab-hide-code-on-start/1.0/jupyterlab_hide_code_on_start-1.0.tar.gz

---
+++ packages/jupyterlab-hide-code-on-start/jupyterlab_hide_code_on_start-1.0.tar.gz/jupyterlab_hide_code_on_start-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyterlab_hide_code_on_start',  # Required
+    version='1.0',  # Required
+    description='A JupyterLab extension',  # Required
+    packages=find_packages(),  # Required
+)
