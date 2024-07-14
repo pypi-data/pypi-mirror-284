0f4c1c973a6b58f75cfb8477b47d1d4c0e1e41f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:17 +0000

    Add package sheetconnect/1.0.1/sheetconnect-1.0.1.tar.gz

---
+++ packages/sheetconnect/sheetconnect-1.0.1.tar.gz/sheetconnect-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sheetconnect',
+    version='1.0.1',
+    description='Simple get data from Google sheet in form of Pandas dataframe',
+    author='Pongsakorn Nimphaya',
+    packages=find_packages(),
+)
---
+++ packages/sheetconnect/sheetconnect-1.0.1.tar.gz/sheetconnect-1.0.1/sheetconnect/__init__.py
@@ -0,0 +1,6 @@
+from .sheetconnect import Connect
+
+def initialize_package():
+    print("Initializing Commect Package")
+
+initialize_package()
