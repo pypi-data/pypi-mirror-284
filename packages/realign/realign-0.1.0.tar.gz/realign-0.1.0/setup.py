167c875b0d7be879df7e7f86fbb1b6431a046e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:04:53 +0000

    Add package realign/0.1.0/realign-0.1.0.tar.gz

---
+++ packages/realign/realign-0.1.0.tar.gz/realign-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='realign',
+version='0.1.0',
+author='Your Name',
+author_email='your.email@example.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
