4b02b532009e05b266f3090af5d37c3295bdda39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:23 +0000

    Add package streamlit-imp-sidebar/0.0.4/streamlit_imp_sidebar-0.0.4.tar.gz

---
+++ packages/streamlit-imp-sidebar/streamlit_imp_sidebar-0.0.4.tar.gz/streamlit_imp_sidebar-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-imp-sidebar",
+    version="0.0.4",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.8",
+    install_requires=[
+        "streamlit==1.36.0",
+    ],
+    extras_require={
+    }
+)
