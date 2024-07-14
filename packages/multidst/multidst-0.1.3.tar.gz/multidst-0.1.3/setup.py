1bbef0227bf004edb6cce1bda0ad76811bc81409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:17 +0000

    Add package multidst/0.1.3/multidst-0.1.3.tar.gz

---
+++ packages/multidst/multidst-0.1.3.tar.gz/multidst-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multidst',
+    version='0.1.3',
+    packages=find_packages(),  # Automatically find all packages and subpackages
+    install_requires=[
+        'numpy', 
+        'pandas',
+        'scipy',
+        'statsmodels',
+        'matplotlib'
+    ],
+    extras_require = {
+        "dev":["twine>4.0.2"],
+    },
+    python_requires=">=3.10",
+)
+
