80cae40e84a4a34a63ff65f8deb87d2b3b667ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:06:04 +0000

    Add package tradeflow/0.0.1/tradeflow-0.0.1.tar.gz

---
+++ packages/tradeflow/tradeflow-0.0.1.tar.gz/tradeflow-0.0.1/setup.py
@@ -0,0 +1,27 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+import pathlib
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="tradeflow",
+    description="A package to simulate autocorrelated time series of signs",  # Optional
+    author="MG",  # Optional
+    packages=find_packages(),  # Required,
+    # version=versioneer.get_version(),
+    version="0.0.1",
+    # cmdclass=versioneer.get_cmdclass(),
+)
