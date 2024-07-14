ffc0fa5f26493bf42189af4689dd6b1455f01b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:05:27 +0000

    Add package wiring/0.1.1/wiring-0.1.1.tar.gz

---
+++ packages/wiring/wiring-0.1.1.tar.gz/wiring-0.1.1/wiring/__init__.py
@@ -0,0 +1,6 @@
+from wiring.multi_platform_bot import MultiPlatformBot, PlatformBotNotFoundError
+from wiring.multi_platform_resources import (PlatformSpecificValue, MultiPlatformChat,
+                                             MultiPlatformChatGroup, MultiPlatformId,
+                                             MultiPlatformMessage, MultiPlatformUser,
+                                             MultiPlatformValue, Platform)
+from wiring.bot_base import Bot, Command, CommandHandler
