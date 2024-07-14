32ccace5b4f4606c2ea1ea69367e50ab1dcbfc0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:27 +0000

    Add package pdfdeal/0.2.0/pdfdeal-0.2.0.tar.gz

---
+++ packages/pdfdeal/pdfdeal-0.2.0.tar.gz/pdfdeal-0.2.0/tests/test_client.py
@@ -0,0 +1,11 @@
+from pdfdeal import Doc2X
+import os
+
+def test_client():
+    client = Doc2X()
+    assert client is not None
+
+
+def test_client_with_personal_key():
+    client = Doc2X(apikey=os.getenv("DOC2X_APIKEY_PERSONAL"))
+    assert client is not None
