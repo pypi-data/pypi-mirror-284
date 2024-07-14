672592775f01000bb166aff38ec06e22245b01e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:22 +0000

    Add package wanabi/1.0.17/wanabi-1.0.17-py3-none-any.whl

---
+++ packages/wanabi/wanabi-1.0.17-py3-none-any.whl/wanabi/extend_exception.py
@@ -0,0 +1,24 @@
+from tkinter import messagebox
+
+
+class NotOpenPathException(Exception):
+    """
+    ファイルを開くのに失敗した時の例外
+    基本的に握りつぶしてデフォルトを適用して続行する目的
+    """
+
+    pass
+
+
+class CannotMakedirsException(Exception):
+    """
+    ディレクトリの作成エラー
+    """
+    pass
+
+
+class FatalError(Exception):
+    """
+    復旧不能なエラーの発生時に投げる
+    """
+    pass
