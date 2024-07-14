ee5bc272bc2a4ed56339c743d8b814fc8262c1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:05 +0000

    Add package cifparse/0.9.2/cifparse-0.9.2-py3-none-any.whl

---
+++ packages/cifparse/cifparse-0.9.2-py3-none-any.whl/cifparse/cifp_procedure_subsegment.py
@@ -0,0 +1,32 @@
+from .cifp_functions import clean_value
+from .cifp_procedure_point import CIFPProcedurePoint
+
+# FOR TRANSITIONS OF PD/PE/PF AND HD/HE/HF
+
+
+class CIFPProcedureSubsegment:
+    def __init__(self) -> None:
+        self.transition_id = None
+        self.points: list[CIFPProcedurePoint] = []
+
+    def from_lines(self, cifp_lines: list) -> None:
+        initial = str(cifp_lines[0])
+        transition_id = initial[20:25].strip()
+        self.transition_id = transition_id
+
+        for cifp_line in cifp_lines:
+            cont_rec_no = int(cifp_line[38:39])
+            if cont_rec_no == 0 or cont_rec_no == 1:
+                self._cont0(cifp_line)
+
+    def _cont0(self, cifp_line: str) -> None:
+        point = CIFPProcedurePoint()
+        point.from_line(cifp_line)
+        self.points.append(point)
+
+    def to_dict(self) -> dict:
+        points = []
+        for item in self.points:
+            points.append(item.to_dict())
+
+        return {"transition_id": clean_value(self.transition_id), "points": points}
