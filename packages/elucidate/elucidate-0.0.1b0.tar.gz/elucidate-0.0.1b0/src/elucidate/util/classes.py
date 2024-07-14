ff2132673c175ab514fdf32319695450ee88f2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:05 +0000

    Add package elucidate/0.0.1b0/elucidate-0.0.1b0.tar.gz

---
+++ packages/elucidate/elucidate-0.0.1b0.tar.gz/elucidate-0.0.1b0/src/elucidate/util/classes.py
@@ -0,0 +1,11 @@
+## Copyright 2024 Kakusui LLC (https://kakusui.org) (https://github.com/Kakusui) (https://github.com/Kakusui/Elucidate)
+## Use of this source code is governed by an GNU Lesser General Public License v2.1
+## license that can be found in the LICENSE file.
+
+## third-party imports
+from openai import AsyncOpenAI, OpenAI
+
+## custom modules
+from .imports.easytl_importer import openai_service, gemini_service
+
+from .imports.easytl_importer import SystemTranslationMessage, ModelTranslationMessage, ChatCompletion, NOT_GIVEN, NotGiven, GenerationConfig, GenerateContentResponse
