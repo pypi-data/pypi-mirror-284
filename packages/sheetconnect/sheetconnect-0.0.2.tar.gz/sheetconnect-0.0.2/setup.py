de3407fc59be15bba63a620ef4e627e240130cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:11 +0000

    Add package sheetconnect/0.0.2/sheetconnect-0.0.2.tar.gz

---
+++ packages/sheetconnect/sheetconnect-0.0.2.tar.gz/sheetconnect-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sheetconnect',
+    version='0.0.2',
+    description='Simple get data from Google sheet in form of pandas dataframe',
+    author='Pongsakorn nimphaya',
+    packages=find_packages(),
+)
