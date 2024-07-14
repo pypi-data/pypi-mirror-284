f959aa4211a1264e92ca39bfda467b161ab008b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:25 +0000

    Add package amazon-product-details-scraper/1.0.5/amazon_product_details_scraper-1.0.5.tar.gz

---
+++ packages/amazon-product-details-scraper/amazon_product_details_scraper-1.0.5.tar.gz/amazon_product_details_scraper-1.0.5/amazon_product_details_scraper/config/__init__.py
@@ -0,0 +1,14 @@
+from amazon_product_details_scraper.config.config import (
+    DEFAULT_LOG_LEVEL,
+    DEFAULT_OUTPUT_FILENAME,
+    DEFAULT_OUTPUT_FOLDER,
+    VALID_DOMAIN_NAMES,
+)
+
+
+__all__ = [
+    "DEFAULT_LOG_LEVEL",
+    "DEFAULT_OUTPUT_FILENAME",
+    "DEFAULT_OUTPUT_FOLDER",
+    "VALID_DOMAIN_NAMES",
+]
