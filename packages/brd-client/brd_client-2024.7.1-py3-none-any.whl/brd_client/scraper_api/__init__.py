50e7041dbebb9a9a0e88c83b2618bd3b30befad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:17 +0000

    Add package brd-client/2024.7.1/brd_client-2024.7.1-py3-none-any.whl

---
+++ packages/brd-client/brd_client-2024.7.1-py3-none-any.whl/brd_client/scraper_api/__init__.py
@@ -0,0 +1,5 @@
+from .e_commerce.amazon import Amazon
+from .social_media.facebook import Facebook
+from .social_media.instagram import Instagram
+from .social_media.tiktok import TikTok
+from .social_media.youtube import YouTube
