4888a123991672056b7552d1fe3a900d42ac0917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:04 +0000

    Add package Hydro-create/2.1.6/Hydro_create-2.1.6.tar.gz

---
+++ packages/Hydro-create/Hydro_create-2.1.6.tar.gz/Hydro_create-2.1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(
+   name='Hydro_create',
+   version='2.1.6',
+   description='A useful module',
+   author='Cloudann',
+   author_email='cloudann@gmail.com',
+   packages=['Hydro_create'],  #same as name
+   install_requires=['pandas', 'xpinyin','openpyxl'], #external packages as dependencies
+)
