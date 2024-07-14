863cb645422ad7a2cfd1eda06ab9a5ed4e2146cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:05:28 +0000

    Add package aiotieba/4.4.6/aiotieba-4.4.6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aiotieba/aiotieba-4.4.6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/api/get_cid/_api.py
@@ -0,0 +1,34 @@
+from typing import Dict, List, Union
+
+import yarl
+
+from ...const import APP_BASE_HOST, APP_SECURE_SCHEME
+from ...core import HttpCore
+from ...exception import TiebaServerError
+from ...helper import parse_json
+
+TypeCates = List[Dict[str, Union[str, int]]]
+
+
+def parse_body(body: bytes) -> TypeCates:
+    res_json = parse_json(body)
+    if code := int(res_json['error_code']):
+        raise TiebaServerError(code, res_json['error_msg'])
+
+    cates = res_json['cates']
+
+    return cates
+
+
+async def request(http_core: HttpCore, fname: str) -> TypeCates:
+    data = [
+        ('BDUSS', http_core.account.BDUSS),
+        ('word', fname),
+    ]
+
+    request = http_core.pack_form_request(
+        yarl.URL.build(scheme=APP_SECURE_SCHEME, host=APP_BASE_HOST, path="/c/c/bawu/goodlist"), data
+    )
+
+    body = await http_core.net_core.send_request(request, read_bufsize=1024)
+    return parse_body(body)
