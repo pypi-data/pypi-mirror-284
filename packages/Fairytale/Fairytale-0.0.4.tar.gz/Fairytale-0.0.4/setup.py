5713d9cbb2a59d0f009b56736fb5698d82841978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:07 +0000

    Add package Fairytale/0.0.4/Fairytale-0.0.4.tar.gz

---
+++ packages/Fairytale/Fairytale-0.0.4.tar.gz/Fairytale-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+VERSION = '0.0.4'
+setup(
+    name='Fairytale',
+    version=VERSION,
+    author="Parag Mehta",
+    author_email="<mehtaparag45@gmail.com>",
+    packages=find_packages(),
+    
+)
+
+
+
+  
+
