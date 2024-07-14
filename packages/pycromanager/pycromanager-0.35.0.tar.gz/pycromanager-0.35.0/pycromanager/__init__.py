227ec91c9db21bbd9c9ae950f37b165ba3b331c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:56 +0000

    Add package pycromanager/0.35.0/pycromanager-0.35.0.tar.gz

---
+++ packages/pycromanager/pycromanager-0.35.0.tar.gz/pycromanager-0.35.0/pycromanager/__init__.py
@@ -0,0 +1,13 @@
+name = "pycromanager"
+
+from pycromanager.acquisition.java_backend_acquisitions import (JavaBackendAcquisition, MagellanAcquisition,
+                                                                XYTiledAcquisition, ExploreAcquisition)
+from pycromanager.acquisition.acquisition_superclass import multi_d_acquisition_events
+from pycromanager.acquisition.acq_constructor import Acquisition
+from pycromanager.mm_java_classes import Studio, Magellan
+from pycromanager.acquisition.acq_eng_py.main.acq_notification import AcqNotification
+
+from pycromanager.headless import start_headless, stop_headless
+from mmpycorex import download_and_install_mm, find_existing_mm_install, Core
+
+from ._version import __version__, version_info
