24378751340933ff44bfc8179c1030a1489b259e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:05:41 +0000

    Add package tf-fast-rnnt/1.1.1/tf_fast_rnnt-1.1.1.tar.gz

---
+++ packages/tf-fast-rnnt/tf_fast_rnnt-1.1.1.tar.gz/tf_fast_rnnt-1.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='tf_fast_rnnt', #Package_Name
+      version='1.1.1',
+      description='Python Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+     )
