be9d8033f046e96cfcfcc7e533bb2a2e1d4eeb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:18 +0000

    Add package sunken-world-lib/0.5/sunken_world_lib-0.5.tar.gz

---
+++ packages/sunken-world-lib/sunken_world_lib-0.5.tar.gz/sunken_world_lib-0.5/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+from setuptools import find_packages, setup
+setup(
+    name = 'sunken_world_lib',
+    version = '0.5',
+    packages=find_packages(),
+    install_requires=[],
+    package_data={"libs": ["jei-1.20.1-forge-15.4.0.9.jar","skinlayers3d-forge-1.6.6-mc1.20.1.jar"]},
+)
