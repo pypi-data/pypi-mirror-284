423906ee3b5a4033630023b37817ba3df586becf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:06:11 +0000

    Add package rapidstats/0.0.12/rapidstats-0.0.12-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/rapidstats/rapidstats-0.0.12-cp310-cp310-macosx_11_0_arm64.whl/rapidstats/_metrics.py
@@ -0,0 +1,89 @@
+import dataclasses
+from typing import Optional
+
+import polars as pl
+from polars.series.series import ArrayLike
+
+from ._rustystats import (
+    _adverse_impact_ratio,
+    _brier_loss,
+    _confusion_matrix,
+    _max_ks,
+    _mean,
+    _roc_auc,
+)
+from ._utils import DataFrame, _to_polars, _y_true_y_pred_to_df, _y_true_y_score_to_df
+
+
+@dataclasses.dataclass
+class ConfusionMatrix:
+    tn: float
+    fp: float
+    fn: float
+    tp: float
+    tpr: float
+    fpr: float
+    fnr: float
+    tnr: float
+    prevalence: float
+    prevalence_threshold: float
+    informedness: float
+    precision: float
+    false_omission_rate: float
+    plr: float
+    nlr: float
+    acc: float
+    balanced_accuracy: float
+    f1: float
+    folkes_mallows_index: float
+    mcc: float
+    threat_score: float
+    markedness: float
+    fdr: float
+    npv: float
+    dor: float
+
+    def to_polars(self) -> pl.DataFrame:
+        dct = self.__dict__
+
+        return pl.DataFrame({"metric": dct.keys(), "value": dct.values()})
+
+
+def confusion_matrix(y_true: ArrayLike, y_pred: ArrayLike) -> ConfusionMatrix:
+    df = _y_true_y_pred_to_df(y_true, y_pred)
+
+    return ConfusionMatrix(*_confusion_matrix(df))
+
+
+def roc_auc(y_true: ArrayLike, y_score: ArrayLike) -> float:
+    df = _y_true_y_score_to_df(y_true, y_score)
+
+    return _roc_auc(df)
+
+
+def max_ks(y_true: ArrayLike, y_score: ArrayLike) -> float:
+    df = _y_true_y_score_to_df(y_true, y_score)
+
+    return _max_ks(df)
+
+
+def brier_loss(y_true: ArrayLike, y_score: ArrayLike) -> float:
+    df = _y_true_y_score_to_df(y_true, y_score)
+
+    return _brier_loss(df)
+
+
+def mean(y: ArrayLike) -> float:
+    return _mean(pl.DataFrame({"y": y}))
+
+
+def adverse_impact_ratio(
+    y_pred: ArrayLike,
+    protected: ArrayLike,
+    control: ArrayLike,
+) -> float:
+    return _adverse_impact_ratio(
+        pl.DataFrame(
+            {"y_pred": y_pred, "protected": protected, "control": control}
+        ).cast(pl.Boolean)
+    )
