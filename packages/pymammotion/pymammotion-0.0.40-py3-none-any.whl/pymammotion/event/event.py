2f7837695c113c0c31c723e8f74044d0b7905913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:30 +0000

    Add package pymammotion/0.0.40/pymammotion-0.0.40-py3-none-any.whl

---
+++ packages/pymammotion/pymammotion-0.0.40-py3-none-any.whl/pymammotion/event/event.py
@@ -0,0 +1,50 @@
+import asyncio
+
+
+class Event:
+    def __init__(self):
+        self.__eventhandlers = []
+
+    def __iadd__(self, handler):
+        self.__eventhandlers.append(handler)
+        return self
+
+    def __isub__(self, handler):
+        self.__eventhandlers.remove(handler)
+        return self
+
+    async def __call__(self, *args, **kwargs):
+        await asyncio.gather(
+            *[handler(*args, **kwargs) for handler in self.__eventhandlers]
+        )
+
+
+class MoveEvent:
+    def __init__(self):
+        self.OnMoveFinished = Event()
+
+    async def MoveFinished(self):
+        # This function will be executed once blufi finishes after a movement command and will
+        # raise an event
+        await self.OnMoveFinished()
+
+    def AddSubscribersForMoveFinishedEvent(self, objMethod):
+        self.OnMoveFinished += objMethod
+
+    def RemoveSubscribersForMoveFinishedEvent(self, objMethod):
+        self.OnMoveFinished -= objMethod
+
+
+class BleNotificationEvent:
+    def __init__(self):
+        self.OnBleNotification = Event()
+
+    async def BleNotification(self, data: bytearray):
+        # This function will be executed when data is received.
+        await self.OnBleNotification(data)
+
+    def AddSubscribersForBleNotificationEvent(self, objMethod):
+        self.OnBleNotification += objMethod
+
+    def RemoveSubscribersForBleNotificationEvent(self, objMethod):
+        self.OnBleNotification -= objMethod
