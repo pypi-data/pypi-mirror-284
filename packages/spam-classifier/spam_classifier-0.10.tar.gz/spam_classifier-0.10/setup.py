ec120d722c802adf2a660a50fd25fbab89974e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:11 +0000

    Add package spam-classifier/0.10/spam_classifier-0.10.tar.gz

---
+++ packages/spam-classifier/spam_classifier-0.10.tar.gz/spam_classifier-0.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spam_classifier',
+    version='0.10',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A library for classifying spam messages',
+    install_requires=[
+        'tensorflow>=2.0.0',
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license='Non-commercial use only. Contact author for commercial use.',
+    Email= 'totoshkus@gmail.com'
+)
