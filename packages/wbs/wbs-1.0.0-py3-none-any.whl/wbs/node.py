b0ecb26c3c2c99a5bc9dc1d0b0907e164ec2264c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:11 +0000

    Add package wbs/1.0.0/wbs-1.0.0-py3-none-any.whl

---
+++ packages/wbs/wbs-1.0.0-py3-none-any.whl/wbs/node.py
@@ -0,0 +1,21 @@
+class Node:
+    def get(self, number):
+        '''
+        Get a node based on the number.
+
+        For example, get the node 1.32.2.
+        '''
+
+        # Convert number string '1.32.2' to [1, 32, 2]
+        if type(number) == str:
+            number = list(map(int, number.split('.')))
+
+        index = number[0] - 1
+        e = self.children[index]
+
+        number = number[1:]
+        if number:
+            # Descend recursively.
+            return e.get(number)
+
+        return e
