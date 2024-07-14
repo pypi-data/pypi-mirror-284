19fed00ef5f9ca39a86524aef3af3cdad7dae608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:20 +0000

    Add package hubertchen-package/0.3/hubertchen_package-0.3.tar.gz

---
+++ packages/hubertchen-package/hubertchen_package-0.3.tar.gz/hubertchen_package-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hubertchen_package',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    url='https://github.com/hubertchen200/mypy-package',
+    license='MIT',
+    author='Hubert Chen',
+    author_email='hubertchen200@gmail.com',
+    description='Hubert Chen''s Python package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
