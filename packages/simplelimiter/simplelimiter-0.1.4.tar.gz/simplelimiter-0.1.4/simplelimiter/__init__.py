4d0a7fd7fad4b8fcbfd1be26730d7815d4ca546c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:04:53 +0000

    Add package simplelimiter/0.1.4/simplelimiter-0.1.4.tar.gz

---
+++ packages/simplelimiter/simplelimiter-0.1.4.tar.gz/simplelimiter-0.1.4/simplelimiter/__init__.py
@@ -0,0 +1,72 @@
+import logging
+
+from redis import Redis
+
+from typing import Optional
+from fastapi import Request, HTTPException, Response
+
+
+TIME_EQUIVALENT_IN_SECONDS = {
+    "second": 1,
+    "minute": 60,
+    "hour": 3600,
+    "day": 86400
+}
+
+
+class Limiter:
+    redis: Redis = None
+    ignore_limiter: bool = False
+    debug: bool = False
+    requests: int
+    timeunit: str
+    exp_time: int
+
+    def __init__(self, requests_slash_timeunit):
+        requests, timeunits = requests_slash_timeunit.split("/")
+        self.requests = int(requests)
+        self.timeunit = timeunits
+        self.exp_time = TIME_EQUIVALENT_IN_SECONDS[timeunits]
+
+    async def __call__(self, request: Request, response: Response):
+        if self.ignore_limiter:
+            return
+
+        if not self.redis:
+            logging.warning("No redis instance was provided")
+            return
+
+        identifier = self.get_identifier(request)
+        requested_by_client = int(self.redis.get(identifier) or 0)
+
+        if requested_by_client >= self.requests:
+            raise HTTPException(status_code=429)
+
+        requested_by_client = self.redis.incr(identifier)
+
+        if requested_by_client == 1:
+            self.redis.expire(identifier, self.exp_time)
+            ttl = self.exp_time
+        else:
+            ttl = self.redis.ttl(identifier)
+
+        if self.debug:
+            logging.info(f"Limiter [{identifier}] will expire in {ttl}")
+
+    @classmethod
+    def init(
+        cls, 
+        redis_instance: Redis, 
+        debug: Optional[bool] = False,
+        ignore_limiter: Optional[bool] = False
+    ):
+        cls.debug = debug
+        cls.redis = redis_instance
+        cls.ignore_limiter = ignore_limiter
+
+    @staticmethod
+    def get_identifier(request: Request) -> str:
+        request_ip = request.client.host
+        identifier = f"LIMITER:{request_ip}:{request.url.path}"
+
+        return identifier
