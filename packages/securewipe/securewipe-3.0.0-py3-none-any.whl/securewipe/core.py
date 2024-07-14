c2de47568fe0d30ae7d0a0287b45b494862cdbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:16 +0000

    Add package securewipe/3.0.0/securewipe-3.0.0-py3-none-any.whl

---
+++ packages/securewipe/securewipe-3.0.0-py3-none-any.whl/securewipe/core.py
@@ -0,0 +1,50 @@
+# Securewipe: Core functionality for securely overwriting files with random data.
+# Author: Fidal
+# Email: hellome@gmail.com
+# Copyright: (c) 2024 Fidal
+
+import os
+
+def replace_random(file_path, passes=7):
+    try:
+        file_size = os.path.getsize(file_path)
+        with open(file_path, 'wb') as file:
+            for i in range(passes):
+                file.write(b'\x00' * file_size)
+                file.flush()
+                os.fsync(file.fileno())
+
+                file.write(b'\xFF' * file_size)
+                file.flush()
+                os.fsync(file.fileno())
+
+                file.write(b'\xAA' * file_size)  
+                file.flush()
+                os.fsync(file.fileno())
+
+                file.write(b'\x55' * file_size)  
+                file.flush()
+                os.fsync(file.fileno())
+
+                random_bytes = os.urandom(file_size)
+                file.write(random_bytes)
+                file.flush()
+                os.fsync(file.fileno())
+
+                random_bytes = os.urandom(file_size)
+                file.write(random_bytes)
+                file.flush()
+                os.fsync(file.fileno())
+
+                random_bytes = os.urandom(file_size)
+                file.write(random_bytes)
+                file.flush()
+                os.fsync(file.fileno())
+    except Exception as e:
+        raise e
+
+def self_destruct(script_path):
+    try:
+        replace_random(script_path)
+    except Exception as e:
+        raise e
