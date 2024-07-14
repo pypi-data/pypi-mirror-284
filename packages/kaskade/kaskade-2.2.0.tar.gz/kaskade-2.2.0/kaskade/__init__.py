2015751a4a1469aa86067c66f4c70db4e3d87583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:05 +0000

    Add package kaskade/2.2.0/kaskade-2.2.0.tar.gz

---
+++ packages/kaskade/kaskade-2.2.0.tar.gz/kaskade-2.2.0/kaskade/__init__.py
@@ -0,0 +1,34 @@
+import logging
+from importlib.metadata import version
+from pathlib import Path
+
+APP_NAME = "kaskade"
+APP_NAME_SHORT = "kskd"
+__version__ = APP_VERSION = version(APP_NAME)
+APP_BANNER = r""" _             _             _
+| | ____ _ ___| | ____ _  __| | ___
+| |/ / _` / __| |/ / _` |/ _` |/ _ \
+|   < (_| \__ \   < (_| | (_| |  __/
+|_|\_\__,_|___/_|\_\__,_|\__,_|\___|"""
+APP_BANNER_SHORT = r""" _        _       _
+| | _____| | ____| |
+| |/ / __| |/ / _` |
+|   <\__ \   < (_| |
+|_|\_\___/_|\_\__,_|"""
+
+
+path_home = Path.home()
+kaskade_path = path_home.joinpath("." + APP_NAME)
+if not kaskade_path.exists():
+    kaskade_path.mkdir()
+
+APP_HOME = str(kaskade_path)
+APP_LOG = str(kaskade_path.joinpath(APP_NAME + ".log"))
+
+logger_handler = logging.FileHandler(APP_LOG)
+logger_handler.setFormatter(logging.Formatter("%(asctime)-15s %(levelname)-8s %(message)s"))
+
+logger = logging.getLogger()
+logger.addHandler(logger_handler)
+logger.setLevel(logging.INFO)
+logging.captureWarnings(True)
