c6b6e1271a6c1fb35b22f356460698bc4e6a2ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:55 +0000

    Add package vscode-project-cli/0.1.4/vscode_project_cli-0.1.4-py3-none-any.whl

---
+++ packages/vscode-project-cli/vscode_project_cli-0.1.4-py3-none-any.whl/source/entities/settings_json.py
@@ -0,0 +1,26 @@
+PYTHON_SCHEMA = {
+    "[python]": {
+        "editor.defaultFormatter": "charliermarsh.ruff",
+        "editor.codeActionsOnSave": {
+            "source.fixAll.ruff": "always",
+            "source.organizeImports.ruff": "always",
+            "source.unusedImports": "always",
+        },
+    },
+    "python.analysis.typeCheckingMode": "standard",
+    "python.analysis.autoImportCompletions": True,
+}
+
+JAVASCRIPT_SCHEMA = {
+    "[javascript]": {},
+}
+
+DART_SCHEMA = {
+    "[dart]": {
+        "editor.rulers": [80],
+        "editor.selectionHighlight": True,
+        "editor.suggestSelection": "first",
+        "editor.tabCompletion": "onlySnippets",
+        "editor.wordBasedSuggestions": "off",
+    }
+}
