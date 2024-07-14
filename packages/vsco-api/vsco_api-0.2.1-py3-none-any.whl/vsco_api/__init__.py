d3527b08c652002560c69cf962705cdbff87d43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:05:12 +0000

    Add package vsco-api/0.2.1/vsco_api-0.2.1-py3-none-any.whl

---
+++ packages/vsco-api/vsco_api-0.2.1-py3-none-any.whl/vsco_api/__init__.py
@@ -0,0 +1,10 @@
+from vsco_api._vsco import VscoMediaType, VscoMedia, ProfileIterator, get_media_page, get_site_id, set_bearer_token
+
+__all__ = [
+    "VscoMediaType",
+    "VscoMedia",
+    "ProfileIterator",
+    "get_media_page",
+    "get_site_id",
+    "set_bearer_token"
+]
