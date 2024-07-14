36a078055f3f399826cd3efd4668bb20f53b2dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:10 +0000

    Add package pygcloud/0.0.38/pygcloud-0.0.38-py3-none-any.whl

---
+++ packages/pygcloud/pygcloud-0.0.38-py3-none-any.whl/pygcloud/gcp/services/certificate.py
@@ -0,0 +1,34 @@
+"""
+echo "INFO: Creating certificate ${NAME}"
+gcloud compute ssl-certificates create ${NAME} \
+--project=${PROJECT_ID} \
+--domains "${DOMAIN}"
+
+@author: jldupont
+"""
+from pygcloud.models import GCPServiceSingletonImmutable
+
+
+class SSLCertificateService(GCPServiceSingletonImmutable):
+    """
+    https://cloud.google.com/sdk/gcloud/reference/beta/compute/ssl-certificates
+    """
+    REQUIRES_DESCRIBE_BEFORE_CREATE = True
+    PREFIX = ["compute", "ssl-certificates"]
+
+    def __init__(self, name: str, domain: str):
+        assert isinstance(domain, str)
+        super().__init__(name=name, ns="ssl")
+        self._domain = domain
+
+    def params_describe(self):
+        return self.PREFIX + [
+            "describe", self.name,
+            "--format", "json"
+        ]
+
+    def params_create(self):
+        return self.PREFIX + [
+            "create", self.name,
+            "--domains", self.domain
+        ]
