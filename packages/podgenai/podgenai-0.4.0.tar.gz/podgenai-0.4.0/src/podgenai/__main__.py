11a03f3c6c9d16a351e7ac07d07e3985ed12b2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:04:53 +0000

    Add package podgenai/0.4.0/podgenai-0.4.0.tar.gz

---
+++ packages/podgenai/podgenai-0.4.0.tar.gz/podgenai-0.4.0/src/podgenai/__main__.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+import fire
+import os
+from pathlib import Path
+
+import podgenai.exceptions
+from podgenai.podgenai import generate_media
+from podgenai.content.topic import get_topic, ensure_topic_is_valid
+from podgenai.util.openai import ensure_openai_key
+from podgenai.util.sys import print_error
+
+
+def main(topic: Optional[str] = None, path: Optional[Path] = None, confirm: bool = True) -> None:
+    """Generate and write an audiobook podcast mp3 file for the given topic to the given output file path.
+
+    Params:
+    * `topic` (-t): Topic. If not given, the user is prompted for it.
+    * `path (-p)`: Output file or directory path.
+        If an intended file path, it must have an ".mp3" suffix. If a directory, it must exist, and the file name is auto-determined.
+        If not given, the output file is written to the repo directory with an auto-determined file name.
+    * `confirm (-c)`: Confirm before full-text and speech generation.
+        If `True`, a confirmation is interactively sought after generating and printing the list of subtopics, before generating the full-text, and also before generating the speech. Its default is `True`.
+        If `False`, the full-text and speech are generated without confirmations.
+
+    A nonzero exitcode exists if there is an error.
+    """
+    try:
+        ensure_openai_key()
+
+        if not topic:
+            topic = get_topic()
+        ensure_topic_is_valid(topic)
+
+        if path:
+            path = Path(path)
+
+        if not isinstance(confirm, bool):
+            raise podgenai.exceptions.InputError(f"`confirm` (-c) argument has an invalid value {confirm!r} of type {type(confirm)}. Its value, if specified, is required to be a boolean.")
+
+        generate_media(topic, output_path=path, confirm=confirm)
+    except podgenai.exceptions.Error as exc:
+        print_error(str(exc))
+        print_error(f"Failed to generate for topic: {topic}")
+        exit(1)
+    except KeyboardInterrupt:
+        print()  # This separates "^C" from the subsequent error.
+        print_error("Interrupted by user.")
+        os._exit(-2)  # Plain `exit` is not used because it may not immediately terminate, with background threads potentially still running.
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
