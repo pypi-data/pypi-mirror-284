2dbca23ce46818710ddafa85c01b85d64f63a71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:05:48 +0000

    Add package phlop/0.0.23/phlop-0.0.23-py3-none-any.whl

---
+++ packages/phlop/phlop-0.0.23-py3-none-any.whl/phlop/run/mpirun_perf.py
@@ -0,0 +1,40 @@
+#
+#
+
+import logging
+import os
+import sys
+
+from phlop.app import perf as p
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+MPI_RANK = os.environ.get("OMPI_COMM_WORLD_RANK")
+
+
+def verify_cli_args(cli_args):
+    try:
+        cli_args.interval = int(cli_args.interval)
+    except ValueError:
+        raise ValueError("Interval must be an integer") from None
+    if cli_args.yaml:
+        cli_args.yaml = f"{cli_args.yaml}.{MPI_RANK}.yaml"
+        cli_args.summary = False
+    sys.argv = [sys.argv[0]]  # drop everything!
+    return cli_args
+
+
+def main():
+    parser = p.cli_args_parser()
+    # cli_args = verify_cli_args(parser.parse_args())
+    try:
+        ...
+    except (Exception, SystemExit) as e:
+        logger.exception(e)
+        parser.print_help()
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
