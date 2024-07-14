b52f54b25f9ac7ce1116cdb592c890aa774dbb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:05:33 +0000

    Add package ipctk/1.2.1/ipctk-1.2.1-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/ipctk/ipctk-1.2.1-cp312-cp312-macosx_11_0_arm64.whl/ipc/candidates/edge_face.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <ipc/ccd/ccd.hpp>
+
+#include <Eigen/Core>
+
+#include <vector>
+
+namespace ipc {
+
+/// @brief Candidate for <b>intersection</b> between edge and face.
+///
+/// Not included in Candidates because it is not a collision candidate.
+class EdgeFaceCandidate {
+public:
+    EdgeFaceCandidate(long edge_id, long face_id);
+
+    bool operator==(const EdgeFaceCandidate& other) const;
+    bool operator!=(const EdgeFaceCandidate& other) const;
+    /// @brief Compare EdgeFaceCandidate for sorting.
+    bool operator<(const EdgeFaceCandidate& other) const;
+
+    template <typename H>
+    friend H AbslHashValue(H h, const EdgeFaceCandidate& fv)
+    {
+        return H::combine(std::move(h), fv.edge_id, fv.face_id);
+    }
+
+    /// @brief ID of the edge
+    long edge_id;
+    /// @brief ID of the face
+    long face_id;
+};
+
+} // namespace ipc
