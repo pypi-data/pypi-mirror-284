7013ef450ff7843cf25b7fbeeb12554de30686ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:31 +0000

    Add package codern/3.7.8/Codern-3.7.8.tar.gz

---
+++ packages/codern/Codern-3.7.8.tar.gz/Codern-3.7.8/Codern/Client.py
@@ -0,0 +1,60 @@
+import requests,urllib3,os
+import json
+from typing import Literal
+
+class Email:
+    def __init__(
+            self,
+            To: str,
+            text: str,
+            Title: str,
+            Token: str,
+            Input: Literal['info', 'app', 'Login', 'support'] = 'info'
+        ) -> None:
+        self.To = To
+        self.text = text
+        self.Title = Title
+        self.Input = Input
+        self.Token = Token
+    
+    def Send(self):
+        Data = {
+            'email': self.To,
+            'text': str(self.text),  # تبدیل به رشته اطمینان می‌دهد که همیشه رشته است
+            'head': self.Title,
+            'token': self.Token,
+            'title': self.Input
+        }
+        
+        url = 'https://api-free.ir/api2/email.php'
+        response = requests.post(url, data=Data)
+        pars = response.json()
+        print(pars)
+        
+        if pars.get('ok', False):
+            return {
+                'state': 'ok',
+                'code': 200
+            }
+        else:
+            return False
+
+class Upload():
+    def __init__(self,data,token)-> None:
+        self.file=data
+        self.token=token
+    def requests(self):
+        requests = urllib3.PoolManager()
+        with open(self.data,'rb') as file:
+            response = requests.request(
+                'POST',
+                'https://api-free.ir/api2/upload.php',
+                fields={
+                    'token': self.token,
+                    'file': (os.path.basename(self.data),
+                             file.read()
+                            )
+                },
+                timeout=60
+                )
+        return json.loads(response.data.decode('utf-8'))
