00451c5c1b5b3ac1c0e1ff1484cf625ac5f6d3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:05:14 +0000

    Add package srtseg/0.2.0/srtseg-0.2.0.tar.gz

---
+++ packages/srtseg/srtseg-0.2.0.tar.gz/srtseg-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="srtseg",
+    version="0.2.0",
+    author="Jianshuo Wang",
+    author_email="jianshuo@hotmail.com",
+    description="""The simplest file format is .srt, and it can be handled pretty well with srt lib.
+srtseg provides the additional feature on top of srt:
+* Get duration of each video (cut in the middle of the space between two subtitle)
+* Segment selection
+* M3U8 export
+* WebVTT export""",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jianshuo/srtseg",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
