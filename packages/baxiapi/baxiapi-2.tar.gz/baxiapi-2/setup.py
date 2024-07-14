651a5ab98680a8f4cd12a1f6e9d32aed1ac02875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:07 +0000

    Add package baxiapi/2/baxiapi-2.tar.gz

---
+++ packages/baxiapi/baxiapi-2.tar.gz/baxiapi-2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='baxiapi',
+    version='2',
+    packages=["baxiapi"],
+    install_requires=["requests", "wavelink", "discord.py", "reds-simple-logger"],
+    author="Red_Wolf2467",
+    author_email="support@pyropixle.com",
+    description="Allows you to interact with our Baxi API.",
+    long_description=long_description,  # Setze die lange Beschreibung
+    long_description_content_type="text/markdown",
+    python_requires='>=3.8'
+)
