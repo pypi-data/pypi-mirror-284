0873288d4fa1402c14eb7b06d75410e4c26f2b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:23 +0000

    Add package sql4/1.0.0/sql4-1.0.0.tar.gz

---
+++ packages/sql4/sql4-1.0.0.tar.gz/sql4-1.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="sql4",
+    version="1.0.0",
+    author="Mahendra Sai Phaneeswar",
+    author_email="mhendrayerramsetti@gmail.com",
+    description="sql4",
+    long_description="sql4",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahendrayerramsetti",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
