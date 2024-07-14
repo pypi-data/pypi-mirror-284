6e1c80bb4f66af141fad1c7936ac7dcdc1b91b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:22 +0000

    Add package pygcloud/0.0.42/pygcloud-0.0.42-py3-none-any.whl

---
+++ packages/pygcloud/pygcloud-0.0.42-py3-none-any.whl/pygcloud/gcp/services/fwd_rules.py
@@ -0,0 +1,42 @@
+"""
+gcloud compute forwarding-rules create fwd-${NAME} \
+    --project=${PROJECT_ID} \
+    --target-https-proxy=${NAME} \
+    --global \
+    --ports=443 \
+    --address=${NAME_IP}
+
+@author: jldupont
+"""
+from pygcloud.models import GCPServiceSingletonImmutable
+
+
+class FwdRuleHTTPSProxyService(GCPServiceSingletonImmutable):
+    """
+    https://cloud.google.com/sdk/gcloud/reference/beta/compute/forwarding-rules
+    """
+    REQUIRES_DESCRIBE_BEFORE_CREATE = True
+    PREFIX = ["compute", "forwarding-rules"]
+
+    def __init__(self, name: str, proxy_name: str, ip_address_name: str):
+        assert isinstance(proxy_name, str)
+        assert isinstance(ip_address_name, str)
+        super().__init__(name=name, ns="fwd-rule")
+        self._proxy_name = proxy_name
+        self._ip_address_name = ip_address_name
+
+    def params_describe(self):
+        return self.PREFIX + [
+            "describe", self.name,
+            "--global",
+            "--format", "json"
+        ]
+
+    def params_create(self):
+        return self.PREFIX + [
+            "create", self.name,
+            "--global",
+            "--target-https-proxy", self._proxy_name,
+            "--address", self._ip_address_name,
+            "--ports", "443"
+        ]
