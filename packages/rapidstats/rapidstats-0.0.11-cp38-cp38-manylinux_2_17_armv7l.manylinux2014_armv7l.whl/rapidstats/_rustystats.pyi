b61a81c459007249c333133bd1bcdeade659afae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:37 +0000

    Add package rapidstats/0.0.11/rapidstats-0.0.11-cp38-cp38-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/rapidstats/rapidstats-0.0.11-cp38-cp38-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/rapidstats/_rustystats.pyi
@@ -0,0 +1,65 @@
+from typing import Literal, Optional
+
+import polars as pl
+
+ConfidenceInterval = tuple[float, float, float]
+
+def _confusion_matrix(df: pl.DataFrame) -> list[float]: ...
+def _bootstrap_confusion_matrix(
+    df: pl.DataFrame,
+    iterations: int,
+    z: float,
+    method: Literal["percentile", "BCa"],
+    seed: Optional[int],
+) -> list[ConfidenceInterval]: ...
+def _roc_auc(df: pl.DataFrame) -> float: ...
+def _bootstrap_roc_auc(
+    df: pl.DataFrame,
+    iterations: int,
+    z: float,
+    method: Literal["percentile", "BCa"],
+    seed: Optional[int],
+) -> ConfidenceInterval: ...
+def _max_ks(df: pl.DataFrame) -> float: ...
+def _bootstrap_max_ks(
+    df: pl.DataFrame,
+    iterations: int,
+    z: float,
+    method: Literal["percentile", "BCa"],
+    seed: Optional[int],
+) -> ConfidenceInterval: ...
+def _brier_loss(df: pl.DataFrame) -> float: ...
+def _bootstrap_brier_loss(
+    df: pl.DataFrame,
+    iterations: int,
+    z: float,
+    method: Literal["percentile", "BCa"],
+    seed: Optional[int],
+) -> ConfidenceInterval: ...
+def _mean(df: pl.DataFrame) -> float: ...
+def _bootstrap_mean(
+    df: pl.DataFrame,
+    iterations: int,
+    z: float,
+    method: Literal["percentile", "BCa"],
+    seed: Optional[int],
+) -> ConfidenceInterval: ...
+def _adverse_impact_ratio(df: pl.DataFrame) -> float: ...
+def _bootstrap_adverse_impact_ratio(
+    df: pl.DataFrame,
+    iterations: int,
+    z: float,
+    method: Literal["percentile", "BCa"],
+    seed: Optional[int],
+) -> ConfidenceInterval: ...
+def _percentile_interval(
+    bootstrap_stats: list[float], alpha: float
+) -> ConfidenceInterval: ...
+def _bca_interval(
+    original_stat: float,
+    bootstrap_stats: list[float],
+    jacknife_stats: list[float],
+    alpha: float,
+) -> ConfidenceInterval: ...
+def _norm_ppf(q: float) -> float: ...
+def _norm_cdf(x: float) -> float: ...
