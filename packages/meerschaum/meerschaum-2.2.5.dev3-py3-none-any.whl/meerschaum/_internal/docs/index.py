3a577082f83e4fccb941cae391beca21805614f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 14:05:27 +0000

    Add package meerschaum/2.2.5.dev3/meerschaum-2.2.5.dev3-py3-none-any.whl

---
+++ packages/meerschaum/meerschaum-2.2.5.dev3-py3-none-any.whl/meerschaum/_internal/docs/index.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+<img src="https://meerschaum.io/assets/banner_1920x320.png" alt="Meerschaum banner" style="width: 100%;"/>
+
+# Meerschaum Python API
+
+Welcome to the Meerschaum Python API technical documentation!
+
+- `meerschaum.actions`  
+  Access functions for actions and subactions.
+- `meerschaum.connectors`
+- `meerschaum.utils`  
+  Utility functions are available in several submodules, such as functions for:
+  - `meerschaum.utils.daemon`  
+    Managing background processes.\n
+  - `meerschaum.utils.dataframe`  
+    Manipulating dataframes.\n
+  - `meerschaum.utils.dtypes`  
+    Working with data types.\n
+  - `meerschaum.utils.formatting`  
+    Formatting output text.\n
+  - `meerschaum.utils.misc`  
+    Miscellaneous utility functions (e.g. `meerschaum.utils.misc.round_time()`).\n
+  - `meerschaum.utils.packages`  
+    Managing Python packages.\n
+  - `meerschaum.utils.prompt`  
+    Reading input from the user.\n
+  - `meerschaum.utils.schedule`  
+    Scheduling processes and threads.\n
+  - `meerschaum.utils.sql`  
+    Building SQL queries.\n
+  - `meerschaum.utils.venv`  
+    Managing virtual environments.\n
+  - `meerschaum.utils.warnings`  
+    Print warnings, errors, info, and debug messages.
+
+"""
