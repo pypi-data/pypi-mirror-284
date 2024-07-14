e82a99b6c3ebddddf4df8ee825fde3350f847e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 04:05:04 +0000

    Add package BerkeleyFCL/0.0.1/BerkeleyFCL-0.0.1.tar.gz

---
+++ packages/BerkeleyFCL/BerkeleyFCL-0.0.1.tar.gz/BerkeleyFCL-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='BerkeleyFCL',
+    version='0.0.1',
+    description='Berkeley Function Calling Lederboard',
+    long_description='The Berkeley Function Calling Leaderboard (also called Berkeley Tool Calling Leaderboard) evaluates the LLM\'s ability to call functions (aka tools) accurately. This leaderboard consists of real-world data and will be updated periodically.',
+    url='https://gorilla.cs.berkeley.edu/leaderboard',
+    author='Shishir Patil',
+    author_email='shishirpatil@berkeley.edu',
+    packages=[],
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+)
