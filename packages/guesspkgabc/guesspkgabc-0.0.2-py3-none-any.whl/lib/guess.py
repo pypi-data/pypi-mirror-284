d5f642e0761723fe1950d168d09cc8e8dad0cbe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 03:04:55 +0000

    Add package guesspkgabc/0.0.2/guesspkgabc-0.0.2-py3-none-any.whl

---
+++ packages/guesspkgabc/guesspkgabc-0.0.2-py3-none-any.whl/lib/guess.py
@@ -0,0 +1,19 @@
+def guess():
+    from random import randint
+    n = randint(1,100)
+    a = -1
+
+    guesses = 0
+
+    while a != n:
+        a = int(input("guss the number : "))
+        if(a>n):
+            print("lower number please")
+            guesses += 1
+
+        elif(a<n):
+            print("higher number please")
+            guesses += 1
+
+    print(f" you have guess the number {n} correctly in {guesses} attempt")
+
