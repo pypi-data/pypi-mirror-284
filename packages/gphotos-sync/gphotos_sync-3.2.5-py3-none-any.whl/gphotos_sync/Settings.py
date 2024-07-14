624e305e6ad4d4cfaadfeba3707dfb52496185ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-14 15:05:25 +0000

    Add package gphotos-sync/3.2.5/gphotos_sync-3.2.5-py3-none-any.whl

---
+++ packages/gphotos-sync/gphotos_sync-3.2.5-py3-none-any.whl/gphotos_sync/Settings.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from pathlib import Path
+
+from attr import dataclass
+
+"""
+Defines a dataclass for passing all configuration information between
+the worker classes
+"""
+
+
+@dataclass
+class Settings:
+    """
+    A Class to hold command line settings
+    """
+
+    start_date: datetime
+    end_date: datetime
+    use_start_date: bool
+
+    photos_path: Path
+    use_flat_path: bool
+
+    albums_path: Path
+    shared_albums_path: Path
+    album_index: bool
+    omit_album_date: bool
+    album_invert: bool
+    no_album_sorting: bool
+    album: str
+    album_regex: str
+    shared_albums: bool
+
+    favourites_only: bool
+    include_video: bool
+    archived: bool
+    use_hardlinks: bool
+
+    retry_download: bool
+    rescan: bool
+    max_retries: int
+    max_threads: int
+    case_insensitive_fs: bool
+    progress: bool
+
+    ntfs_override: bool
+
+    month_format: str
+    path_format: str
+
+    image_timeout: int
+    video_timeout: int
