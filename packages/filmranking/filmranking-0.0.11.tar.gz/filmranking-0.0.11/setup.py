04bdc6925ebe94a94c3c5fc202f2ef0f34f965e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:14 +0000

    Add package filmranking/0.0.11/filmranking-0.0.11.tar.gz

---
+++ packages/filmranking/filmranking-0.0.11.tar.gz/filmranking-0.0.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.11'
+DESCRIPTION = 'Final Project "FILM RANKING" for Python course mimuw'
+
+setup(
+    name="filmranking",
+    version=VERSION,
+    author="Hanna Bernikova",
+    author_email="<hb448385@students.mimuw.edu.pl>",
+    description=DESCRIPTION,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=['filmranking'],
+    install_requires=['pandas', 'numpy'],
+    entry_points={
+        'console_scripts': ['filmranking=filmranking:main']}
+)
