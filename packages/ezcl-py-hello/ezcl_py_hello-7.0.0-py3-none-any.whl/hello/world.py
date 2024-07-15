85a1899ed025c5513513907b95f60ef902c3f103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 04:05:29 +0000

    Add package ezcl-py-hello/7.0.0/ezcl_py_hello-7.0.0-py3-none-any.whl

---
+++ packages/ezcl-py-hello/ezcl_py_hello-7.0.0-py3-none-any.whl/hello/world.py
@@ -0,0 +1,52 @@
+# hello/world.py
+
+def hello(name):
+    """
+    Return a greeting message for the given name.
+
+    Parameters:
+    - name (str): Name to greet.
+
+    Returns:
+    - str: Greeting message.
+    """
+    return f"Hello, {name}!"
+
+def hello_world():
+    """
+    Return a generic greeting message.
+
+    Returns:
+    - str: Greeting message.
+    """
+    return "Hello, World!"
+
+def hi(name):
+    """
+    Return a casual greeting message for the given name.
+
+    Parameters:
+    - name (str): Name to greet.
+
+    Returns:
+    - str: Greeting message.
+    """
+    return f"Hi, {name}"
+
+def hi_world():
+    """
+    Return a casual greeting message.
+
+    Returns:
+    - str: Greeting message.
+    """
+    return "Hi, World!"
+
+def version():
+    """
+    Return the version number of the package.
+
+    Returns:
+    - str: Version number.
+    """
+    return "7.0.0"
