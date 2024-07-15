09b370256041b56c00b4a8a2b8cd3d7443e5e2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 14:06:11 +0000

    Add package FashTextPy/1.1/fashtextpy-1.1.tar.gz

---
+++ packages/FashTextPy/fashtextpy-1.1.tar.gz/fashtextpy-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='FashTextPy',
+	version='1.1',
+    author='Botik1220',
+	author_email='artemav2006@mail.ru',
+    description='Beautiful output to the python console/Красивый вывод в консоль python.',
+    url='https://github.com/Fashons/FashText',
+    packages=['FashTextPy'],
+)
