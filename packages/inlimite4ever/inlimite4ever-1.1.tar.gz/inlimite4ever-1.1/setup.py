3663c1a05d395f0aa271893cdfcf14788a4eb4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 04:06:17 +0000

    Add package inlimite4ever/1.1/inlimite4ever-1.1.tar.gz

---
+++ packages/inlimite4ever/inlimite4ever-1.1.tar.gz/inlimite4ever-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inlimite4ever',
+    version='1.1',
+    description="Im tring to use anything free, unlimite and FAST And also reliable",
+    author="UndrDskM = Ehsan vk",
+    author_email="gamer.good1400@gmail.com",
+    url="https://github.com/UndrDsk0M/unlimite4ever",
+    packages=find_packages()
+    
+)
