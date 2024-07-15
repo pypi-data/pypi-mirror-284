3c5ac8bc10c819f8e30772b5bafd461a146fd0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 05:05:40 +0000

    Add package onebusaway/0.1.0a1/onebusaway-0.1.0a1.tar.gz

---
+++ packages/onebusaway/onebusaway-0.1.0a1.tar.gz/onebusaway-0.1.0a1/examples/agency.py
@@ -0,0 +1,17 @@
+
+
+import onebusaway
+
+
+def main_sync() -> None:
+    client = onebusaway.OnebusawaySDK(api_key="TEST")
+    agency = client.agency.retrieve("1")
+
+    if agency.data and agency.data.entry:
+        print(agency.data.entry)
+    else:
+        print("Agency data or entry is None.")
+
+if __name__ == "__main__":
+    print("Running synchronous main function:")
+    main_sync()
---
+++ packages/onebusaway/onebusaway-0.1.0a1.tar.gz/onebusaway-0.1.0a1/src/onebusaway/types/__init__.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import References as References, ResponseWrapper as ResponseWrapper
+from .trip_retrieve_response import TripRetrieveResponse as TripRetrieveResponse
+from .route_retrieve_response import RouteRetrieveResponse as RouteRetrieveResponse
+from .agency_retrieve_response import AgencyRetrieveResponse as AgencyRetrieveResponse
+from .config_retrieve_response import ConfigRetrieveResponse as ConfigRetrieveResponse
+from .stops_for_route_list_params import StopsForRouteListParams as StopsForRouteListParams
+from .trip_detail_retrieve_params import TripDetailRetrieveParams as TripDetailRetrieveParams
+from .stops_for_route_list_response import StopsForRouteListResponse as StopsForRouteListResponse
+from .trip_detail_retrieve_response import TripDetailRetrieveResponse as TripDetailRetrieveResponse
+from .current_time_retrieve_response import CurrentTimeRetrieveResponse as CurrentTimeRetrieveResponse
+from .arrival_and_departure_list_params import ArrivalAndDepartureListParams as ArrivalAndDepartureListParams
+from .stops_for_location_retrieve_params import StopsForLocationRetrieveParams as StopsForLocationRetrieveParams
+from .trips_for_location_retrieve_params import TripsForLocationRetrieveParams as TripsForLocationRetrieveParams
+from .arrival_and_departure_list_response import ArrivalAndDepartureListResponse as ArrivalAndDepartureListResponse
+from .stops_for_location_retrieve_response import StopsForLocationRetrieveResponse as StopsForLocationRetrieveResponse
+from .trips_for_location_retrieve_response import TripsForLocationRetrieveResponse as TripsForLocationRetrieveResponse
+from .arrival_and_departure_retrieve_params import (
+    ArrivalAndDepartureRetrieveParams as ArrivalAndDepartureRetrieveParams,
+)
+from .arrival_and_departure_retrieve_response import (
+    ArrivalAndDepartureRetrieveResponse as ArrivalAndDepartureRetrieveResponse,
+)
+from .agencies_with_coverage_retrieve_response import (
+    AgenciesWithCoverageRetrieveResponse as AgenciesWithCoverageRetrieveResponse,
+)
