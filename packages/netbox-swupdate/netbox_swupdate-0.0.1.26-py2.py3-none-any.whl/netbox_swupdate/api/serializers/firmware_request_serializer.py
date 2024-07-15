9a145be10d0b6a1e115ec50e24f5d1f7d8b1c302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 16:05:04 +0000

    Add package netbox-swupdate/0.0.1.26/netbox_swupdate-0.0.1.26-py2.py3-none-any.whl

---
+++ packages/netbox-swupdate/netbox_swupdate-0.0.1.26-py2.py3-none-any.whl/netbox_swupdate/api/serializers/firmware_request_serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from netbox_swupdate.models import FirmwareRequest
+
+__all__ = ("FirmwareRequestSerializer",)
+
+
+class FirmwareRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FirmwareRequest
+        fields = [
+            "firmware_request_id",
+            "device",
+            "deploy",
+            "status",
+            "deploy_time",
+            "retries",
+            "retry_interval",
+        ]
