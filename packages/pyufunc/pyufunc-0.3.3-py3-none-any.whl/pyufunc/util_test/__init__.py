1e7e74733bd9f27b3678972d290329284584dd6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 14:06:02 +0000

    Add package pyufunc/0.3.3/pyufunc-0.3.3-py3-none-any.whl

---
+++ packages/pyufunc/pyufunc-0.3.3-py3-none-any.whl/pyufunc/util_test/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+__all__ = []
+
+# TODO: pytest for general assertion
+# TODO: pytest for connect to DB
+# TODO: pytest for raise exception
+# TODO: pytest for warning
