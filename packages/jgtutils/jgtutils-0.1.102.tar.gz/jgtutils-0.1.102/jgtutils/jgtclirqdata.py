901d4806e2feac1a1a2aa6c3fd63e167a6203c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 15:05:27 +0000

    Add package jgtutils/0.1.102/jgtutils-0.1.102.tar.gz

---
+++ packages/jgtutils/jgtutils-0.1.102.tar.gz/jgtutils-0.1.102/jgtutils/jgtclirqdata.py
@@ -0,0 +1,123 @@
+"""
+Future use: JSON data for requests that we load from this to the cli args.
+
+example:
+
+--load_pattern CDS_RQ_JSON_NORM_FRESH
+
+"""
+
+PDS_RQ_JSON_BASE="""
+{
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+
+PDS_RQ_JSON_NORMAL="""
+{
+  "use_full": false,
+  "use_fresh": false,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+PDS_RQ_JSON_NORMAL_FRESH="""
+{
+  "use_full": false,
+  "use_fresh": true,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+PDS_RQ_JSON_FULL="""
+{
+  "use_full": true,
+  "use_fresh": false,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+
+PDS_RQ_JSON_FULL_FRESH="""
+{
+  "use_full": true,
+  "use_fresh": false,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+
+IDS_RQ_JSON_BASE="""
+{
+  "mfi_flag": true,
+  "balligator_flag": true,
+  "talligator_flag": true,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+
+CDS_RQ_JSON_NORMAL="""
+{
+  "use_full": false,
+  "use_fresh": false,
+  "mfi_flag": true,
+  "balligator_flag": true,
+  "talligator_flag": true,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+CDS_RQ_JSON_FULL="""
+{
+  "use_full": true,
+  "use_fresh": false,
+  "mfi_flag": true,
+  "balligator_flag": true,
+  "talligator_flag": true,
+  "quotescount": -1,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+
+
+CDS_RQ_JSON_FULL_FRESH="""
+{
+  "use_full": true,
+  "use_fresh": true,
+  "mfi_flag": true,
+  "balligator_flag": true,
+  "talligator_flag": true,
+  "quotescount": -1,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+
+CDS_RQ_JSON_NORM_FRESH="""
+{
+  "use_full": false,
+  "use_fresh": true,
+  "mfi_flag": true,
+  "balligator_flag": true,
+  "talligator_flag": true,
+  "quotescount": -1,
+  "dropna_volume": true,
+  "keep_bid_ask": true
+}
+"""
+
+CDS_RQ_JSON_NORM_FRESH__NO_MFI="""
+{
+  "use_full": false,
+  "use_fresh": true,
+  "mfi_flag": false,
+  "balligator_flag": true,
+  "talligator_flag": true,
+  "quotescount": -1,
+  "dropna_volume": false,
+  "keep_bid_ask": true
+}
+"""
