ef935eb1ff24a3721ed3cfac426da4d6e7d9c800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:17 +0000

    Add package ammg/0.2.2/ammg-0.2.2.tar.gz

---
+++ packages/ammg/ammg-0.2.2.tar.gz/ammg-0.2.2/README.md
@@ -0,0 +1,115 @@
+# Apple Music Metadata Grabber (ammg)
+
+ammg  is a  tool for  appending metadata  to music
+files  from Apple  Music. Mainly  files downloaded
+from Youtube Music  (not youtube), because Youtube
+Music (albums/singles/ep) and Apple Music (albums)
+holds the same song info...
+
+With ammg you choose your youtube music songs, and
+you choose which metadata.
+
+__Note:__  Supported file  types  are __m4a__  and
+__opus__.  Using __opus__  is better  than __m4a__
+because the  best quality available on  youtube is
+__opus__.
+
+However __m4a__  is used if  you want to  open the
+files in iTunes and sync them to your iPhone/iPad.
+
+## Installation
+
+You                  can                  download
+[ammg](https://pypi.org/project/ammg/) using pip:
+
+```sh
+pip install --upgrade ammg
+```
+
+## Usage
+
+- __First:__ Choose your song, album, or single.
+- __Second:__ Download the __album playlist__ from Youtube Music.
+- __Third:__ Copy the __album ID__ from the search bar from Apple Music.
+
+__Example:__   We  will   be  downloading   `Scary
+Pockets,  Elise Trouw  -  Careless Whisper  (feat.
+Dave Koz)`.
+
+- Apple Music link: https://music.apple.com/us/album/careless-whisper-feat-dave-koz-single/1681177202
+    - Album ID: 1681177202
+- Youtube Music link: https://music.youtube.com/playlist?list=OLAK5uy_kVIMiCrxX4gmnZI-IufqRvJte6Hk3NTbY
+
+>   Notice:   the   Youtube  Music   link   is   a
+> __playlist__. By downloading  using the __playlist
+> (album link)__,  you will be able  to add metadata
+> without manual intervetion.
+
+__Command  Process:__  Download  the  music  using
+[yt-dlp](https://github.com/yt-dlp/yt-dlp)   (make
+sure you download yt-dlp first).
+
+- __Downloading__ `.opus` file using `yt-dlp`:
+
+```sh
+yt-dlp -cw -o "music/%(title)s.%(ext)s" -x --audio-quality "0" "https://music.youtube.com/playlist?list=OLAK5uy_kVIMiCrxX4gmnZI-IufqRvJte6Hk3NTbY"
+```
+
+- __Downloading__ `.m4a` file using `yt-dlp`:
+
+```sh
+yt-dlp -cw -o "music/%(title)s.%(ext)s" -x --audio-format m4a --audio-quality "0" "https://music.youtube.com/playlist?list=OLAK5uy_kVIMiCrxX4gmnZI-IufqRvJte6Hk3NTbY"
+```
+
+This  will  download  `Careless  Whisper`  in  the
+`music`  folder. So  the  path  of the  downloaded
+song will  be `music/Careless Whisper  (feat. Dave
+Koz).opus`.
+
+
+__Adding__ the metadata using `ammg`:
+
+__Required__ arguments:
+
+- `-i`: Apple album id.
+- `-o`: Output directory.
+    - The directory where the file will be once it is saved.
+- `positional argument`: here `music` where the downloaded song is.
+
+```sh
+ammg get -i 1681177202 -o Ordered_Music music
+```
+
+If you are querying a lot of songs in one session,
+it is adviced to use `--do-not-check-token`.
+
+```sh
+ammg get --do-not-check-token -i 1681177202 -o Ordered_Music music
+```
+
+After  embedding   metadata,  the  file   will  be
+moved  to `Ordered_Music/S/Scary  Pockets &  Elise
+Trouw/Careless   Whisper   (feat.  Dave   Koz)   -
+Single/`. That  way we will have  an ordered music
+library.
+
+## Simplifying our work
+
+We can using `bash` scripting on Linux, to created
+alias  or functions  that simplify  our work,  and
+make the process faster.
+
+Alias `domus` to download music:
+
+```sh
+alias domus='yt-dlp -cw -o "music/%(title)s.%(ext)s" -x --audio-quality "0"'
+```
+
+A shell function that accepts a link and album id:
+
+```sh
+domuspp() {
+    domus "$1";
+    ammg get --do-not-check-token -i "$2" -o Ordered_Music music;
+}
+```
---
+++ packages/ammg/ammg-0.2.2.tar.gz/ammg-0.2.2/src/ammg/ammg.py
@@ -0,0 +1,488 @@
+#!/usr/bin/env python3
+
+# Author: Raphael Tannous
+# Source: https://github.com/rofe33/ammg
+# License: GPLv3
+
+import argparse
+import pathlib
+import sys
+import mutagen
+import shutil
+import requests
+import math
+import platform
+import os
+
+from .embed_metadata_to_file import EmbedMetadataToFile
+from .get_apple_music_token import GetAppleMusicToken
+from .api_work import ApiMusicApple
+from .ammg_cache import AmmgCache
+
+# Some Terminal Colors
+FR = '\033[00m'  # Reset
+FB = '\033[01m'  # Bold
+CR = '\033[31m'  # Red
+CG = '\033[32m'  # Green
+CY = '\033[33m'  # Yellow
+
+CACHE = AmmgCache().cache_dir
+
+if platform.system() == 'Windows':
+    os.system('')
+
+
+def parse_arguments():
+    epilog = 'All The Glory To Jesus God...'
+    parser = argparse.ArgumentParser(
+        prog='ammg',
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=(
+            'A tool for appending metadata from Apple Music to music files.'
+        ),
+        epilog=epilog,
+        add_help=True,
+        allow_abbrev=True,
+        exit_on_error=True,
+    )
+
+    # Version
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        version='%(prog)s 0.2.2',
+    )
+
+    subparsers = parser.add_subparsers(
+        title='Commamds',
+        required=True,
+    )
+
+    # Main Program
+    get_parser_help = 'Download music metadata and embed them into file.'
+    get_parser = subparsers.add_parser(
+        name='get',
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=get_parser_help,
+        epilog=epilog,
+        help=get_parser_help,
+        add_help=True,
+        allow_abbrev=True,
+        exit_on_error=True,
+    )
+
+    get_parser.add_argument(
+        'directory',
+        type=pathlib.Path,
+        help=(
+            'Path to the directory that contains the '
+            'music downladed using yt-dlp'
+        ),
+        metavar='path/to/directory',
+    )
+
+    get_parser.add_argument(
+        '-i',
+        '--album-id',
+        type=int,
+        help='Album id from Apple Music',
+        required=True,
+    )
+
+    get_parser.add_argument(
+        '-o',
+        '--output-directory',
+        type=pathlib.Path,
+        help='Output directory in which the music files will be moved',
+        metavar='path/to/output_directory',
+        required=True,
+    )
+
+    get_parser.add_argument(
+        '-s',
+        '--storefront',
+        type=str,
+        help=(
+            'Storefront to be used when querying metadata'
+            f' {FB}(default: us){FR}'
+        ),
+        default='us',
+    )
+
+    get_parser.add_argument(
+        '-t',
+        '--tracks',
+        nargs='+',
+        type=int,
+        help='Make metadata for just a bunch of tracks',
+    )
+
+    get_parser.add_argument(
+        '-c',
+        '--clean-request',
+        action='store_true',
+        help=f'Don\'t get cached requests {FB}(default: False){FR}',
+    )
+
+    get_parser.add_argument(
+        '--cover-width',
+        type=int,
+        help=f'Cover width  {FB}(default: 500){FR}',
+        default=500,
+
+    )
+
+    get_parser.add_argument(
+        '--cover-height',
+        type=int,
+        help=f'Cover height {FB}(default: 500){FR}',
+        default=500,
+    )
+
+    get_parser.add_argument(
+        '--do-not-check-token',
+        action='store_false',
+        default=True,  # that means check_token is true by default
+        help=(
+            'Whether to not check token before using it'
+            f' {FB}(default: false){FR}'
+        ),
+    )
+
+    get_parser.add_argument(
+        '--duration-error',
+        type=int,
+        default=5,
+        help=(
+            'Duration error when attempting to look correct file music'
+            f' {FB}(default: 5){FR}'
+        ),
+    )
+
+    get_parser.add_argument(
+        '--create-lrc-file',
+        action='store_true',
+        help=(
+            'Whether to create a .lrc file next to the opus file'
+            f' {FB}(default: false){FR}'
+        ),
+    )
+
+    get_parser.add_argument(
+        '--lrc-file-text',
+        type=str,
+        default='No Lyrics.',
+        help=(
+            'Text written in the .lrc file'
+            f' {FB}(default: No Lyrics.){FR}'
+        ),
+    )
+
+    get_parser.add_argument(
+        '--create-txt-file',
+        action='store_true',
+        help=(
+            'Whether to create a .txt file next to the opus file'
+            f' {FB}(default: false){FR}'
+        ),
+    )
+
+    get_parser.add_argument(
+        '--txt-file-text',
+        type=str,
+        default='No Lyrics.',
+        help=(
+            'Text written in the .txt file'
+            f' {FB}(default: No Lyrics.){FR}'
+        ),
+    )
+
+    # Cache Work
+    cache_parser_help = 'Check and work with cache.'
+    cache_parser = subparsers.add_parser(
+        name='cache',
+        description=cache_parser_help,
+        epilog=epilog,
+        help=cache_parser_help,
+        add_help=True,
+        allow_abbrev=True,
+        exit_on_error=True,
+    )
+
+    cache_group = cache_parser.add_argument_group(
+        title='Cache Work',
+    )
+
+    cache_group_exclusive = cache_group.add_mutually_exclusive_group(
+        required=False,
+    )
+
+    cache_group.add_argument(
+        '--dry-run',
+        action='store_true',
+        default=False,
+        help='Can be used with --clean-cache, but not removing anything.'
+    )
+
+    cache_group_exclusive.add_argument(
+        '--get-cache-size',
+        action='store_true',
+        default=False,
+        help='Print the size of the cache in Megabytes.',
+    )
+
+    cache_group_exclusive.add_argument(
+        '--clean-cache',
+        action='store_true',
+        default=False,
+        help='Removes the cache folder from system.',
+    )
+
+    cache_group_exclusive.add_argument(
+        '--get-cache-path',
+        action='store_true',
+        default=False,
+        help='Print the path of the cache.',
+    )
+
+    get_parser.set_defaults(
+        func=get_args,
+    )
+
+    cache_parser.set_defaults(
+        func=cache_args,
+    )
+
+    args = parser.parse_args()
+
+    args.func(args)
+
+    return None
+
+
+def get_args(args):
+    """Work with the get command."""
+    token = GetAppleMusicToken(
+        check_token=args.do_not_check_token,
+    ).get_token()
+
+    api = ApiMusicApple(
+        token=token,
+        album_id=args.album_id,
+        storefront=args.storefront,
+        clean_request=args.clean_request,
+    )
+
+    # Requesting album info and analyzing int
+    api_response = api.get_response_data()
+    album_info = api.analyze_response_album(api_response)
+
+    # Downloading Cover to cache
+    cover_path: pathlib.Path = CACHE.joinpath(f'{args.album_id}.jpg')
+    cover_url: str = album_info.get('cover_url', '')
+    cover_url = cover_url.replace(
+        '{w}',
+        str(args.cover_width),
+    ).replace(
+        '{h}',
+        str(args.cover_height),
+    )
+
+    # Cover Info
+    cover_info = {
+        'type': 3,
+        'mime': 'image/jpeg',
+        'width': args.cover_width,
+        'height': args.cover_height,
+        'depth': 8,
+    }
+
+    # If clean request re-download the cover
+    if args.clean_request or not cover_path.is_file():
+        response = requests.get(cover_url, stream=True)
+
+        with open(cover_path, 'wb') as cf:
+            shutil.copyfileobj(response.raw, cf)
+
+    # Output directory work
+    output_directory: pathlib.Path = args.output_directory
+
+    alphabet_directory: pathlib.Path = output_directory.joinpath(
+        album_info.get('album_artist_name', [''])[0].upper()
+    )
+
+    artist_directory: pathlib.Path = alphabet_directory.joinpath(
+        album_info.get('album_artist_name', '')
+    )
+
+    album_directory = artist_directory.joinpath(
+        album_info.get('album_name', '')
+    )
+
+    for track in album_info.get('tracks', []):
+        track_number = track.get('track_number')
+
+        if (args.tracks is not None
+                and track_number not in args.tracks):
+            continue
+
+        print(
+            f'Looking for {FB}{track.get('title')}{FR}'
+            f' in {FB}{args.directory}{FR}.'
+        )
+
+        # Valid file for tags, is based on:
+        #   - Youtube title found in apple title
+        #   - Duration error is under 5 seconds (Default)
+        directory: pathlib.Path = args.directory
+        apple_duration = track.get('duration') // 1000
+        apple_title = track.get('title').lower()
+
+        directory_files = [x for x in directory.iterdir() if x.is_file()]
+
+        music_file = None
+        for file in directory_files:
+            file_duration = math.floor(mutagen.File(file).info.length)
+            file_title = file.stem.lower()
+
+            # Error for duration difference
+            error = math.fabs(apple_duration - file_duration)
+
+            # 5 seconds difference (Default)
+            if error <= args.duration_error and file_title in apple_title:
+                music_file = file
+
+        if music_file is None:
+            print(f'\t{FB}{CR}No music found.{FR}')
+            continue
+
+        if not (isinstance(
+                mutagen.File(music_file).info,
+                mutagen.oggopus.OggOpusInfo
+            )
+                or isinstance(
+                    mutagen.File(music_file).info,
+                    mutagen.mp4.MP4Info
+                )):
+            print(
+                f'\t{music_file} is not supported'
+                f' (Just {FB}opus{FR} and {FB}m4a{FR} are supported).'
+            )
+
+            sys.exit(2)
+
+        new_music_file = album_directory.joinpath(
+            f'{track.get("track_number"):#02} {track.get("title")}'
+            f'{music_file.suffix}'
+        )
+        new_txt_file = album_directory.joinpath(
+            f'{track.get("track_number"):#02} {track.get("title")}'
+            f'.txt'
+        )
+        new_lrc_file = album_directory.joinpath(
+            f'{track.get("track_number"):#02} {track.get("title")}'
+            f'.lrc'
+        )
+
+        # If  there's more  than one  disc create  a
+        # Disc ## folder for each disc.
+        if album_info['disc_count'] >= 2:
+            disc_dir = album_directory.joinpath(
+                f'Disc {track.get("discnumber"):#02}'
+            )
+
+            new_music_file = disc_dir.joinpath(
+                f'{track.get("track_number"):#02} {track.get("title")}'
+                f'{music_file.suffix}'
+            )
+            new_txt_file = disc_dir.joinpath(
+                f'{track.get("track_number"):#02} {track.get("title")}'
+                f'.txt'
+            )
+            new_lrc_file = disc_dir.joinpath(
+                f'{track.get("track_number"):#02} {track.get("title")}'
+                f'.lrc'
+            )
+
+        if music_file.is_file():
+            print(f'\tFound {FB}{music_file}{FR}')
+            print(f'\t{CY}Embeding Metadata{FR} ', end='')
+
+            music_info: dict[str, int] = track
+            music_info['album'] = album_info.get('album_name')
+            music_info['album_artist'] = album_info.get('album_artist_name')
+            music_info['date'] = album_info.get('album_release_date')
+            music_info['disctotal'] = album_info.get('disc_count')
+            music_info['tracktotal'] = album_info.get('track_count')
+            music_info['releasetype'] = album_info.get('release_type')
+            music_info['copyright'] = album_info.get('copyright')
+            music_info['label'] = album_info.get('record_label')
+
+            embed_data = EmbedMetadataToFile(
+                music_file=music_file,
+                music_info=music_info,
+                cover=cover_path,
+                cover_info=cover_info,
+            )
+
+            embed_data.save_music()
+            print(f'{CG}(done).{FR}')
+
+            if not album_directory.is_dir():
+                print('\tCreating album directory.')
+                album_directory.mkdir(parents=True)
+
+            if album_info['disc_count'] >= 2 and not disc_dir.is_dir():
+                print('\tCreating disc directory.')
+                disc_dir.mkdir()
+
+            print(f'\tMoving file to {new_music_file}.')
+
+            music_file.rename(new_music_file)
+
+            if args.create_lrc_file:
+                new_lrc_file.write_text('No Lyrics.')
+                print('\tCreating lrc file.')
+
+            if args.create_txt_file:
+                new_txt_file.write_text('No Lyrics.')
+                print('\tCreating txt file.')
+
+    sys.exit(0)
+
+
+def cache_args(args):
+    """Work with the cache command."""
+    cache = AmmgCache()
+
+    if args.get_cache_size:
+        cache_size = cache.get_cache_size()
+
+        print(
+            f'Actual file size: {cache_size} bytes'
+            f' ({cache_size / (1024 ** 2):.2f} Megabytes).'
+        )
+
+        sys.exit(0)
+
+    if args.get_cache_path:
+        print(cache.cache_dir.absolute())
+
+        sys.exit(0)
+
+    if args.clean_cache:
+        cache.clean_cache(args.dry_run)
+
+        sys.exit(0)
+
+    if args.dry_run:
+        print('You should use --dry-run with --clean-cache.')
+
+        sys.exit(0)
+
+
+def main():
+    parse_arguments()
+
+    sys.exit(0)
