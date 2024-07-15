115a6feeec7c363848febe2b5f3f9a8570008c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 16:05:11 +0000

    Add package django-channels-utils/0.1.5/django-channels-utils-0.1.5.tar.gz

---
+++ packages/django-channels-utils/django-channels-utils-0.1.5.tar.gz/django-channels-utils-0.1.5/django_channels_utils/abstract_consumer_handler/abstract_consumer_handler.py
@@ -0,0 +1,27 @@
+from .abstract_consumer_handler_errors import TypeMissingError, MethodMissingError
+
+HANDLER_ABSTRACT_CLASS_NAME= "AbstractConsumerHandler"
+
+
+class HandlerMeta(type):
+    def __new__(cls, name, bases, class_dict):
+        new_cls = super().__new__(cls, name, bases, class_dict)
+        if not hasattr(new_cls, 'handlers'):
+            new_cls.handlers = {}
+        if not HANDLER_ABSTRACT_CLASS_NAME in bases:
+            return new_cls
+        print("bases", bases)
+        if HANDLER_ABSTRACT_CLASS_NAME in bases and not hasattr(new_cls, 'type'):
+            raise TypeMissingError(name)
+        if HANDLER_ABSTRACT_CLASS_NAME in bases and not hasattr(class_dict, 'handle'):
+            raise MethodMissingError(name, 'handle')
+        new_cls.handlers[new_cls.type]=new_cls                        
+        
+
+class AbstractConsumerHandler(metaclass=HandlerMeta):
+    '''
+    Abstract class for websocket host handlers to subclass from
+    Subclassed handlers should have type attribute and handler method    
+    '''
+    def __init__(self):
+        super().__init__()
