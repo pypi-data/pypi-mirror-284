e0fdaf5b5e0001b7a93815595669433124aca59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 15:04:58 +0000

    Add package fastapi-hcytools/1.1.1/fastapi_hcytools-1.1.1-py3-none-any.whl

---
+++ packages/fastapi-hcytools/fastapi_hcytools-1.1.1-py3-none-any.whl/app/db_create.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+
+def db_create(db_url):
+	print(db_url)
+	engine = create_engine(db_url, echo=True)
+
+	from app.db.database import Base
+	from db.models.project_info import ProjectInfo
+
+	table_list = [
+		ProjectInfo.__table__,
+	]
+
+	Base.metadata.create_all(bind=engine, tables=table_list)
+	print("##### DB 생성 완료")
+	pass
+
+
+if __name__ == '__main__':
+	db_create("mysql://ictk:#ictk1234@localhost:3306/table-name?charset=utf8")
