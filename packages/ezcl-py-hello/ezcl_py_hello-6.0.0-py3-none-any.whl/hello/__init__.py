1e7b980c90b69da2d4bfd94c131bccd98c98e3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 04:05:25 +0000

    Add package ezcl-py-hello/6.0.0/ezcl_py_hello-6.0.0-py3-none-any.whl

---
+++ packages/ezcl-py-hello/ezcl_py_hello-6.0.0-py3-none-any.whl/hello/__init__.py
@@ -0,0 +1,13 @@
+def get_version():
+    # Logic to retrieve version dynamically, e.g., from a file or environment variable
+    return "6.0.0"
+
+__version__ = get_version()
+
+from .world import (
+    hello,
+    hello_world,
+    hi,
+    hi_world,
+    version
+)
