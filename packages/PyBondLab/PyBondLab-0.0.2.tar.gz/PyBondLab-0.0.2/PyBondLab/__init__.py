f8f55bc78c6da2ef1a9193f18e00238fd6b7cc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 04:05:10 +0000

    Add package PyBondLab/0.0.2/PyBondLab-0.0.2.tar.gz

---
+++ packages/PyBondLab/PyBondLab-0.0.2.tar.gz/PyBondLab-0.0.2/PyBondLab/__init__.py
@@ -0,0 +1,7 @@
+from .PyBondLab import StrategyFormation, load_breakpoints_WRDS
+from .StrategyClass import SingleSort, DoubleSort, Momentum
+
+
+__version__ = '0.0.2'
+__all__ = ['StrategyFormation', 'load_breakpoints_WRDS', 'SingleSort', 'DoubleSort', 'Momentum']
+name = 'PyBondLab' 
