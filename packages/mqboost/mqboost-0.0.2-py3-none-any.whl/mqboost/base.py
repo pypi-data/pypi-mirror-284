4017523919cb949c3eb72504fd3ccccad111afef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 14:06:31 +0000

    Add package mqboost/0.0.2/mqboost-0.0.2-py3-none-any.whl

---
+++ packages/mqboost/mqboost-0.0.2-py3-none-any.whl/mqboost/base.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+from collections.abc import Callable
+from dataclasses import dataclass
+
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+
+from mqboost.objective import check_loss_grad_hess, huber_loss_grad_hess
+
+
+# Name
+class BaseName:
+    def get(self, name):
+        if name in self.__annotations__:
+            return getattr(self, name)
+        else:
+            available_attributes = list(self.__annotations__.keys())
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'. "
+                f"Available attributes are: {', '.join(available_attributes)}"
+            )
+
+
+@dataclass
+class ModelName(BaseName):
+    lightgbm: str = "lightgbm"
+    xgboost: str = "xgboost"
+
+
+@dataclass
+class ObjectiveName(BaseName):
+    check: str = "check"
+    huber: str = "huber"
+
+
+# Functions
+TRAIN_DATASET_FUNC: dict[str, Callable] = {
+    "lightgbm": lgb.Dataset,
+    "xgboost": xgb.DMatrix,
+}
+
+MONOTONE_CONSTRAINTS_TYPE: dict[str, Callable] = {
+    "lightgbm": list,
+    "xgboost": tuple,
+}
+
+PREDICT_DATASET_FUNC: dict[str, Callable] = {
+    "lightgbm": lambda x: x,
+    "xgboost": xgb.DMatrix,
+}
+
+OBJECTIVE_FUNC: dict[str, Callable] = {
+    "check": check_loss_grad_hess,
+    "huber": huber_loss_grad_hess,
+}
+
+# Type
+XdataLike = pd.DataFrame | pd.Series | np.ndarray
+YdataLike = pd.Series | np.ndarray
+AlphaLike = list[float] | float
+ModelLike = lgb.basic.Booster | xgb.Booster
+
+# Exception
+class FittingException(Exception):
+    pass
+
+
+class ValidationException(Exception):
+    pass
