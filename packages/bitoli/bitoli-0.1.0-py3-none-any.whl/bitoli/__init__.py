50ab81d0e2df742793211d7a08d22fd54038e796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 04:05:09 +0000

    Add package bitoli/0.1.0/bitoli-0.1.0-py3-none-any.whl

---
+++ packages/bitoli/bitoli-0.1.0-py3-none-any.whl/bitoli/__init__.py
@@ -0,0 +1,11 @@
+from .bitoli import generate_key, encrypt_data, decrypt_data, adaptive_compress, adaptive_decompress, encode, decode
+
+__all__ = [
+    'generate_key',
+    'encrypt_data',
+    'decrypt_data',
+    'adaptive_compress',
+    'adaptive_decompress',
+    'encode',
+    'decode',
+]
