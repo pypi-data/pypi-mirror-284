380241e03172936680c8e8be2469701b9915b985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 16:05:28 +0000

    Add package ModelCompression/0.1.5/ModelCompression-0.1.5.tar.gz

---
+++ packages/ModelCompression/ModelCompression-0.1.5.tar.gz/ModelCompression-0.1.5/setup.py
@@ -0,0 +1,29 @@
+
+# Licensed under the MIT license.
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
+
+setup(
+    name = 'ModelCompression',
+    version = '0.1.5',
+    author = 'AI Team',
+    author_email = '710783765@qq.com',
+    description = 'Neural Network Compression',
+    long_description = read('README.rst'),
+    license = 'MIT',
+    url = None,
+
+	packages=find_packages('ModelCompression',exclude=["*.ModelZoo"]),
+	package_dir = {'':'ModelCompression'},
+    python_requires = '>=3.6.2',
+    install_requires = [
+        'thop',
+
+    ]
+
+
+)
