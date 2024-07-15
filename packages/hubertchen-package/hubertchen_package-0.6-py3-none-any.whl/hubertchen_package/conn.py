d83471e6856308d49ddcd6cac236c2402572f157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 04:06:14 +0000

    Add package hubertchen-package/0.6/hubertchen_package-0.6-py3-none-any.whl

---
+++ packages/hubertchen-package/hubertchen_package-0.6-py3-none-any.whl/hubertchen_package/conn.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+db_config = {
+    'host': os.getenv("DB_HOST"),
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'database': os.getenv("DB_DATABASE"),
+    'port':os.getenv('DB_PORT'),
+    'ssl_ca': 'api/mysql.cert'
+}
+
+
+def get_connection():
+
+    try:
+        conn = mysql.connector.connect(**db_config)
+        if conn.is_connected():
+            return conn
+    except Exception as e:
+        return e
+
