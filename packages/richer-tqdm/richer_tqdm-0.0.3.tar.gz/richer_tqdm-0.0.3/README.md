2f7f47e58f3a310ae9eb54247f8f16dadc75e364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:17 +0000

    Add package richer-tqdm/0.0.3/richer_tqdm-0.0.3.tar.gz

---
+++ packages/richer-tqdm/richer_tqdm-0.0.3.tar.gz/richer_tqdm-0.0.3/README.md
@@ -0,0 +1,3 @@
+# ⚡`richer_tqdm`
+
+Improved [`tqdm.rich`](https://github.com/tqdm/tqdm/blob/master/tqdm/rich.py) decorator for iterators and associated machinery.
---
+++ packages/richer-tqdm/richer_tqdm-0.0.3.tar.gz/richer_tqdm-0.0.3/richer_tqdm/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ──────────────────────────────────────────────────────────────────────────────
+from .richer import tqdm
+from .richer import tqdm_rich
+from .richer import trange
+from .richer import trrange
+
+# ──────────────────────────────────────────────────────────────────────────────
+del richer
