579778db441fdc80a4556c5070485595425f61bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:19 +0000

    Add package pyfunc-config/0.0.0/pyfunc_config-0.0.0-py3-none-any.whl

---
+++ packages/pyfunc-config/pyfunc_config-0.0.0-py3-none-any.whl/pyfunc-config/__init__.py
@@ -0,0 +1,12 @@
+# Auto-generated __init__.py
+
+# Version of the pyfunc2 package
+from _version import __version__
+
+# Import necessary modules and functions here
+from img_to_base64 import img_to_base64
+from to_lower_case import convert_char
+from to_lower_case import to_lower_case
+
+# Public API of the package
+__all__ = [img_to_base64, convert_char, to_lower_case]
