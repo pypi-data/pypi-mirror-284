bcedf291e3c9f08fec55467283534dd6a555b308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 16:05:29 +0000

    Add package pyf-pagination/0.0.3/pyf_pagination-0.0.3.tar.gz

---
+++ packages/pyf-pagination/pyf_pagination-0.0.3.tar.gz/pyf_pagination-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyf_pagination",
+    version="0.0.3",
+    packages=find_packages(),  # 自动找到所有包含的包
+    install_requires=[
+        "djangorestframework",
+    ],
+    author="ycx",
+    author_email="ycx3030@126.com",
+    description="A pagination package for Django REST Framework",
+    url="https://github.com/",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ],
+)
