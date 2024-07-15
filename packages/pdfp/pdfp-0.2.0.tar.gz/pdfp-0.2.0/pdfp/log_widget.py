d07c9235c7beb2a13cb71f8536450e59f54c4259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 05:05:44 +0000

    Add package pdfp/0.2.0/pdfp-0.2.0.tar.gz

---
+++ packages/pdfp/pdfp-0.2.0.tar.gz/pdfp-0.2.0/pdfp/log_widget.py
@@ -0,0 +1,133 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from pdfp.operations.file2pdf import file2pdf
+from pdfp.operations.png import pdf2png
+from pdfp.operations.ocr import ocr
+from pdfp.operations.crop import crop
+from pdfp.operations.trim import trim
+from pdfp.operations.clean_copy import clean_copy
+from pdfp.operations.tts import tts
+from pdfp.utils.tts_limit import ttsl
+from pdfp.utils.clean_text import ct
+from pdfp.button_widget import ButtonWidget
+from pdfp.file_tree_widget import FileTreeWidget
+from pdfp.progress_widget import ProgressWidget
+
+class LogWidget(QWidget):
+    """
+    Display logs and progress bars for pdfp operations.
+
+    This widget connects to various signals from different operations and displays log messages and progress.
+    
+    Attributes:
+        log_widget (QTextEdit): A text edit widget for displaying log messages.
+        pb_label (QLabel): A label for the progress bar.
+        progress_bar (QProgressBar): A progress bar to show operation progress.
+        pb_scroll_area (QScrollArea): A scroll area containing the progress bar and label.
+        layout (QHBoxLayout): The main layout of the widget.
+    """
+    def __init__(self):
+        super().__init__()
+        #logbox connections
+        file2pdf.op_msgs.connect(self.add_log_message)
+        pdf2png.op_msgs.connect(self.add_log_message)
+        ocr.op_msgs.connect(self.add_log_message)
+        crop.op_msgs.connect(self.add_log_message)
+        trim.op_msgs.connect(self.add_log_message)
+        clean_copy.op_msgs.connect(self.add_log_message)
+        tts.op_msgs.connect(self.add_log_message)
+        button_widget = ButtonWidget.instance()
+        button_widget.button_msgs.connect(self.add_log_message)
+        file_tree_widget = FileTreeWidget.instance()
+        file_tree_widget.file_added.connect(self.add_log_message)
+        ttsl.util_msgs.connect(self.add_log_message)
+        ct.util_msgs.connect(self.add_log_message)
+
+        #logbox
+        self.log_widget = QTextEdit()
+        self.log_widget.setReadOnly(True)
+
+        self.log_widget.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.log_widget.customContextMenuRequested.connect(self.show_context_menu)
+
+        #progress bar
+        self.progress_widget = ProgressWidget()
+
+        #layout
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.log_widget)
+        self.layout.addWidget(self.progress_widget)
+        self.layout.setContentsMargins(10,2,10,10)
+        self.layout.setSpacing(10)
+        self.layout.setStretch(0, 600)
+        self.layout.setStretch(1, 200)
+        self.progress_widget.setVisible(False)
+        self.setLayout(self.layout)
+
+    def add_log_message(self, message):
+        """
+        Append a log message to the log widget display.
+        Args:
+            message (str): The log message to append.
+        """
+        self.log_widget.append(message)
+
+    def show_context_menu(self, position):
+        """
+        Handle context menu events.
+        Args:
+            event (QContextMenuEvent): The context menu event.
+        """
+        copy_action = QAction(QIcon.fromTheme("edit-copy"), "Copy", self)
+        copy_action.triggered.connect(self.copy)
+        copy_action.setShortcut(QKeySequence("Ctrl+C"))
+        select_all_action = QAction(QIcon.fromTheme("edit-select-all"), "Select All", self)
+        select_all_action.triggered.connect(self.select_all)
+        select_all_action.setShortcut(QKeySequence("Ctrl+A"))
+        save_as_action = QAction(QIcon.fromTheme("document-save"), "Save to File", self)
+        save_as_action.triggered.connect(self.save_txt_file)
+        save_as_action.setShortcut(QKeySequence("Ctrl+S"))
+
+        menu = QMenu(self)
+        menu.addAction(copy_action)
+        menu.addAction(select_all_action)
+        menu.addAction(save_as_action)
+        has_text = bool(self.log_widget.toPlainText())
+        has_selection = self.log_widget.textCursor().selectedText() != ""
+        select_all_action.setEnabled(has_text)
+        save_as_action.setEnabled(has_text)
+        copy_action.setEnabled(False)
+        if has_text and has_selection:
+            copy_action.setEnabled(True)
+        menu.exec(self.log_widget.viewport().mapToGlobal(position))
+
+    def copy(self):
+        if (selected_text := self.log_widget.textCursor().selectedText()):
+            QApplication.clipboard().setText(selected_text)
+
+    def select_all(self):
+        self.log_widget.selectAll()
+
+    def save_txt_file(self):
+        """Open a file dialog to select or create an TXT file and write the log to that file."""
+        project_root = QDir.currentPath()
+        file_path, _ = QFileDialog.getSaveFileName(self,"Select or Create TXT File",project_root,"TXT Files (*.txt);;All Files (*)")
+        if file_path:
+            if not file_path.endswith(".txt"):
+                file_path += ".txt"
+            text = self.log_widget.toPlainText()
+            with open(file_path, 'w', encoding='utf-8') as output_txt_file:
+                output_txt_file.write(text)
+        return
+
+    def keyPressEvent(self, event):
+        """
+        Handle key press events.
+        Args:
+            event (QKeyEvent): The key press event.
+        """
+        if event.key() == Qt.Key_S and event.modifiers() == (Qt.ControlModifier):
+            self.save_txt_file()
+        else:
+            super().keyPressEvent(event)
---
+++ packages/pdfp/pdfp-0.2.0.tar.gz/pdfp-0.2.0/pdfp/operations/ocr.py
@@ -0,0 +1,140 @@
+import logging
+from PySide6.QtCore import QObject, Signal
+from PySide6.QtWidgets import QApplication
+from pdfp.settings_window import SettingsWindow
+from pdfp.utils.filename_constructor import construct_filename
+import ocrmypdf
+import pymupdf
+
+class SharedState:
+    """
+    Holds shared state information for tracking progress of operations.
+    Attributes:
+        progress (int): Current progress of the operation.
+        total_parts (int): Total parts of the operation.
+        progress_percentage (float): Percentage of completion of the operation.
+    """
+    def __init__(self):
+        self.progress = 0
+        self.total_parts = 0 
+        self.progress_percentage = 0
+
+class QueueHandler(logging.Handler):
+    """
+    Custom logging handler to process log messages and update shared state and UI elements accordingly.
+    Args:
+        shared_state (SharedState): Instance of SharedState to track operation progress.
+        op_msgs (Signal): Signal to emit operation messages. Connects to log_widget.
+        worker_progress (Signal): Signal to emit progress updates with worker name and percentage. Connects to progress_widget.
+        revise_worker_label (Signal): Signal to update worker labels. Connects to progress_widget.
+        worker_name (str): Name of the worker for logging purposes. Connects to progress_widget.
+    """
+    def __init__(self, shared_state, op_msgs, worker_progress, revise_worker_label, worker_name):
+        super().__init__()
+        self.shared_state = shared_state
+        self.op_msgs = op_msgs
+        self.worker_progress = worker_progress
+        self.revise_worker_label = revise_worker_label
+        self.worker_name = worker_name
+
+    def emit(self, record):
+        """
+        Processes the log record and updates the shared state and UI elements as needed.
+        Args:
+            record (LogRecord): Log record containing information about the operation.
+        Notes:
+            - If the log message is "Grafting", updates progress and progress percentage.
+            - If the log message is "Postprocessing...", updates progress bar label.
+        """
+        try:
+            msg = self.format(record)
+            if msg == "Grafting":
+                self.shared_state.progress += 1
+                self.shared_state.progress_percentage = (self.shared_state.progress / self.shared_state.total_parts) * 100
+                self.worker_progress.emit(self.worker_name, self.shared_state.progress_percentage)
+                QApplication.processEvents()
+            if msg == "Postprocessing...":
+                self.revise_worker_label.emit(self.worker_name, "OCR Postprocessing")
+                QApplication.processEvents()
+
+        except Exception:
+            self.handleError(record)
+
+class Converter(QObject):
+    """
+    Handles OCR (Optical Character Recognition) operations on PDF files using ocrmypdf.
+
+    Uses ocrmypdf and PyMuPDF to perform OCR on a specified PDF file and emits signals to update progress.
+
+    Signals:
+        op_msgs: Emits messages about the status of the OCR process. Connects to log_widget.
+        worker_progress: Updates the value of the progress bar during OCR. Connects to progress_widget.
+        revise_worker_label: Updates the label of the progress bar during OCR. Connects to progress_widget.
+        worker_done: Signals the completion of the OCR process. Connects to progress_widget.
+    """
+
+    op_msgs = Signal(str)
+    worker_progress = Signal(str, int)
+    revise_worker_label = Signal(str, str)
+    worker_done = Signal(str)
+    def __init__(self):
+        super().__init__()
+    def convert(self, file_tree, pdf):
+        """
+        Performs OCR on the specified PDF file.
+        Args:
+            file_tree (QWidget): The file tree widget where output files may be added.
+            pdf (str): Path of the PDF file to perform OCR on.
+        Notes:
+            - Emits a message if the provided file is not a PDF.
+            - Initializes shared state and sets up logging for progress tracking.
+            - Uses ocrmypdf to perform OCR on the PDF file.
+            - Emits progress updates and completion signals during the OCR process.
+        """
+        if not pdf.endswith('.pdf'):
+            self.util_msgs.emit(f"File is not a PDF.")
+            return
+
+        self.op_msgs.emit(f"OCRing {pdf}...")
+        QApplication.processEvents()
+
+        shared_state = SharedState()
+
+        worker_name = f"OCR_{pdf}"
+        self.worker_progress.emit(worker_name, 0)
+
+        logger = logging.getLogger('ocrmypdf')
+        logger.setLevel(logging.DEBUG)
+        handler = QueueHandler(shared_state, self.op_msgs, self.worker_progress, self.revise_worker_label, worker_name)
+        logger.addHandler(handler)
+
+        shared_state.total_parts = len(pymupdf.open(pdf))
+
+        output_file = construct_filename(pdf, "ocr_ps")
+
+        self.settings = SettingsWindow.instance()
+        deskew_toggle = self.settings.ocr_deskew_checkbox.isChecked()
+        # print(f"deskew: {deskew_toggle}")
+        if self.settings.ocr_pdf_radio.isChecked():
+            ocr_filetype = 'pdf'
+        else:
+            ocr_filetype = 'pdfa'
+        # print(f"filetype: {ocr_filetype}")
+        optimize_level = self.settings.ocr_optimize_level.value()
+        # print(f"optimize: {optimize_level}")
+
+        try:
+            ocrmypdf.ocr(pdf, output_file, deskew=deskew_toggle, output_type=ocr_filetype, optimize=optimize_level, progress_bar=False, force_ocr=True)
+            self.op_msgs.emit(f"OCR complete. Output: {output_file}")
+            if self.settings.add_file_checkbox.isChecked():
+                file_tree.add_file(output_file)
+        except Exception as e:
+            error_msg = f"Error converting {pdf}: {str(e)}"
+            self.op_msgs.emit(error_msg)
+
+        self.worker_done.emit(worker_name)
+        logger.disabled = True
+        logger.removeHandler(handler)
+        handler.close()
+
+ocr = Converter()
