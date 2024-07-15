9473ce75e1568335a4e67fac806e2de3e3fec0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 15:05:23 +0000

    Add package CFSpy/1.0.1/cfspy-1.0.1.tar.gz

---
+++ packages/CFSpy/cfspy-1.0.1.tar.gz/cfspy-1.0.1/CFS/single_iter_lie.py
@@ -0,0 +1,8 @@
+def single_iter_lie(eta, h,vector_field,z):
+    # The list that will contain all the Lie derivatives is initiated. 
+    LT = sp.Matrix([h])
+    
+    for i in eta:
+        
+        LT = LT.jacobian(z)*vector_field[:,i]
+    return LT
