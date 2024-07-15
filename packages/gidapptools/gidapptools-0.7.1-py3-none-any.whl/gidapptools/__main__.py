1429e948a79c0255561f0d311d885d5d0b0a1346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 14:06:14 +0000

    Add package gidapptools/0.7.1/gidapptools-0.7.1-py3-none-any.whl

---
+++ packages/gidapptools/gidapptools-0.7.1-py3-none-any.whl/gidapptools/__main__.py
@@ -0,0 +1,41 @@
+"""
+WiP.
+
+Soon.
+"""
+
+# region [Imports]
+
+# * Standard Library Imports ---------------------------------------------------------------------------->
+from pathlib import Path
+
+# * Gid Imports ----------------------------------------------------------------------------------------->
+from gidapptools.cli_info import cli_show_info
+# endregion [Imports]
+
+# region [TODO]
+
+
+# endregion [TODO]
+
+# region [Logging]
+
+
+# endregion [Logging]
+
+# region [Constants]
+
+THIS_FILE_DIR = Path(__file__).parent.absolute()
+
+# endregion [Constants]
+
+
+# region [Main_Exec]
+
+
+if __name__ == '__main__':
+
+    cli_show_info()
+
+
+# endregion [Main_Exec]
