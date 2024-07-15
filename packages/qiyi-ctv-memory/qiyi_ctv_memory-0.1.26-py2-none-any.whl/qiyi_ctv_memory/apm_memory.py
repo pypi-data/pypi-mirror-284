1110df7d2f5ecc88e6da840f29556d1c65b67e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 15:05:32 +0000

    Add package qiyi-ctv-memory/0.1.26/qiyi_ctv_memory-0.1.26-py2-none-any.whl

---
+++ packages/qiyi-ctv-memory/qiyi_ctv_memory-0.1.26-py2-none-any.whl/qiyi_ctv_memory/apm_memory.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*--
+import argparse
+import datetime
+import subprocess
+
+from qiyi_ctv_memory.Adb import Adb
+from qiyi_ctv_memory.java import javaHeap
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='argument')
+    parser.add_argument('-c', '--categories', dest='CATEGORIES',  default='', help='what do you want to do?')
+    parser.add_argument('-o', '--output',  dest='OUTPUT', default='', help='output path')
+    parser.add_argument('-f', '--focus',  dest='FOCUS', default='', help='focus class')
+    parser.add_argument('-m', '--mapping',  dest='MAPPING', default='', help='apk mapping file')
+    args, unknown_args = parser.parse_known_args()
+    return args, unknown_args
+
+def main():
+    args, unknown_args = parse_arguments()
+    categories = args.CATEGORIES
+    outputPath = args.OUTPUT
+    if categories == 'native':
+        command = ['python', 'qiyi_ctv_memory/native_memory.py'] + unknown_args
+        try:
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            print(stdout)
+            print(stderr)
+        except Exception as e:
+            print(f"Error during subprocess execution: {e}")
+    else :
+        adb = Adb()
+        package_name = adb.get_current_package_name()
+        now = datetime.datetime.now()
+        # 格式化当前时间
+        formatted_time = now.strftime("%Y.%m.%d-%H.%M.%S.%f")[:-3]
+        hprof = javaHeap.dumpHprof(adb, package_name)
+        hprof = javaHeap.pullHprof(adb, hprof, str(formatted_time), outputPath)
+        mappingPath = args.MAPPING
+        if categories == 'leak':
+            javaHeap.parseLeak(hprof, outputPath, mappingPath, 'low')
+        elif categories == 'javadetail':
+            javaHeap.parseAll(hprof, outputPath, mappingPath, 'high')
+        elif categories == 'focus':
+            focus = args.FOCUS
+            javaHeap.parseFocus(hprof, outputPath, mappingPath, focus)
+        pass
+
+if __name__ == "__main__":
+    main()
