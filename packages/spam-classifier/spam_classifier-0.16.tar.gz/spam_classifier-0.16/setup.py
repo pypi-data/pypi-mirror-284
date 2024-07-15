4c765f540bfb23ef813b300523a242b582053b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:16 +0000

    Add package spam-classifier/0.16/spam_classifier-0.16.tar.gz

---
+++ packages/spam-classifier/spam_classifier-0.16.tar.gz/spam_classifier-0.16/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spam_classifier',
+    version='0.16',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A library for classifying spam messages',
+    install_requires=[
+        'tensorflow>=2.0.0',
+        'requests',
+        'huggingface-hub',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license='Non-commercial use only. Contact author for commercial use.',
+    Email= 'totoshkus@gmail.com'
+)
