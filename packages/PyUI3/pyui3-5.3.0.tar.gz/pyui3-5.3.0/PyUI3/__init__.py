f0be8538c068aeeaa4bed674f0b276d6d9c94896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 05:05:39 +0000

    Add package PyUI3/5.3.0/pyui3-5.3.0.tar.gz

---
+++ packages/PyUI3/pyui3-5.3.0.tar.gz/pyui3-5.3.0/PyUI3/__init__.py
@@ -0,0 +1,56 @@
+"""
+WinUI3 with Python!
+"""
+
+__version__ = "5.3.0"
+
+import pythoncom
+from win32more.Microsoft.UI.Xaml.Controls import (
+    AnimatedIcon, AutoSuggestBox, BreadcrumbBar, Button, CalendarDatePicker,
+    CalendarView, CheckBox, ColorPicker, ComboBox, CommandBar, CommandBarFlyout,
+    ContentDialog, DatePicker, DropDownButton, Expander, FlipView, Flyout,
+    GridView, HyperlinkButton, Image, InfoBar, ListView, MapControl,
+    MediaPlayerElement, MenuBar, MenuFlyout, NavigationView, NumberBox,
+    ParallaxView, PasswordBox, PersonPicture, PipsPager, ProgressBar,
+    ProgressRing, RadioButton, RatingControl, RichEditBox, RichTextBlock,
+    ScrollViewer, SemanticZoom, Slider, SplitButton, SplitView, SwipeControl,
+    TabView, TeachingTip, TextBlock, TextBox, TimePicker, ToggleSwitch,
+    ToggleSplitButton, ToolTip, TreeView, TwoPaneView,
+)
+
+from win32more.Microsoft.UI.Xaml import Window
+from win32more.xaml import XamlApplication
+from win32more.Microsoft.UI.Xaml.Media import MicaBackdrop
+
+class App(XamlApplication):
+    def __init__(self):
+        pythoncom.CoInitialize()
+        super().__init__()
+        self.win = None
+
+    def OnLaunched(self, args):
+        self.win = Window()
+
+class ApplicationWindow:
+    def __init__(self):
+        self.roster = []
+        self.app = App()
+        self.app.Start()
+        self.win = self.app.win
+
+    def add(self, item):
+        self.roster.append(item)
+
+    def run(self):
+        for item in self.roster:
+            self.win.Content = item
+        self.win.Activate()
+
+    def set_title(self, title):
+        self.win.Title = str(title)
+
+    def set_mica(self, mode):
+        if mode:
+            self.win.SystemBackdrop = MicaBackdrop()
+        else:
+            self.win.SystemBackdrop = None
