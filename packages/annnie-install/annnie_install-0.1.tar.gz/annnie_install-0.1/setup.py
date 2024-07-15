72c79a1b2790e69a1c98ac2a3b7e983669318822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 16:05:01 +0000

    Add package annnie-install/0.1/annnie_install-0.1.tar.gz

---
+++ packages/annnie-install/annnie_install-0.1.tar.gz/annnie_install-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='annnie_install',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        # pip install openai
+        # pip install Flask
+        # sudo apt-get install python3-tk
+        'numpy',
+        'openai',
+        'Flask',
+        # 'python3-tk',
+    ],
+    author='Siyu Qiu',
+    author_email='siyu.qiu1@unsw.edu.au',
+    description='Everything you need to install for the LLM4HW tool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/annnnie-qiu/annie_install',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
