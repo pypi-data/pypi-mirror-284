9731c8622ae1c6a436816ffdec6e0e9bf9e9b889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 05:05:48 +0000

    Add package mochapy/2.0.0/mochapy-2.0.0-py3-none-any.whl

---
+++ packages/mochapy/mochapy-2.0.0-py3-none-any.whl/mochapy/mocha_request.py
@@ -0,0 +1,10 @@
+"""
+Request class for mocha
+"""
+
+class request:
+    def __init__(self):
+        self.parameter = {}
+        self.payload = {}
+        self.cookie = {}
+        self.header = ""
