166376e7cbd0469058497f6e57bbc3be63708e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:17 +0000

    Add package fasthx/0.2407.0/fasthx-0.2407.0.tar.gz

---
+++ packages/fasthx/fasthx-0.2407.0.tar.gz/fasthx-0.2407.0/fasthx/__init__.py
@@ -0,0 +1,8 @@
+from .core_decorators import hx as hx
+from .core_decorators import page as page
+from .dependencies import DependsHXRequest as DependsHXRequest
+from .dependencies import get_hx_request as get_hx_request
+from .jinja import Jinja as Jinja
+from .jinja import JinjaContext as JinjaContext
+from .typing import HTMLRenderer as HTMLRenderer
+from .typing import JinjaContextFactory as JinjaContextFactory
