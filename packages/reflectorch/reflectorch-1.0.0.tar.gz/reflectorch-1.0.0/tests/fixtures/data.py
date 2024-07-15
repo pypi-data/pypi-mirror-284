ed8748ed049bfd1e2871d85cbc7c6266949c5e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:09 +0000

    Add package reflectorch/1.0.0/reflectorch-1.0.0.tar.gz

---
+++ packages/reflectorch/reflectorch-1.0.0.tar.gz/reflectorch-1.0.0/tests/fixtures/data.py
@@ -0,0 +1,45 @@
+import pytest
+import numpy as np
+
+from reflectorch.paths import TEST_DATA_PATH
+from tests.helpers.generate_data import load_generated_data
+
+
+@pytest.fixture(
+    params=["demo_scan.csv"],
+    scope="session"
+)
+def raw_data_with_preprocessing_params(request):
+    scan_path = TEST_DATA_PATH / request.param
+    data = np.loadtxt(str(scan_path), skiprows=1, delimiter=',')
+
+    raw_data = dict(
+        intensity=data[:, 5],
+        scattering_angle=data[:, 0] * 2,
+        attenuation=data[:, 4],
+    )
+
+    preprocessing_params = dict(
+        q_interp=np.linspace(0.02, 0.15, 64),
+        wavelength=1,
+        beam_width=0.2,
+        sample_length=10.,
+    )
+
+    return raw_data, preprocessing_params
+
+
+@pytest.fixture(
+    params=['test_preprocessed_curve_2'],
+    scope="session",
+)
+def preprocessed_data(request):
+    return load_generated_data(request.param)
+
+
+@pytest.fixture(
+    params=['test_raw_data_input_1'],
+    scope="session",
+)
+def raw_data_input(request):
+    pass
