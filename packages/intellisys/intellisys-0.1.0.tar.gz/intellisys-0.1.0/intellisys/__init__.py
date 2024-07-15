abaaa7781d8af1d469afba7f5f483449aed7363a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 14:05:33 +0000

    Add package intellisys/0.1.0/intellisys-0.1.0.tar.gz

---
+++ packages/intellisys/intellisys-0.1.0.tar.gz/intellisys-0.1.0/intellisys/__init__.py
@@ -0,0 +1,5 @@
+from .intellisys import get_completion_api, get_completion_api_async, get_api_key, render_template, format_json
+
+__version__ = "0.1.0"
+
+__all__ = ['get_completion_api', 'get_completion_api_async', 'get_api_key', 'render_template', 'format_json']
