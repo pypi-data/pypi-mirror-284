0f38c667a0f986bc7586e18a1d0062a27168dd78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 03:05:06 +0000

    Add package mak-framework/1.0.0/mak_framework-1.0.0-py3-none-any.whl

---
+++ packages/mak-framework/mak_framework-1.0.0-py3-none-any.whl/framework/Utils/static_methods.py
@@ -0,0 +1,50 @@
+import os
+import allure
+from allure_commons.types import LinkType
+from framework.Utils.create_general_directory import RESULTS_DIR
+
+
+def get_test_result_folder_name():
+    folder_name = os.environ.get('PYTEST_CURRENT_TEST').replace('::', '-').replace('/', '-').replace(" ",
+                                                                                                     "-").replace(
+        '[', '-').replace(']', '').replace('_', '-').replace('.', '-').replace("-(call)", "").replace("-(setup)",
+                                                                                                      "").lower()
+    return folder_name
+
+
+def get_test_output_dir():
+    return os.path.join(os.path.join(f"{RESULTS_DIR}\\Test_Results\\", get_test_result_folder_name()))
+
+
+def write_file(directory, name_with_extension, content):
+    folder_path = directory
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    file_name = name_with_extension
+    file_path = os.path.join(folder_path, file_name)
+    with open(file_path, 'w') as f:
+        f.write(content)
+
+
+def attach_artifact_folder_link_to_allure():
+    write_file(get_test_output_dir(), "execution.bat", f"playwright show-trace trace.zip\nexit")
+    write_file(RESULTS_DIR, "allure_single_file.bat", "allure generate --single-file Allure_Results\nexit")
+    write_file(RESULTS_DIR, "allure_serve.bat", "allure serve Allure_Results")
+    allure.dynamic.link(url=get_test_output_dir(), link_type=LinkType.TEST_CASE,
+                        name=get_test_output_dir())
+
+
+def get_file_location(filename, file_extension=""):
+    current_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+    # print(f"Searching for file: {filename} with extension: {file_extension} in directory: {current_dir}")
+
+    for root, dirs, files in os.walk(current_dir):
+        for file in files:
+            if file_extension == "" or file.endswith(file_extension):
+                if file == filename:
+                    file_location = os.path.join(root, file)
+                    # print(f"Found file at location: {file_location}")
+                    return file_location
+
+    # print(f"File not found: {filename} with extension: {file_extension}")
+    return None
---
+++ packages/mak-framework/mak_framework-1.0.0-py3-none-any.whl/framework/pages/allure_page.py
@@ -0,0 +1,71 @@
+import os
+import allure
+import json
+from datetime import datetime
+
+from framework.Utils.create_general_directory import RESULTS_DIR
+
+
+class AllureUtils:
+    @staticmethod
+    def _get_timestamp():
+        return datetime.now().strftime("%d-%m-%Y__%H-%M-%S")
+
+    @staticmethod
+    def attach_screenshot(driver, name='screenshot'):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(driver.screenshot_as_png, name=f"{name}_{timestamp}", attachment_type=allure.attachment_type.PNG)
+
+    @staticmethod
+    def attach_text(text, name='text_attachment', attachment_type=allure.attachment_type.TEXT):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(text, name=f"{name}_{timestamp}", attachment_type=attachment_type)
+
+    @staticmethod
+    def attach_html(html_content, name='html_attachment'):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(html_content, name=f"{name}_{timestamp}", attachment_type=allure.attachment_type.HTML)
+
+    @staticmethod
+    def attach_file(file_path, name=None):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach.file(file_path, name=f"{name or os.path.basename(file_path)}_{timestamp}")
+
+    @staticmethod
+    def attach_json(json_data, name='json_attachment'):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(json.dumps(json_data), name=f"{name}_{timestamp}", attachment_type=allure.attachment_type.JSON)
+
+    @staticmethod
+    def attach_xml(xml_data, name='xml_attachment'):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(xml_data, name=f"{name}_{timestamp}", attachment_type=allure.attachment_type.XML)
+
+    @staticmethod
+    def attach_pdf(pdf_data, name='pdf_attachment'):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(pdf_data, name=f"{name}_{timestamp}", attachment_type=allure.attachment_type.PDF)
+
+    @staticmethod
+    def attach_jpg(jpg_data, name='jpg_attachment'):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(jpg_data, name=f"{name}_{timestamp}", attachment_type=allure.attachment_type.JPG)
+
+    @staticmethod
+    def attach_gif(gif_data, name='gif_attachment'):
+        timestamp = AllureUtils._get_timestamp()
+        allure.attach(gif_data, name=f"{name}_{timestamp}", attachment_type=allure.attachment_type.GIF)
+
+    @staticmethod
+    def attach_screenshot_to_allure(self, step_name: str = "Screenshot"):
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        screenshots_dir = os.path.join(RESULTS_DIR, "Screenshots")
+        os.makedirs(screenshots_dir, exist_ok=True)
+        screenshot_path = os.path.join(screenshots_dir, f"{step_name.replace(' ', '_').lower()}_{timestamp}.png")
+        self.page.screenshot(path=screenshot_path)
+        allure.attach.file(screenshot_path, attachment_type=allure.attachment_type.PNG)
+
+    @staticmethod
+    def attach_video_to_allure(self, step_name: str = "Complete Test Case Video"):
+        with allure.step(step_name):
+            allure.attach.file(self.page.video.path(), attachment_type=allure.attachment_type.WEBM)
