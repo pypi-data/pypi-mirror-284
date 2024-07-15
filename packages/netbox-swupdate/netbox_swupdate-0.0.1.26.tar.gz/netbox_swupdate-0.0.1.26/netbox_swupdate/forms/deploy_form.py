b304ad6677aaa41c03cf425294797a519bc6277c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 16:05:04 +0000

    Add package netbox-swupdate/0.0.1.26/netbox_swupdate-0.0.1.26.tar.gz

---
+++ packages/netbox-swupdate/netbox_swupdate-0.0.1.26.tar.gz/netbox_swupdate-0.0.1.26/netbox_swupdate/forms/deploy_form.py
@@ -0,0 +1,19 @@
+from netbox.forms import NetBoxModelForm
+
+from netbox_swupdate.models import Deploy
+
+__all__ = ("DeployForm",)
+
+
+class DeployForm(NetBoxModelForm):
+    class Meta:
+        model = Deploy
+        fields = [
+            "name",
+            "type",
+            "deploy_time",
+            "devices",
+            "software",
+            "retries",
+            "retry_interval",
+        ]
