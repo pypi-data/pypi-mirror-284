a97ab13507498ee19958c47026988ef35a637ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-15 15:04:54 +0000

    Add package pyfunc-config/1.2.13.dev2/pyfunc_config-1.2.13.dev2-py3-none-any.whl

---
+++ packages/pyfunc-config/pyfunc_config-1.2.13.dev2-py3-none-any.whl/pyfunc_config/__init__.py
@@ -0,0 +1,17 @@
+# Auto-generated __init__.py
+
+# Version of the pyfunc2 package
+from pathlib import Path
+
+# you can use os.path and open() as well
+__version__ = Path(__file__).parent.joinpath("VERSION").read_text()
+
+# Import necessary modules and functions here
+from get_email_path import get_email_path
+from get_config import get_config
+from ftp_update import ftp_update
+from ftp_download import ftp_download
+from get_ftp_path import get_ftp_path
+
+# Public API of the package
+__all__ = [get_ftp_path, get_email_path, get_config, ftp_update, ftp_download]
