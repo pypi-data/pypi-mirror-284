5fce6fda28e01f09d483bb80c9da46ba6b01bb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:33 +0000

    Add package hat-sbs/0.7.2/hat_sbs-0.7.2-cp310.cp311.cp312-abi3-manylinux_2_24_armv7l.whl

---
+++ packages/hat-sbs/hat_sbs-0.7.2-cp310.cp311.cp312-abi3-manylinux_2_24_armv7l.whl/hat/sbs/__init__.py
@@ -0,0 +1,68 @@
+"""Simple binary serializer
+
+This implementation of SBS encoder/decoder translates between SBS types and
+Python types according to following translation table:
+
+    +----------+------------------+
+    | SBS type | Python type      |
+    +==========+==================+
+    | None     | NoneType         |
+    +----------+------------------+
+    | Boolean  | bool             |
+    +----------+------------------+
+    | Integer  | int              |
+    +----------+------------------+
+    | Float    | float            |
+    +----------+------------------+
+    | String   | str              |
+    +----------+------------------+
+    | Bytes    | bytes            |
+    +----------+------------------+
+    | Array    | List[Data]       |
+    +----------+------------------+
+    | Record   | Dict[str, Data]  |
+    +----------+------------------+
+    | Choice   | Tuple[str, Data] |
+    +----------+------------------+
+
+Example usage of SBS serializer::
+
+    import hat.sbs
+
+    repo = hat.sbs.Repository('''
+        module Module
+
+        Entry(K, V) = Record {
+            key: K
+            value: V
+        }
+
+        T = Array(Optional(Entry(String, Integer)))
+    ''')
+    data = [
+        ('none', None),
+        ('value', {
+            'key': 'abc',
+            'value': 123
+        })
+    ]
+    encoded_data = repo.encode('Module.T', data)
+    decoded_data = repo.decode('Module.T', encoded_data)
+    assert data == decoded_data
+
+"""
+
+from hat.sbs.common import Data
+from hat.sbs.repository import Repository
+from hat.sbs.serializer import (Serializer,
+                                CSerializer,
+                                PySerializer,
+                                DefaultSerializer)
+
+
+__all__ = ['Repository',
+           'Data',
+           'Serializer',
+           'CSerializer',
+           'PySerializer',
+           'DefaultSerializer']
