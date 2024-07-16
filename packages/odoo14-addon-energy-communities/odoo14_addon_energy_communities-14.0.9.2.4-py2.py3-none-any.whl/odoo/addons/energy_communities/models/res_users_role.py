e9302968bb8b10a326c923baa263b33bd7feee7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:42 +0000

    Add package odoo14-addon-energy-communities/14.0.9.2.4/odoo14_addon_energy_communities-14.0.9.2.4-py2.py3-none-any.whl

---
+++ packages/odoo14-addon-energy-communities/odoo14_addon_energy_communities-14.0.9.2.4-py2.py3-none-any.whl/odoo/addons/energy_communities/models/res_users_role.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class ResUsersRole(models.Model):
+    _inherit = "res.users.role"
+
+    code = fields.Char(string="Code")
+
+
+class ResUsersRoleLine(models.Model):
+    _inherit = "res.users.role.line"
+
+    code = fields.Char(related="role_id.code")
