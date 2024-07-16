9665ee6010c9bef85bd45f32db9fc4b604686d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:43 +0000

    Add package vytal/0.1.8/vytal-0.1.8-py3-none-any.whl

---
+++ packages/vytal/vytal-0.1.8-py3-none-any.whl/vytal/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.8"
+__author__ = "Sritan Motati"
+__author_email__ = "sritanmotati@gmail.com"
+__url__ = "https://github.com/vytal-ai/vytalgaze-client"
+__description__ = "A Python client for the Vytal Gaze API."
+
+from .client import *
+
+__all__ = ["Client"]
