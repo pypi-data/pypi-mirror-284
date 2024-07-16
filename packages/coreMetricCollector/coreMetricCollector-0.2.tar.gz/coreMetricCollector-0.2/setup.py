c33d21f2bc0efc29be8e1cf579f153b54c31fcfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:07:34 +0000

    Add package coreMetricCollector/0.2/coreMetricCollector-0.2.tar.gz

---
+++ packages/coreMetricCollector/coreMetricCollector-0.2.tar.gz/coreMetricCollector-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coreMetricCollector',
+    version='0.2',
+    description='A Prometheus metrics collector for thread counts',
+    author='coreteam',
+    author_email='coreteam@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'prometheus_client',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
