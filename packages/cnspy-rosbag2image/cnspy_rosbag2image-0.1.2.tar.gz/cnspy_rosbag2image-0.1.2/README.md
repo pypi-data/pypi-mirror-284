d8309705870356962b800b9d5ef28aa99b8220a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:05:28 +0000

    Add package cnspy-rosbag2image/0.1.2/cnspy_rosbag2image-0.1.2.tar.gz

---
+++ packages/cnspy-rosbag2image/cnspy_rosbag2image-0.1.2.tar.gz/cnspy_rosbag2image-0.1.2/README.md
@@ -0,0 +1,40 @@
+#  cnspy_rosbag2image -- Package
+
+A package to convert [ROS1] image messages into image files. 
+
+
+## Installation
+
+Install the current code base from GitHub and pip install a link to that cloned copy
+```
+git clone https://github.com/aau-cns/cnspy_rosbag2image.git
+cd cnspy_rosbag2image
+pip install -e .
+```
+or the [official package](https://pypi.org/project/cnspy-rosbag2image/) via
+```commandline
+pip install cnspy-rosbag2image
+```
+## Dependencies
+
+It is part of the [cnspy eco-system](https://github.com/aau-cns/cnspy_eco_system_test) of the [cns-github](https://github.com/aau-cns) group.  
+Main dependencies are:
+* [numpy]()
+* [rospkg]()
+* [opencv-python]()
+
+## Collaborators
+
+* [Christian Brommer](https://github.com/Chris-Bee)
+* [Thomas Jantos](https://www.aau.at/team/jantos-thomas-georg/)
+
+## Run from terminal
+
+* ROSbag2Image 
+
+
+## License
+
+Software License Agreement (GNU GPLv3  License), refer to the LICENSE file.
+
+*Sharing is caring!* - [Roland Jung](https://github.com/jungr-ait)  
---
+++ packages/cnspy-rosbag2image/cnspy_rosbag2image-0.1.2.tar.gz/cnspy_rosbag2image-0.1.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cnspy_rosbag2image',
+    version="0.1.2",
+    author='Roland Jung',
+    author_email='roland.jung@aau.at',    
+    description='ROS1 rosbag to image file converter.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/aau-cns/cnspy_rosbag2image/',
+    project_urls={
+        "Bug Tracker": "https://github.com/aau-cns/cnspy_rosbag2image/issues",
+    },    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    
+    packages=find_packages(exclude=["test_*", "TODO*"]),
+    python_requires='>=3.6',
+    install_requires=['numpy', 'tqdm', 'pandas', 'argparse', 'PyYAML', 'rospkg', 'opencv-python', 'pycryptodomex', 'pycryptodome', 'gnupg', 'lz4'],
+    entry_points={
+        'console_scripts': [
+            'ROSbag2Image = cnspy_rosbag2image.ROSbag2Image:main',
+        ],
+    },
+)
