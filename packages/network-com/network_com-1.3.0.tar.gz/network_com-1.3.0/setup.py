05cce7214301ef84c32892922f9259da0683b8cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:32 +0000

    Add package network-com/1.3.0/network_com-1.3.0.tar.gz

---
+++ packages/network-com/network_com-1.3.0.tar.gz/network_com-1.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="network_com",
+    version="1.3.0",
+    packages=find_packages(),
+    install_requires=[
+        "inputimeout"
+    ],
+    entry_points={
+        'console_scripts': [
+            'run-client=network_com.client:udp_listener',
+            'run-server=network_com.server:send_ip'
+        ]
+    },
+    author="Karam Belmoujoud",
+    author_email="karam.bd.kb@gmail.com",
+    description="A package for client-server communication over TCP and UDP with SSL/TLS encryption",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KaramBelmoujoud/Multi-Client-TCP-UDP-Communication-Server-in-Python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
