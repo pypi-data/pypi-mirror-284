6ba09a56d39409bf910488a338b65bca164650c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:29 +0000

    Add package vval/2.1.8/vval-2.1.8.tar.gz

---
+++ packages/vval/vval-2.1.8.tar.gz/vval-2.1.8/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="vval",
+    version="2.1.8",
+    author="Arturo Munoz",
+    author_email="munoz.arturoroman@gmail.com",
+    description="Input validation in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/munozarturo/vval",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="validation",
+    python_requires=">=3.11",
+    install_requires=["varname==0.11.0", "typing_inspect==0.8.0"],
+    extras_require={
+        "dev": ["pytest", "mypy", "pytest-cov", "flake8", "black"],
+        "docs": ["sphinx", "sphinx_rtd_theme"],
+    },
+    project_urls={
+        "Bug Reports": "https://github.com/munozarturo/vval/issues",
+        "Source": "https://github.com/munozarturo/vval/",
+    },
+)
