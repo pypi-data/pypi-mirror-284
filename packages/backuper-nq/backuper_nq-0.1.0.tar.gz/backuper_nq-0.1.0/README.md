17f6f6cb68587367cc231c01429abbd32ef1bf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:05:11 +0000

    Add package backuper-nq/0.1.0/backuper_nq-0.1.0.tar.gz

---
+++ packages/backuper-nq/backuper_nq-0.1.0.tar.gz/backuper_nq-0.1.0/README.md
@@ -0,0 +1,18 @@
+# Backuper
+Personal backup script
+
+TBA
+
+## Development
+### Install
+```
+pip install poetry==1.8.3
+poetry install
+pre-commit install
+```
+
+### Run
+```
+python backuper config.yml
+echo "<config>" | python backuper -
+```
---
+++ packages/backuper-nq/backuper_nq-0.1.0.tar.gz/backuper_nq-0.1.0/backuper/actions/backup.py
@@ -0,0 +1,41 @@
+from collections.abc import Iterator
+from os import system
+from typing import Literal
+
+from backuper.actions.abstract import Action
+from backuper.utils import BaseModelForbidExtra
+from backuper.variables import SubstitutedStr
+
+
+class BackupExcludeSchema(BaseModelForbidExtra):
+    directory_names: list[SubstitutedStr] = []
+    filename_patterns: list[SubstitutedStr] = []
+
+
+class BackupAction(Action):
+    type: Literal["backup"]
+    source: SubstitutedStr
+    target: SubstitutedStr
+    override_permissions: bool = False
+    exclude: BackupExcludeSchema = BackupExcludeSchema()
+
+    def collect_command(self) -> Iterator[str]:
+        yield "robocopy"
+        yield str(self.source)
+        yield str(self.target)
+
+        yield "/mir"
+
+        if self.override_permissions:
+            yield "/b"
+
+        if self.exclude.directory_names:
+            yield "/xd"
+            yield from self.exclude.directory_names
+
+        if self.exclude.filename_patterns:
+            yield "/xf"
+            yield from self.exclude.filename_patterns
+
+    def run(self) -> None:
+        system(" ".join(self.collect_command()))
