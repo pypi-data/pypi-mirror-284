2403861702fa893bb4d322d0c316e8ac5c70d0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:07:14 +0000

    Add package faunanet/0.0.12/faunanet-0.0.12.tar.gz

---
+++ packages/faunanet/faunanet-0.0.12.tar.gz/faunanet-0.0.12/README.md
@@ -0,0 +1,209 @@
+[![tests](https://github.com/ssciwr/iSparrow/actions/workflows/main.yml/badge.svg?event=push)](https://github.com/ssciwr/iSparrow/actions/workflows/main.yml)
+[![codecov](https://codecov.io/gh/ssciwr/iSparrow/graph/badge.svg?token=FwyE0PNiOk)](https://codecov.io/gh/ssciwr/iSparrow)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=ssciwr_iSparrow&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=ssciwr_iSparrow)
+[![Supported OS: Linux | macOS | Windows](https://img.shields.io/badge/OS-Linux%20%7C%20macOS%20%7C%20Windows-green.svg)](https://www.linux.org/)
+[![Documentation](https://readthedocs.org/projects/isparrow/badge/?version=latest)](https://isparrow.readthedocs.io/en/latest/?badge=latest)
+[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+# faunanet - A bioacoustics platform based on birdnetlib using neural networks 
+## What is faunanet? 
+`faunanet` is an extension of [*Birdnet-Analyzer*](https://github.com/kahst/BirdNET-Analyzer), and uses [*birdnetlib*](https://github.com/joeweiss/birdnetlib) as its basis. 
+`faunanet` was developed with the goal to provide a platform for bioacoustics research projects, started at the Interdisciplinary center for scientific computing at the University of Heidelberg. 
+
+## Features
+Using the birdnetv2.4 model by default, `faunanet` provides three core features: 
+
+- Easily and arbitrarily exchange the underlying maching learning model for bioacoustics.
+- Easy configuration using YAML files which are stored alongside the analysis results. 
+- Integrated, extendible REPL with which to interact with which to interact with a running instance.
+
+The main element of `faunanet` is a 'watcher' class that continuously monitors a folder for incoming data files and allows for on-the-fly model- or parameter-change via the REPL. It can also be used as a library in your python project.
+By extending elements of bridnetlib rather, `faunanet` conserves the latter's capabilities while still adding its own functionality on top of it. 
+
+## Getting started 
+Please refer to the 'Getting faunanet up and running' section in the documentation for an introduction. 
+
+## Bugs, issues and feature requests
+Please use the [issue tab of the github page](https://github.com/ssciwr/iSparrow/issues) to report any bugs or feature requests. 
+
+## Contributions 
+Feel free to contribute to this project by opening a pull request [here](https://github.com/ssciwr/iSparrow/pulls). 
+
+## Usage
+### Installation
+Make sure that ffmpeg is installed on your system: 
+```bash
+# for debian based linux
+sudo apt install ffmpeg
+```
+or for macOS using homebrew:
+```bash 
+brew install ffmpeg
+```
+On windows, you can use chocolatey or other package managers to the same effect: 
+```bash
+choco install ffmpeg
+```
+ffmpeg is needed for the audio preprocessing that is done by faunanet when analyzing your data.
+
+You can install `faunanet` from pip with 
+```bash 
+pip install faunanet[option]
+```
+with option being "tensorflow" for the full tensorflow suite, or "tensorflow-lite" to install only the `tflite_runtime` package which only provides restricted support for model operations, while full tensorflow supports all models built with it. Installing it from pip will install the newest release version of `faunanet`. **One of the two is needed to run `faunanet`, with `tensorflow` being the appropriate one for most usecases.** 
+
+You can also install it from source to get the latest development chagnes. To do this, clone the repository, create a [python virtual environment](https://docs.python.org/3/library/venv.html), and install `faunanet`into it: 
+```bash 
+    cd path/to/dir/where/faunanet/should/live 
+
+    git clone https://github.com/ssciwr/iSparrow.git
+
+    python3 -m venv ./path/to/faunanet/venv
+
+    source ./path/to/where/venv/bin/activate 
+
+    cd ./faunanet 
+
+    python3 -m pip install .[tensorflow]  # or .[tensorflow-lite] for tflite models
+``` 
+On windows, the line ```source ./path/to/faunanet/venv/bin/activate``` must be replaced with 
+```python3 ./path/to/faunanet/venv/scripts/activate```. You can also select which installation options you want in the last line, either ```tensorflow``` or ```tensorflow-lite```. Additionally, 
+if you want to do further development, the installation must be modified by replacing the install command, adding 'editable' mode and development dependencies.
+```
+    python3 -m pip install  -e .[tensorflow, dev]
+```
+
+For building the documentation locally, you have to install with yet another set of dependencies: 
+
+### Setup 
+faunanet needs three destinations on your filesystem in order to run: 
+- a directory where the machine learning models for sound classification should be stored 
+- a directory where incoming data files are stored. 
+- a directory where the analysis results should be stored
+
+faunanet has a method `set_up` for this purpose, that is called with the path to a .yml configuration file. This file contains all the necessary information to set up a working faunanet installation.
+**This method must be called upon first usage in order to have a fully functioning faunanet installation that documents its state correctly upon usage**. 
+Below is an example of the structure faunanet demands: 
+```yaml 
+Directories: 
+  home: ~/faunanet
+  data: ~/faunanet/data
+  models: ~/faunanet/models 
+  output: ~/faunanet/output
+```
+You can copy the above into a .yml file and customize the paths to whatever you want them to be. The `~` will be automatically expanded to the path to your home directory.
+The method can be used as follows in python code
+
+```python 
+    from faunanet import faunanet_setup as sps 
+    filepath = Path("path", "to", "the", "install_config.yml")
+
+    sps.set_up(filepath)
+```
+It is important to have an outermost node called  `Directories`. Aside from creating the directories named in the installation config file, the installation method will download the default tensorflow models from [hugginface](https://huggingface.co/MaHaWo/faunanet_test_models/tree/main) and will create `faunanet` directories in you OS default config and cache folders. On Linux, these would be `~/.config` and `~/.cache`, respectively.
+
+In the repl, this would work like this: 
+```bash
+   faunanet # enter repl 
+
+   set_up --cfg=./path/to/custom/install_config.yml 
+``` 
+This does the same as the call to ```sps.set_up``` call above, so you can refer to the documentation of that function for further information, too. 
+
+### Using faunanet as a library 
+faunanet can be used as a library in your own application. Just add  
+```python 
+import faunanet 
+```
+to have it available in your module, and make sure you use the correct virtual environment in which faunanet is installed. Keep in mind that `faunanet` in itself extends `birdnetlib`, so make sure to check the latter's documentation, too. 
+
+### Running faunanet 
+faunanet provides its own, small, REPL for interacting with a running instance. This can be used to start, stop, pause or continue it, to change classifier models or to query it's current state, input and output folders and so on. To get an overview over the available commands, you can just type  ```faunanet``` in a terminal with the virtual environment being activated that faunanet has been installed into. Alternatively, refer to the documentation.
+
+
+### Using the docker image
+
+#### Run the docker-hub image
+You can also run `faunanet` in docker by pulling the latest `faunanet` image from docker-hub and 
+running it via terminal command: 
+```bash
+docker run -ti --rm \ 
+     -v /path/on/host/for/faunanet/configs:/root/faunanet/config \
+     -v /path/on/host/for/faunanet/output:/root/faunanet/output \ 
+     -v /path/on/host/for/faunanet/models:/root/faunanet/models \ 
+     -v /path/on/host/for/faunanet/data:/root/faunanet/data \ 
+     mahawo/faunanet_{OPTION}:latest
+```
+Of special interest are the mounted volumes, i.e., the paths behind the `-v` arguments: 
+- first: for config files 
+- second: for analysis output 
+- third: for models 
+- forth: for incoming data. If you run the system via docker compose (see below) in conjunction with faunanet-record you do not need this, because `faunanet-record` will take care of this folder. 
+
+`{OPTION}` corresponds ot `tf` for tensorflow or `tflite` for tensorflow-lite.
+
+#### Built the image yourself
+To built the dockerfile that comes with the package yourself you can use the following docker command: 
+```bash
+docker build --build-arg INSTALL_OPTION=TENSORFLOW_OPTION \
+    -t your-dockerhub-username/your-image-name:tag \
+    -f path/to/Dockerfile .
+```
+Where the `TENSORFLOW_OPTION` has to be replaced with either `tensorflow` or `tensorflow-lite`. The dockerfile itself is very simple and can be modified to your liking.
+```dockerfile
+FROM python:3.11-slim
+
+WORKDIR /root
+
+RUN apt-get update && apt-get install --no-install-recommends -y ffmpeg -y --no-install-recommends && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# add install option 
+ARG INSTALL_OPTION
+
+# install with the necessary option
+RUN pip install faunanet[${INSTALL_OPTION}]
+WORKDIR /root
+
+RUN mkdir /root/faunanet/config 
+
+# add entrypoint
+CMD ["faunanet"]
+```
+
+In order to build an image for the ARM64 architecture often used by raspberry PI or other edge devices, you can use `docker buildx` in conjunction with `qemu` (tested on linux machine):
+
+```bash
+ docker buildx build --platform=linux/arm64 -t containername:tag -f ~/path/to/docker/file/dockerfile.dockerfile . --push
+```
+Have a look [here](https://www.docker.com/blog/multi-arch-images/) for more info.
+
+#### Using `faunanet` with other services via docker compose
+`faunanet` can be run together with `faunanet-record` using [docker compose](https://docs.docker.com/compose/) or together with other containers of your choice. You can use the following docker-compose file as a starting point, which also comes with the installation: 
+```yaml 
+services:
+  faunanet:
+    image: mahawo/faunanet:latest
+    tty: true 
+    stdin_open: true
+    volumes:
+      - ~/faunanet/config:/root/faunanet/config
+      - ~/faunanet/output:/root/faunanet/output
+      - ~/faunanet/models:/root/faunanet/models
+      - ~/faunanet/data:/root/faunanet/data
+    environment:
+      - RUN_CONFIG=analysis_config.yml
+  faunanet_record:
+    image: mahawo/faunanet_record:latest
+    tty: true 
+    stdin_open: true
+    volumes:
+      - ~/faunanet/config:/root/faunanet/config
+      - ~/faunanet/data:/root/faunanet/data
+    devices:
+      - /dev/snd:/dev/snd # this needs to be the microphone device
+    environment:
+      - RUN_CONFIG=record_config.yml
+```
+To locate the files from an existing pip installation, use the following python script, or pull them from the `docker` directory in `faunanet` home directory after it has been set up (see [Setup](#Setup)). The environment variables `RUN_CONFIG` for each service here can hold the name of config files that are stored in the directory mounted into `/root/faunanet/config`.
+
+When working with config files for the docker version, make sure to refer to the folders within the docker container and not the local ones. Otherwise, the system will fail with a 
---
+++ packages/faunanet/faunanet-0.0.12.tar.gz/faunanet-0.0.12/src/faunanet/faunanet_setup.py
@@ -0,0 +1,324 @@
+import shutil
+import pooch
+import yaml
+import os
+from pathlib import Path
+from platformdirs import user_config_dir, user_cache_dir
+from faunanet import utils
+from importlib.resources import files
+import faunanet
+
+FAUNANET_HOME = None
+FAUNANET_MODELS = None
+FAUNANET_OUTPUT = None
+FAUNANET_EXAMPLES = None
+FAUNANET_CONFIG = None
+FAUNANET_CACHE = None
+
+
+def make_directories(base_cfg_dirs: dict):
+    """
+    make_directories Make all the directories for faunanet.
+
+
+    Args:
+        base_cfg_dirs (dict): Dictionary containing paths for the main install ("home"),
+        the directory where models are stored ("models"),
+        and the "output" directory to store inference results and potentially other data in ("output")
+    Raises:
+        KeyError: A folder given in the config does not exist
+
+    Returns:
+        tuple: created folders: (faunanet-homefolder, modelsfolder, datafolder, outputfolder, examplefolder)
+    """
+    if "home" not in base_cfg_dirs:
+        raise KeyError("The home folder for faunanet must be given in the base config")
+
+    if "models" not in base_cfg_dirs:
+        raise KeyError(
+            "The models folder for faunanet must be given in the base config"
+        )
+
+    if "output" not in base_cfg_dirs:
+        raise KeyError(
+            "The output folder for faunanet must be given in the base config"
+        )
+
+    ish = Path(base_cfg_dirs["home"]).expanduser().resolve()
+    ism = Path(base_cfg_dirs["models"]).expanduser().resolve()
+    iso = Path(base_cfg_dirs["output"]).expanduser().resolve()
+    ise = (Path(base_cfg_dirs["home"]).expanduser() / Path("example")).resolve()
+    iscfg = Path(user_config_dir()) / "faunanet"
+    iscache = Path(user_cache_dir()) / "faunanet"
+
+    if os.getenv("FAUNANET_TEST_MODE") == "True":
+        iscfg = Path(user_config_dir()) / "faunanet_tests"
+        iscache = Path(user_cache_dir()) / "faunanet_tests"
+
+    for p in [ish, ism, iso, ise]:
+        p.mkdir(parents=True, exist_ok=False)
+
+    for p in [iscfg, iscache]:
+        p.mkdir(parents=True, exist_ok=True)
+
+    return ish, ism, iso, ise, iscfg, iscache
+
+
+def download_model_files(model_dir: str = "models"):
+    """
+    download_model_files Download default model files for faunanet. This is required to run before anything else is done with faunanet.
+
+    Args:
+        model_dir (str, optional): Path to put the model files at. Defaults to "models".
+
+    Raises:
+        FileNotFoundError: When the model directory does not exist.
+    """
+    print("... Downloading model files...")
+
+    ism = Path(model_dir)
+
+    if ism.exists() is False:
+        raise FileNotFoundError(f"The folder {model_dir} does not exist")
+
+    # have filenames and checksum hardcoded for now. "None" indicates unknown or changing filenames
+    model_file_names = {
+        "birdnet_default_v2.4/model.tflite": "sha256:55f3e4055b1a13bfa9a2452731d0d34f6a02d6b775a334362665892794165e4c",
+        "birdnet_custom_v2.4/model.tflite": "sha256:5eee37652430c54490321e0d2096d55817b4ac4da2301b594f03fcb9d52af741",
+        "birdnet_default_v2.4/species_presence_model.tflite": "sha256:1226f23fc20362617deb09178f366111b70cf085c2c82893f2814e5acedce6c2",
+        "google_bird_classification/saved_model.pb": "sha256:c6bdf0c2659f6d4713c670b92cceb083f8c1b401aee79ef6912614a1d89b6f97",
+        "google_bird_classification/variables/variables.index": None,
+        "google_bird_classification/variables/variables.data-00000-of-00001": "sha256:d967ea7bfaccdcfe4ff59f8d9f3bdec69181e4e79fd7abebf692100efcdfcc56",
+    }
+
+    label_file_names = {
+        "birdnet_default_v2.4/labels.txt": None,
+        "birdnet_custom_v2.4/labels.txt": None,
+        "google_bird_classification/assets/family.csv": None,
+        "google_bird_classification/assets/genus.csv": None,
+        "google_bird_classification/assets/label.csv": None,
+        "google_bird_classification/assets/order.csv": None,
+        "google_bird_classification/labels.txt": None,
+        "google_bird_classification/train_metadata.csv": None,
+    }
+
+    model_code = {
+        "birdnet_default_v2.4/model.py": None,
+        "birdnet_custom_v2.4/model.py": None,
+        "google_bird_classification/model.py": None,
+    }
+
+    preprocessor_code = {
+        "birdnet_default_v2.4/preprocessor.py": None,
+        "birdnet_custom_v2.4/preprocessor.py": None,
+        "google_bird_classification/preprocessor.py": None,
+    }
+
+    config_files = {
+        "birdnet_default_v2.4/default.yml": None,
+        "birdnet_custom_v2.4/default.yml": None,
+        "google_bird_classification/default.yml": None,
+    }
+
+    models_data = pooch.create(
+        path=pooch.os_cache("faunanet_downloads"),
+        base_url="https://huggingface.co/MaHaWo/faunanet_test_models/resolve/main",
+        registry=(
+            model_file_names
+            | label_file_names
+            | model_code
+            | preprocessor_code
+            | config_files
+        ),
+    )
+
+    for name in [
+        "model.tflite",
+        "labels.txt",
+        "species_presence_model.tflite",
+        "model.py",
+        "preprocessor.py",
+        "default.yml",
+    ]:
+        (ism / Path("birdnet_default")).mkdir(parents=True, exist_ok=True)
+        shutil.copy(
+            Path(models_data.fetch(f"birdnet_default_v2.4/{name}", progressbar=False)),
+            ism / Path("birdnet_default"),
+        )
+
+    for name in [
+        "model.tflite",
+        "labels.txt",
+        "model.py",
+        "preprocessor.py",
+        "default.yml",
+    ]:
+        (ism / Path("birdnet_custom")).mkdir(parents=True, exist_ok=True)
+
+        shutil.copy(
+            Path(models_data.fetch(f"birdnet_custom_v2.4/{name}", progressbar=False)),
+            ism / Path("birdnet_custom"),
+        )
+
+    for name in [
+        "saved_model.pb",
+        "labels.txt",
+        "train_metadata.csv",
+        "model.py",
+        "preprocessor.py",
+        "default.yml",
+    ]:
+        (ism / Path("google_perch")).mkdir(parents=True, exist_ok=True)
+
+        shutil.copy(
+            Path(
+                models_data.fetch(
+                    f"google_bird_classification/{name}", progressbar=False
+                )
+            ),
+            ism / Path("google_perch"),
+        )
+
+    # assets and variables need to live in separate directories to load the model correctly
+    for name in [
+        "variables/variables.index",
+        "variables/variables.data-00000-of-00001",
+    ]:
+
+        (ism / Path("google_perch") / "variables").mkdir(parents=True, exist_ok=True)
+
+        shutil.copy(
+            Path(
+                models_data.fetch(
+                    f"google_bird_classification/{name}", progressbar=False
+                )
+            ),
+            ism / Path("google_perch") / "variables",
+        )
+
+    for name in [
+        "assets/family.csv",
+        "assets/genus.csv",
+        "assets/label.csv",
+        "assets/order.csv",
+    ]:
+
+        (ism / Path("google_perch") / "assets").mkdir(parents=True, exist_ok=True)
+
+        shutil.copy(
+            Path(
+                models_data.fetch(
+                    f"google_bird_classification/{name}", progressbar=False
+                )
+            ),
+            ism / Path("google_perch") / "assets",
+        )
+
+
+def download_example_data(example_dir: str = "examples"):
+    """
+    download_example_data Download the example audio files used by faunanet for its tests and examples.
+
+    Args:
+        example_dir (str, optional): Path to the faunanet example directory. Defaults to 'examples'.
+    """
+
+    print("... Downloading example files...")
+
+    ise = Path(example_dir)
+
+    if ise.exists() is False:
+        raise FileNotFoundError(f"The folder {example_dir} does not exist")
+
+    # create registries to pull files from. hardcode names and hashes for now
+    example_data_file_names = {
+        "corrupted.wav": "sha256:68cbc7c63bed90c2ad4fb7d3b5cc608c82cebeaf5e91d5e8d6793d8645b30b95",
+        "soundscape.wav": "sha256:df312b45bc82ce4c638c3e9e09d748702ea14a91ec29e4e8e0676d3e3e015fd7",
+        "trimmed.wav": "sha256:a4b3c078d2c617264dc5e6a0d62deedba9232699580c9c73d237496726c194b3",
+        "species_list.txt": None,
+    }
+
+    example_data = pooch.create(
+        path=pooch.os_cache("faunanet_downloads"),
+        base_url="https://huggingface.co/datasets/MaHaWo/iSparrow_test_data/resolve/main",
+        registry=example_data_file_names,
+    )
+
+    # don't leave them in the cached folder but copy to where user can actually see them.
+    for name in example_data_file_names.keys():
+        shutil.copy(
+            Path(example_data.fetch(name, progressbar=False)),
+            ise,
+        )
+
+
+def set_up(custom_config: str = None):
+    """
+    set_up Set up the faunanet directories and download the necessary data. This is required to run before anything else is done with faunanet.
+
+    Args:
+        custom_config (str, optional): Path to a custom installation file. See the 'install.yml' file provided with this package for possible customization options. Defaults to None.
+
+    Raises:
+        FileExistsError: When there is an existing faunanet installation.
+    """
+    print("Creating faunanet folders and downloading data... ")
+
+    packagebase = files(faunanet)
+    install_cfg = utils.read_yaml(packagebase / "install.yml")
+
+    if custom_config is not None:
+        custom_install_config = utils.read_yaml(custom_config)
+        utils.update_dict_leafs_recursive(install_cfg, custom_install_config)
+
+    for key in ["home", "models", "output"]:
+        if Path(install_cfg["Directories"][key]).expanduser().resolve().exists():
+            raise FileExistsError(
+                f"{key} directory already exists. Please remove it before running the installation."
+            )
+
+    home, models, output, examples, config, cache = make_directories(
+        install_cfg["Directories"]
+    )
+
+    if Path(config, "install.yml").exists():
+
+        with open(Path(config) / "install.yml", "r") as yfile:
+            old_cfg = yaml.safe_load(yfile)["Directories"]
+            old_dirs = [Path(p).expanduser().resolve() for p in old_cfg.values()]
+
+        for dir in [home, models, output, examples, config, cache]:
+
+            # avoid compromising existing installations
+            if Path(dir).exists() and Path(dir).expanduser().resolve() not in old_dirs:
+                shutil.rmtree(dir)
+        p = Path(config, "install.yml")
+        raise FileExistsError(
+            f"An faunanet installation already exists at {p}. Please remove it before running the installation."
+        )
+
+    # copy over config files
+    with open(Path(config) / "install.yml", "w") as yfile:
+        yaml.safe_dump(install_cfg, yfile)
+    shutil.copy(packagebase / "default.yml", config)
+
+    # copy over docker files 
+    Path(home, "docker").mkdir(parents=True, exist_ok=True)
+    shutil.copy(packagebase / Path("faunanet.dockerfile"), home / "docker")
+    shutil.copy(packagebase / Path("docker-compose.yml"), home / "docker")
+    shutil.copy(packagebase / Path("startup_docker.py"), home / "docker")
+
+    download_model_files(model_dir=models.resolve())
+
+    download_example_data(example_dir=examples.resolve())
+
+    global FAUNANET_HOME, FAUNANET_MODELS, FAUNANET_OUTPUT, FAUNANET_EXAMPLES, FAUNANET_CACHE, FAUNANET_CONFIG
+
+    FAUNANET_HOME = home
+    FAUNANET_MODELS = models
+    FAUNANET_OUTPUT = output
+    FAUNANET_EXAMPLES = examples
+    FAUNANET_CACHE = cache
+    FAUNANET_CONFIG = config
+
+    print("Installation finished")
