e2fdaf21f9b07259f160e32bf40135bf27b44e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:36 +0000

    Add package xlsx-export/0.0.28/xlsx_export-0.0.28.tar.gz

---
+++ packages/xlsx-export/xlsx_export-0.0.28.tar.gz/xlsx_export-0.0.28/xlsx_export/__init__.py
@@ -0,0 +1,16 @@
+from testy.plugins.hooks import TestyPluginConfig, hookimpl
+
+class XlsxExportPluginConfig(TestyPluginConfig):
+    package_name = 'xlsx_export'
+    verbose_name = 'XLSX Export Plugin'
+    description = 'Export data to XLSX format'
+    version = '0.28'
+    plugin_base_url = 'xlsx_export'
+    index_reverse_name = 'uploadxlsx'
+    urls_module = 'xlsx_export.urls'
+    author = 'Maxim Tuchkov'
+
+
+@hookimpl
+def config():
+    return XlsxExportPluginConfig
