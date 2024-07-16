f7c5fa520f490b8fce5314eca9d00ae47931c74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:05:31 +0000

    Add package airflow-data-validation/4.1.4/airflow-data-validation-4.1.4.tar.gz

---
+++ packages/airflow-data-validation/airflow-data-validation-4.1.4.tar.gz/airflow-data-validation-4.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['airflow_data_validation']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-cloud-bigquery>=2.4.0,<3.0.0', 'requests>=2.0.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'airflow-data-validation',
+    'version': '4.1.4',
+    'description': '',
+    'long_description': '',
+    'author': 'sapir.krespil',
+    'author_email': 'sapir.krespil@guesty.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/guestyorg/airflow_data_validation',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
