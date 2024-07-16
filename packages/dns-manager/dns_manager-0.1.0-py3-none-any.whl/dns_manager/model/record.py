bb926c1f4f541135904db179cf4a1edc6a57d27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:06:55 +0000

    Add package dns-manager/0.1.0/dns_manager-0.1.0-py3-none-any.whl

---
+++ packages/dns-manager/dns_manager-0.1.0-py3-none-any.whl/dns_manager/model/record.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+
+class Record(BaseModel):
+    subdomain: str
+    value: str
+    type: str
+
+    def __hash__(self) -> int:
+        return hash(self.subdomain)
+
+    def __eq__(self, other: Record) -> bool:
+        return (
+            self.subdomain == other.subdomain
+            and self.value == other.value
+            and self.type == other.type
+        )
+
+    def __str__(self) -> str:
+        return "[bold blue]{:}[/]: {:} [bold blue]➡️[/] {:}".format(self.type, self.subdomain, self.value)
