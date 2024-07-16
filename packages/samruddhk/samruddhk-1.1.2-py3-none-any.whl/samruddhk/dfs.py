10a7da4a3c1362f723cd0d26fd16d4dd2425d8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:28 +0000

    Add package samruddhk/1.1.2/samruddhk-1.1.2-py3-none-any.whl

---
+++ packages/samruddhk/samruddhk-1.1.2-py3-none-any.whl/samruddhk/dfs.py
@@ -0,0 +1,22 @@
+print("****************************DFS*****************************************")
+
+
+print("graph = {")
+print("    'A': ['B', 'C', 'D'],")
+print("    'B': ['E'],")
+print("    'C': ['D', 'E'],")
+print("    'D': [],")
+print("    'E': []")
+print("}")
+
+print("\nvisited = set()")
+
+print("\ndef dfs(visited, graph, root):")
+print("    if root not in visited:")
+print("        print(root)")
+print("        visited.add(root)")
+print("        for neighbor in graph[root]:")
+print("            dfs(visited, graph, neighbor)")
+
+print("\ndfs(visited, graph, 'A')")
+print("*********************************************************************************")
