11b168f09f505b35355160b2fe64775af6175af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:06:06 +0000

    Add package chemllmhack/0.1.2/chemllmhack-0.1.2.tar.gz

---
+++ packages/chemllmhack/chemllmhack-0.1.2.tar.gz/chemllmhack-0.1.2/chemllmhack/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+File name: __init__.py
+Author: Bowen
+Date created: 15/7/2024
+Description: init file.
+
+Copyright information: © 2024 QDX
+"""
---
+++ packages/chemllmhack/chemllmhack-0.1.2.tar.gz/chemllmhack-0.1.2/chemllmhack/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+File name: test.py
+Author: Bowen
+Date created: 15/7/2024
+Description: This Python file provides an example of mathematical operations.
+
+Copyright information: © 2024 QDX
+"""
