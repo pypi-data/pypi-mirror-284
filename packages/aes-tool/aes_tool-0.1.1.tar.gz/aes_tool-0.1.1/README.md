fc21f805bffa69c30044ce4a1b4f15369097d07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:49 +0000

    Add package aes-tool/0.1.1/aes_tool-0.1.1.tar.gz

---
+++ packages/aes-tool/aes_tool-0.1.1.tar.gz/aes_tool-0.1.1/README.md
@@ -0,0 +1,19 @@
+# Advanced AES Encryption Decyption Tools
+
+aes-tool is advanced AES encryption and decryption tools.
+
+## Features
+- Generate AES key
+- Encrypt text
+- Decrypt text
+- Save and load keys
+- Save and load IVs
+
+## Installation
+You can install the package using pip:
+
+pip install aes-tool
+
+After Installing you will need to execute in command prompt or terminal directly using:
+
+aes-tool
---
+++ packages/aes-tool/aes_tool-0.1.1.tar.gz/aes_tool-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aes-tool',
+    version='0.1.1',
+    description='Advanced AES Encryption Decyption Toolkit',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Babar Ali Jamali',
+    author_email='babar995@gmail.com',
+    url='https://github.com/babaralijamali/aes-tool',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pycryptodome',
+    ],
+    entry_points={
+        'console_scripts': [
+            'aes-tool=aes_tool.aes_tool:main',
+        ],
+    },
+)
