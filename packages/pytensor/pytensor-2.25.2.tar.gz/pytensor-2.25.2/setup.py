ea257ff8cf008fb85cf0f7d1e1c26326d4fde27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:07:51 +0000

    Add package pytensor/2.25.2/pytensor-2.25.2.tar.gz

---
+++ packages/pytensor/pytensor-2.25.2.tar.gz/pytensor-2.25.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import numpy
+import versioneer
+from setuptools import Extension, setup
+from setuptools.dist import Distribution
+
+
+dist = Distribution()
+dist.parse_config_files()
+
+
+NAME: str = dist.get_name()  # type: ignore
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        ext_modules=[
+            Extension(
+                name="pytensor.scan.scan_perform",
+                sources=["pytensor/scan/scan_perform.pyx"],
+                include_dirs=[numpy.get_include()],
+            ),
+        ],
+    )
