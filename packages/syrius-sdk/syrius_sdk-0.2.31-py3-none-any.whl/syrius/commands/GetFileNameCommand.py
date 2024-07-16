0794abb2033da13476ac7c4cc3b2196a69733bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:35 +0000

    Add package syrius-sdk/0.2.31/syrius_sdk-0.2.31-py3-none-any.whl

---
+++ packages/syrius-sdk/syrius_sdk-0.2.31-py3-none-any.whl/syrius/commands/GetFileNameCommand.py
@@ -0,0 +1,9 @@
+from syrius.commands.LoopInputCommand import loopType
+from syrius.commands.abstract import Command, AbstractCommand
+
+
+class GetFileNameCommand(Command):
+    """ """
+    id: int = 46
+    filename: str | AbstractCommand | loopType
+    extension: str | AbstractCommand | loopType
