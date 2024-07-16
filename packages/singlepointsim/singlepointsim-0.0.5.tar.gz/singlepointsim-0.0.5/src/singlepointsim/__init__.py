cd4a85a4c929b72c650d6677a944fb13b2bdf155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:33 +0000

    Add package singlepointsim/0.0.5/singlepointsim-0.0.5.tar.gz

---
+++ packages/singlepointsim/singlepointsim-0.0.5.tar.gz/singlepointsim-0.0.5/src/singlepointsim/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+__version__ = "0.0.5"
+
+from .compile_umat import compile_umat
+from .patch_meson import patch_meson
+
+__all__ = [
+    "compile_umat",
+    "patch_meson",
+]
