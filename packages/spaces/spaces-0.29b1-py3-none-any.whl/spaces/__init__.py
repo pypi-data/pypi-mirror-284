95bde246d46bd48079b73bf1c8525ad336dc604b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:32 +0000

    Add package spaces/0.29b1/spaces-0.29b1-py3-none-any.whl

---
+++ packages/spaces/spaces-0.29b1-py3-none-any.whl/spaces/__init__.py
@@ -0,0 +1,21 @@
+"""
+"""
+
+import sys
+
+if sys.version_info.minor < 8: # pragma: no cover
+    raise RuntimeError("Importing PySpaces requires Python 3.8+")
+
+
+from .zero.decorator import GPU
+from .gradio import gradio_auto_wrap
+from .gradio import disable_gradio_auto_wrap
+from .gradio import enable_gradio_auto_wrap
+
+
+__all__ = [
+    'GPU',
+    'gradio_auto_wrap',
+    'disable_gradio_auto_wrap',
+    'enable_gradio_auto_wrap',
+]
