10be68a7b1e79e72a991e22259b858702f5c559b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:32 +0000

    Add package mlprg/0.2.1/mlprg-0.2.1.tar.gz

---
+++ packages/mlprg/mlprg-0.2.1.tar.gz/mlprg-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlprg',
+    version='0.2.1',
+    description='A package for nothing',
+    author='petteer',
+    packages=find_packages(),
+    install_requires=[],
+)
