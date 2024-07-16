e1029e50579e13afdda56a76e9b1111b55e3e576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:32 +0000

    Add package bullpenfi/0.1.6/bullpenfi-0.1.6-py3-none-any.whl

---
+++ packages/bullpenfi/bullpenfi-0.1.6-py3-none-any.whl/bullpenfi/auth.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+
+class Authenticator:
+    def __init__(self):
+        self.encryption_key = os.getenv("BULLPENFI_ENCRYPTION_KEY")
+        if not self.encryption_key:
+            raise ValueError("Encryption key not found in environment variables")
+        self.valid_api_keys = self._decrypt_keys()
+
+    def _decrypt_keys(self):
+        encrypted_keys = b"gAAAAABmlU0dJpNsnxGZjj2jBHjV1JJIyq2wOv4gtMGzY9KW9WFjuzm9SkIKTuuITIAuRh8GWAyZJBdclqwD3o5UhqkI35pHAPYZc3xK7_j8elQeJ5oxTQpu582u_1Ak37KBpylt9enYysOkSjZh81Cwr6-d2npdZwhdsIPb9L747TSLVjxPJT_tdhrFO0RXre7kr5UBocBTf_kCLJNP7z3GNTHvdjNyP9AbjLvn3FqGxDfjMoDU2M9zXbcffs7-_bMqxpobU3EH7EahGYCjNUGNHTDHL03irLWdyAjVUSbGLcZ_F0Lxi_j5zKuWTNfPOs64aMtri-CkO1ASUKOYMAmHTuSsKzbhlNwhnfYPh8FfqRxpxbbraSE="
+        cipher_suite = Fernet(self.encryption_key)
+        decrypted_keys = cipher_suite.decrypt(encrypted_keys)
+        return decrypted_keys.decode().split(",")
+
+    def authenticate(self, api_key):
+        if api_key not in self.valid_api_keys:
+            raise PermissionError("Invalid API Key")
+
+
+authenticator = Authenticator()
