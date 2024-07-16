d0a0cd073d01415fc1c21508d7fde8cf10cb8f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:36 +0000

    Add package finch-api/0.21.11/finch_api-0.21.11-py3-none-any.whl

---
+++ packages/finch-api/finch_api-0.21.11-py3-none-any.whl/finch/types/hris/benefit_create_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import TypedDict
+
+from .benefit_type import BenefitType
+from .benefit_frequency import BenefitFrequency
+
+__all__ = ["BenefitCreateParams"]
+
+
+class BenefitCreateParams(TypedDict, total=False):
+    description: str
+
+    frequency: Optional[BenefitFrequency]
+
+    type: Optional[BenefitType]
+    """Type of benefit."""
