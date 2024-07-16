32b86eea0b85f157dbaa885dd8f02c31ea18ca17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:07 +0000

    Add package python-alist/0.0.13.4/python_alist-0.0.13.4.tar.gz

---
+++ packages/python-alist/python_alist-0.0.13.4.tar.gz/python_alist-0.0.13.4/alist/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+def main():
+    from alist.cmd import parser
+
+    args = parser.parse_args()
+    if args.func:
+        args.func(args)
+    else:
+        parser.parse_args(["-h"])
+
+if __name__ == "__main__":
+    main()
