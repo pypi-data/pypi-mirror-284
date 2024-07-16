7c68a6a38512c7d4c1b66ea9bb4acfb539340456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:06:33 +0000

    Add package plex-footage-sorter/4.5.1/plex_footage_sorter-4.5.1.tar.gz

---
+++ packages/plex-footage-sorter/plex_footage_sorter-4.5.1.tar.gz/plex_footage_sorter-4.5.1/src/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2023 Benjamin Mummery
+
+"""Initialiser to make plex_footage_sorter accessible externally."""
+
+# Version number, automatically updated by semantic-release
+__version__ = "4.5.1"
