baa281bfaa9fa3b8d4c743ba63f70509ddb5a064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:53 +0000

    Add package eidolon-typedai/0.1.8/eidolon_typedai-0.1.8-py3-none-any.whl

---
+++ packages/eidolon-typedai/eidolon_typedai-0.1.8-py3-none-any.whl/typedai/__init__.py
@@ -0,0 +1,5 @@
+from .client import TypedAI as TypedAI
+from .models import (
+    TypedChatCompletion as TypedChatCompletion,
+    TypedStream as TypedStream,
+)
---
+++ packages/eidolon-typedai/eidolon_typedai-0.1.8-py3-none-any.whl/typedai/client.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from openai import OpenAI
+
+from .completions import TypedCompletions
+
+
+class TypedAI:
+    client: OpenAI
+    default_model: Optional[str]
+
+    def __init__(self, client: OpenAI = None, default_model: Optional[str] = None):
+        if client is None:
+            client = OpenAI()
+        self.client = client
+        self.default_model = default_model
+
+    @property
+    def completions(self, default_model: Optional[str] = None) -> TypedCompletions:
+        return TypedCompletions(
+            self.client.chat.completions, default_model or self.default_model
+        )
+
+
+# class AsyncTypedAI:
+#     _client: AsyncOpenAI
+#
+#     def __init__(self, *args, **kwargs):
+#         self._client = AsyncOpenAI(*args, **kwargs)
+#
+#     @property
+#     def completions(self) -> AsyncTypedCompletions:
+#         return AsyncTypedCompletions(self._client.completions)
