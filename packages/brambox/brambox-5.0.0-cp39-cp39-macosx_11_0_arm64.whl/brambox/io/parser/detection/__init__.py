a9de66c448a7a4c3605746a5b95167f5623c3753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:20 +0000

    Add package brambox/5.0.0/brambox-5.0.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/brambox/brambox-5.0.0-cp39-cp39-macosx_11_0_arm64.whl/brambox/io/parser/detection/__init__.py
@@ -0,0 +1,11 @@
+"""
+Brambox detection parsers module |br|
+These parsers can be used to parse and generate detection files.
+"""
+
+from ._coco import *
+from ._dollar import *
+from ._erti import *
+from ._mot import *
+from ._pascalvoc import *
+from ._yaml import *
