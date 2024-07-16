3250236fd98018a529d8c37fc49c8cf61176914d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:08:06 +0000

    Add package xlsx-export/0.0.23/xlsx_export-0.0.23.tar.gz

---
+++ packages/xlsx-export/xlsx_export-0.0.23.tar.gz/xlsx_export-0.0.23/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xlsx_export',
+    version='0.0.23',
+    description='Plugin for exporting test cases to XLSX format for TestY',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'pandas',
+        'django',
+        'openpyxl',
+        'testy',
+    ],
+    entry_points={
+        'testy': [
+            'xlsx_export = xlsx_export',
+        ],
+    },
+)
