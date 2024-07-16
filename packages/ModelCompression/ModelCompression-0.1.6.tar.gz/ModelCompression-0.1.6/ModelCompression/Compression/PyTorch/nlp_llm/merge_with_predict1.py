1aaa7f1932ea2c268b82ea1c16db2aee924bb79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:47 +0000

    Add package ModelCompression/0.1.6/ModelCompression-0.1.6.tar.gz

---
+++ packages/ModelCompression/ModelCompression-0.1.6.tar.gz/ModelCompression-0.1.6/ModelCompression/Compression/PyTorch/nlp_llm/merge_with_predict1.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*- #
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import json
+import torch
+
+# ------------------------------------------------------------------
+from transformers import AutoModelForCausalLM, AutoTokenizer,BitsAndBytesConfig
+from transformers.generation import GenerationConfig
+
+
+quantization_config = BitsAndBytesConfig(
+load_in_4bit=True,
+bnb_4bit_compute_dtype=torch.float16
+)
+tokenizer = AutoTokenizer.from_pretrained("fine-tuned-model_merge", trust_remote_code=True)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "fine-tuned-model_merge",
+    device_map="auto",
+quantization_config=quantization_config,
+
+    trust_remote_code=True
+).eval()
+
+
+
+pre=[]
+true_=[]
+
+json_res=[]
+
+jsonresults={}
+with open('test11.csv','r',encoding='gbk') as fw:
+    lines = fw.readlines()
+    for idx, line in enumerate(lines):
+        if idx == 0:
+            continue
+        # if idx >10:
+        #     break
+        p_ = line.strip().split(',')
+
+        # response, history = model.chat(tokenizer, "怎么获取免息 。 如何获取7天免息券。。 上述两句话表达的意思是否相同,如果意思相同则输出1，反之则输出0。", history=None)
+        # tokern=f"{p_[1]}。{p_[2]} 。  上述两句话表达的意思是否相同,如果意思相同response则为1，如果意思不相同response则为0。"
+        tokern = f"{p_[1]},精简总结这句话"
+        response, history = model.chat(tokenizer, tokern, history=None)
+        print('response----',response)
+        print('tokern----',tokern)
+
+        jsonresults[p_[0]] = response
+
+res_ = json.dumps(jsonresults, indent=4, ensure_ascii=False)
+
+with open('result1_2.json', 'w', encoding='utf-8') as fw:
+    fw.write(res_)
---
+++ packages/ModelCompression/ModelCompression-0.1.6.tar.gz/ModelCompression-0.1.6/README.rst
@@ -0,0 +1,7 @@
+pypi_modelcompression
+
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided
+with the code, and a loose standing reference guide, available from
