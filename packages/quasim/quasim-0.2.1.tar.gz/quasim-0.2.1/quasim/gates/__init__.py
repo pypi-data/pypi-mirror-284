0d2550a4b3eab0702f0545c8fcb0a40a9486dca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:39 +0000

    Add package quasim/0.2.1/quasim-0.2.1.tar.gz

---
+++ packages/quasim/quasim-0.2.1.tar.gz/quasim-0.2.1/quasim/gates/__init__.py
@@ -0,0 +1,5 @@
+from .interface import IGate
+from .swap import Swap
+from .single_qubit_gates import Gate, H, X, Y, Z, RX, RY, RZ, Phase
+from .controlled_gates import CGate, CX, CY, CZ, CRX, CRY, CRZ, CH
+from .double_controlled_gates import CCGate, CCX, CCZ
