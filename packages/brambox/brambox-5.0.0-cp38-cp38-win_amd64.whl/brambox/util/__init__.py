5e217fbbb4dbac1640984a98a1641c469825320f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:19 +0000

    Add package brambox/5.0.0/brambox-5.0.0-cp38-cp38-win_amd64.whl

---
+++ packages/brambox/brambox-5.0.0-cp38-cp38-win_amd64.whl/brambox/util/__init__.py
@@ -0,0 +1,10 @@
+"""
+Brambox util module |br|
+This package contains utilitary functions to use with brambox dataframes.
+"""
+
+from ._boundingbox import *
+from ._dualgroupby import *
+from ._pd import *
+from ._sparsematrix import *
+from ._visual import *
