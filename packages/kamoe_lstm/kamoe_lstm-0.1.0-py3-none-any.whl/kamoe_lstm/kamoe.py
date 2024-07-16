4014d11112b177dbcdddbaaceb8b42ca2fd7a52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:05:07 +0000

    Add package kamoe_lstm/0.1.0/kamoe_lstm-0.1.0-py3-none-any.whl

---
+++ packages/kamoe_lstm/kamoe_lstm-0.1.0-py3-none-any.whl/kamoe_lstm/kamoe.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, Dense, LSTM, Dropout
+from kamoe_lstm import GRKAN
+
+class KAMoE_LSTM(Layer):
+    def __init__(self, unit, n_lstm = 4, return_sequences = False, dropout = 0., **kwargs):
+        super().__init__(**kwargs)
+        self.unit = unit
+        self.hidden_layer = Dense(unit, 'relu')
+        self.n_lstm = n_lstm
+        self.return_sequences = return_sequences
+        self.lstm_layers = [LSTM(unit, return_sequences=return_sequences) for _ in range(n_lstm)]
+        self.hidden_to_weight = GRKAN(n_lstm, activation = 'softmax', dropout = dropout)
+        self.dropout = Dropout(dropout)
+        
+    def call(self, inputs):
+        if self.return_sequences:
+            hidden = self.hidden_layer(inputs)
+        else:
+            hidden = self.hidden_layer(inputs[:,-1,:])
+        weights = self.hidden_to_weight(hidden)
+        lstm_outs = []
+        for lstm in self.lstm_layers:
+            lstm_outs.append(lstm(inputs))
+        lstm_outs = tf.stack(lstm_outs, axis=-1)
+        lstm_outs = lstm_outs * weights[...,tf.newaxis,:]
+        return tf.reduce_sum(lstm_outs, axis=-1)
+
+
+class MoE_LSTM(Layer):
+    def __init__(self, unit, n_lstm = 4, return_sequences = False, dropout = 0., **kwargs):
+        super().__init__(**kwargs)
+        self.unit = unit
+        self.hidden_layer = Dense(unit, 'relu')
+        self.n_lstm = n_lstm
+        self.return_sequences = return_sequences
+        self.lstm_layers = [LSTM(unit, return_sequences=return_sequences) for _ in range(n_lstm)]
+        self.hidden_to_weight = GRN(n_lstm, activation = 'softmax', dropout = dropout)
+        self.dropout = Dropout(dropout)
+        
+    def call(self, inputs):
+        if self.return_sequences:
+            hidden = self.hidden_layer(inputs)
+        else:
+            hidden = self.hidden_layer(inputs[:,-1,:])
+        weights = self.hidden_to_weight(hidden)
+        lstm_outs = []
+        for lstm in self.lstm_layers:
+            lstm_outs.append(lstm(inputs))
+        lstm_outs = tf.stack(lstm_outs, axis=-1)
+        lstm_outs = lstm_outs * weights[...,tf.newaxis,:]
+        return tf.reduce_sum(lstm_outs, axis=-1)
