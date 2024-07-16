7bcb5422b4e7ab439cbf76694909aa7df5bae69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:06:33 +0000

    Add package QuickGraphLib/0.1.0a6/QuickGraphLib-0.1.0a6-cp37-abi3-manylinux_2_28_x86_64.whl

---
+++ packages/QuickGraphLib/QuickGraphLib-0.1.0a6-cp37-abi3-manylinux_2_28_x86_64.whl/quickgraphlib_helpers/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 Refeyn Ltd and other QuickGraphLib contributors
+# SPDX-License-Identifier: MIT
+
+import pathlib
+
+from . import contours
+
+QML_IMPORT_PATH = str(pathlib.Path(__file__).parent.parent)
