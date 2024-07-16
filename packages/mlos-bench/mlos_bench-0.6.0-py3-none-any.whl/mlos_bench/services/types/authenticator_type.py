ea132f5d67c515b06c2b305143d97a239f38d843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 05:04:26 +0000

    Add package mlos-bench/0.6.0/mlos_bench-0.6.0-py3-none-any.whl

---
+++ packages/mlos-bench/mlos_bench-0.6.0-py3-none-any.whl/mlos_bench/services/types/authenticator_type.py
@@ -0,0 +1,32 @@
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+"""Protocol interface for authentication for the cloud services."""
+
+from typing import Protocol, runtime_checkable
+
+
+@runtime_checkable
+class SupportsAuth(Protocol):
+    """Protocol interface for authentication for the cloud services."""
+
+    def get_access_token(self) -> str:
+        """
+        Get the access token for cloud services.
+
+        Returns
+        -------
+        access_token : str
+            Access token.
+        """
+
+    def get_auth_headers(self) -> dict:
+        """
+        Get the authorization part of HTTP headers for REST API calls.
+
+        Returns
+        -------
+        access_header : dict
+            HTTP header containing the access token.
+        """
